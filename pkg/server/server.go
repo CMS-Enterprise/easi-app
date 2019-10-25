2b41e5dca0964a812188d6366a6c1cdac03009a8
Mikena Wood <mikena@truss.works>
2019-10-25 15:29:22 -0700

    add a test to work with

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,3 +1,7 @@
+/*
+This is some throwaway server code to setup testing
+ */
+
 package server
 
 import (
@@ -6,12 +10,12 @@ import (
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func HandleLanding(w http.ResponseWriter, r *http.Request) {
 	_, _ = fmt.Fprint(w, "The EASi web app!")
 }
 
 func Serve() {
 	fmt.Print("Serving application on localhost:8080")
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", HandleLanding)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
