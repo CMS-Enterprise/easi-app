ce4f20e55faceaf4cc0768c93c8b33c35b3e2d0e
Christopher Hui <christopher@truss.works>
2019-11-13 14:25:34 -0800

    try a different way to organize navbar and 'template' components

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { withAuth } from '@okta/okta-react';
-import NavBar from 'components/shared/NavBar';
 import useAuth from 'hooks/useAuth';
 
 type HomeProps = {
@@ -9,13 +8,8 @@ type HomeProps = {
 
 const Home = ({ auth }: HomeProps) => {
   const [isAuthenticated] = useAuth(auth);
-  const handleLogout = () => {
-    auth.logout('/');
-  };
-
   return (
     <div>
-      <NavBar isAuthenticated={isAuthenticated} handleLogout={handleLogout} />
       <h1>Home</h1>
       <h3>{`A user is ${isAuthenticated ? '' : 'NOT'} authenticated`}</h3>
     </div>
