35a4d0d9813c43a4a088be8d5e61dd4c12080a55
Isaac Garfinkle <isaac@truss.works>
2019-11-20 14:07:08 -0800

    Send json back on landing page

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -2,14 +2,48 @@
 package server
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
+
+type status string
+
+const (
+	draft status = "DRAFT"
+)
+
+type profile struct {
+	Name         string
+	SystemOwners []string
+	CreatedAt    time.Time
+	UpdatedAt    time.Time
+	Status       status
+}
 
 // HandleLanding is the handler for the root path
 func HandleLanding(w http.ResponseWriter, r *http.Request) {
-	_, _ = fmt.Fprint(w, "The EASi web app!")
+	sampleProfile := profile{
+		Name:         "My Favorite Project",
+		SystemOwners: []string{"admin@example.com", "operations@example.com"},
+		CreatedAt:    time.Now(),
+		UpdatedAt:    time.Now(),
+		Status:       draft,
+	}
+	js, err := json.Marshal(sampleProfile)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	_, err = w.Write(js)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
 // Serve serves all the handlers
