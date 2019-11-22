6337ced8d458cc8d50e172822c4c08e04dea1bbb
Christopher Hui <christopher@truss.works>
2019-11-22 15:14:11 -0800

    add back in all the routes that got ovewritten during merge

--- src/views/App/index.tsx
+++ src/views/App/index.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
+import Home from 'views/Home';
+import Login from 'views/Login';
+import SuperSecret from 'views/SuperSecret';
+import './index.scss';
 
 type MainState = {
   name: string;
 };
 
 type MainProps = {};
+
+// This can do anything. It doesn't have to redirect
+// It can be a pop up modal, alert message, etc.
+function onAuthRequired({ history }: any): void {
+  history.push('/login');
+}
 
 class App extends React.Component<MainProps, MainState> {
   constructor(props: MainProps) {
@@ -26,9 +38,24 @@ class App extends React.Component<MainProps, MainState> {
 
   render() {
     const { name } = this.state;
+    console.log(name);
     return (
       <div>
-        <h1>{name}</h1>
+        <BrowserRouter>
+          <Security
+            issuer={`${process.env.REACT_APP_OKTA_ISSUER}/oauth2/default`}
+            clientId={process.env.REACT_APP_OKTA_CLIENT_ID}
+            redirectUri={process.env.REACT_APP_OKTA_REDIRECT_URI}
+            onAuthRequired={onAuthRequired}
+            responseType={['code']}
+            pkce
+          >
+            <Route path="/" exact component={Home} />
+            <Route path="/login" exact component={Login} />
+            <SecureRoute path="/protected" exact component={SuperSecret} />
+            <Route path="/implicit/callback" component={ImplicitCallback} />
+          </Security>
+        </BrowserRouter>
       </div>
     );
   }
