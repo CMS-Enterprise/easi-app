a30f922d1cc887d6aa5732311c4c01e63dbea051
Isaac Garfinkle <isaac@truss.works>
2020-01-08 15:54:46 -0800

    Move verifier instantiation so it's only called once when server is generated

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -11,23 +11,12 @@ import (
 	"strings"
 )
 
-func isAuthenticated(authHeader string) bool {
+func isAuthenticated(authHeader string, verifier jwtverifier.JwtVerifier) bool {
 	tokenParts := strings.Split(authHeader, "Bearer ")
 	bearerToken := tokenParts[1]
 	if bearerToken == "" {
 		return false
 	}
-
-	toValidate := map[string]string{}
-	toValidate["cid"] = os.Getenv("OKTA_CLIENT_ID")
-	toValidate["aud"] = "EASi"
-
-	jwtVerifierSetup := jwtverifier.JwtVerifier{
-		Issuer:           os.Getenv("OKTA_ISSUER"),
-		ClaimsToValidate: toValidate,
-	}
-
-	verifier := jwtVerifierSetup.New()
 
 	_, err := verifier.VerifyAccessToken(bearerToken)
 
@@ -37,12 +26,27 @@ func isAuthenticated(authHeader string) bool {
 	return true
 }
 
+func newJwtVerifier() *jwtverifier.JwtVerifier {
+	toValidate := map[string]string{}
+	toValidate["cid"] = os.Getenv("OKTA_CLIENT_ID")
+	toValidate["aud"] = "EASi"
+
+	jwtVerifierSetup := jwtverifier.JwtVerifier{
+		Issuer:           os.Getenv("OKTA_ISSUER"),
+		ClaimsToValidate: toValidate,
+	}
+
+	return jwtVerifierSetup.New()
+}
+
 func authorizeHandler(next http.Handler) http.Handler {
+
+	verifier := newJwtVerifier()
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "OPTIONS" {
 			return
 		}
-		if !isAuthenticated(r.Header.Get("Authorization")) {
+		if !isAuthenticated(r.Header.Get("Authorization"), *verifier) {
 			http.Error(w, http.StatusText(401), http.StatusUnauthorized)
 			return
 		}
