c97faa18bea8bf30b9a2c97fada22c177d015a6b
Christopher Hui <christopher@truss.works>
2020-01-10 17:35:17 -0800

    WIP search bar autocomplete

--- src/components/shared/SearchBar/index.tsx
+++ src/components/shared/SearchBar/index.tsx
@@ -1,21 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Autosuggest from 'react-autosuggest';
 
 type SearchBarProps = {
   name: string;
+  results?: any[];
   onSearch: () => void;
+  getSuggestionValue?: () => string;
+  renderSuggestion?: () => HTMLElement;
 };
 
-const SearchBar = ({ name, onSearch }: SearchBarProps) => {
+const SearchBar = ({
+  name,
+  onSearch,
+  results,
+  getSuggestionValue,
+  renderSuggestion
+}: SearchBarProps) => {
+  const [searchValue] = useState('');
+  const [suggestions] = useState([]);
+  const inputProps = {
+    className: 'use-input',
+    id: 'basic-search-field-small',
+    type: 'search',
+    name,
+    value: searchValue,
+    onChange: onSearch
+  };
+
+  const onSuggestionsFetchRequested = () => {};
+
+  const onSuggestionsClearRequested = () => {};
+
   return (
     <form className="usa-search usa-search--small">
       <div role="search">
-        <input
-          className="usa-input"
-          id="basic-search-field-small"
-          type="search"
-          name={name}
-          onChange={onSearch}
-        />
+        {results && getSuggestionValue && renderSuggestion ? (
+          <Autosuggest
+            suggestions={suggestions}
+            onSuggestionsFetchRequested={onSuggestionsFetchRequested}
+            onSuggestionsClearRequested={onSuggestionsClearRequested}
+            getSuggestionValue={getSuggestionValue}
+            renderSuggestion={renderSuggestion}
+            inputProps={inputProps}
+          />
+        ) : (
+          <input
+            className="usa-input"
+            id="basic-search-field-small"
+            type="search"
+            name={name}
+            onChange={onSearch}
+          />
+        )}
         <button className="usa-button" type="submit">
           <span className="usa-sr-only">Search</span>
         </button>
