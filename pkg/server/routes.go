81a9b346d6c3df40caa6ff72e63ff2a564df9515
Isaac Garfinkle <isaac@truss.works>
2020-01-16 16:18:24 -0800

    Make dummy data and add systems endpont

--- pkg/server/routes.go
+++ pkg/server/routes.go
@@ -2,4 +2,5 @@ package server
 
 func (s *server) routes() {
 	s.router.HandleFunc("/", s.corsMiddleware(s.authorizeHandler(s.handleLanding())))
+	s.router.HandleFunc("/systems/", s.corsMiddleware(s.authorizeHandler(s.handleSystemsList())))
 }
