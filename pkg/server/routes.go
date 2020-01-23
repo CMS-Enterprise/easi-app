8b8e04cc6b2c973ab3668ec0bf0d50cebfd1053e
Isaac Garfinkle <isaac@truss.works>
2020-01-22 16:53:02 -0800

    Delete landing handler

--- pkg/server/routes.go
+++ pkg/server/routes.go
@@ -6,6 +6,5 @@ import (
 )
 
 func (s *server) routes() {
-	s.router.HandleFunc("/", s.corsMiddleware(s.authorizeHandler(s.handleLanding())))
 	s.router.HandleFunc("/systems/", s.corsMiddleware(s.authorizeHandler(handlers.SystemsListHandler{FetchSystems: services.NewFetchFakeSystems()}.Handle())))
 }
