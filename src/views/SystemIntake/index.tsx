97b32f2907680a7c0f27a743c6761e98b92d89fa
Isaac Garfinkle <isaac@truss.works>
2020-02-05 12:43:05 -0800

    Fix styling

--- src/views/SystemIntake/index.tsx
+++ src/views/SystemIntake/index.tsx
@@ -2,29 +2,26 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import Header from 'components/Header';
 import './index.scss';
+import HeaderWrapper from '../../components/Header/HeaderWrapper';
 
 type SystemProfileProps = {
   match: any;
 };
 
 export const SystemIntake = ({ match }: SystemProfileProps) => {
-  const saveButton = (
-    <button
-      type="button"
-      className="easi-header__save-button usa-button"
-      id="save-button"
-    >
-      <span>Save & Exit</span>
-    </button>
-  );
-
   return (
     <div className="system-profile">
-      <Header
-        activeNavListItem={match.params.profileId}
-        name="INTAKE"
-        headerButton={saveButton}
-      />
+      <Header activeNavListItem={match.params.profileId} name="INTAKE">
+        <HeaderWrapper className="grid-container margin-bottom-3">
+          <button
+            type="button"
+            className="easi-header__save-button usa-button"
+            id="save-button"
+          >
+            <span>Save & Exit</span>
+          </button>
+        </HeaderWrapper>
+      </Header>
       <div className="grid-container">
         <p className="system-profile__text">
           The EASi System Intake process can guide you through all stages of
