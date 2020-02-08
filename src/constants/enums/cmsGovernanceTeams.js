2914a1020711f5763ed8b2c7a75745c0adac2230
Christopher Hui <christopher@truss.works>
2020-02-08 11:12:35 -0800

    add various enum values to be cached on the frontend

---
+++ src/constants/enums/cmsGovernanceTeams.js
@@ -0,0 +1,20 @@
+const cmsGovernanceTeams = [
+  {
+    name: 'Technical Review Board (TRB)',
+    value: 'Technical Review Board'
+  },
+  {
+    name: 'Security/Privacy',
+    value: 'Security/Privacy'
+  },
+  {
+    name: '508 Compliance',
+    value: '508 Compliance'
+  },
+  {
+    name: 'Enterprise Architecture (EA)',
+    value: 'Enterprise Architecture'
+  }
+];
+
+export default cmsGovernanceTeams;
