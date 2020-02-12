65b58e2a2f8c873d609b3c93c7a8b786858536d2
Isaac Garfinkle <isaac@truss.works>
2020-02-11 16:07:56 -0800

    Request is being made successfully (though response not handled)

--- src/sagas/searchSaga.ts
+++ src/sagas/searchSaga.ts
@@ -1,9 +1,19 @@
-import { takeLatest } from 'redux-saga/effects';
+import axios from 'axios';
+import { takeLatest, call } from 'redux-saga/effects';
 import { GET_ALL_SYSTEM_SHORTS } from '../constants/search';
 
-// eslint-disable-next-line require-yield
-export function* fetchAllSystemShorts() {
-  console.log('fetching all systems');
+function requestSystemShorts(accessToken: string) {
+  return axios.get('http://localhost:8080/systems', {
+    headers: {
+      Authorization: `Bearer ${accessToken}`
+    }
+  });
+}
+
+export function* fetchAllSystemShorts(action: any) {
+  const obj = yield call(requestSystemShorts, action.payload);
+  // eslint-disable-next-line no-console
+  console.log(obj);
 }
 
 export function* searchSaga() {
