ac7c4c3da97157b6d07266f323a3fbbc32f7f8ce
Mikena Wood <mikena@truss.works>
2020-02-13 12:02:13 -0800

    add healtcheck handler

---
+++ pkg/handlers/health_check_test.go
@@ -0,0 +1,21 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+)
+
+func (s HandlerTestSuite) TestHealthcheckHandler() {
+	rr := httptest.NewRecorder()
+
+	HealthCheckHandler{}.Handle()(rr, nil)
+
+	s.Equal(http.StatusOK, rr.Code)
+
+	var healthCheckActual healthCheck
+	err := json.Unmarshal(rr.Body.Bytes(), &healthCheckActual)
+
+	s.NoError(err)
+	s.Equal(statusPass, healthCheckActual.Status)
+}
