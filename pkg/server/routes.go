03b714a2f8dea0b3020904a9c80840f506aea3de
Mikena Wood <mikena@truss.works>
2020-02-14 12:12:29 -0800

    fix auth empty header bug

--- pkg/server/routes.go
+++ pkg/server/routes.go
@@ -7,6 +7,6 @@ import (
 
 func (s *server) routes() {
 	api := s.router.PathPrefix("/api/v1").Subrouter()
-	api.HandleFunc("/systems/", s.corsMiddleware(s.authorizeHandler(handlers.SystemsListHandler{FetchSystems: services.NewFetchFakeSystems()}.Handle())))
+	api.HandleFunc("/systems", s.corsMiddleware(s.authorizeHandler(handlers.SystemsListHandler{FetchSystems: services.NewFetchFakeSystems()}.Handle())))
 	api.HandleFunc("/healthcheck", handlers.HealthCheckHandler{}.Handle())
 }
