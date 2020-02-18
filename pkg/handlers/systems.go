bdbbe8dc777cb6008a9ac14881b70c8748013ae7
Mikena Wood <mikena@truss.works>
2020-02-18 10:03:03 -0800

    move the marshaller out for testing

--- pkg/handlers/systems.go
+++ pkg/handlers/systems.go
@@ -1,39 +1,43 @@
 package handlers
 
 import (
-	"encoding/json"
 	"net/http"
 
 	"github.com/cmsgov/easi-app/pkg/models"
 )
 
 type fetchSystems func() (models.SystemShorts, error)
+type marshal func(interface{}) ([]byte, error)
 
 // SystemsListHandler is the handler for listing systems
 type SystemsListHandler struct {
 	FetchSystems fetchSystems
+	Marshal      marshal
 }
 
 // Handle handles a web request and returns a list of systems
 func (h SystemsListHandler) Handle() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		// TODO: Add logger for errors
 		systems, err := h.FetchSystems()
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+			http.Error(w, "failed to fetch systems", http.StatusInternalServerError)
 			return
 		}
 
-		js, err := json.Marshal(systems)
+		js, err := h.Marshal(systems)
+		// TODO: Add logger for errors
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+			http.Error(w, "failed to fetch systems", http.StatusInternalServerError)
 			return
 		}
 
 		w.Header().Set("Content-Type", "application/json")
 
+		// TODO: Add logger for errors
 		_, err = w.Write(js)
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+			http.Error(w, "failed to fetch systems", http.StatusInternalServerError)
 			return
 		}
 	}
--- pkg/server/routes.go
+++ pkg/server/routes.go
@@ -1,14 +1,29 @@
 package server
 
 import (
+	"encoding/json"
+
 	"github.com/cmsgov/easi-app/pkg/cedar"
 	"github.com/cmsgov/easi-app/pkg/handlers"
 )
 
 func (s *server) routes() {
-	api := s.router.PathPrefix("/api/v1").Subrouter()
+	// set to standard library marshaller
+	marshalFunc := json.Marshal
+
+	// set up CEDAR client
 	cedarClient := cedar.NewTranslatedClient(s.Config.GetString("CEDAR_API_KEY"))
-	systemHandler := handlers.SystemsListHandler{FetchSystems: cedarClient.FetchSystems}
-	api.HandleFunc("/systems", s.corsMiddleware(s.authorizeHandler(systemHandler.Handle())))
+
+	// API base path is versioned
+	api := s.router.PathPrefix("/api/v1").Subrouter()
+
+	// endpoint for system list
+	systemHandler := handlers.SystemsListHandler{
+		FetchSystems: cedarClient.FetchSystems,
+		Marshal:      marshalFunc,
+	}
+	api.HandleFunc("/systems", systemHandler.Handle())
+
+	// health check endpoint
 	api.HandleFunc("/healthcheck", handlers.HealthCheckHandler{}.Handle())
 }
