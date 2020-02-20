5feff63e00ab2eba374426973399df78a9e95002
Christopher Hui <christopher@truss.works>
2020-02-19 16:07:58 -0800

    configure cypress-otp and add login integration test

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -14,4 +14,7 @@
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-}
+  on('task', {
+    generateOTP: require('cypress-otp')
+  });
+};
