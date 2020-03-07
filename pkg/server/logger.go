e90d2d96f86d17937f6c204e9ea8f028c0d249c4
Mikena Wood <mikena@truss.works>
2020-03-06 18:25:56 -0800

    add logging to handler

--- pkg/server/logger.go
+++ pkg/server/logger.go
@@ -15,11 +15,25 @@ func loggerMiddleware(logger *zap.Logger, next http.Handler) http.Handler {
 		ctx := r.Context()
 		traceID, ok := requestcontext.Trace(ctx)
 		if ok {
-			ctx = requestcontext.WithLogger(ctx, logger.With(zap.String(traceField, traceID.String())))
+			logger = logger.With(zap.String(traceField, traceID.String()))
 		} else {
-			ctx = requestcontext.WithLogger(ctx, logger)
 			logger.Error("Failed to get trace ID from context")
 		}
+		ctx = requestcontext.WithLogger(ctx, logger)
+
+		fields := []zap.Field{
+			zap.String("accepted-language", r.Header.Get("accepted-language")),
+			zap.Int64("content-length", r.ContentLength),
+			zap.String("host", r.Host),
+			zap.String("method", r.Method),
+			zap.String("protocol-version", r.Proto),
+			zap.String("referer", r.Header.Get("referer")),
+			zap.String("source", r.RemoteAddr),
+			zap.String("url", r.URL.String()),
+			zap.String("user-agent", r.UserAgent()),
+		}
+		logger.Info("Request", fields...)
+
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
