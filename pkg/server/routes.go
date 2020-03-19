f0cf5bbd8885df42c4c0137fc1b804ecee64cd72
Isaac Garfinkle <isaac@truss.works>
2020-03-19 14:09:29 -0700

    Set up nullable string

--- pkg/server/routes.go
+++ pkg/server/routes.go
@@ -1,10 +1,19 @@
 package server
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/jmoiron/sqlx"
+
+	// Need blank import to connect to db. TODO delete this.
+	_ "github.com/lib/pq"
 
 	"github.com/cmsgov/easi-app/pkg/cedar"
 	"github.com/cmsgov/easi-app/pkg/handlers"
+	"github.com/cmsgov/easi-app/pkg/models"
 )
 
 func (s *server) routes(
@@ -39,6 +48,20 @@ func (s *server) routes(
 
 	// protect all API routes with authorization middleware
 	api.Use(authorizationMiddleware)
+
+	db, err := sqlx.Connect("postgres", "user=postgres sslmode=disable")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	tx := db.MustBegin()
+	_, _ = tx.NamedExec("INSERT INTO system_intake (id, eua_user_id, requester, component) VALUES (:id, :eua_user_id, :requester, :component)", &models.SystemIntake{ID: uuid.New(), EUAUserID: "sample", Requester: "test", Component: "test"})
+	_ = tx.Commit()
+	rows := models.SystemIntakes{}
+	_ = db.Select(&rows, "SELECT eua_user_id FROM system_intake")
+	byteArray, _ := json.Marshal(rows)
+	fmt.Println(string(byteArray))
 
 	// endpoint for system list
 	systemHandler := handlers.SystemsListHandler{
