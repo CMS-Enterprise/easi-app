2fd9cb26b57e71e74074be37e69927763633081d
Mikena Wood <mikena@truss.works>
2020-03-27 11:52:13 -0700

    move database operations to separate package

--- pkg/services/services_test.go
+++ pkg/services/services_test.go
@@ -8,20 +8,26 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/stretchr/testify/suite"
 	"go.uber.org/zap"
+
+	"github.com/cmsgov/easi-app/pkg/storage"
 )
 
 type ServicesTestSuite struct {
 	suite.Suite
 	logger *zap.Logger
+	store  *storage.Store
 	db     *sqlx.DB
 }
 
 func TestServicesTestSuite(t *testing.T) {
 	db, err := sqlx.Connect("postgres", "user=postgres sslmode=disable")
 	fmt.Println(err)
+	logger := zap.NewNop()
+	store := storage.NewStore(db, logger)
 	servicesTestSuite := &ServicesTestSuite{
 		Suite:  suite.Suite{},
-		logger: zap.NewNop(),
+		logger: logger,
+		store:  store,
 		db:     db,
 	}
 	suite.Run(t, servicesTestSuite)
