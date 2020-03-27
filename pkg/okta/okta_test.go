1006bd06fd189b71f9d2abf6222790a15a4679c2
Mikena Wood <mikena@truss.works>
2020-03-27 08:50:02 -0700

    add tests for okta middleware

--- pkg/okta/okta_test.go
+++ pkg/okta/okta_test.go
@@ -1,6 +1,10 @@
 package okta
 
 import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
@@ -27,7 +31,69 @@ func TestOktaTestSuite(t *testing.T) {
 }
 
 func (s OktaTestSuite) TestAuthorizeMiddleware() {
-	accessToken, err := testhelpers.OktaAccessToken()
+	oktaDomain := os.Getenv("OKTA_DOMAIN")
+	oktaIssuer := os.Getenv("OKTA_ISSUER")
+	oktaClientID := os.Getenv("OKTA_CLIENT_ID")
+	oktaRedirectURL := os.Getenv("OKTA_REDIRECT_URI")
+	username := os.Getenv("OKTA_TEST_USERNAME")
+	password := os.Getenv("OKTA_TEST_PASSWORD")
+	secret := os.Getenv("OKTA_TEST_SECRET")
+	accessToken, err := testhelpers.OktaAccessToken(
+		oktaDomain,
+		oktaIssuer,
+		oktaClientID,
+		oktaRedirectURL,
+		username,
+		password,
+		secret,
+	)
 	s.NoError(err, "couldn't get access token")
 	s.NotEmpty(accessToken, "empty access token")
+	authMiddleware := NewOktaAuthorizeMiddleware(
+		s.logger,
+		oktaClientID,
+		oktaIssuer,
+	)
+
+	s.Run("a valid token executes the handler", func() {
+		req := httptest.NewRequest("GET", "/systems/", nil)
+		req.Header.Set("AUTHORIZATION", fmt.Sprintf("Bearer %s", accessToken))
+		rr := httptest.NewRecorder()
+		handlerRun := false
+		testHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			handlerRun = true
+		})
+
+		authMiddleware(testHandler).ServeHTTP(rr, req)
+
+		s.True(handlerRun)
+	})
+
+	s.Run("a invalid token does not execute the handler", func() {
+		req := httptest.NewRequest("GET", "/systems/", nil)
+		req.Header.Set("AUTHORIZATION", fmt.Sprintf("Bearer isNotABear"))
+		rr := httptest.NewRecorder()
+		handlerRun := false
+		testHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			handlerRun = true
+		})
+
+		authMiddleware(testHandler).ServeHTTP(rr, req)
+
+		s.False(handlerRun)
+	})
+
+	s.Run("an empty token does not execute the handler", func() {
+		req := httptest.NewRequest("GET", "/systems/", nil)
+		rr := httptest.NewRecorder()
+		handlerRun := false
+		testHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			handlerRun = true
+		})
+
+		authMiddleware(testHandler).ServeHTTP(rr, req)
+
+		s.False(handlerRun)
+	})
+
 }
