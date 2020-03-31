c82656def59d0b4367b628290fcb66a88908e11e
Christopher Hui <christopher@truss.works>
2020-03-31 16:33:21 -0700

    add preferred solution page to business case

--- src/types/businessCase.ts
+++ src/types/businessCase.ts
@@ -1,5 +1,24 @@
 import { LifecyclePhase } from 'types/estimatedLifecycle';
 
+export type BusinessCaseSolution = {
+  title: string;
+  summary: string;
+  pros: string;
+  cons: string;
+  estimatedLifecycleCost: {
+    year1: LifecyclePhase[];
+    year2: LifecyclePhase[];
+    year3: LifecyclePhase[];
+    year4: LifecyclePhase[];
+    year5: LifecyclePhase[];
+  };
+  costSavings: string;
+};
+
+export type ProposedBusinessCaseSolution = BusinessCaseSolution & {
+  acquisitionApproach: string;
+};
+
 export type BusinessCaseModel = {
   projectName: string;
   requestor: {
@@ -14,18 +33,6 @@ export type BusinessCaseModel = {
   cmsBenefit: string;
   priorityAlignment: string;
   successIndicators: string;
-  asIsSolution: {
-    title: string;
-    summary: string;
-    pros: string;
-    cons: string;
-    estimatedLifecycleCost: {
-      year1: LifecyclePhase[];
-      year2: LifecyclePhase[];
-      year3: LifecyclePhase[];
-      year4: LifecyclePhase[];
-      year5: LifecyclePhase[];
-    };
-    costSavings: string;
-  };
+  asIsSolution: BusinessCaseSolution;
+  preferredSolution: ProposedBusinessCaseSolution;
 };
