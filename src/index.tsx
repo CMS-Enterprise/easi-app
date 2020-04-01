1372bd5481d65231bcf2d5281c6360a9de9348a1
Christopher Hui <christopher@truss.works>
2020-04-01 14:36:49 -0700

    try 1

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { TextEncoder } from 'text-encoding';
+import { detect } from 'detect-browser';
 import 'uswds';
 import App from './views/App';
 import store from './store';
@@ -13,12 +15,26 @@ if (process.env.NODE_ENV === 'development') {
   });
 }
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+if (typeof (window as any).TextEncoder === 'undefined') {
+  (window as any).TextEncoder = TextEncoder;
+}
+
+let app;
+const browser: any = detect();
+console.log(browser.name);
+if (browser.name === 'ie') {
+  app = (
+    <h1>Internet Explorer is not supported. Please use a modern browser.</h1>
+  );
+} else {
+  app = (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+}
+
+ReactDOM.render(app, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
