86cad628e771b2a0b40cc0292e1d39ad31fd6199
Kara Louie <kara@truss.works>
2020-04-10 14:06:22 -0700

    Add error handling for unfound system intake

--- src/views/GRTSystemIntakeReview/index.tsx
+++ src/views/GRTSystemIntakeReview/index.tsx
@@ -1,42 +1,40 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { withAuth } from '@okta/okta-react';
-import { useDispatch } from 'react-redux'; // , useSelector } from 'react-redux';
-// import { DateTime } from 'luxon';
-// import {
-//   DescriptionList,
-//   DescriptionTerm,
-//   DescriptionDefinition
-// } from 'components/shared/DescriptionGroup';
+import { useDispatch, useSelector } from 'react-redux';
+import { RouteComponentProps } from 'react-router-dom';
+import { DateTime } from 'luxon';
+import {
+  DescriptionList,
+  DescriptionTerm,
+  DescriptionDefinition
+} from 'components/shared/DescriptionGroup';
 // import convertBoolToYesNo from 'utils/convertBoolToYesNo';
 import { getSystemIntake } from '../../actions/systemIntakeActions';
-// import { AppState } from '../../reducers/rootReducer';
+import { AppState } from '../../reducers/rootReducer';
 // import { GovernanceCollaborationTeam } from '../../types/systemIntake';
 
-type GRTSystemIntakeReviewProps = {
+export type SystemIDRouterProps = {
+  systemID: string;
+};
+
+type GRTSystemIntakeReviewProps = RouteComponentProps<SystemIDRouterProps> & {
   auth: any;
 };
 
-const GRTSystemIntakeReview = ({ auth }: GRTSystemIntakeReviewProps) => {
-  const [isLoading, setIsLoading] = useState(false);
+const GRTSystemIntakeReview = ({ match, auth }: GRTSystemIntakeReviewProps) => {
   const dispatch = useDispatch();
 
   useEffect(() => {
     const fetchSystemIntake = async (): Promise<void> => {
-      setIsLoading(true);
       dispatch(
-        getSystemIntake(
-          await auth.getAccessToken(),
-          '50dab0f6-f532-43b2-965b-ff0b41cd3a1f'
-        )
+        getSystemIntake(await auth.getAccessToken(), match.params.systemID)
       );
-      setIsLoading(false);
     };
     fetchSystemIntake();
-  });
-
-  // const systemIntake = useSelector(
-  //   (state: AppState) => state.systemIntake.systemIntake
-  // );
+  }, [auth, dispatch, match.params.systemID]);
+  const systemIntake = useSelector(
+    (state: AppState) => state.systemIntake.systemIntake
+  );
   // const fundingDefinition = () => {
   //   const isFunded = convertBoolToYesNo(systemIntake.fundingSource.isFunded);
   //   if (systemIntake.fundingSource.isFunded) {
@@ -54,23 +52,42 @@ const GRTSystemIntakeReview = ({ auth }: GRTSystemIntakeReviewProps) => {
 
   return (
     <div className="system-intake__review margin-bottom-7">
-      {isLoading ? (
-        <div>Loading ...</div>
-      ) : (
+      <h1 className="font-heading-xl margin-top-4">CMS Sytem Request</h1>
+      {!systemIntake && (
+        <h2 className="font-heading-xl">
+          System intake with ID: {match.params.systemID} was not found
+        </h2>
+      )}
+      {systemIntake && (
         <div>
-          <h1 className="font-heading-xl margin-top-4">CMS Sytem Request</h1>
-          <hr className="system-intake__hr" />
-          <h2 className="font-heading-xl">What to do after reviewing?</h2>
-          <p>Email the requester and:</p>
-          <ul className="usa-list">
-            <li>Ask them to fill in the business case and submit it</li>
-            <li>
-              Tell them what to expect after they submit the business case
-            </li>
-            <li>And how to get in touch if they have any questions.</li>
-          </ul>
+          <DescriptionList title="System Request">
+            <div className="system-intake__review-row">
+              <div>
+                <DescriptionTerm term="Submission Date" />
+                <DescriptionDefinition
+                  definition={DateTime.local().toLocaleString(
+                    DateTime.DATE_MED
+                  )}
+                />
+              </div>
+              <div>
+                <DescriptionTerm term="Request for" />
+                <DescriptionDefinition
+                  definition={systemIntake.process_status}
+                />
+              </div>
+            </div>
+          </DescriptionList>
         </div>
       )}
+      <hr className="system-intake__hr" />
+      <h2 className="font-heading-xl">What to do after reviewing?</h2>
+      <p>Email the requester and:</p>
+      <ul className="usa-list">
+        <li>Ask them to fill in the business case and submit it</li>
+        <li>Tell them what to expect after they submit the business case</li>
+        <li>And how to get in touch if they have any questions.</li>
+      </ul>
     </div>
   );
 };
