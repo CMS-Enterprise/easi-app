5d9be290022ea2c5368d586138ecb9becbd298ef
Isaac Garfinkle <isaac@truss.works>
2020-04-09 23:52:42 -0700

    Should not be running failing auth test on local (since auth is faked)

--- pkg/integration/system_intakes_test.go
+++ pkg/integration/system_intakes_test.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/google/uuid"
 
+	"github.com/cmsgov/easi-app/pkg/appconfig"
 	"github.com/cmsgov/easi-app/pkg/models"
 )
 
@@ -35,14 +36,16 @@ func (s IntegrationTestSuite) TestSystemIntakeEndpoints() {
 
 	client := &http.Client{}
 
-	s.Run("PUT will fail with no Authorization", func() {
-		req, err := http.NewRequest(http.MethodPut, putURL.String(), bytes.NewBuffer(body))
-		s.NoError(err)
-		resp, err := client.Do(req)
+	if s.environment != appconfig.LocalEnv.String() {
+		s.Run("PUT will fail with no Authorization", func() {
+			req, err := http.NewRequest(http.MethodPut, putURL.String(), bytes.NewBuffer(body))
+			s.NoError(err)
+			resp, err := client.Do(req)
 
-		s.NoError(err)
-		s.Equal(http.StatusUnauthorized, resp.StatusCode)
-	})
+			s.NoError(err)
+			s.Equal(http.StatusUnauthorized, resp.StatusCode)
+		})
+	}
 
 	s.Run("PUT will succeed first time with token", func() {
 		req, err := http.NewRequest(http.MethodPut, putURL.String(), bytes.NewBuffer(body))
