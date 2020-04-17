916628745b26a58867a195f6f1af7cd11e26294c
Isaac Garfinkle <isaac@truss.works>
2020-04-16 18:40:33 -0700

    Add broken autosave

--- pkg/services/system_intakes.go
+++ pkg/services/system_intakes.go
@@ -14,7 +14,7 @@ import (
 
 // FetchSystemIntakesByEuaID fetches all system intakes by a given user
 func FetchSystemIntakesByEuaID(euaID string, db *sqlx.DB) (models.SystemIntakes, error) {
-	var intakes []models.SystemIntake
+	intakes := []models.SystemIntake{}
 	err := db.Select(&intakes, "SELECT * FROM system_intake WHERE eua_user_id=$1", euaID)
 	if err != nil {
 		return models.SystemIntakes{}, err
