5d17a9663ce05a03caa19c9d0380a3dae98156f7
Kara Louie <kara@truss.works>
2020-04-17 13:26:19 -0700

    Move shared data transformation to data folder

--- src/reducers/systemIntakeReducer.ts
+++ src/reducers/systemIntakeReducer.ts
@@ -1,98 +1,16 @@
-import cmsGovernanceTeams from 'constants/enums/cmsGovernanceTeams';
 import { STORE_SYSTEM_INTAKE } from '../constants/actions';
+import { SystemIntakeState, SystemIntakeForm } from '../types/systemIntake';
 import {
-  SystemIntakeState,
-  SystemIntakeForm,
-  GovernanceCollaborationTeam
-} from '../types/systemIntake';
+  initialSystemIntakeForm,
+  prepareSystemIntakeForApp
+} from '../data/systemIntake';
 
 const initialState: SystemIntakeState = {};
 
 function systemIntakeReducer(state = initialState, action: any): any {
-  let systemIntake: SystemIntakeForm = {
-    id: '',
-    euaUserID: '',
-    projectName: '',
-    status: 'DRAFT',
-    requester: {
-      name: '',
-      component: ''
-    },
-    businessOwner: {
-      name: '',
-      component: ''
-    },
-    productManager: {
-      name: '',
-      component: ''
-    },
-    isso: {
-      isPresent: null,
-      name: ''
-    },
-    governanceTeams: {
-      isPresent: false,
-      teams: []
-    },
-    fundingSource: {
-      isFunded: null,
-      fundingNumber: ''
-    },
-    businessNeed: '',
-    businessSolution: '',
-    currentStage: '',
-    needsEaSupport: null,
-    hasContract: ''
-  };
+  let systemIntake: SystemIntakeForm = initialSystemIntakeForm;
   if (action.systemIntake) {
-    const governanceTeams = () => {
-      const teams: GovernanceCollaborationTeam[] = [];
-      cmsGovernanceTeams.forEach(team => {
-        if (action.systemIntake[team.collaboratorKey]) {
-          teams.push({
-            collaborator: action.systemIntake[team.collaboratorKey],
-            name: team.value
-          });
-        }
-      });
-      return teams;
-    };
-
-    systemIntake = {
-      id: action.systemIntake.id || '',
-      euaUserID: action.systemIntake.euaUserID || '',
-      projectName: action.systemIntake.name || '',
-      status: action.systemIntake.status || 'DRAFT',
-      requester: {
-        name: action.systemIntake.requester || '',
-        component: action.systemIntake.component || ''
-      },
-      businessOwner: {
-        name: action.systemIntake.businessOwner || '',
-        component: action.systemIntake.businessOwnerComponent || ''
-      },
-      productManager: {
-        name: action.systemIntake.productManager || '',
-        component: action.systemIntake.productManagerComponent || ''
-      },
-      isso: {
-        isPresent: !!action.systemIntake.isso || null,
-        name: action.systemIntake.isso || ''
-      },
-      governanceTeams: {
-        isPresent: governanceTeams().length !== 0,
-        teams: governanceTeams() || []
-      },
-      fundingSource: {
-        isFunded: action.systemIntake.existingFunding || null,
-        fundingNumber: action.systemIntake.fundingSource || ''
-      },
-      businessNeed: action.systemIntake.businessNeed || '',
-      businessSolution: action.systemIntake.businessSolution || '',
-      currentStage: action.systemIntake.processStatus || '',
-      needsEaSupport: action.systemIntake.eaSupportRequest || null,
-      hasContract: action.systemIntake.existingContract || ''
-    };
+    systemIntake = prepareSystemIntakeForApp(action.systemIntake);
   }
   switch (action.type) {
     case STORE_SYSTEM_INTAKE:
