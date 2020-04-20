70146365d8c881874146683ef7abac6bd5d87eca
Isaac Garfinkle <isaac@truss.works>
2020-04-20 11:58:18 -0700

    Don't save on initial render

--- src/components/shared/AutoSave/index.tsx
+++ src/components/shared/AutoSave/index.tsx
@@ -1,5 +1,5 @@
 import debounce from 'lodash/debounce';
-import { useEffect, useCallback } from 'react';
+import { useEffect, useCallback, useRef } from 'react';
 
 type AutoSaveProps = {
   values: any;
@@ -8,10 +8,15 @@ type AutoSaveProps = {
 
 const AutoSave = ({ values, onSave }: AutoSaveProps) => {
   const debounceSave = useCallback(debounce(onSave, 3000), [onSave]);
+  const initialValues = useRef(values);
 
   useEffect(() => {
+    // Don't save until values are changed from initial values (avoids save on initial render)
+    if (initialValues.current === values) {
+      return;
+    }
     debounceSave();
-  }, [values, onSave]);
+  }, [debounceSave, values, onSave]);
 
   return null;
 };
