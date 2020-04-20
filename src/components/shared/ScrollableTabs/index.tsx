701b73b36ec98c98321c018bd13409e72ca56cc3
Christopher Hui <christopher@truss.works>
2020-04-20 16:47:34 -0700

    update scrollable tabs with readding tabs and more menu

--- src/components/shared/ScrollableTabs/index.tsx
+++ src/components/shared/ScrollableTabs/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useEffect, useLayoutEffect, useRef } from 'react';
 import classnames from 'classnames';
 import './index.scss';
 
@@ -6,39 +6,175 @@ type ScrollableTabsProps = {
   tabs: string[];
   children: React.ReactNode | React.ReactNodeArray;
 };
+
 const ScrollableTabs = ({ tabs, children }: ScrollableTabsProps) => {
   const [selectedIndex, setSelectedIndex] = useState(0);
-  const tabListRef: any = useRef(null);
+  const [displayedTabs, setDisplayedTabs] = useState(tabs);
+  const [moreTabsList, setMoreTabsList] = useState<string[]>([]);
+  const [componentWidth, setComponentWidth] = useState(0);
   const [tabListWidth, setTabListWidth] = useState(0);
+  const [tabInfo, setTabInfo] = useState<{ name: string; width: number }[]>([]);
+  const [isMoreMenuOpen, setIsMoreMenuOpen] = useState(false);
+  const dropdownNode = useRef<any>();
+  const moreButtonWidth = 73;
 
-  useEffect(() => {
-    const isScreen = typeof window === 'object';
-
-    if (isScreen) {
-      setTabListWidth(tabListRef.current.clientWidth);
+  const handleClick = (e: Event) => {
+    if (
+      dropdownNode &&
+      dropdownNode.current &&
+      dropdownNode.current.contains(e.target)
+    ) {
+      return;
     }
-  });
+
+    setIsMoreMenuOpen(false);
+  };
+
+  // Set tabs/widths on component mount
+  useEffect(() => {
+    const tabElements: any = document.querySelectorAll(
+      '.easi-scrollable-tabs__tab'
+    );
+    const arr: { name: string; width: number }[] = [];
+    tabElements.forEach((tab: HTMLElement) => {
+      arr.push({
+        name: tab.innerText,
+        width: tab.offsetWidth
+      });
+    });
+    setTabInfo(arr);
+  }, []);
+
+  // Set widths and event listners to watch for screen resizing
+  useEffect(() => {
+    const handleResize = () => {
+      const component: any = document.querySelector('.easi-scrollable-tabs');
+      const tabList: any = document.querySelector(
+        '.easi-scrollable-tabs__tab-list'
+      );
+
+      setComponentWidth(component.offsetWidth);
+      setTabListWidth(tabList.offsetWidth);
+    };
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  // Remove tabs when they don't fit
+  useLayoutEffect(() => {
+    if (componentWidth < tabListWidth + moreButtonWidth) {
+      const tabElements = document.querySelectorAll(
+        '.easi-scrollable-tabs__tab'
+      );
+      let updatedTabListWidth = tabListWidth + moreButtonWidth;
+      if (componentWidth < updatedTabListWidth) {
+        let numberOfTabsToRemove = 0;
+
+        while (componentWidth < updatedTabListWidth) {
+          numberOfTabsToRemove += 1;
+          updatedTabListWidth -=
+            tabElements[tabElements.length - numberOfTabsToRemove].clientWidth;
+        }
+        if (updatedTabListWidth !== tabListWidth) {
+          setTabListWidth(updatedTabListWidth);
+        }
+        if (numberOfTabsToRemove > 0) {
+          setDisplayedTabs(prevTabs =>
+            prevTabs.slice(0, prevTabs.length - numberOfTabsToRemove)
+          );
+          setMoreTabsList(
+            tabs.slice(displayedTabs.length - numberOfTabsToRemove, tabs.length)
+          );
+        }
+      }
+    }
+  }, [tabListWidth, componentWidth]);
+
+  // Add tabs when they fit
+  useLayoutEffect(() => {
+    if (moreTabsList.length > 0) {
+      const firstMoreTab = tabInfo.find(
+        (tab: any) => tab.name === moreTabsList[0]
+      );
+      if (
+        firstMoreTab &&
+        componentWidth >= tabListWidth + firstMoreTab.width + moreButtonWidth
+      ) {
+        setDisplayedTabs(prevTabs => [...prevTabs, moreTabsList[0]]);
+        setMoreTabsList(prevTabs =>
+          prevTabs.filter(tab => tab !== moreTabsList[0])
+        );
+      }
+    }
+  }, [tabListWidth, componentWidth]);
+
+  // Close "More Tabs" menu when it is empty
+  useEffect(() => {
+    if (moreTabsList.length === 0) {
+      setIsMoreMenuOpen(false);
+    }
+  }, [moreTabsList.length]);
+
+  // Add event listner for closing the "More Tabs" menu
+  useEffect(() => {
+    document.addEventListener('mouseup', handleClick);
+
+    return () => {
+      document.removeEventListener('mouseup', handleClick);
+    };
+  }, []);
 
   return (
     <div className="easi-scrollable-tabs">
-      <ul ref={tabListRef} className="easi-scrollable-tabs__tab-list">
-        {tabs.map((tab, index) => (
-          <li
-            key={tab}
-            className={classnames('easi-scrollable-tabs__tab', {
-              'easi-scrollable-tabs__tab--selected': selectedIndex === index
-            })}
-          >
-            <button
-              type="button"
-              className="easi-scrollable-tabs__tab-btn"
-              onClick={() => setSelectedIndex(index)}
+      <div className="easi-scrollable-tabs__tabs-wrapper">
+        <ul className="easi-scrollable-tabs__tab-list">
+          {displayedTabs.map((tab, index) => (
+            <li
+              key={tab}
+              className={classnames('easi-scrollable-tabs__tab', {
+                'easi-scrollable-tabs__tab--selected': selectedIndex === index
+              })}
             >
-              <span className="easi-scrollable-tabs__tab-text">{tab}</span>
-            </button>
-          </li>
-        ))}
-      </ul>
+              <button
+                type="button"
+                className="easi-scrollable-tabs__tab-btn"
+                onClick={() => setSelectedIndex(index)}
+              >
+                <span className="easi-scrollable-tabs__tab-text">{tab}</span>
+              </button>
+            </li>
+          ))}
+        </ul>
+        {moreTabsList.length > 0 && (
+          <button
+            type="button"
+            ref={dropdownNode}
+            className="easi-scrollable-tabs__more-btn"
+            onClick={() => {
+              setIsMoreMenuOpen(true);
+            }}
+            aria-label={
+              isMoreMenuOpen ? 'Close More Tabs Menu' : 'Expand More Tabs Menu'
+            }
+            aria-controls="ScrollableTabs-MoreMenu"
+            aria-expanded={isMoreMenuOpen}
+          >
+            <i className="fa fa-angle-right easi-scrollable-tabs__angle-right" />
+            <span>More</span>
+          </button>
+        )}
+        {isMoreMenuOpen && (
+          <ul
+            id="ScrollableTabs-MoreMenu"
+            className="easi-scrollable-tabs__more-menu"
+          >
+            {moreTabsList.map(tab => (
+              <li key={`menu-tab-${tab}`}>{tab}</li>
+            ))}
+          </ul>
+        )}
+      </div>
       {children}
     </div>
   );
