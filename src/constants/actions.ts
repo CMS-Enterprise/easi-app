ebb0591e3a8ba8d4cc3183a6df3ae4bbdc3de145
Isaac Garfinkle <isaac@truss.works>
2020-04-27 15:00:35 -0700

    Migrate putSystemIntake to routines

--- src/constants/actions.ts
+++ src/constants/actions.ts
@@ -1,9 +1,3 @@
 // Actions when dealing with system shorts
 export const GET_ALL_SYSTEM_SHORTS: string = 'GET_ALL_SYSTEM_SHORTS';
 export const STORE_SYSTEM_SHORTS: string = 'STORE_SYSTEM_SHORTS';
-
-// Actions when dealing with a single system intake
-export const PUT_SYSTEM_INTAKE: string = 'PUT_SYSTEM_INTAKE';
-
-// Actions when dealing with plural system intakes
-export const STORE_SYSTEM_INTAKES: string = 'STORE_SYSTEM_INTAKES';
