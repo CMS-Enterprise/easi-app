5fda9d66360bda6b61d9253a8720dd84179e01d2
Isaac Garfinkle <isaac@truss.works>
2020-04-29 16:03:49 -0700

    move timestamp to SUCCESS

--- src/sagas/systemIntakeSaga.ts
+++ src/sagas/systemIntakeSaga.ts
@@ -4,6 +4,7 @@ import { PutSystemIntakeAction } from 'types/systemIntake';
 import { prepareSystemIntakeForApi } from 'data/systemIntake';
 import { fetchSystemIntake, saveSystemIntake } from 'types/routines';
 import { Action } from 'redux-actions';
+import { Routine } from 'redux-saga-routines';
 
 function putSystemIntakeRequest({ id, formData }: PutSystemIntakeAction) {
   // Make API save request
@@ -11,35 +12,32 @@ function putSystemIntakeRequest({ id, formData }: PutSystemIntakeAction) {
   return axios.put(`${process.env.REACT_APP_API_ADDRESS}/system_intake`, data);
 }
 
-function* putSystemIntake(action: Action<any>) {
-  try {
-    yield put(saveSystemIntake.request());
-    const response = yield call(putSystemIntakeRequest, action.payload);
-    yield put(saveSystemIntake.success(response.data));
-  } catch (error) {
-    yield put(saveSystemIntake.failure(error.message));
-  } finally {
-    yield put(saveSystemIntake.fulfill());
-  }
+function createRoutineSaga(routine: Routine, requestFunction: any) {
+  // eslint-disable-next-line func-names
+  return function*(action: Action<any>) {
+    try {
+      yield put(routine.request());
+      const response = yield call(requestFunction, action.payload);
+      yield put(routine.success(response.data));
+    } catch (error) {
+      yield put(routine.failure(error.message));
+    } finally {
+      yield put(routine.fulfill());
+    }
+  };
 }
 
 function getSystemIntakeRequest(id: string) {
   return axios.get(`${process.env.REACT_APP_API_ADDRESS}/system_intake/${id}`);
 }
 
-function* getSystemIntake(action: Action<any>) {
-  try {
-    yield put(fetchSystemIntake.request());
-    const response = yield call(getSystemIntakeRequest, action.payload);
-    yield put(fetchSystemIntake.success(response.data));
-  } catch (error) {
-    yield put(fetchSystemIntake.failure(error.message));
-  } finally {
-    yield put(fetchSystemIntake.fulfill());
-  }
-}
-
 export default function* systemIntakeSaga() {
-  yield takeLatest(fetchSystemIntake.TRIGGER, getSystemIntake);
-  yield takeLatest(saveSystemIntake.TRIGGER, putSystemIntake);
+  yield takeLatest(
+    fetchSystemIntake.TRIGGER,
+    createRoutineSaga(fetchSystemIntake, getSystemIntakeRequest)
+  );
+  yield takeLatest(
+    saveSystemIntake.TRIGGER,
+    createRoutineSaga(saveSystemIntake, putSystemIntakeRequest)
+  );
 }
