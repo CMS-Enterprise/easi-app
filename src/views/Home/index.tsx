a5e7de9899cae06b42fc38cc10fccd67d02d795b
Christopher Hui <christopher@truss.works>
2020-04-30 12:58:40 -0700

    WIP gotta go

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
 import { withAuth } from '@okta/okta-react';
 
 import useAuth from 'hooks/useAuth';
 import Header from 'components/Header';
+import Button from 'components/shared/Button';
+import { fetchSystemIntakes } from 'types/routines';
 
 type HomeProps = {
   auth: any;
@@ -10,13 +14,29 @@ type HomeProps = {
 
 const Home = ({ auth }: HomeProps) => {
   const [isAuthenticated] = useAuth(auth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      dispatch(fetchSystemIntakes());
+    }
+  }, [isAuthenticated]);
 
   return (
     <div>
       <Header />
       <div className="grid-container">
-        <h1>Home</h1>
-        <h3>{`A user is ${isAuthenticated ? '' : 'NOT'} authenticated`}</h3>
+        <h1>Welcome to EASi</h1>
+        <p>
+          You can use EASi to go through the set of steps needed for Lifecycle
+          ID approval by the Governance Review Board (GRB).
+        </p>
+
+        {isAuthenticated ? (
+          <Button type="button">Start now</Button>
+        ) : (
+          <Button type="button">Sign in to start</Button>
+        )}
       </div>
     </div>
   );
