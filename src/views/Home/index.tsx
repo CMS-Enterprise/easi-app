88ce4d89d9ca397b33440641ee4839b36435bcc0
Christopher Hui <christopher@truss.works>
2020-04-30 21:09:32 -0700

    add info text

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -11,6 +11,7 @@ import ActionBanner from 'components/shared/ActionBanner';
 import { AppState } from 'reducers/rootReducer';
 import { fetchSystemIntakes } from 'types/routines';
 import { SystemIntakeForm } from 'types/systemIntake';
+import './index.scss';
 
 type HomeProps = RouteComponentProps & {
   auth: any;
@@ -57,33 +58,52 @@ const Home = ({ auth, history }: HomeProps) => {
   return (
     <div>
       <Header />
-      <div className="grid-container margin-top-6">
-        {getSystemIntakeBanners()}
-        <h1 className="margin-top-6">Welcome to EASi</h1>
-        <p>
-          You can use EASi to go through the set of steps needed for Lifecycle
-          ID approval by the Governance Review Board (GRB).
-        </p>
-
-        {isAuthenticated ? (
-          <Button
-            type="button"
-            onClick={() => {
-              history.push('/system/new');
-            }}
-          >
-            Start now
-          </Button>
-        ) : (
-          <Button
-            type="button"
-            onClick={() => {
-              history.push('/login');
-            }}
-          >
-            Sign in to start
-          </Button>
-        )}
+      <div role="main" className="grid-container margin-top-6">
+        <div className="tablet:grid-col-9">
+          {getSystemIntakeBanners()}
+          <h1 className="margin-top-6">Welcome to EASi</h1>
+          <p className="easi-home__body">
+            You can use EASi to go through the set of steps needed for Lifecycle
+            ID approval by the Governance Review Board (GRB).
+          </p>
+          <div className="easi-home__info-wrapper">
+            <div className="easi-home__info-icon">
+              <i className="fa fa-info" />
+            </div>
+            <p className="line-height-body-5">
+              Use this process only if you&apos;d like to add a new system,
+              service or make major changes and upgrades to an existing one. For
+              all other requests, please use the{' '}
+              <a
+                href="http://google.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                alternative request form
+              </a>
+              .
+            </p>
+          </div>
+          {isAuthenticated ? (
+            <Button
+              type="button"
+              onClick={() => {
+                history.push('/system/new');
+              }}
+            >
+              Start now
+            </Button>
+          ) : (
+            <Button
+              type="button"
+              onClick={() => {
+                history.push('/login');
+              }}
+            >
+              Sign in to start
+            </Button>
+          )}
+        </div>
       </div>
     </div>
   );
