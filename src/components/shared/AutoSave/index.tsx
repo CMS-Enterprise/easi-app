fdc25c126998050cae22eb188142485f705c55b9
Christopher Hui <christopher@truss.works>
2020-04-30 22:20:38 -0700

    test run at reloading saved intake form
    # Conflicts:
    #	src/components/shared/AutoSave/index.tsx
    #	src/views/SystemIntake/index.tsx

--- src/components/shared/AutoSave/index.tsx
+++ src/components/shared/AutoSave/index.tsx
@@ -1,32 +1,19 @@
 import debounce from 'lodash/debounce';
-import isEqual from 'lodash/isEqual';
 import { useEffect, useCallback } from 'react';
 
 type AutoSaveProps = {
   values: any;
   onSave: () => void;
   debounceDelay: number;
-  lastSavedValues: any;
 };
 
-const AutoSave = ({
-  values,
-  onSave,
-  debounceDelay,
-  lastSavedValues
-}: AutoSaveProps) => {
+const AutoSave = ({ values, onSave, debounceDelay }: AutoSaveProps) => {
   const debounceSave = useCallback(debounce(onSave, debounceDelay), [
     onSave,
     debounceDelay
   ]);
 
-  useEffect(() => {
-    // Don't save until values are changed from initial values (avoids save on initial render)
-    if (isEqual(lastSavedValues, values)) {
-      return;
-    }
-    debounceSave();
-  }, [values]);
+  useEffect(debounceSave, [debounceSave, values]);
 
   return null;
 };
--- src/reducers/systemIntakeReducer.ts
+++ src/reducers/systemIntakeReducer.ts
@@ -3,11 +3,12 @@ import {
   initialSystemIntakeForm,
   prepareSystemIntakeForApp
 } from 'data/systemIntake';
-import { fetchSystemIntake } from 'types/routines';
+import { fetchSystemIntake, storeSystemIntakeId } from 'types/routines';
 import { Action } from 'redux-actions';
 
 const initialState: SystemIntakeState = {
-  systemIntake: initialSystemIntakeForm
+  systemIntake: initialSystemIntakeForm,
+  isLoading: null
 };
 
 function systemIntakeReducer(
@@ -26,6 +27,25 @@ function systemIntakeReducer(
         systemIntake: prepareSystemIntakeForApp(action.payload)
       };
     case fetchSystemIntake.FULFILL:
+      return {
+        ...state,
+        isLoading: false
+      };
+    case storeSystemIntakeId.TRIGGER:
+      return {
+        ...state,
+        systemIntake: {
+          ...state.systemIntake,
+          id: action.payload
+        },
+        isLoading: false
+      };
+    case storeSystemIntakeId.FAILURE:
+      return {
+        ...state,
+        error: action.payload
+      };
+    case storeSystemIntakeId.FULFILL:
       return {
         ...state,
         isLoading: false
