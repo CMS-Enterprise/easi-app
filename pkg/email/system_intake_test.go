f0bf2bbc1c4d05759da8ddc6bacc4fb757b281e6
Mikena Wood <mikena@truss.works>
2020-05-11 12:19:01 -0700

    use real path for GRT review

--- pkg/email/system_intake_test.go
+++ pkg/email/system_intake_test.go
@@ -3,7 +3,7 @@ package email
 import "github.com/google/uuid"
 
 func (s *SESTestSuite) TestSendSystemIntakeEmail() {
-	intakeID, _ := uuid.NewUUID()
+	intakeID, _ := uuid.Parse("1abc2671-c5df-45a0-b2be-c30899b473bf")
 	err := s.client.SendSystemIntakeSubmissionEmail("Test McTester", intakeID)
 	s.NoError(err)
 }
