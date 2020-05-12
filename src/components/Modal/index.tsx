6f0d4d02ec4ef7cc145d52e24520f95168f6df8d
Isaac Garfinkle <isaac@truss.works>
2020-05-12 15:44:49 -0700

    Modal prohibits scrolling, adds more props

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,26 +1,36 @@
 import React, { ReactNode, ReactNodeArray } from 'react';
 import classNames from 'classnames';
 import ReactModal from 'react-modal';
+import noScroll from 'no-scroll';
 import './index.scss';
 
 type ModalProps = {
   title: string;
   children: ReactNode | ReactNodeArray;
+  isOpen: boolean;
+  closeModal: () => void;
 };
 
-const Modal = ({ title, children }: ModalProps) => {
+const Modal = ({ title, children, isOpen, closeModal }: ModalProps) => {
   const overlayClassName = classNames('easi-modal__overlay');
   const contentClassName = classNames('easi-modal__content');
 
   return (
     <ReactModal
-      isOpen
+      isOpen={isOpen}
       overlayClassName={overlayClassName}
       className={contentClassName}
+      onAfterOpen={noScroll.on}
+      onAfterClose={noScroll.off}
+      onRequestClose={closeModal}
     >
       <div className="easi-modal__header">
         <div className="easi-modal__title">{title}</div>
-        <button type="button" className="easi-modal__x-button">
+        <button
+          type="button"
+          className="easi-modal__x-button"
+          onClick={closeModal}
+        >
           <i className="fa fa-times" />
         </button>
       </div>
--- src/views/Sandbox/index.tsx
+++ src/views/Sandbox/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from 'components/Header';
 import Modal from 'components/Modal';
 import Button from 'components/shared/Button';
@@ -7,12 +7,38 @@ import Button from 'components/shared/Button';
 // This is just a sandbox page for us to test things out
 
 const Sandbox = () => {
+  const [isModalOpen, setModalOpen] = useState(false);
+
   return (
     <div>
       <Header />
       <div className="grid-container">
+        <button type="button" onClick={() => setModalOpen(true)}>
+          Open modal
+        </button>
+        {/* So we can test that it doesn't scroll when modal open */}
         <h1>Sandbox</h1>
-        <Modal title="EASi">
+        <h1>Sandbox</h1>
+        <h1>Sandbox</h1>
+        <h1>Sandbox</h1>
+        <h1>Sandbox</h1>
+        <h1>Sandbox</h1>
+        <h1>Sandbox</h1>
+        <h1>Sandbox</h1>
+        <h1>Sandbox</h1>
+        <h1>Sandbox</h1>
+        <h1>Sandbox</h1>
+        <h1>Sandbox</h1>
+        <h1>Sandbox</h1>
+        <h1>Sandbox</h1>
+        <h1>Sandbox</h1>
+        <h1>Sandbox</h1>
+        <h1>Sandbox</h1>
+        <Modal
+          title="EASi"
+          isOpen={isModalOpen}
+          closeModal={() => setModalOpen(false)}
+        >
           <h1 style={{ margin: 0 }}>
             Your access to EASi is about to expire in 5 minutes
           </h1>
@@ -22,7 +48,9 @@ const Sandbox = () => {
             minutes and will need to sign back in. We do this to keep your
             information secure.
           </p>
-          <Button type="button">Return to EASi</Button>
+          <Button type="button" onClick={() => setModalOpen(false)}>
+            Return to EASi
+          </Button>
         </Modal>
       </div>
     </div>
