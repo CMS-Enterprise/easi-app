0b1d5cbd9b97579efdbfcea763a947643ecc2aac
Christopher Hui <christopher@truss.works>
2020-05-13 10:19:45 -0700

    update storesystemintakeid to accept multiple fields and add tests

--- src/types/routines.ts
+++ src/types/routines.ts
@@ -6,7 +6,7 @@ export const fetchSystemIntakes = createRoutine('FETCH_SYSTEM_INTAKES');
 // SystemIntake routines
 export const fetchSystemIntake = createRoutine('FETCH_SYSTEM_INTAKE');
 export const saveSystemIntake = createRoutine('PUT_SYSTEM_INTAKE');
-export const storeSystemIntakeId = createRoutine('STORE_SYSTEM_INTAKE_ID');
+export const storeSystemIntake = createRoutine('STORE_SYSTEM_INTAKE');
 
 // SystemShorts routines
 export const fetchSystemShorts = createRoutine('FETCH_SYSTEM_SHORTS');
