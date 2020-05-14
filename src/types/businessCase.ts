d3cb652d7b6e1e8b40c33b27b142edcb3bd79e95
Isaac Garfinkle <isaac@truss.works>
2020-05-14 09:04:45 -0700

    prepareBusinessCaseForApp

--- src/types/businessCase.ts
+++ src/types/businessCase.ts
@@ -1,17 +1,19 @@
 import { LifecyclePhase } from 'types/estimatedLifecycle';
 
+export type EstimatedLifecycleCostLines = {
+  year1: LifecyclePhase[];
+  year2: LifecyclePhase[];
+  year3: LifecyclePhase[];
+  year4: LifecyclePhase[];
+  year5: LifecyclePhase[];
+};
+
 export type BusinessCaseSolution = {
   title: string;
   summary: string;
   pros: string;
   cons: string;
-  estimatedLifecycleCost: {
-    year1: LifecyclePhase[];
-    year2: LifecyclePhase[];
-    year3: LifecyclePhase[];
-    year4: LifecyclePhase[];
-    year5: LifecyclePhase[];
-  };
+  estimatedLifecycleCost: EstimatedLifecycleCostLines;
   costSavings: string;
 };
 
