edcb4ad171cac79e2925fbffe70c32603ccd9bf6
Kara Louie <kara@truss.works>
2020-05-15 15:32:31 -0700

    Manage conflicts with rebasing on master

--- pkg/services/business_case.go
+++ pkg/services/business_case.go
@@ -19,8 +19,29 @@ func NewFetchBusinessCaseByID(
 			logger.Error("failed to fetch business case")
 			return &models.BusinessCase{}, &apperrors.QueryError{
 				Err:       err,
-				Model:     "Business Case",
-				Operation: "fetch",
+				Model:     businessCase,
+				Operation: apperrors.QueryFetch,
+			}
+		}
+		return businessCase, nil
+	}
+}
+
+// NewFetchBusinessCaseByID is a service to fetch the business case by id
+func NewCreateBusinessCase(
+	create func(businessCase *models.BusinessCase) (*models.BusinessCase, error),
+	logger *zap.Logger,
+) func(businessCase *models.BusinessCase) (*models.BusinessCase, error) {
+	return func(businessCase *models.BusinessCase) (*models.BusinessCase, error) {
+		id := uuid.New()
+		businessCase.ID = id
+		businessCase, err := create(businessCase)
+		if err != nil {
+			logger.Error("failed to create a business case")
+			return &models.BusinessCase{}, &apperrors.QueryError{
+				Err:       err,
+				Model:     businessCase,
+				Operation: apperrors.QueryPost,
 			}
 		}
 		return businessCase, nil
