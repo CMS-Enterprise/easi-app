21e587271868aa311d4b3af0b1c6350619b67f16
Isaac Garfinkle <isaac@truss.works>
2020-05-14 23:58:27 -0700

    Fix fieldName

--- src/views/GovernanceOverview/index.tsx
+++ src/views/GovernanceOverview/index.tsx
@@ -6,20 +6,20 @@ import CollapsableLink from 'components/shared/CollapsableLink';
 import Button from 'components/shared/Button';
 
 type NumberedListItemProps = {
-  index: string;
+  stepName: string;
   header: string;
   body: string;
 };
 
 const NumberedListContinuingItem = ({
-  index,
+  stepName,
   header,
   body
 }: NumberedListItemProps) => {
   return (
     <div className="easi-governance-overview__numbered-list-item">
       <div className="easi-governance-overview__list-item-continuing">
-        <span className="easi-governance-overview__dot">{index}</span>
+        <span className="easi-governance-overview__dot">{stepName}</span>
       </div>
       <div className="margin-top-05 margin-left-1 margin-bottom-3 tablet:margin-bottom-4">
         <p className="text-bold margin-top-0 margin-bottom-05">{header}</p>
@@ -32,14 +32,14 @@ const NumberedListContinuingItem = ({
 };
 
 const NumberedListTerminalItem = ({
-  index,
+  stepName,
   header,
   body
 }: NumberedListItemProps) => {
   return (
     <div className="easi-governance-overview__numbered-list-item">
       <div className="easi-governance-overview__list-item-terminal">
-        <span className="easi-governance-overview__dot">{index}</span>
+        <span className="easi-governance-overview__dot">{stepName}</span>
       </div>
       <div className="margin-top-05 margin-left-1 margin-bottom-0">
         <p className="text-bold margin-top-0 margin-bottom-05">{header}</p>
@@ -92,44 +92,44 @@ const GovernanceOverview = ({ history }: RouteComponentProps) => {
         </span>
         <h1 className="font-heading-xl">Steps in the governance process</h1>
         <NumberedListContinuingItem
-          index="1"
+          stepName="1"
           header="Fill the intake request form"
           body="Tell the Governance admin team about your project/ idea."
         />
         <NumberedListTerminalItem
-          index="2"
+          stepName="2"
           header="Feedback from initial review"
           body="The Governance admin team will review your intake request form and decide if
            it needs further governance. If it does, they’ll direct you to go through the remaining steps."
         />
         <hr className="margin-y-3" />
         <NumberedListContinuingItem
-          index="3"
+          stepName="3"
           header="Prepare your business case"
           body="Make a first draft of your initial solutions and the corresponding costs involved."
         />
         <NumberedListContinuingItem
-          index="4"
+          stepName="4"
           header="Attend the Governance Review Team meeting"
           body="Discuss your draft business case with Governance Review Team. They will help you refine and make your business case in the best shape possible. "
         />
         <NumberedListContinuingItem
-          index="5"
+          stepName="5"
           header="Feedback from the Governance Review Team"
           body="If the Governance Review Team has any additional comments, they will ask you to update your business case before it’s submitted to the Governance Review Board."
         />
         <NumberedListContinuingItem
-          index="6"
+          stepName="6"
           header="Submit the business case for final approval"
           body="Update the business case based on feedback from the review meeting and submit it to the Governance Review Board."
         />
         <NumberedListContinuingItem
-          index="7"
+          stepName="7"
           header="Attend the Governance Review Board Meeting"
           body="The Governance Review Board will discuss and make decisions based on the business case and recommendations from the Governance Review Team."
         />
         <NumberedListTerminalItem
-          index="8"
+          stepName="8"
           header="Decision and next steps"
           body="If your business case is approved you will receive a unique Lifecycle ID. Depending on allocation, funding may or may not be immediately approved."
         />
