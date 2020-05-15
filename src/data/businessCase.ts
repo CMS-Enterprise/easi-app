18a33a963d0d5ec0804cbbd799221eb61cced268
Isaac Garfinkle <isaac@truss.works>
2020-05-14 21:42:24 -0700

    Adds prepareBusinessCaseForApi but it won't work

--- src/data/businessCase.ts
+++ src/data/businessCase.ts
@@ -2,6 +2,7 @@ import {
   BusinessCaseModel,
   EstimatedLifecycleCostLines
 } from 'types/businessCase';
+import { LifecyclePhase } from 'types/estimatedLifecycle';
 
 export const defaultEstimatedLifecycle = {
   year1: [{ phase: '', cost: '' }],
@@ -132,5 +133,119 @@ export const prepareBusinessCaseForApp = (
       costSavings: businessCase.alternativeBCostSavings,
       estimatedLifecycleCost: lifecycleCostLines.B
     }
+  };
+};
+
+export const prepareBusinessCaseForApi = (
+  businessCase: BusinessCaseModel
+): any => {
+  const solutionNameMap: {
+    solutionLifecycleCostLines: EstimatedLifecycleCostLines;
+    solutionApiName: string;
+  }[] = [
+    {
+      solutionLifecycleCostLines:
+        businessCase.asIsSolution.estimatedLifecycleCost,
+      solutionApiName: 'As Is'
+    },
+    {
+      solutionLifecycleCostLines:
+        businessCase.preferredSolution.estimatedLifecycleCost,
+      solutionApiName: 'Preferred'
+    },
+    {
+      solutionLifecycleCostLines:
+        businessCase.alternativeA.estimatedLifecycleCost,
+      solutionApiName: 'A'
+    },
+    ...(businessCase.alternativeB
+      ? [
+          {
+            solutionLifecycleCostLines:
+              businessCase.alternativeB.estimatedLifecycleCost,
+            solutionApiName: 'B'
+          }
+        ]
+      : [])
+  ];
+
+  const yearMap = (
+    lifecycleCostLines: EstimatedLifecycleCostLines
+  ): { phases: LifecyclePhase[]; year: string }[] => {
+    return [
+      { phases: lifecycleCostLines.year1, year: '1' },
+      { phases: lifecycleCostLines.year2, year: '2' },
+      { phases: lifecycleCostLines.year3, year: '3' },
+      { phases: lifecycleCostLines.year4, year: '4' },
+      { phases: lifecycleCostLines.year5, year: '5' }
+    ];
+  };
+
+  const lifecycleCostLines = solutionNameMap
+    .map(({ solutionLifecycleCostLines, solutionApiName }) => {
+      return yearMap(solutionLifecycleCostLines)
+        .map(({ phases, year }) => {
+          return phases.map(lifecyclePhase => {
+            return {
+              solution: solutionApiName,
+              phase: lifecyclePhase.phase,
+              cost: lifecyclePhase.cost,
+              year
+            };
+          });
+        })
+        .flat();
+    })
+    .flat();
+
+  return {
+    id: '',
+    euaUserId: '',
+    projectName: businessCase.requestName,
+    requester: businessCase.requester.name,
+    requesterPhoneNumber: businessCase.requester.phoneNumber,
+    businessOwner: businessCase.businessOwner,
+    businessNeed: businessCase.businessNeed,
+    cmsBenefit: businessCase.cmsBenefit,
+    priorityAlignment: businessCase.priorityAlignment,
+    successIndicators: businessCase.successIndicators,
+    asIsTitle: businessCase.asIsSolution.title,
+    asIsSummary: businessCase.asIsSolution.summary,
+    asIsPros: businessCase.asIsSolution.pros,
+    asIsCons: businessCase.asIsSolution.cons,
+    asIsCostSavings: businessCase.asIsSolution.costSavings,
+    preferredTitle: businessCase.preferredSolution.title,
+    preferredSummary: businessCase.preferredSolution.summary,
+    preferredAcquisitionApproach:
+      businessCase.preferredSolution.acquisitionApproach,
+    preferredPros: businessCase.preferredSolution.pros,
+    preferredCons: businessCase.preferredSolution.cons,
+    preferredCostSavings: businessCase.preferredSolution.costSavings,
+    alternativeATitle: businessCase.alternativeA.title,
+    alternativeASummary: businessCase.alternativeA.summary,
+    alternativeAAcquisitionApproach:
+      businessCase.alternativeA.acquisitionApproach,
+    alternativeAPros: businessCase.alternativeA.pros,
+    alternativeACons: businessCase.alternativeA.cons,
+    alternativeACostSavings: businessCase.alternativeA.costSavings,
+    alternativeBTitle: businessCase.alternativeB
+      ? businessCase.alternativeB.title
+      : '',
+    alternativeBSummary: businessCase.alternativeB
+      ? businessCase.alternativeB.summary
+      : '',
+    alternativeBAcquisitionApproach: businessCase.alternativeB
+      ? businessCase.alternativeB.acquisitionApproach
+      : '',
+    alternativeBPros: businessCase.alternativeB
+      ? businessCase.alternativeB.pros
+      : '',
+    alternativeBCons: businessCase.alternativeB
+      ? businessCase.alternativeB.cons
+      : '',
+    alternativeBCostSavings: businessCase.alternativeB
+      ? businessCase.alternativeB.costSavings
+      : '',
+    lifecycleCostLines
   };
 };
