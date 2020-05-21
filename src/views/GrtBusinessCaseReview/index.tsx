fc521ebb23bf6d5d3ccb23ecf7e3d0502c007b03
Christopher Hui <christopher@truss.works>
2020-05-21 08:46:48 -0700

    update grt business case view to accept business case by param

--- src/views/GrtBusinessCaseReview/index.tsx
+++ src/views/GrtBusinessCaseReview/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { RouteComponentProps } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import BusinessCaseReview from 'components/BusinessCaseReview';
 import Header from 'components/Header';
 import { fetchBusinessCase } from 'types/routines';
@@ -10,24 +10,20 @@ export type BusinessCaseRouterProps = {
   businessCaseId: string;
 };
 
-export type GrtBusinessCaseReviewProps = RouteComponentProps<
-  BusinessCaseRouterProps
-> & {
-  match: any;
-};
-
-export const GrtBusinessCaseReview = ({
-  match
-}: GrtBusinessCaseReviewProps) => {
+export const GrtBusinessCaseReview = () => {
   const dispatch = useDispatch();
+  const { businessCaseId } = useParams();
 
   useEffect(() => {
-    dispatch(fetchBusinessCase(match.params.businessCaseId));
-  }, [dispatch, match.params.businessCaseId]);
+    dispatch(fetchBusinessCase(businessCaseId));
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const businessCase = useSelector(
     (state: AppState) => state.businessCase.form
   );
+
   return (
     <div>
       <Header />
@@ -36,20 +32,11 @@ export const GrtBusinessCaseReview = ({
           <h1 className="font-heading-xl margin-top-4">CMS Business Case</h1>
           {!businessCase && (
             <h2 className="font-heading-xl">
-              Business Case with ID: {match.params.businessCaseId} was not found
+              Business Case with ID: {businessCaseId} was not found
             </h2>
           )}
           {businessCase && <BusinessCaseReview values={businessCase} />}
           <hr className="opacity-30" />
-          <h2 className="font-heading-xl">What to do after reviewing?</h2>
-          <p>Email the requester and:</p>
-          <ul className="usa-list">
-            <li>Ask them to fill in the business case and submit it</li>
-            <li>
-              Tell them what to expect after they submit the business case
-            </li>
-            <li>And how to gjet in touch if they have any questions.</li>
-          </ul>
         </div>
       </div>
     </div>
