588fcb4d80172147540f61c6670237a385c6c224
Kara Louie <kara@truss.works>
2020-05-22 13:27:37 -0700

    Add status to business case

--- pkg/integration/business_case_test.go
+++ pkg/integration/business_case_test.go
@@ -33,6 +33,7 @@ func (s IntegrationTestSuite) TestBusinessCaseEndpoints() {
 
 	body, err := json.Marshal(map[string]string{
 		"systemIntake": intakeID.String(),
+		"status":       "DRAFT",
 	})
 	s.NoError(err)
 
