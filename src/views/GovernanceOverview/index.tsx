5aca211f2a6a3aa9c751e53d1c66123a899278e8
Christopher Hui <christopher@truss.works>
2020-06-02 14:27:42 -0700

    content updates to home and governance overview

--- src/views/GovernanceOverview/index.tsx
+++ src/views/GovernanceOverview/index.tsx
@@ -85,11 +85,10 @@ const GovernanceOverview = () => {
         <span>This step by step process will help you:</span>
         <ul className="margin-top-1 padding-left-205 line-height-body-5">
           <li>
-            Get help from Subject Matter Experts (SMEs) to make an effective
-            business case
+            work with Subject Matter Experts (SMEs) to refine your business case
           </li>
-          <li>Get a lifecycle ID</li>
-          <li>Get a decision on funding for your project</li>
+          <li>get a Lifecycle ID</li>
+          <li>get approval for your request to then seek funding</li>
         </ul>
         <span>
           It can take between 4 to 6 weeks to go through all the steps and get a
@@ -105,19 +104,18 @@ const GovernanceOverview = () => {
           <NumberedListTerminalItem
             stepName="2"
             header="Feedback from initial review"
-            body="The Governance admin team will review your intake request form and decide if
-             it needs further governance. If it does, they’ll direct you to go through the remaining steps."
+            body="The Governance admin team will review your intake request form and decide if it it needs further governance. If it does, they’ll direct you to go through the remaining steps."
           />
           <hr className="margin-y-3" />
           <NumberedListContinuingItem
             stepName="3"
             header="Prepare your business case"
-            body="Make a first draft of your initial solutions and the corresponding costs involved."
+            body="Draft different solutions and the corresponding costs involved."
           />
           <NumberedListContinuingItem
             stepName="4"
             header="Attend the Governance Review Team meeting"
-            body="Discuss your draft business case with Governance Review Team. They will help you refine and make your business case in the best shape possible. "
+            body="Discuss your draft business case with Governance Review Team. They will help you refine your business case into the best shape possible."
           />
           <NumberedListContinuingItem
             stepName="5"
@@ -137,7 +135,7 @@ const GovernanceOverview = () => {
           <NumberedListTerminalItem
             stepName="8"
             header="Decision and next steps"
-            body="If your business case is approved you will receive a unique Lifecycle ID. Depending on allocation, funding may or may not be immediately approved."
+            body="If your business case is approved you will receive a unique Lifecycle ID. If it is not approved, you would need address the concerns to proceed."
           />
         </div>
         <div className="margin-top-6 margin-bottom-7">
@@ -145,12 +143,9 @@ const GovernanceOverview = () => {
             <div>
               These steps make sure
               <ul className="margin-bottom-0 margin-top-1 padding-left-205 line-height-body-5">
-                <li>CMS has enough budget to allocate for your project</li>
-                <li>They can assist and help you consider various solutions</li>
-                <li>
-                  They can help your team not re-build a solution that already
-                  exists at CMS
-                </li>
+                <li>your request fits into current CMS IT strategy</li>
+                <li>to avoid duplicate solutions that already exists at CMS</li>
+                <li>you have considered various solutions</li>
                 <li>CMS meets various policies and remains compliant</li>
               </ul>
             </div>
