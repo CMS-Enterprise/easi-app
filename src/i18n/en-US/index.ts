56f418f26a8a77bee7a910c689b64cae72575089
Christopher Hui <christopher@truss.works>
2020-06-11 16:59:16 -0700

    add initial implementation with i18n

---
+++ src/i18n/en-US/index.ts
@@ -0,0 +1,11 @@
+import general from './general';
+import systemIntake from './systemIntake';
+import businessCase from './businessCase';
+
+const enUS = {
+  general,
+  systemIntake,
+  businessCase
+};
+
+export default enUS;
