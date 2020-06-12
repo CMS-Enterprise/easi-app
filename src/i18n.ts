f940114d3c80b801e9a5f4a232845da54dd651f5
Christopher Hui <christopher@truss.works>
2020-06-12 10:44:37 -0700

    remove unnecessary comments

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,17 +2,13 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import enUS from 'i18n/en-US';
 
-// the translations
-// (tip move them in a JSON file and import them)
 const resources = {
   'en-US': enUS
 };
 
-i18n
-  .use(initReactI18next) // passes i18n down to react-i18next
-  .init({
-    resources,
-    fallbackLng: 'en-US'
-  });
+i18n.use(initReactI18next).init({
+  resources,
+  fallbackLng: 'en-US'
+});
 
 export default i18n;
