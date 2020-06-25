1e2f4df2c68e0aad3a7cf2bb57bced035b3fe3bd
Isaac Garfinkle <isaac@truss.works>
2020-06-25 14:22:37 -0700

    Make gray background for Next Steps section.

--- src/views/GRTSystemIntakeReview/index.tsx
+++ src/views/GRTSystemIntakeReview/index.tsx
@@ -29,18 +29,13 @@ export const GRTSystemIntakeReview = () => {
             </h2>
           )}
           {systemIntake && <SystemIntakeReview systemIntake={systemIntake} />}
-          <hr className="system-intake__hr" />
-          <h2 className="font-heading-xl">What to do after reviewing?</h2>
-          <p>Email the requester and:</p>
-          <ul className="usa-list">
-            <li>Ask them to fill in the business case and submit it</li>
-            <li>
-              Tell them what to expect after they submit the business case
-            </li>
-            <li>And how to get in touch if they have any questions.</li>
-          </ul>
         </div>
       </MainContent>
+      <div className="bg-gray-5">
+        <MainContent className="grid-container">
+          <h1 className="font-heading-xl">Next steps</h1>
+        </MainContent>
+      </div>
     </div>
   );
 };
