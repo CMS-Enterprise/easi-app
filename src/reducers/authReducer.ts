5943437c8a70a4e7d44d5ec8d55deb46cc03f1a9
Christopher Hui <christopher@truss.works>
2020-06-30 15:38:04 -0700

    readd session renewal timer

--- src/reducers/authReducer.ts
+++ src/reducers/authReducer.ts
@@ -3,17 +3,24 @@ import { Action } from 'redux-actions';
 const newTimeStamp = () => Date.now();
 
 type authReducerState = {
-  lastActiveAt: number | null;
+  lastActiveAt: number;
+  lastSessionRenew: number | null;
 };
 
 const UPDATE_LAST_ACTIVE_AT = 'AUTH_REDUCER_UPDATE_LAST_ACTIVE_AT';
+const UPDATE_LAST_SESSION_RENEW = 'AUTH_REDUCER_UPDATE_LAST_SESSION_RENEW';
 
 export const updateLastActiveAt = {
   type: UPDATE_LAST_ACTIVE_AT
 };
 
+export const updateLastSessionRenew = {
+  type: UPDATE_LAST_SESSION_RENEW
+};
+
 const initialState: authReducerState = {
-  lastActiveAt: null
+  lastActiveAt: newTimeStamp(),
+  lastSessionRenew: null
 };
 
 function authReducer(
@@ -25,6 +32,11 @@ function authReducer(
       return {
         ...state,
         lastActiveAt: newTimeStamp()
+      };
+    case UPDATE_LAST_SESSION_RENEW:
+      return {
+        ...state,
+        lastSessionRenew: newTimeStamp()
       };
     default:
       return state;
--- src/views/TimeOutWrapper/index.tsx
+++ src/views/TimeOutWrapper/index.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useOktaAuth } from '@okta/okta-react';
 import { DateTime } from 'luxon';
-import { updateLastActiveAt } from 'reducers/authReducer';
+import { updateLastSessionRenew } from 'reducers/authReducer';
 import { AppState } from 'reducers/rootReducer';
 
 type TimeOutWrapperProps = {
@@ -11,10 +11,7 @@ type TimeOutWrapperProps = {
 
 const sessionTimeout = { minutes: 14 };
 
-const registerExpire = async (
-  authService: any,
-  lastActiveAt: number | null
-) => {
+const registerExpire = async (authService: any, lastActiveAt: number) => {
   const tokenManager = await authService.getTokenManager();
 
   // clear the old listener so we don't register millions of them
@@ -25,7 +22,6 @@ const registerExpire = async (
       .minus(sessionTimeout)
       .toMillis();
     if (
-      lastActiveAt &&
       lastActiveAt > activeSessionWindow &&
       ['idToken', 'accessToken'].includes(key)
     ) {
@@ -46,25 +42,31 @@ const TimeOutWrapper = ({ children }: TimeOutWrapperProps) => {
     (state: AppState) => state.auth.lastActiveAt
   );
 
+  const lastSessionRenew = useSelector(
+    (state: AppState) => state.auth.lastSessionRenew
+  );
+
   // Getting the session renews it
   const renewSession = async () => {
     // eslint-disable-next-line no-underscore-dangle
     await authService._oktaAuth.session.get();
-    dispatch(updateLastActiveAt);
+    dispatch(updateLastSessionRenew);
   };
 
   useEffect(() => {
     const currentTime = Date.now();
     const renewThreshold = 30 * 1000;
-    if (lastActiveAt && currentTime > lastActiveAt + renewThreshold) {
+    if (lastSessionRenew && currentTime > lastSessionRenew + renewThreshold) {
       console.log('Renew Session');
       renewSession();
     }
   });
 
   useEffect(() => {
-    console.log('Storing session');
-    if (authState.isAuthenticated) renewSession();
+    if (authState.isAuthenticated) {
+      console.log('Storing session');
+      renewSession();
+    }
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [authState.isAuthenticated]);
