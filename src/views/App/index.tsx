80f5df03e2c235b9f2b29bca4be14e33ba734dec
Christopher Hui <christopher@truss.works>
2020-06-30 16:28:12 -0700

    try again

--- src/views/App/index.tsx
+++ src/views/App/index.tsx
@@ -29,7 +29,7 @@ class App extends React.Component<MainProps, MainState> {
   };
 
   render() {
-    console.log('App Env', process.env.REACT_APP_APP_ENV);
+    console.log('App Env ', process.env.REACT_APP_APP_ENV);
     return (
       <div>
         <div className="usa-overlay" />
