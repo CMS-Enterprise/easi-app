ed753a60e57012f647a5f0f02ae28b0d1a220dad
Kara Louie <kara@truss.works>
2020-07-06 10:17:28 -0700

    Add business case to task list in FE

---
+++ src/data/taskList.ts
@@ -0,0 +1,72 @@
+import { SystemIntakeForm } from 'types/systemIntake';
+import { BusinessCaseModel } from 'types/businessCase';
+import { businessCaseInitialData } from 'data/businessCase';
+
+export const calculateIntakeStatus = (intake: SystemIntakeForm) => {
+  if (intake.id === '') {
+    return 'START';
+  }
+  if (intake.status === 'DRAFT') {
+    return 'CONTINUE';
+  }
+  return 'COMPLETED';
+};
+
+export const chooseIntakeLink = (intake: SystemIntakeForm, status: string) => {
+  const newIntakeLink = '/system/new';
+  if (intake.id === '') {
+    return newIntakeLink;
+  }
+  let link: string;
+  switch (status) {
+    case 'CONTINUE':
+      link = `/system/${intake.id}/contact-details`;
+      break;
+    case 'COMPLETED':
+      // This will need to be changed once we have an intake review page
+      link = '/';
+      break;
+    default:
+      link = newIntakeLink;
+  }
+  return link;
+};
+
+export const calculateIntakeFeedbackStatus = (intakeStatus: string) => {
+  switch (intakeStatus) {
+    case 'SUBMITTED':
+      return 'SUBMITTED';
+    case 'ACCEPTED':
+    case 'APPROVED':
+    case 'CLOSED':
+      return 'COMPLETED';
+    default:
+      return 'CANNOT_START';
+  }
+};
+
+export const calculateBusinessCaseStatus = (
+  intakeStatus: string,
+  businessCase: BusinessCaseModel
+  // eslint-disable-next-line consistent-return
+) => {
+  if (businessCase === businessCaseInitialData) {
+    switch (intakeStatus) {
+      case 'ACCEPTED':
+        return 'START';
+      case 'APPROVED':
+      case 'CLOSED':
+        return 'NOT_NEEDED';
+      default:
+        return 'CANNOT_START';
+    }
+  }
+  switch (businessCase.status) {
+    case 'DRAFT':
+      return 'CONTINUE';
+    case 'SUBMITTED':
+      return 'COMPLETED';
+    default:
+      return 'CANNOT_START';
+  }
+};
