0c146fe69cbe0470cdb602c40cbfb4e940e60564
Christopher Hui <christopher@truss.works>
2020-07-14 10:07:31 -0700

    move text to i18n and add tests

---
+++ src/views/BusinessCase/Confirmation/index.test.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+import Confirmation from './index';
+
+describe('The Business Case Confirmation page', () => {
+  it('renders without crashing', () => {
+    shallow(
+      <MemoryRouter>
+        <Confirmation />
+      </MemoryRouter>
+    );
+  });
+
+  it('matches the snapshot', () => {
+    const tree = renderer
+      .create(
+        <MemoryRouter>
+          <Confirmation />
+        </MemoryRouter>
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
--- src/views/BusinessCase/Confirmation/index.tsx
+++ src/views/BusinessCase/Confirmation/index.tsx
@@ -1,32 +1,38 @@
 import React from 'react';
 import { Link, useParams } from 'react-router-dom';
+import { Trans, useTranslation } from 'react-i18next';
 
 const Confirmation = () => {
   const { businessCaseId } = useParams();
+  const { t } = useTranslation();
   return (
     <div className="margin-bottom-7">
       <div className="grid-container">
         <h1 className="font-heading-xl margin-top-4">
-          Your Business Case has been submitted
+          {t('businessCase:confirmation.heading')}
         </h1>
         <h2 className="margin-bottom-8 text-normal">
-          Your reference ID is: {businessCaseId}
+          {t('businessCase:confirmation.subheading', {
+            referenceId: businessCaseId
+          })}
         </h2>
         <p className="margin-y-8">
-          Help us improve EASi.{' '}
-          <a
-            href="https://www.surveymonkey.com/r/JNYSMZP"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="Open EASi survey in a new tab"
-          >
-            Tell us what you think of this service (opens in a new tab)
-          </a>
+          <Trans i18nKey="businessCase:confirmation.improveEasi">
+            helpUsImproveEasi
+            <a
+              href="https://www.surveymonkey.com/r/JNYSMZP"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Open EASi survey in a new tab"
+            >
+              tellUsWhatYouThink
+            </a>
+          </Trans>
         </p>
         <div>
           <Link to="/">
             <i className="fa fa-angle-left margin-x-05" aria-hidden />
-            Go back to EASi homepage
+            {t('businessCase:confirmation.homeCta')}
           </Link>
         </div>
       </div>
