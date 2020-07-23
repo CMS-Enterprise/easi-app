aa0e4dd387def5662e23529196b4f7cccd576076
Isaac Garfinkle <isaac@truss.works>
2020-07-23 16:18:49 -0700

    use i18n pattern

--- src/views/GRTSystemIntakeReview/index.tsx
+++ src/views/GRTSystemIntakeReview/index.tsx
@@ -16,10 +16,12 @@ import Button from 'components/shared/Button';
 import { SystemIntakeForm, SystemIntakeStatus } from 'types/systemIntake';
 import Alert from 'components/shared/Alert';
 import { DateTime } from 'luxon';
+import { useTranslation } from 'react-i18next';
 
 export const GRTSystemIntakeReview = () => {
   const { systemId } = useParams();
   const dispatch = useDispatch();
+  const { t } = useTranslation();
 
   useEffect(() => {
     dispatch(fetchSystemIntake(systemId));
@@ -37,10 +39,14 @@ export const GRTSystemIntakeReview = () => {
       <MainContent>
         <div className="grid-container">
           <div className="system-intake__review margin-bottom-7">
-            <h1 className="font-heading-xl margin-top-4">CMS System Request</h1>
+            <h1 className="font-heading-xl margin-top-4">
+              {t('grtReview:review_form:page_title')}
+            </h1>
             {error && (
               <h2 className="font-heading-xl">
-                System intake with ID: {systemId} was not found
+                {t('grtReview:review_form:intake_not_found', {
+                  intakeId: systemId
+                })}
               </h2>
             )}
             {!isLoading && !error && (
@@ -63,21 +69,23 @@ export const GRTSystemIntakeReview = () => {
                     return (
                       <Form>
                         <h1 className="font-heading-xl margin-top-0">
-                          Next steps
+                          {t('grtReview:review_form:next_steps')}
                         </h1>
                         <div className="grid-row flex-justify">
                           <div className="grid-col-4">
                             <FieldGroup>
                               <fieldset className="usa-fieldset">
                                 <legend className="usa-label margin-bottom-2">
-                                  How to proceed?
+                                  {t('grtReview:review_form:how_to_proceed')}
                                 </legend>
                                 <Field
                                   as={RadioField}
                                   checked={values.status === 'APPROVED'}
                                   id="GrtIntakeReviewForm-Approved"
                                   name="decision"
-                                  label="Issue Lifecycle ID with no further governance"
+                                  label={t(
+                                    'grtReview:review_form:approved_label'
+                                  )}
                                   onChange={() => {
                                     setFieldValue('status', 'APPROVED');
                                   }}
@@ -89,7 +97,9 @@ export const GRTSystemIntakeReview = () => {
                                   checked={values.status === 'ACCEPTED'}
                                   id="GrtIntakeReviewForm-Accepted"
                                   name="decision"
-                                  label="Governance review process needed"
+                                  label={t(
+                                    'grtReview:review_form:accepted_label'
+                                  )}
                                   onChange={() => {
                                     setFieldValue('status', 'ACCEPTED');
                                   }}
@@ -101,7 +111,9 @@ export const GRTSystemIntakeReview = () => {
                                   checked={values.status === 'CLOSED'}
                                   id="GrtIntakeReviewForm-Closed"
                                   name="decision"
-                                  label="Governance not needed (close this request)"
+                                  label={t(
+                                    'grtReview:review_form:closed_label'
+                                  )}
                                   onChange={() => {
                                     setFieldValue('status', 'CLOSED');
                                   }}
@@ -111,8 +123,7 @@ export const GRTSystemIntakeReview = () => {
                             </FieldGroup>
                             <hr className="border-black border-bottom-0" />
                             <HelpText>
-                              If there isn&apos;t enough info on this request,
-                              please get in touch with the requester over email
+                              {t('grtReview:review_form:radio_help')}
                             </HelpText>
                           </div>
                           <div className="grid-col-6">
@@ -121,7 +132,7 @@ export const GRTSystemIntakeReview = () => {
                                 className="margin-bottom-2 margin-top-0"
                                 htmlFor="GrtIntakeReviewForm-EmailText"
                               >
-                                This email will be sent to the requester
+                                {t('grtReview:review_form:email_field_label')}
                               </Label>
                               <Field
                                 as={TextAreaField}
@@ -133,7 +144,7 @@ export const GRTSystemIntakeReview = () => {
                               />
                             </FieldGroup>
                             <Button className="margin-top-3" type="submit">
-                              Email Decision and Progress to next step
+                              {t('grtReview:review_form:submit_button')}
                             </Button>
                           </div>
                         </div>
@@ -146,21 +157,28 @@ export const GRTSystemIntakeReview = () => {
                 systemIntake.status
               ) && (
                 <div>
-                  <h1 className="font-heading-xl margin-top-0">Next steps</h1>
-                  <Alert type="success" heading="Email sent">
-                    An email has been sent to {systemIntake.requester.email}
+                  <h1 className="font-heading-xl margin-top-0">
+                    {t('grtReview:review_form:next_steps')}
+                  </h1>
+                  <Alert
+                    type="success"
+                    heading={t('grtReview:review_form:alert_header')}
+                  >
+                    {t('grtReview:review_form:alert_body', {
+                      address: systemIntake.requester.email
+                    })}
                   </Alert>
                   <p className="margin-bottom-3">
                     Decision:{' '}
                     {((status: SystemIntakeStatus) => {
                       if (status === 'APPROVED') {
-                        return 'Issue Lifecycle ID with no further governance';
+                        return t('grtReview:review_form:approved_label');
                       }
                       if (status === 'ACCEPTED') {
-                        return 'Governance review process needed';
+                        return t('grtReview:review_form:accepted_label');
                       }
                       if (status === 'CLOSED') {
-                        return 'Governance not needed (close this request)';
+                        return t('grtReview:review_form:closed_label');
                       }
                       return '';
                     })(systemIntake.status)}
@@ -170,11 +188,11 @@ export const GRTSystemIntakeReview = () => {
                   </p>
                   {systemIntake.decidedAt && (
                     <i>
-                      An email was sent to the requester on{' '}
-                      {systemIntake.decidedAt &&
-                        systemIntake.decidedAt.toLocaleString(
+                      {t('grtReview:review_form:closed_label', {
+                        date: systemIntake.decidedAt.toLocaleString(
                           DateTime.DATETIME_FULL
-                        )}
+                        )
+                      })}
                     </i>
                   )}
                 </div>
