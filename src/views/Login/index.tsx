71f84bb09634f586328d05527fc5020ed1cca74e
Sara Bocciardi <sara@truss.works>
2020-07-23 12:17:05 -0700

    Set renew time when we log in.

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { useOktaAuth } from '@okta/okta-react';
 
 import Header from 'components/Header';
 import MainContent from 'components/MainContent';
 import OktaSignInWidget from 'components/shared/OktaSignInWidget';
+import { updateLastRenewAt } from 'reducers/authReducer';
 
 const Login = () => {
   const { authService }: { authService: any } = useOktaAuth();
+  const dispatch = useDispatch();
 
   return (
     <div>
@@ -15,8 +18,9 @@ const Login = () => {
         <OktaSignInWidget
           onSuccess={response => {
             authService.getTokenManager().then((tokenManager: any) => {
-              tokenManager.add('idToken', response);
-              tokenManager.add('accessToken', response);
+              tokenManager.add('idToken', response.token.idToken);
+              tokenManager.add('accessToken', response.token.accessToken);
+              dispatch(updateLastRenewAt);
             });
           }}
           onError={() => {
