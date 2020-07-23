0296d05f65cec8ca86e74b59995257676285415e
Sara <sjbocciardi@gmail.com>
2020-07-23 11:19:15 -0700

    Update auth.ts
    Update text based on usability research

--- src/i18n/en-US/auth.ts
+++ src/i18n/en-US/auth.ts
@@ -6,13 +6,13 @@ const auth = {
     title_second_plural: 'Your access to EASi will expire in {{count}} seconds',
     dataSaved: 'Your data has been saved.',
     inactivityWarning_minute:
-      'If you do not do anything on this page, you will be signed out in {{count}} minute and will need to sign back in. We do this to keep your information secure.',
+      'If you do not click the big button below, you will be signed out in {{count}} minute and will need to sign back in. We do this to keep your information secure.',
     inactivityWarning_minute_plural:
-      'If you do not do anything on this page, you will be signed out in {{count}} minutes and will need to sign back in. We do this to keep your information secure.',
+      'If you do not click the big button below, you will be signed out in {{count}} minutes and will need to sign back in. We do this to keep your information secure.',
     inactivityWarning_second:
-      'If you do not do anything on this page, you will be signed out in {{count}} second and will need to sign back in. We do this to keep your information secure.',
+      'If you do not click the big button below, you will be signed out in {{count}} second and will need to sign back in. We do this to keep your information secure.',
     inactivityWarning_second_plural:
-      'If you do not do anything on this page, you will be signed out in {{count}} seconds and will need to sign back in. We do this to keep your information secure.',
+      'If you do not click the big button below, you will be signed out in {{count}} seconds and will need to sign back in. We do this to keep your information secure.',
     cta: 'Return to EASi'
   }
 };
