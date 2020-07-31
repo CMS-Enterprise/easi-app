d0ba1870a27c4e29f5419a8c9cfd7dd80cca67a0
Isaac Garfinkle <isaac@truss.works>
2020-07-31 14:20:18 -0700

    Add withdraw request modal

---
+++ src/i18n/en-US/taskList.ts
@@ -0,0 +1,11 @@
+const taskList = {
+  withdraw_modal: {
+    title: 'EASi',
+    header: 'Are you sure you want to remove your request?',
+    warning: 'You will lose any information you have filled in.',
+    confirm: 'Remove request',
+    cancel: 'Cancel'
+  }
+};
+
+export default taskList;
