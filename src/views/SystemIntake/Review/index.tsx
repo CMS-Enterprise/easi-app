73a0906efa08155942872c0d898a7b49eb3ce8b3
Sara Bocciardi <sara@truss.works>
2020-08-03 15:45:51 -0700

    The review page doesn't need to be a form

--- src/views/SystemIntake/Review/index.tsx
+++ src/views/SystemIntake/Review/index.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import { Formik, FormikProps, Form } from 'formik';
 import { SystemIntakeForm } from 'types/systemIntake';
 import { SystemIntakeReview } from 'components/SystemIntakeReview';
 import { Button } from '@trussworks/react-uswds';
@@ -10,11 +9,10 @@ import { submitSystemIntake } from 'types/routines';
 import usePrevious from 'hooks/usePrevious';
 
 type ReviewProps = {
-  formikRef: any;
   systemIntake: SystemIntakeForm;
 };
 
-const Review = ({ formikRef, systemIntake }: ReviewProps) => {
+const Review = ({ systemIntake }: ReviewProps) => {
   const history = useHistory();
   const dispatch = useDispatch();
 
@@ -33,56 +31,41 @@ const Review = ({ formikRef, systemIntake }: ReviewProps) => {
   }, [isSubmitting]);
 
   return (
-    <Formik
-      initialValues={systemIntake}
-      onSubmit={values => {
-        dispatch(submitSystemIntake(values));
-      }}
-      validateOnBlur={false}
-      validateOnChange={false}
-      validateOnMount={false}
-      innerRef={formikRef}
-    >
-      {(formikProps: FormikProps<SystemIntakeForm>) => {
-        const { values } = formikProps;
-        return (
-          <div className="system-intake__review margin-bottom-7">
-            <h1 className="font-heading-xl margin-top-4">
-              Check your answers before sending
-            </h1>
-            <Form>
-              <SystemIntakeReview systemIntake={values} />
-              <hr className="system-intake__hr" />
-              <h2 className="font-heading-xl">What happens next?</h2>
-              <p>
-                The Governance Review Admin Team will review and get back to you
-                with <strong>one of these</strong> outcomes:
-              </p>
-              <ul className="usa-list">
-                <li>direct you to go through the Governance Review process</li>
-                <li>or decide there is no further governance needed</li>
-              </ul>
-              <p>They will get back to you in two business days.</p>
-              <Button
-                type="button"
-                outline
-                onClick={() => {
-                  formikProps.setErrors({});
-                  const newUrl = 'request-details';
-                  history.push(newUrl);
-                  window.scrollTo(0, 0);
-                }}
-              >
-                Back
-              </Button>
-              <Button type="submit" disabled={isSubmitting}>
-                Send my intake request
-              </Button>
-            </Form>
-          </div>
-        );
-      }}
-    </Formik>
+    <div className="system-intake__review margin-bottom-7">
+      <h1 className="font-heading-xl margin-top-4">
+        Check your answers before sending
+      </h1>
+      <SystemIntakeReview systemIntake={systemIntake} />
+      <hr className="system-intake__hr" />
+      <h2 className="font-heading-xl">What happens next?</h2>
+      <p>
+        The Governance Review Admin Team will review and get back to you with{' '}
+        <strong>one of these</strong> outcomes:
+      </p>
+      <ul className="usa-list">
+        <li>direct you to go through the Governance Review process</li>
+        <li>or decide there is no further governance needed</li>
+      </ul>
+      <p>They will get back to you in two business days.</p>
+      <Button
+        type="button"
+        outline
+        onClick={() => {
+          const newUrl = 'request-details';
+          history.push(newUrl);
+          window.scrollTo(0, 0);
+        }}
+      >
+        Back
+      </Button>
+      <Button
+        type="submit"
+        disabled={isSubmitting}
+        onClick={() => dispatch(submitSystemIntake(systemIntake))}
+      >
+        Send my intake request
+      </Button>
+    </div>
   );
 };
 
