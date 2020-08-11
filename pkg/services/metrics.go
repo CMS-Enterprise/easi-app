0409b602ce1159d08b3698aff95bb64f9ff76f32
Nelz Carpentier <nelz@truss.works>
2020-08-11 14:24:11 -0700

    prefer context logger

--- pkg/services/metrics.go
+++ pkg/services/metrics.go
@@ -17,13 +17,9 @@ func NewFetchMetrics(
 	fetchSystemIntakeMetrics func(time.Time, time.Time) (models.SystemIntakeMetrics, error),
 ) func(ctx context.Context, startTime time.Time, endTime time.Time) (models.MetricsDigest, error) {
 	return func(ctx context.Context, startTime time.Time, endTime time.Time) (models.MetricsDigest, error) {
-		localLogger, ok := appcontext.Logger(ctx)
-		if !ok {
-			localLogger = config.logger
-		}
 		systemIntakeMetrics, err := fetchSystemIntakeMetrics(startTime, endTime)
 		if err != nil {
-			localLogger.Error("failed to query system intake metrics", zap.Error(err))
+			appcontext.ZLogger(ctx).Error("failed to query system intake metrics", zap.Error(err))
 			return models.MetricsDigest{}, &apperrors.QueryError{
 				Err:       err,
 				Model:     models.SystemIntakeMetrics{},
