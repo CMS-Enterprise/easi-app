b1c50c946f4f66b6dfea932fe1e022576ef1ed54
Sara Bocciardi <sara@truss.works>
2020-08-12 12:58:35 -0700

    Fix unit tests.

--- src/views/Login/index.test.tsx
+++ src/views/Login/index.test.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import OktaSignInWidget from 'components/shared/OktaSignInWidget';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
 import Login from './index';
 
 jest.mock('@okta/okta-react', () => ({
@@ -12,12 +14,22 @@ jest.mock('@okta/okta-react', () => ({
 }));
 
 describe('The Login page', () => {
+  const mockStore = configureMockStore();
+  const store = mockStore();
+
+  const renderLogin = () =>
+    shallow(
+      <Provider store={store}>
+        <Login />
+      </Provider>
+    );
+
   it('renders without crashing', () => {
-    shallow(<Login />);
+    expect(renderLogin).not.toThrow();
   });
 
   it('renders the OktaSignInWidget', () => {
-    const wrapper = shallow(<Login />);
+    const wrapper = renderLogin();
     expect(wrapper.find(OktaSignInWidget));
   });
 });
