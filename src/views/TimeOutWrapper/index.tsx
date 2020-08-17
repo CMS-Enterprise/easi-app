434c35326d2b2bfc71f26f86c40e38fb22560560
Sara Bocciardi <sara@truss.works>
2020-08-17 16:59:26 -0700

    Force logout when a user's session expires. Only register listener when a user is logged in.

--- src/views/TimeOutWrapper/index.tsx
+++ src/views/TimeOutWrapper/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useOktaAuth } from '@okta/okta-react';
 import { DateTime } from 'luxon';
@@ -11,27 +11,34 @@ type TimeOutWrapperProps = {
 
 const sessionTimeout = { minutes: 14 };
 
-const registerExpire = async (authService: any, lastActiveAt: number) => {
-  const tokenManager = await authService.getTokenManager();
-  // clear the old listener so we don't register millions of them
-  tokenManager.off('expired');
-  tokenManager.on('expired', (key: any) => {
-    const activeSessionWindow = DateTime.local()
-      .minus(sessionTimeout)
-      .toMillis();
-    if (lastActiveAt > activeSessionWindow) {
-      tokenManager.renew(key);
-    }
-  });
-};
-
 const TimeOutWrapper = ({ children }: TimeOutWrapperProps) => {
   const lastActiveAt = useSelector(
     (state: AppState) => state.auth.lastActiveAt
   );
+  const { authState, authService } = useOktaAuth();
 
-  const { authService }: { authService: any } = useOktaAuth();
-  registerExpire(authService, lastActiveAt);
+  const registerExpire = async () => {
+    const tokenManager = await authService.getTokenManager();
+    // clear the old listener so we don't register millions of them
+    tokenManager.off('expired');
+    tokenManager.on('expired', (key: any) => {
+      const activeSessionWindow = DateTime.local()
+        .minus(sessionTimeout)
+        .toMillis();
+      if (lastActiveAt > activeSessionWindow) {
+        tokenManager.renew(key);
+      } else {
+        authService.logout('/login');
+      }
+    });
+  };
+
+  useEffect(() => {
+    if (authState.isAuthenticated) {
+      registerExpire();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [authState.isAuthenticated, lastActiveAt]);
 
   return <>{children}</>;
 };
