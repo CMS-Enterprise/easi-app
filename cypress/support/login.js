c517f94d5d2debf17506e8dbe53108fd117e3e25
Christopher Hui <christopher@truss.works>
2020-08-21 12:58:12 -0700

    add log false to OTP generation
    Co-authored-by: Josh Clarridge <2491291+jsclarridge@users.noreply.github.com>

--- cypress/support/login.js
+++ cypress/support/login.js
@@ -18,7 +18,7 @@ Cypress.Commands.add('login', () => {
   cy.get('body').then($body => {
     if ($body.find('input[name="answer"]').length) {
       cy.get('input[name="answer"]').then(() => {
-        cy.task('generateOTP', Cypress.env('otpSecret')).then(token => {
+        cy.task('generateOTP', Cypress.env('otpSecret'), { log: false }).then(token => {
           cy.get('input[name="answer"]').type(token, { log: false });
           cy.get('input[name="rememberDevice"]').check({ force: true });
           cy.get('input[value="Verify"').click();
