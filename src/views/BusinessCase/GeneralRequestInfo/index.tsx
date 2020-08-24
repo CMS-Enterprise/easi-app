b83c4edd7f0c6e5198f6c248bbbc26bbc6ac3230
Christopher Hui <christopher@truss.works>
2020-08-24 13:27:44 -0700

    wip: refactor general request info

--- src/views/BusinessCase/GeneralRequestInfo/index.tsx
+++ src/views/BusinessCase/GeneralRequestInfo/index.tsx
@@ -1,102 +1,172 @@
 import React from 'react';
-import { Field, FormikProps } from 'formik';
+import { useHistory } from 'react-router-dom';
+import { Button } from '@trussworks/react-uswds';
+import { Field, Form, Formik, FormikProps } from 'formik';
 
 import MandatoryFieldsAlert from 'components/MandatoryFieldsAlert';
+import PageNumber from 'components/PageNumber';
+import { ErrorAlert, ErrorAlertMessage } from 'components/shared/ErrorAlert';
 import FieldErrorMsg from 'components/shared/FieldErrorMsg';
 import FieldGroup from 'components/shared/FieldGroup';
 import Label from 'components/shared/Label';
 import TextField from 'components/shared/TextField';
-import { BusinessCaseModel } from 'types/businessCase';
+import { BusinessCaseModel, GeneralRequestInfoForm } from 'types/businessCase';
 import flattenErrors from 'utils/flattenErrors';
+import BusinessCaseValidationSchema from 'validations/businessCaseSchema';
 
 type GeneralRequestInfoProps = {
-  formikProps: FormikProps<BusinessCaseModel>;
+  businessCase: BusinessCaseModel;
+  formikRef: any;
+  dispatchSave: () => void;
 };
-const GeneralRequestInfo = ({ formikProps }: GeneralRequestInfoProps) => {
-  const { errors } = formikProps;
-  const flatErrors = flattenErrors(errors);
+const GeneralRequestInfo = ({
+  formikRef,
+  businessCase,
+  dispatchSave
+}: GeneralRequestInfoProps) => {
+  const history = useHistory();
+  const initialValues: GeneralRequestInfoForm = {
+    requestName: businessCase.requestName,
+    requester: businessCase.requester,
+    businessOwner: businessCase.businessOwner
+  };
   const allowedPhoneNumberCharacters = /[\d- ]+/g;
 
   return (
-    <div className="grid-container">
-      <h1 className="font-heading-xl">General request information</h1>
-      <p className="line-height-body-6">
-        Make a first draft of the various solutions you’ve thought of and the
-        costs involved to build or buy them. Once you have a draft business case
-        ready for review, send it to the Governance Review Admin Team who will
-        ensure it is ready to be presented at the Governance Review Team (GRT)
-        Meeting.
-      </p>
-      <div className="tablet:grid-col-5">
-        <MandatoryFieldsAlert />
-      </div>
-      <div className="tablet:grid-col-9 margin-bottom-7">
-        <FieldGroup
-          scrollElement="requestName"
-          error={!!flatErrors.requestName}
-        >
-          <Label htmlFor="BusinessCase-RequestName">Request Name</Label>
-          <FieldErrorMsg>{flatErrors.requestName}</FieldErrorMsg>
-          <Field
-            as={TextField}
-            error={!!flatErrors.requestName}
-            id="BusinessCase-RequestName"
-            maxLength={50}
-            name="requestName"
-          />
-        </FieldGroup>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={dispatchSave}
+      validationSchema={BusinessCaseValidationSchema.generalRequestInfo}
+      validateOnBlur={false}
+      validateOnChange={false}
+      validateOnMount={false}
+      innerRef={formikRef}
+    >
+      {(formikProps: FormikProps<GeneralRequestInfoForm>) => {
+        const { errors, validateForm } = formikProps;
+        const flatErrors = flattenErrors(errors);
+        return (
+          <div className="grid-container">
+            {Object.keys(errors).length > 0 && (
+              <ErrorAlert
+                classNames="margin-top-3"
+                heading="Please check and fix the following"
+              >
+                {Object.keys(flatErrors).map(key => {
+                  return (
+                    <ErrorAlertMessage
+                      key={`Error.${key}`}
+                      errorKey={key}
+                      message={flatErrors[key]}
+                    />
+                  );
+                })}
+              </ErrorAlert>
+            )}
+            <h1 className="font-heading-xl">General request information</h1>
+            <p className="line-height-body-6">
+              Make a first draft of the various solutions you’ve thought of and
+              the costs involved to build or buy them. Once you have a draft
+              business case ready for review, send it to the Governance Review
+              Admin Team who will ensure it is ready to be presented at the
+              Governance Review Team (GRT) Meeting.
+            </p>
+            <div className="tablet:grid-col-5">
+              <MandatoryFieldsAlert />
+            </div>
+            <div className="tablet:grid-col-9 margin-bottom-7">
+              <Form>
+                <FieldGroup
+                  scrollElement="requestName"
+                  error={!!flatErrors.requestName}
+                >
+                  <Label htmlFor="BusinessCase-RequestName">Request Name</Label>
+                  <FieldErrorMsg>{flatErrors.requestName}</FieldErrorMsg>
+                  <Field
+                    as={TextField}
+                    error={!!flatErrors.requestName}
+                    id="BusinessCase-RequestName"
+                    maxLength={50}
+                    name="requestName"
+                  />
+                </FieldGroup>
 
-        <FieldGroup
-          scrollElement="requester.name"
-          error={!!flatErrors['requester.name']}
-        >
-          <Label htmlFor="BusinessCase-RequesterName">Requester</Label>
-          <FieldErrorMsg>{flatErrors['requester.name']}</FieldErrorMsg>
-          <Field
-            as={TextField}
-            error={!!flatErrors['requester.name']}
-            id="BusinessCase-RequesterName"
-            maxLength={50}
-            name="requester.name"
-          />
-        </FieldGroup>
+                <FieldGroup
+                  scrollElement="requester.name"
+                  error={!!flatErrors['requester.name']}
+                >
+                  <Label htmlFor="BusinessCase-RequesterName">Requester</Label>
+                  <FieldErrorMsg>{flatErrors['requester.name']}</FieldErrorMsg>
+                  <Field
+                    as={TextField}
+                    error={!!flatErrors['requester.name']}
+                    id="BusinessCase-RequesterName"
+                    maxLength={50}
+                    name="requester.name"
+                  />
+                </FieldGroup>
 
-        <FieldGroup
-          scrollElement="businessOwner.name"
-          error={!!flatErrors['businessOwner.name']}
-        >
-          <Label htmlFor="BusinessCase-BusinessOwnerName">Business Owner</Label>
-          <FieldErrorMsg>{flatErrors['businessOwner.name']}</FieldErrorMsg>
-          <Field
-            as={TextField}
-            error={!!flatErrors['businessOwner.name']}
-            id="BusinessCase-BusinessOwnerName"
-            maxLength={50}
-            name="businessOwner.name"
-          />
-        </FieldGroup>
+                <FieldGroup
+                  scrollElement="businessOwner.name"
+                  error={!!flatErrors['businessOwner.name']}
+                >
+                  <Label htmlFor="BusinessCase-BusinessOwnerName">
+                    Business Owner
+                  </Label>
+                  <FieldErrorMsg>
+                    {flatErrors['businessOwner.name']}
+                  </FieldErrorMsg>
+                  <Field
+                    as={TextField}
+                    error={!!flatErrors['businessOwner.name']}
+                    id="BusinessCase-BusinessOwnerName"
+                    maxLength={50}
+                    name="businessOwner.name"
+                  />
+                </FieldGroup>
 
-        <FieldGroup
-          scrollElement="requester.phoneNumber"
-          error={!!flatErrors['requester.phoneNumber']}
-        >
-          <Label htmlFor="BusinessCase-RequesterPhoneNumber">
-            Requester Phone Number
-          </Label>
-          <FieldErrorMsg>{flatErrors['requester.phoneNumber']}</FieldErrorMsg>
-          <div className="width-card-lg">
-            <Field
-              as={TextField}
-              error={!!flatErrors['requester.phoneNumber']}
-              id="BusinessCase-RequesterPhoneNumber"
-              maxLength={20}
-              name="requester.phoneNumber"
-              match={allowedPhoneNumberCharacters}
-            />
+                <FieldGroup
+                  scrollElement="requester.phoneNumber"
+                  error={!!flatErrors['requester.phoneNumber']}
+                >
+                  <Label htmlFor="BusinessCase-RequesterPhoneNumber">
+                    Requester Phone Number
+                  </Label>
+                  <FieldErrorMsg>
+                    {flatErrors['requester.phoneNumber']}
+                  </FieldErrorMsg>
+                  <div className="width-card-lg">
+                    <Field
+                      as={TextField}
+                      error={!!flatErrors['requester.phoneNumber']}
+                      id="BusinessCase-RequesterPhoneNumber"
+                      maxLength={20}
+                      name="requester.phoneNumber"
+                      match={allowedPhoneNumberCharacters}
+                    />
+                  </div>
+                </FieldGroup>
+              </Form>
+            </div>
+            <Button
+              type="button"
+              onClick={() => {
+                validateForm().then(err => {
+                  if (Object.keys(err).length === 0) {
+                    const newUrl = 'request-description';
+                    history.push(newUrl);
+                  }
+                });
+                window.scrollTo(0, 0);
+              }}
+            >
+              Next
+            </Button>
+            <PageNumber currentPage={1} totalPages={6} />
           </div>
-        </FieldGroup>
-      </div>
-    </div>
+        );
+      }}
+    </Formik>
   );
 };
 
