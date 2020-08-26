8dfab0c0c042368585c8608f644df979a6e32151
Nick Downey <Nicholas.Downey@cms.hhs.gov>
2020-08-26 17:42:11 -0400

    Added Cookies and Accesibility Statement pages with associated i18n (en) files

---
+++ src/i18n/en-US/accessibilityStatement.ts
@@ -0,0 +1,72 @@
+const accessibilityStatement = {
+  // Main Title
+  mainTitle: 'Accessibility & Nondiscrimination Notice',
+
+  // CMS Accessible Communications
+  communicationsTitle: 'CMS accessible communications',
+
+  communicationsParagraph1:
+    'CMS provides free auxiliary aids and services including information in accessible formats like Braille, large print, data/audio files, relay services and TTY communications. If you request information in an accessible format from CMS, you won’t be disadvantaged by any additional time necessary to provide it. This means you will get extra time to take any action if there’s a delay in fulfilling your request.',
+  communicationsParagraph2:
+    'To request Medicare or Marketplace information in an accessible format you can:',
+
+  communicationsHowToRequestListElem1: '<strong>Call us:</strong>',
+  communicationsHowToRequestListElem1SubList: [
+    'For Medicare: 1-800-MEDICARE (1-800-633-4227). TTY: 1-877-486-2048',
+    'For the Health Insurance Marketplace: (1-800-318-2596). TTY: 1-855-889-4325'
+  ],
+
+  communicationsHowToRequestListElem2: '<strong>Email us:</strong>: ',
+  communicationsHowToRequestListElem2Email: 'altformatrequest@cms.hhs.gov',
+  communicationsHowToRequestListElem3:
+    '<strong>Send us a fax:</strong> 1-844-530-3676',
+  communicationsHowToRequestListElem4: '<strong>Send us a letter:</strong>',
+  communicationsHowToRequestListCMSAddressList: [
+    'Centers for Medicare & Medicaid Services',
+    'Offices of Hearings and Inquiries (OHI)',
+    '7500 Security Boulevard, Mail Stop S1-13-25',
+    'Baltimore, MD 21244-1850',
+    'Attn: Customer Accessibility Resource Staff'
+  ],
+
+  communicationsParagraph3:
+    'Your request should include your name, phone number, type of information you need (if known) and the mailing address where we should send the materials. We may contact you for additional information.',
+  communicationsParagraph4:
+    '<strong>Note:</strong> If you’re enrolled in a Medicare Advantage Plan or Prescription Drug Plan, contact your plan to request their information in an accessible format. For Medicaid, contact your State or local Medicaid office.',
+
+  // Nondiscrimination Notice
+  nondiscriminationTitle: 'Nondiscrimination Notice',
+  nondiscriminationParagraph:
+    "The Centers for Medicare & Medicaid Services (CMS) is the federal agency that runs the Medicare, Medicaid, and Children's Health Insurance Programs, and the federally facilitated Marketplace. CMS doesn’t exclude, deny benefits to, or otherwise discriminate against any person on the basis of race, color, national origin, disability, sex, or age in admission to, participation in, or receipt of the services and benefits under any of its programs and activities, whether carried out by CMS directly or through a contractor or any other entity with which CMS arranges to carry out its programs and activities.",
+
+  // How to file a complaint
+  fileComplaintHowToTitle: 'How to file a complaint',
+
+  fileComplaintHowToParagraph1:
+    'You can contact CMS in any of the ways included in this notice if you have any concerns about getting information in a format that you can use.',
+  fileComplaintHowToParagraph2:
+    'You may also file a complaint if you think you’ve been subjected to discrimination in a CMS program or activity, including experiencing issues with getting information in an accessible format from any Medicare Advantage Plan, Medicare Prescription Drug Plan, State or local Medicaid office or Marketplace Qualified Health Plans. There are 3 ways to file a complaint with the U.S. Department of Health and Human Services, Office for Civil Rights:',
+
+  fileComplaintHowToListElem1: 'Online',
+  fileComplaintHowToListElem2:
+    '<strong>By phone:</strong> Call 1-800-368-1019. TTY users can call 1-800-537-7697.',
+  fileComplaintHowToListElem3:
+    '<strong>In writing:</strong> Send information about your complaint to:',
+
+  // Medicare.gov accessibility & compliance with Section 508
+  complianceTitle: 'Medicare.gov accessibility & compliance with Section 508',
+  complianceParagraph1:
+    'CMS is committed to making its electronic and information technologies accessible to people with disabilities. If you can’t access content or use features on this website due to a disability, contact our Section 508 Team at ',
+  complianceEmail: '508Feedback@cms.hhs.gov',
+  complianceParagraph2:
+    '. To help us better serve you, upload the material in question and/or include the URL if possible and let us know the specific problems you’re having.',
+
+  // Additional Information
+  additionalInformationTitle: 'Additional Information',
+
+  additionalInformation508:
+    'What is Section 504 & how does it relate to Section 508?',
+  additionalInformationCivilRights: 'Civil Rights for Individuals & Advocates'
+};
+
+export default accessibilityStatement;
