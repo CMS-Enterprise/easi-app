3a1b9ae6b53f997f1be91ba506b9feb0320561c1
Christopher Hui <christopher@truss.works>
2020-08-31 17:57:51 -0700

    guess n check time

--- cypress/integration/businessCase.spec.js
+++ cypress/integration/businessCase.spec.js
@@ -44,7 +44,10 @@ describe('The Business Case Form', () => {
 
   beforeEach(() => {
     cy.restoreLocalStorage();
-    cy.route('GET', '/api/v1/system_intake/*').as('getSystemIntake');
+    cy.server()
+      .route('GET', '/api/v1/system_intake/*')
+      .as('getSystemIntake');
+
     cy.visit(`/governance-task-list/${intakeId}`);
     cy.wait('@getSystemIntake');
     cy.get('[data-testid="prepare-bus-case-item"')
