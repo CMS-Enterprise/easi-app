f9fcaa25e3a75f65d34d51a22436234a22820414
Christopher Hui <christopher@truss.works>
2020-08-31 23:12:34 -0700

    works when running docker services locally

--- cypress/integration/businessCase.spec.js
+++ cypress/integration/businessCase.spec.js
@@ -43,9 +43,12 @@ describe('The Business Case Form', () => {
   });
 
   beforeEach(() => {
+    cy.server();
+    cy.route('GET', '/api/v1/system_intake/*').as('getSystemIntake');
     cy.restoreLocalStorage();
 
     cy.visit(`/governance-task-list/${intakeId}`);
+    cy.wait('@getSystemIntake');
     cy.get('[data-testid="prepare-bus-case-item"]')
       .get('button')
       .contains('Start')
