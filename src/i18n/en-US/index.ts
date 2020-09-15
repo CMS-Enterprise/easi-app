0bad3ef43ac51e44c163b4a7c99b495cde0748bb
Jim Benton <jim@truss.works>
2020-09-15 15:17:11 -0500

    Reorganize the GRB localizations.

--- src/i18n/en-US/index.ts
+++ src/i18n/en-US/index.ts
@@ -7,34 +7,32 @@ import cookies from './cookies';
 import error from './error';
 import footer from './footer';
 import general from './general';
+import governanceReviewBoard from './governanceReviewBoard';
 import governanceReviewTeam from './governanceReviewTeam';
 import grtReview from './grtReview';
 import header from './header';
 import home from './home';
-import prepareForGRB from './prepareForGRB';
-import prepareForGRT from './prepareForGRT';
 import privacyPolicy from './privacyPolicy';
 import systemIntake from './systemIntake';
 import termsAndConditions from './termsAndConditions';
 
 const enUS = {
+  accessibilityStatement,
   auth,
+  businessCase,
+  cookies,
   error,
   footer,
   general,
+  governanceReviewBoard,
+  governanceReviewTeam,
+  grtReview,
   header,
   home,
-  governanceReviewTeam,
-  systemIntake,
-  businessCase,
-  grtReview,
-  prepareForGRB,
-  prepareForGRT,
   privacyPolicy,
   taskList,
   termsAndConditions,
-  cookies,
-  accessibilityStatement
+  systemIntake
 };
 
 export default enUS;
