d74ab5027e597def2af9f068c6a43d53a820448d
Jim Benton <jim@truss.works>
2020-09-22 17:13:59 -0500

    Render within a Router in tests.

--- src/views/PrepareForGRT/index.test.tsx
+++ src/views/PrepareForGRT/index.test.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
 import { shallow } from 'enzyme';
+import configureMockStore from 'redux-mock-store';
 
 import PrepareForGRT from './index';
 
 describe('The PrepareForGRT component', () => {
   it('renders without crashing', () => {
-    shallow(<PrepareForGRT />);
+    const mockStore = configureMockStore();
+    const store = mockStore({});
+    shallow(
+      <MemoryRouter initialEntries={['/']} initialIndex={0}>
+        <Provider store={store}>
+          <PrepareForGRT />
+        </Provider>
+      </MemoryRouter>
+    );
   });
 });
