52bd76a88b3724bc3ec0446bdacbcd9491841659
Isaac Garfinkle <isaac@truss.works>
2020-10-06 14:49:33 -0700

    Update tests to reflect db changes

--- pkg/models/system_intake_action.go
+++ pkg/models/system_intake_action.go
@@ -16,10 +16,11 @@ const (
 
 // SystemIntakeAction is the model for an action on a system intake
 type SystemIntakeAction struct {
-	ID         uuid.UUID              `json:"id"`
-	IntakeID   uuid.UUID              `json:"intake_id" db:"intake_id"`
-	ActionType SystemIntakeActionType `json:"action_type" db:"action_type"`
-	ActorName  *string                `json:"actor_name" db:"actor_name"`
-	ActorEmail *string                `json:"actor_email" db:"actor_email"`
-	CreatedAt  *time.Time             `json:"createdAt" db:"created_at"`
+	ID             uuid.UUID              `json:"id"`
+	IntakeID       uuid.UUID              `json:"intake_id" db:"intake_id"`
+	ActionType     SystemIntakeActionType `json:"action_type" db:"action_type"`
+	ActorName      string                 `json:"actor_name" db:"actor_name"`
+	ActorEmail     string                 `json:"actor_email" db:"actor_email"`
+	ActorEUAUserID string                 `json:"actor_eua_user_id" db:"actor_eua_user_id"`
+	CreatedAt      *time.Time             `json:"createdAt" db:"created_at"`
 }
