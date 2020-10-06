a9515f716d6d402a3e874e750be424e15263dcac
Isaac Garfinkle <isaac@truss.works>
2020-10-06 14:35:50 -0700

    Add EUAID column to actions table and add nonempty constraints to others

--- migrations/V24__Add_Actions_Table.sql
+++ migrations/V24__Add_Actions_Table.sql
@@ -3,8 +3,9 @@ CREATE TYPE action_type AS ENUM ('SUBMIT');
 create table actions (
     id uuid primary key not null,
     action_type action_type not null,
-    actor_name text not null,
-    actor_email text not null,
+    actor_name text not null CHECK (actor_name <> ''),
+    actor_email text not null CHECK (actor_name <> ''),
+    actor_eua_user_id text not null CHECK (actor_eua_user_id ~ '[A-Z0-9]{4}'),
     created_at timestamp with time zone,
     intake_id uuid references system_intake(id)
 )
