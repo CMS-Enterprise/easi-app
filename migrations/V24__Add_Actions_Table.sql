1b971f51f3dcddf417a17b2247b9e77758c63021
Isaac Garfinkle <isaac@truss.works>
2020-10-07 15:17:58 -0700

    Fix euaID db constraints to not allow longer IDs

--- migrations/V24__Add_Actions_Table.sql
+++ migrations/V24__Add_Actions_Table.sql
@@ -5,7 +5,7 @@ create table actions (
     action_type action_type not null,
     actor_name text not null CHECK (actor_name <> ''),
     actor_email text not null CHECK (actor_name <> ''),
-    actor_eua_user_id text not null CHECK (actor_eua_user_id ~ '[A-Z0-9]{4}'),
+    actor_eua_user_id text not null CHECK (actor_eua_user_id ~ '^[A-Z0-9]{4}$'),
     created_at timestamp with time zone,
     intake_id uuid references system_intake(id)
 )
