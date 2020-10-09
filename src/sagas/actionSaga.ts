23bcf3f8a3d836625c4ded39ffcf373b912df178
Isaac Garfinkle <isaac@truss.works>
2020-10-09 13:21:06 -0700

    Put action type in request path

--- src/sagas/actionSaga.ts
+++ src/sagas/actionSaga.ts
@@ -8,8 +8,7 @@ import { postSystemIntakeAction } from 'types/routines';
 
 function postSystemIntakeActionRequest(formData: Action) {
   return axios.post(
-    `${process.env.REACT_APP_API_ADDRESS}/system_intake/${formData.intakeId}/actions`,
-    `"${formData.actionType}"`
+    `${process.env.REACT_APP_API_ADDRESS}/system_intake/${formData.intakeId}/actions/${formData.actionType}`
   );
 }
 
