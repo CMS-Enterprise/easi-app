4819d9a96087676eb82334a0a0fdbcdb09d8eaa6
Christopher Hui <christopher@truss.works>
2020-10-19 22:07:54 -0700

    add request repository reducer

--- src/sagas/rootSaga.ts
+++ src/sagas/rootSaga.ts
@@ -3,6 +3,7 @@ import { all } from 'redux-saga/effects';
 import actionSaga from 'sagas/actionSaga';
 import businessCaseSaga from 'sagas/businessCaseSaga';
 import businessCasesSaga from 'sagas/businessCasesSaga';
+import requestRepositorySaga from 'sagas/requestRepositorySaga';
 import searchSaga from 'sagas/searchSaga';
 import systemIntakeSaga from 'sagas/systemIntakeSaga';
 import systemIntakesSaga from 'sagas/systemIntakesSaga';
@@ -14,6 +15,7 @@ export default function* rootSaga() {
     systemIntakeSaga(),
     businessCaseSaga(),
     businessCasesSaga(),
-    actionSaga()
+    actionSaga(),
+    requestRepositorySaga()
   ]);
 }
