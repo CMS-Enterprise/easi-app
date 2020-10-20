293e95670215a71d7b04df0aac77afe1c9f2122e
Isaac Garfinkle <isaac@truss.works>
2020-10-19 21:14:35 -0700

    Remove review from update intake service, remove CLOSED status

--- pkg/server/routes.go
+++ pkg/server/routes.go
@@ -140,18 +140,12 @@ func (s *Server) routes(
 			store.CreateSystemIntake,
 		),
 		services.NewUpdateSystemIntake(
-			serviceConfig,
-			store.UpdateSystemIntake,
 			store.FetchSystemIntakeByID,
-			services.NewAuthorizeUserIsIntakeRequester(),
-			cedarLdapClient.FetchUserInfo,
-			emailClient.SendSystemIntakeReviewEmail,
 			services.NewUpdateDraftSystemIntake(
 				serviceConfig,
 				services.NewAuthorizeUserIsIntakeRequester(),
 				store.UpdateSystemIntake,
 			),
-			!s.environment.Prod(),
 		),
 		services.NewFetchSystemIntakeByID(
 			serviceConfig,
