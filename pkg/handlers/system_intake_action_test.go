fe7a50ef6ef7bf43f5ebe4504fcd3f8adbaf1b91
Isaac Garfinkle <isaac@truss.works>
2020-10-22 14:18:13 -0700

    Server expects action type in payload

--- pkg/handlers/system_intake_action_test.go
+++ pkg/handlers/system_intake_action_test.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
@@ -25,19 +26,22 @@ func newMockCreateSystemIntakeAction(err error) createSystemIntakeAction {
 func (s HandlerTestSuite) TestSystemIntakeActionHandler() {
 	requestContext := context.Background()
 	requestContext = appcontext.WithPrincipal(requestContext, &authn.EUAPrincipal{EUAID: "FAKE", JobCodeEASi: true})
-	id, err := uuid.NewUUID()
-	s.NoError(err)
+	id, _ := uuid.NewUUID()
 
 	s.Run("golden path POST passes", func() {
+		body, err := json.Marshal(map[string]string{
+			"action_type": "SUBMIT",
+		})
+		s.NoError(err)
 		rr := httptest.NewRecorder()
 		req, reqErr := http.NewRequestWithContext(
 			requestContext,
 			"POST",
 			fmt.Sprintf("/system_intake/%s/action/SUBMIT", id.String()),
-			bytes.NewBufferString(""),
+			bytes.NewBuffer(body),
 		)
 		s.NoError(reqErr)
-		req = mux.SetURLVars(req, map[string]string{"intake_id": id.String(), "action_type": "SUBMIT"})
+		req = mux.SetURLVars(req, map[string]string{"intake_id": id.String()})
 		SystemIntakeActionHandler{
 			HandlerBase:              s.base,
 			CreateSystemIntakeAction: newMockCreateSystemIntakeAction(nil),
@@ -46,17 +50,38 @@ func (s HandlerTestSuite) TestSystemIntakeActionHandler() {
 		s.Equal(http.StatusCreated, rr.Code)
 	})
 
+	s.Run("POST fails with empty request body", func() {
+		rr := httptest.NewRecorder()
+		req, reqErr := http.NewRequestWithContext(
+			requestContext,
+			"POST",
+			fmt.Sprintf("/system_intake/%s/action/SUBMIT", id.String()),
+			bytes.NewBufferString(""),
+		)
+		s.NoError(reqErr)
+		req = mux.SetURLVars(req, map[string]string{"intake_id": id.String()})
+		SystemIntakeActionHandler{
+			HandlerBase:              s.base,
+			CreateSystemIntakeAction: newMockCreateSystemIntakeAction(nil),
+		}.Handle()(rr, req)
+
+		s.Equal(http.StatusBadRequest, rr.Code)
+	})
+
 	s.Run("POST fails if a error is thrown by service", func() {
+		body, err := json.Marshal(map[string]string{
+			"action_type": "SUBMIT",
+		})
 		s.NoError(err)
 		rr := httptest.NewRecorder()
 		req, err := http.NewRequestWithContext(
 			requestContext,
 			"POST",
 			fmt.Sprintf("/system_intake/%s/action/SUBMIT", id.String()),
-			bytes.NewBufferString(""),
+			bytes.NewBuffer(body),
 		)
 		s.NoError(err)
-		req = mux.SetURLVars(req, map[string]string{"intake_id": id.String(), "action_type": "SUBMIT"})
+		req = mux.SetURLVars(req, map[string]string{"intake_id": id.String()})
 		expectedErr := apperrors.ValidationError{
 			Model:   models.Action{},
 			ModelID: "",
--- pkg/server/routes.go
+++ pkg/server/routes.go
@@ -248,7 +248,7 @@ func (s *Server) routes(
 			),
 		),
 	)
-	api.Handle("/system_intake/{intake_id}/actions/{action_type}", systemIntakeActionHandler.Handle())
+	api.Handle("/system_intake/{intake_id}/action", systemIntakeActionHandler.Handle())
 
 	s.router.PathPrefix("/").Handler(handlers.NewCatchAllHandler(
 		base,
