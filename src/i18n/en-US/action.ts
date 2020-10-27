22e487db6d34bd97525ea45903756d77e8e18157
Sara Bocciardi <sara@truss.works>
2020-10-26 18:02:31 -0700

    Add first three fields for issuing a LCID.

--- src/i18n/en-US/action.ts
+++ src/i18n/en-US/action.ts
@@ -4,6 +4,17 @@ const action = {
     needBizCase: 'Request a business case',
     readyForGrt: 'Mark as ready for GRT'
   },
+  issueLCID: {
+    backLink: 'Change',
+    heading: 'Actions on intake request',
+    subheading: 'How to proceed?',
+    feedbackLabel: 'This email will be sent to the requester',
+    submit: 'Send email',
+    nextStepsLabel: 'Next Steps',
+    nextStepsHelpText: 'Next Steps Help Text',
+    scopeLabel: 'Scope of Lifecycle ID',
+    scopeHelpText: 'Scope Help Text'
+  },
   submitAction: {
     backLink: 'Change',
     heading: 'Actions on intake request',
