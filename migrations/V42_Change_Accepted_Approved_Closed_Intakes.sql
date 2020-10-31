9ac0c76dfcf7817df489358b7837b76154fae8aa
Isaac Garfinkle <isaac@truss.works>
2020-10-30 17:01:34 -0700

    Migrate statuses in db

---
+++ migrations/V42_Change_Accepted_Approved_Closed_Intakes.sql
@@ -0,0 +1,4 @@
+UPDATE system_intake SET status = 'NOT_IT_REQUEST' WHERE status = 'CLOSED';
+UPDATE system_intake SET status = 'LCID_ISSUED' WHERE status = 'APPROVED';
+UPDATE system_intake SET status = 'NEED_BIZ_CASE' WHERE status = 'ACCEPTED';
+UPDATE system_intake SET status = 'WITHDRAWN' WHERE status = 'ARCHIVED';
