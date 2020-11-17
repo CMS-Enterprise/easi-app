c148ca5ca4408b45ccbab98a4fcea58bb2c6c597
Christopher Hui <christopher@truss.works>
2020-11-17 09:38:22 -0800

    remove unnecessary return

--- src/data/taskList.ts
+++ src/data/taskList.ts
@@ -42,7 +42,6 @@ export const businessCaseTag = (intake: SystemIntakeForm): TagEnum => {
       return 'CANNOT_START';
     case 'BIZ_CASE_DRAFT_SUBMITTED':
     case 'BIZ_CASE_FINAL_NEEDED':
-      return 'COMPLETED';
     case 'BIZ_CASE_FINAL_SUBMITTED':
     case 'READY_FOR_GRB':
     case 'LCID_ISSUED':
