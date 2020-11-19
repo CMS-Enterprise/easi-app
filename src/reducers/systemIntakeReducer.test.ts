c1ef1fb896b1c2ad35cae9e29db95914354144fa
Christopher Hui <christopher@truss.works>
2020-11-19 10:45:15 -0800

    move test to correct describe block

--- src/reducers/systemIntakeReducer.test.ts
+++ src/reducers/systemIntakeReducer.test.ts
@@ -483,64 +483,64 @@ describe('The system intake reducer', () => {
         notes: []
       });
     });
-  });
 
-  it('handles fetchIntakeNotes.SUCCESS', () => {
-    const initialState = {
-      systemIntake: initialSystemIntakeForm,
-      isLoading: false,
-      isSaving: true,
-      isNewIntakeCreated: null,
-      error: null,
-      notes: []
-    };
+    it('handles fetchIntakeNotes.SUCCESS', () => {
+      const initialState = {
+        systemIntake: initialSystemIntakeForm,
+        isLoading: false,
+        isSaving: true,
+        isNewIntakeCreated: null,
+        error: null,
+        notes: []
+      };
 
-    const mockSuccessAction = {
-      type: fetchIntakeNotes.SUCCESS,
-      payload: [
-        {
-          id: '12345',
-          authorName: 'John Brown',
-          authorId: 'ABCD',
-          content: 'Test Note 1',
-          systemIntakeId: 'test-uuid-note-1',
-          createdAt: '2020-11-17 08:13:18.936399+00'
-        },
-        {
-          id: '67890',
-          authorName: 'John Brown',
-          authorId: 'ABCD',
-          content: 'Test Note 2',
-          systemIntakeId: 'test-uuid-note-2',
-          createdAt: '2020-11-17 08:13:18.936399+00'
-        }
-      ]
-    };
+      const mockSuccessAction = {
+        type: fetchIntakeNotes.SUCCESS,
+        payload: [
+          {
+            id: '12345',
+            authorName: 'John Brown',
+            authorId: 'ABCD',
+            content: 'Test Note 1',
+            systemIntakeId: 'test-uuid-note-1',
+            createdAt: '2020-11-17 08:13:18.936399+00'
+          },
+          {
+            id: '67890',
+            authorName: 'John Brown',
+            authorId: 'ABCD',
+            content: 'Test Note 2',
+            systemIntakeId: 'test-uuid-note-2',
+            createdAt: '2020-11-17 08:13:18.936399+00'
+          }
+        ]
+      };
 
-    expect(systemIntakeReducer(initialState, mockSuccessAction)).toEqual({
-      systemIntake: initialSystemIntakeForm,
-      isLoading: false,
-      isSaving: true,
-      isNewIntakeCreated: null,
-      error: null,
-      notes: [
-        {
-          id: '12345',
-          authorName: 'John Brown',
-          authorId: 'ABCD',
-          content: 'Test Note 1',
-          systemIntakeId: 'test-uuid-note-1',
-          createdAt: DateTime.fromISO('2020-11-17 08:13:18.936399+00')
-        },
-        {
-          id: '67890',
-          authorName: 'John Brown',
-          authorId: 'ABCD',
-          content: 'Test Note 2',
-          systemIntakeId: 'test-uuid-note-2',
-          createdAt: DateTime.fromISO('2020-11-17 08:13:18.936399+00')
-        }
-      ]
+      expect(systemIntakeReducer(initialState, mockSuccessAction)).toEqual({
+        systemIntake: initialSystemIntakeForm,
+        isLoading: false,
+        isSaving: true,
+        isNewIntakeCreated: null,
+        error: null,
+        notes: [
+          {
+            id: '12345',
+            authorName: 'John Brown',
+            authorId: 'ABCD',
+            content: 'Test Note 1',
+            systemIntakeId: 'test-uuid-note-1',
+            createdAt: DateTime.fromISO('2020-11-17 08:13:18.936399+00')
+          },
+          {
+            id: '67890',
+            authorName: 'John Brown',
+            authorId: 'ABCD',
+            content: 'Test Note 2',
+            systemIntakeId: 'test-uuid-note-2',
+            createdAt: DateTime.fromISO('2020-11-17 08:13:18.936399+00')
+          }
+        ]
+      });
     });
   });
 });
