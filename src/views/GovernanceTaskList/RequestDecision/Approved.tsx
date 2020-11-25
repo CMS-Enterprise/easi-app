b092fad5ae01bec63fb154ca029a28a565eee578
Christopher Hui <christopher@truss.works>
2020-11-25 11:47:52 -0800

    add info that comes from what the reviewer filled out

--- src/views/GovernanceTaskList/RequestDecision/Approved.tsx
+++ src/views/GovernanceTaskList/RequestDecision/Approved.tsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 import { Alert, Link as UswdsLink } from '@trussworks/react-uswds';
 import { DateTime } from 'luxon';
 
-import CollapsableLink from 'components/shared/CollapsableLink';
 import { SystemIntakeForm } from 'types/systemIntake';
 
 import './index.scss';
@@ -22,11 +21,7 @@ const Approved = ({ intake }: ApprovedProps) => {
             {intake.lcid}
           </dd>
         </dl>
-        <p>
-          This lifecycle ID lets you explore the solution and a proof of
-          concept. Once that is done, your project team will need another review
-          and approval.
-        </p>
+        <p className="text-pre-wrap">{intake.lcidScope}</p>
         {intake.lcidExpiration && (
           <p className="text-bold">
             {`This ID expires on ${intake.lcidExpiration.toLocaleString(
@@ -34,44 +29,18 @@ const Approved = ({ intake }: ApprovedProps) => {
             )}`}
           </p>
         )}
-        <CollapsableLink
-          className="easi-governance-decision__accordion"
-          id="Decision-LifecycleExpiration"
-          label="Why does my Lifecycle ID have an expiration?"
-        >
-          <div />
-        </CollapsableLink>
       </div>
 
-      <h3>Next steps</h3>
-      <Alert type="info">
-        Finish these next steps to complete the governance review process.
-      </Alert>
+      {intake.lifecycleNextSteps && (
+        <>
+          <h3>Next steps</h3>
+          <Alert type="info">
+            Finish these next steps to complete the governance review process.
+          </Alert>
 
-      <h4>Update the CMS Portfolio Management Tool (PMT)</h4>
-      <p className="line-height-body-4">
-        In order to achieve compliance with OMB rules, the current state of this
-        effort should be reflected in the appropriate Investment(s) in the CMS
-        Portfolio Management Tool (PMT). You should also update that
-        Investment’s Acquisition Strategy, if applicable, to reflect this
-        effort, ensuring alignment of your Acquisition Plan(s)/Interagency
-        Agreement with your Acquisition Strategy. For any questions, please
-        contact IT_Investments@cms.hhs.gov.
-      </p>
-      <UswdsLink href="/" target="_blank" rel="oponener noreferrer">
-        Update the Portfolio Management Tool
-      </UswdsLink>
-
-      <h4>Update your System Profile</h4>
-      <p className="line-height-body-4">
-        Update your System Profile in the CMS System Census to baseline the
-        project, and continuously update the Profile as you develop the system
-        and/or begin planning for any system changes.
-      </p>
-      <UswdsLink href="/" target="_blank" rel="oponener noreferrer">
-        Set up system profile
-      </UswdsLink>
-
+          <p className="text-pre-wrap">{intake.lifecycleNextSteps}</p>
+        </>
+      )}
       <h3>Help us improve</h3>
       <UswdsLink href="/" target="_blank" rel="oponener noreferrer">
         Tell us what you think of this service
