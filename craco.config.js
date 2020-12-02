a9684757cdd38c352b1e6592af165b06c6d83d28
Jim Benton <jim@truss.works>
2020-12-02 16:10:39 -0600

    Speed up compiles.

--- craco.config.js
+++ craco.config.js
@@ -1,15 +1,25 @@
 const sassResourcesLoader = require('craco-sass-resources-loader');
 
-module.exports = {
-  plugins: [
-    {
-      plugin: sassResourcesLoader,
-      options: {
-        resources: [
-          './src/stylesheets/_colors.scss',
-          './src/stylesheets/_variables.scss'
-        ]
+module.exports = () => {
+  return {
+    plugins: [
+      {
+        plugin: sassResourcesLoader,
+        options: {
+          resources: [
+            './src/stylesheets/_colors.scss',
+            './src/stylesheets/_variables.scss'
+          ]
+        }
+      }
+    ],
+    webpack: {
+      configure: (webpackConfig, { env, paths }) => {
+        webpackConfig.plugins = webpackConfig.plugins.filter(
+          plugin => !(plugin.options && 'eslintPath' in plugin.options)
+        );
+        return webpackConfig;
       }
     }
-  ]
+  };
 };
