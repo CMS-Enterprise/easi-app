a9736fd541b49f337693f8dab1838044a2a05a5f
Isaac Garfinkle <isaac@truss.works>
2020-12-03 21:59:52 -0800

    Add separate email for issuing LCID

---
+++ pkg/email/issue_lcid.go
@@ -0,0 +1,54 @@
+package email
+
+import (
+	"bytes"
+	"errors"
+	"time"
+
+	"github.com/cmsgov/easi-app/pkg/apperrors"
+)
+
+type issueLCID struct {
+	LifecycleID string
+	ExpiresAt   string
+	Scope       string
+	NextSteps   string
+	Feedback    string
+}
+
+func (c Client) issueLCIDBody(lcid string, expiresAt *time.Time, scope string, nextSteps string, feedback string) (string, error) {
+	data := issueLCID{
+		LifecycleID: lcid,
+		ExpiresAt:   expiresAt.Format("January 2, 2006"),
+		Scope:       scope,
+		NextSteps:   nextSteps,
+		Feedback:    feedback,
+	}
+	var b bytes.Buffer
+	if c.templates.issueLCIDTemplate == nil {
+		return "", errors.New("issue LCID template is nil")
+	}
+	err := c.templates.issueLCIDTemplate.Execute(&b, data)
+	if err != nil {
+		return "", err
+	}
+	return b.String(), nil
+}
+
+// SendIssueLCIDEmail sends an email for issuing an LCID
+func (c Client) SendIssueLCIDEmail(recipient string, lcid string, expirationDate *time.Time, scope string, nextSteps string, feedback string) error {
+	subject := "Your request has been approved"
+	body, err := c.issueLCIDBody(lcid, expirationDate, scope, nextSteps, feedback)
+	if err != nil {
+		return &apperrors.NotificationError{Err: err, DestinationType: apperrors.DestinationTypeEmail}
+	}
+	err = c.sender.Send(
+		recipient,
+		subject,
+		body,
+	)
+	if err != nil {
+		return &apperrors.NotificationError{Err: err, DestinationType: apperrors.DestinationTypeEmail}
+	}
+	return nil
+}
---
+++ pkg/email/issue_lcid_test.go
@@ -0,0 +1,75 @@
+package email
+
+import (
+	"time"
+
+	"github.com/cmsgov/easi-app/pkg/apperrors"
+)
+
+func (s *EmailTestSuite) TestSendIssueLCIDEmail() {
+	sender := mockSender{}
+
+	recipient := "fake@fake.com"
+	lcid := "123456"
+	expiresAt, _ := time.Parse("2020-12-25", "2021-12-25")
+	scope := "scope"
+	nextSteps := "nextSteps"
+	feedback := "feedback"
+
+	s.Run("successful call has the right content", func() {
+		client, err := NewClient(s.config, &sender)
+		s.NoError(err)
+
+		expectedEmail := "<p>Lifecycle ID: 123456</p>\n<p>Expiration Date: January 1, 0001</p>\n<p>Scope: scope</p>\n" +
+			"<p>Next Steps: nextSteps</p>\n\n<p>feedback</p>"
+		err = client.SendIssueLCIDEmail(recipient, lcid, &expiresAt, scope, nextSteps, feedback)
+
+		s.NoError(err)
+		s.Equal(recipient, sender.toAddress)
+		s.Equal("Your request has been approved", sender.subject)
+		s.Equal(expectedEmail, sender.body)
+	})
+
+	s.Run("if the template is nil, we get the error from it", func() {
+		client, err := NewClient(s.config, &sender)
+		s.NoError(err)
+		client.templates = templates{}
+
+		err = client.SendIssueLCIDEmail(recipient, lcid, &expiresAt, scope, nextSteps, feedback)
+
+		s.Error(err)
+		s.IsType(err, &apperrors.NotificationError{})
+		e := err.(*apperrors.NotificationError)
+		s.Equal(apperrors.DestinationTypeEmail, e.DestinationType)
+		s.Equal("issue LCID template is nil", e.Err.Error())
+	})
+
+	s.Run("if the template fails to execute, we get the error from it", func() {
+		client, err := NewClient(s.config, &sender)
+		s.NoError(err)
+		client.templates.issueLCIDTemplate = mockFailedTemplateCaller{}
+
+		err = client.SendIssueLCIDEmail(recipient, lcid, &expiresAt, scope, nextSteps, feedback)
+
+		s.Error(err)
+		s.IsType(err, &apperrors.NotificationError{})
+		e := err.(*apperrors.NotificationError)
+		s.Equal(apperrors.DestinationTypeEmail, e.DestinationType)
+		s.Equal("template caller had an error", e.Err.Error())
+	})
+
+	s.Run("if the sender fails, we get the error from it", func() {
+		sender := mockFailedSender{}
+
+		client, err := NewClient(s.config, &sender)
+		s.NoError(err)
+
+		err = client.SendIssueLCIDEmail(recipient, lcid, &expiresAt, scope, nextSteps, feedback)
+
+		s.Error(err)
+		s.IsType(err, &apperrors.NotificationError{})
+		e := err.(*apperrors.NotificationError)
+		s.Equal(apperrors.DestinationTypeEmail, e.DestinationType)
+		s.Equal("sender had an error", e.Err.Error())
+	})
+}
