cfd9c1083fb551fc55297ee715d4a71c751ee32c
Nick Downey <Nicholas.Downey@cms.hhs.gov>
2020-12-04 17:35:01 -0500

    Added survey component, added 'improve EASi' survey to governance task list

---
+++ src/i18n/en-US/survey.ts
@@ -0,0 +1,7 @@
+const survey = {
+  improveEasi:
+    'Help us improve EASi. <1>Tell us what you think of this service (opens in a new tab)</1>',
+  anythingWrong: 'Is there anything wrong on this page?'
+};
+
+export default survey;
