f9386e1ab6d8fd1f96244858b69491bc2ccdbc59
Nick Downey <Nicholas.Downey@cms.hhs.gov>
2020-12-09 17:00:36 -0500

    Removed survey i18n and moved contents to general

--- src/components/Survey/index.tsx
+++ src/components/Survey/index.tsx
@@ -5,26 +5,52 @@ export const AnythingWrongSurvey = () => {
   const { t } = useTranslation();
   return (
     <div>
-      <p className="margin-y-4">
+      <p>
         <a
           href="https://www.surveymonkey.com/r/DM6NYRX"
           target="_blank"
           rel="noopener noreferrer"
           aria-label="Open EASi survey in a new tab"
         >
-          {t('survey:anythingWrong')}
+          {t('general:feedback.anythingWrong')}
         </a>
       </p>
     </div>
   );
 };
 
+/* There are multiple versions of the Improve EASi survey depending on format of
+  page it is being added to */
+
+// This is the common version for most pages
 export const ImproveEasiSurvey = () => {
   return (
     <div>
-      <p className="margin-y-4">
-        <Trans i18nKey="survey:improveEasi">
+      <p>
+        <Trans i18nKey="general:feedback.improveEasi">
           helpUsImproveEasi
+          <a
+            href="https://www.surveymonkey.com/r/JNYSMZP"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Open EASi survey in a new tab"
+          >
+            tellUsWhatYouThink
+          </a>
+        </Trans>
+      </p>
+    </div>
+  );
+};
+
+/* This version is only for decision pages, it has a header so that it fits
+  with the theme of the pages */
+export const ImproveEasiDecisionPageSurvey = () => {
+  return (
+    <div>
+      <p>
+        <Trans i18nKey="general:feedback.improveEasi">
+          <h3>helpUsImproveEasi</h3>
           <a
             href="https://www.surveymonkey.com/r/JNYSMZP"
             target="_blank"
