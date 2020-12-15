5049fb8cb7af3e8584150b2fd73eef1ba3c75670
Sara Bocciardi <sara@truss.works>
2020-12-15 15:30:50 -0800

    Add more cases for decisions.

--- src/views/GovernanceReviewTeam/Decision/index.tsx
+++ src/views/GovernanceReviewTeam/Decision/index.tsx
@@ -15,81 +15,115 @@ type DecisionProps = {
 
 const Decision = ({ systemIntake }: DecisionProps) => {
   const Approved = () => (
-    <DescriptionList title="Decision Details">
-      <ReviewRow>
-        <div>
-          <DescriptionTerm term="Lifecycle ID" />
-          <DescriptionDefinition
-            className="text-pre-wrap"
-            definition={systemIntake.lcid}
-          />
-        </div>
-      </ReviewRow>
-      <ReviewRow>
-        <div>
-          <DescriptionTerm term="Lifecycle ID Expiration" />
-          <DescriptionDefinition
-            definition={
-              systemIntake.lcidExpiration
-                ? systemIntake.lcidExpiration.toLocaleString(DateTime.DATE_FULL)
-                : ''
-            }
-          />
-        </div>
-      </ReviewRow>
-      <ReviewRow>
-        <div>
-          <DescriptionTerm term="Lifecycle ID Scope" />
-          <DescriptionDefinition
-            className="text-pre-wrap"
-            definition={systemIntake.lcidScope}
-          />
-        </div>
-      </ReviewRow>
-      <ReviewRow>
-        <div>
-          <DescriptionTerm term="Next Steps" />
-          <DescriptionDefinition
-            className="text-pre-wrap"
-            definition={systemIntake.decisionNextSteps}
-          />
-        </div>
-      </ReviewRow>
-    </DescriptionList>
+    <>
+      <h1>Decision - Accepted</h1>
+      <DescriptionList title="Decision Details">
+        <ReviewRow>
+          <div>
+            <DescriptionTerm term="Lifecycle ID" />
+            <DescriptionDefinition
+              className="text-pre-wrap"
+              definition={systemIntake.lcid}
+            />
+          </div>
+        </ReviewRow>
+        <ReviewRow>
+          <div>
+            <DescriptionTerm term="Lifecycle ID Expiration" />
+            <DescriptionDefinition
+              definition={
+                systemIntake.lcidExpiration
+                  ? systemIntake.lcidExpiration.toLocaleString(
+                      DateTime.DATE_FULL
+                    )
+                  : ''
+              }
+            />
+          </div>
+        </ReviewRow>
+        <ReviewRow>
+          <div>
+            <DescriptionTerm term="Lifecycle ID Scope" />
+            <DescriptionDefinition
+              className="text-pre-wrap"
+              definition={systemIntake.lcidScope}
+            />
+          </div>
+        </ReviewRow>
+        <ReviewRow>
+          <div>
+            <DescriptionTerm term="Next Steps" />
+            <DescriptionDefinition
+              className="text-pre-wrap"
+              definition={systemIntake.decisionNextSteps}
+            />
+          </div>
+        </ReviewRow>
+      </DescriptionList>
+    </>
   );
 
   const Rejected = () => (
-    <DescriptionList title="Decision Details">
-      <ReviewRow>
-        <div>
-          <DescriptionTerm term="Rejection Reason" />
-          <DescriptionDefinition
-            className="text-pre-wrap"
-            definition={systemIntake.rejectionReason}
-          />
-        </div>
-      </ReviewRow>
-      <ReviewRow>
-        <div>
-          <DescriptionTerm term="Next Steps" />
-          <DescriptionDefinition
-            className="text-pre-wrap"
-            definition={systemIntake.decisionNextSteps}
-          />
-        </div>
-      </ReviewRow>
-    </DescriptionList>
+    <>
+      <h1>Decision - Rejected</h1>
+      <DescriptionList title="Decision Details">
+        <ReviewRow>
+          <div>
+            <DescriptionTerm term="Rejection Reason" />
+            <DescriptionDefinition
+              className="text-pre-wrap"
+              definition={systemIntake.rejectionReason}
+            />
+          </div>
+        </ReviewRow>
+        <ReviewRow>
+          <div>
+            <DescriptionTerm term="Next Steps" />
+            <DescriptionDefinition
+              className="text-pre-wrap"
+              definition={systemIntake.decisionNextSteps}
+            />
+          </div>
+        </ReviewRow>
+      </DescriptionList>
+    </>
   );
 
-  return (
+  const NotItRequest = () => (
     <>
-      <h1>
-        Decision -{' '}
-        {systemIntake.status === 'LCID_ISSUED' ? 'Approved' : 'Rejected'}
-      </h1>
-      {systemIntake.status === 'LCID_ISSUED' && <Approved />}
-      {systemIntake.status === 'NOT_APPROVED' && <Rejected />}
+      <h1>Decision - Closed</h1>
+      <p>Request was marked &quot;Not an IT Request&quot; </p>
     </>
+  );
+
+  const NoGovernance = () => (
+    <>
+      <h1>Decision - Closed</h1>
+      <p>REquest was marked &quot;No further governance needed&quot;</p>
+    </>
+  );
+
+  if (systemIntake.status === 'LCID_ISSUED') {
+    return <Approved />;
+  }
+
+  if (systemIntake.status === 'NOT_APPROVED') {
+    return <Rejected />;
+  }
+
+  if (systemIntake.status === 'NOT_IT_REQUEST') {
+    return <NotItRequest />;
+  }
+
+  if (systemIntake.status === 'NO_GOVERNANCE') {
+    return <NoGovernance />;
+  }
+
+  return (
+    <h1>
+      <h1> Decision</h1>
+      <p>Decision not yet made</p>
+    </h1>
   );
 };
 
