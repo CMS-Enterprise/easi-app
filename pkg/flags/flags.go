864012659362387536fce92af70ccf809fac339f
Nelz Carpentier <nelz@truss.works>
2020-12-15 15:57:55 -0800

    bump launchdarkly dependency version

--- pkg/flags/flags.go
+++ pkg/flags/flags.go
@@ -1,13 +1,12 @@
 package flags
 
 import (
-	ld "gopkg.in/launchdarkly/go-server-sdk.v4"
+	"gopkg.in/launchdarkly/go-sdk-common.v2/lduser"
 )
 
 // NewFetchFlags returns all flags from LD
-func NewFetchFlags() func(client FlagClient, ldUser ld.User) FlagValues {
-	return func(client FlagClient, u ld.User) FlagValues {
+func NewFetchFlags() func(client FlagClient, ldUser lduser.User) FlagValues {
+	return func(client FlagClient, u lduser.User) FlagValues {
 		return client.Flags(u)
 	}
-
 }
