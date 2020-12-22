421d8f09b31c040977b25fba12430ecadff04796
Isaac Garfinkle <isaac@truss.works>
2020-12-21 23:48:01 -0800

    cleanup

--- src/contexts/flagContext.tsx
+++ src/contexts/flagContext.tsx
@@ -9,15 +9,12 @@ type FlagProviderProps = {
 export const FlagProvider = ({ children }: FlagProviderProps) => {
   const LDProviderRef = useRef<React.FunctionComponent>(() => <div />);
 
-  const testFN = async () => {
-    console.log('start');
-    LDProviderRef.current = await asyncWithLDProvider({
-      clientSideID: process.env.REACT_APP_LD_CLIENT_ID as string
-    });
-    console.log('end');
-  };
   useEffect(() => {
-    testFN();
+    (async () => {
+      LDProviderRef.current = await asyncWithLDProvider({
+        clientSideID: process.env.REACT_APP_LD_CLIENT_ID as string
+      });
+    })();
   }, []);
 
   const LDProvider = LDProviderRef.current;
