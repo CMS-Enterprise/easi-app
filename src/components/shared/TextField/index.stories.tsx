f0f0aa3dc6af1c2f048f788cd9d252aaded6e9f2
Christopher Hui <christopher@truss.works>
2021-01-12 14:26:48 -0800

    make text field editable

--- src/components/shared/TextField/index.stories.tsx
+++ src/components/shared/TextField/index.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import HelpText from 'components/shared/HelpText';
 import Label from 'components/shared/Label';
@@ -10,42 +10,51 @@ export default {
   component: TextField
 };
 
-export const Default = () => (
-  <TextField
-    id="Test1"
-    name="Test1"
-    onChange={() => {}}
-    onBlur={() => {}}
-    value=""
-  />
-);
-
-export const WithLabel = () => (
-  <>
-    <Label htmlFor="FirstName">First Name</Label>
+export const Default = () => {
+  const [value, setValue] = useState('');
+  return (
     <TextField
-      id="FirstName"
-      name="firstName"
-      onChange={() => {}}
+      id="Test1"
+      name="Test1"
+      onChange={e => setValue(e.target.value)}
       onBlur={() => {}}
-      value=""
+      value={value}
     />
-  </>
-);
+  );
+};
 
-export const WithLabelAndHelpText = () => (
-  <>
-    <Label htmlFor="FirstName">First Name</Label>
-    <HelpText id="FirstNameHelp">
-      <span>Please provide only your first name</span>
-    </HelpText>
-    <TextField
-      id="FirstName"
-      name="firstName"
-      aria-describedby="FirstNameHelp"
-      onChange={() => {}}
-      onBlur={() => {}}
-      value=""
-    />
-  </>
-);
+export const WithLabel = () => {
+  const [value, setValue] = useState('');
+  return (
+    <>
+      <Label htmlFor="FirstName">First Name</Label>
+      <TextField
+        id="FirstName"
+        name="firstName"
+        onChange={e => setValue(e.target.value)}
+        onBlur={() => {}}
+        value={value}
+      />
+    </>
+  );
+};
+
+export const WithLabelAndHelpText = () => {
+  const [value, setValue] = useState('');
+  return (
+    <>
+      <Label htmlFor="FirstName">First Name</Label>
+      <HelpText id="FirstNameHelp">
+        <span>Please provide only your first name</span>
+      </HelpText>
+      <TextField
+        id="FirstName"
+        name="firstName"
+        aria-describedby="FirstNameHelp"
+        onChange={e => setValue(e.target.value)}
+        onBlur={() => {}}
+        value={value}
+      />
+    </>
+  );
+};
