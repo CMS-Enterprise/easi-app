ec36fbf200fd9691e4abe5fb9aa0b6a9903c2846
Sara Bocciardi <sara@truss.works>
2021-01-13 13:17:58 -0800

    Add new user checks for 508.

--- src/utils/user.tsx
+++ src/utils/user.tsx
@@ -9,8 +9,32 @@ export const isGrtReviewer = (groups: Array<String> | null) => {
   return false;
 };
 
+export const is508Tester = (groups: Array<String> | null) => {
+  if (
+    groups &&
+    (groups.includes('EASI_D_508TESTER') || groups.includes('EASI_P_508TESTER'))
+  ) {
+    return true;
+  }
+
+  return false;
+};
+
+export const is508Admin = (groups: Array<String> | null) => {
+  if (
+    groups &&
+    (groups.includes('EASI_D_508ADMIN') || groups.includes('EASI_P_508ADMIN'))
+  ) {
+    return true;
+  }
+
+  return false;
+};
+
 const user = {
-  isGrtReviewer
+  isGrtReviewer,
+  is508Tester,
+  is508Admin
 };
 
 export default user;
