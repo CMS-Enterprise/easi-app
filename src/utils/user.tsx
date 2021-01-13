027241ab0d890794e5cff34c6ebce341a60e1ff2
Sara Bocciardi <sara@truss.works>
2021-01-13 13:29:03 -0800

    Use constants file for job code strings.

--- src/utils/user.tsx
+++ src/utils/user.tsx
@@ -1,7 +1,16 @@
+import {
+  FIVE08ADMIN_DEV,
+  FIVE08ADMIN_PROD,
+  FIVE08TESTER_DEV,
+  FIVE08TESTER_PROD,
+  GOVTEAM_DEV,
+  GOVTEAM_PROD
+} from 'constants/jobCodes';
+
 export const isGrtReviewer = (groups: Array<String> | null) => {
   if (
     groups &&
-    (groups.includes('EASI_D_GOVTEAM') || groups.includes('EASI_P_GOVTEAM'))
+    (groups.includes(GOVTEAM_DEV) || groups.includes(GOVTEAM_PROD))
   ) {
     return true;
   }
@@ -12,7 +21,7 @@ export const isGrtReviewer = (groups: Array<String> | null) => {
 export const is508Tester = (groups: Array<String> | null) => {
   if (
     groups &&
-    (groups.includes('EASI_D_508TESTER') || groups.includes('EASI_P_508TESTER'))
+    (groups.includes(FIVE08TESTER_DEV) || groups.includes(FIVE08TESTER_PROD))
   ) {
     return true;
   }
@@ -23,7 +32,7 @@ export const is508Tester = (groups: Array<String> | null) => {
 export const is508Admin = (groups: Array<String> | null) => {
   if (
     groups &&
-    (groups.includes('EASI_D_508ADMIN') || groups.includes('EASI_P_508ADMIN'))
+    (groups.includes(FIVE08ADMIN_DEV) || groups.includes(FIVE08ADMIN_PROD))
   ) {
     return true;
   }
