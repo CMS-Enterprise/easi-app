8dbe77c89a606fc627d8ddeaf8d0580dc61e31a0
Christopher Hui <christopher@truss.works>
2021-01-26 15:00:03 -0800

    add file upload file uploaded state and error state

--- src/components/FileUpload/index.tsx
+++ src/components/FileUpload/index.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classnames from 'classnames';
 
+// TBD: multiple files
+// Multiple files in a single input is not recommended, but it might be worth
+// supporting, just in case.
 type FileUploadProps = {
   id: string;
   name: string;
   accept: string;
-  multiple?: boolean;
+  // multiple?: boolean;
   disabled?: boolean;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   onBlur: () => void;
@@ -17,34 +20,84 @@ const FileUpload = (props: FileUploadProps) => {
     id,
     name,
     accept,
-    multiple = false,
+    // multiple = false,
     disabled = false,
     onChange,
     onBlur,
     ariaDescribedBy
   } = props;
+  const [file, setFile] = useState<File>(null);
+  const [error, setError] = useState(false);
   const fileInputWrapper = classnames('easi-file-upload', 'usa-file-input', {
     'usa-file-input--disabled': disabled
   });
+  const targetWrapperClasses = classnames('usa-file-input__target', {
+    'has-invalid-file': error
+  });
+  const instructionsClasses = classnames('usa-file-input__instructions', {
+    'display-none': !!file
+  });
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files.length > 0 && isFileTypeValid(e.target.files[0])) {
+      setError(false);
+      setFile(e.target.files[0]);
+      onChange(e);
+    } else {
+      setError(true);
+      setFile(null);
+    }
+  };
+
+  const isFileTypeValid = (localFile: File) => {
+    let isFileTypeAcceptable = false;
+    if (accept) {
+      const accepetedFileTypes = accept.split(',');
+      accepetedFileTypes.forEach(fileType => {
+        if (
+          localFile.name.indexOf(fileType) > 0 ||
+          localFile.type.includes(fileType.replace(/\*/g, ''))
+        ) {
+          isFileTypeAcceptable = true;
+        }
+      });
+    }
+    return isFileTypeAcceptable;
+  };
 
   return (
     <div className={fileInputWrapper} aria-disabled={disabled}>
-      <div className="usa-file-input__target">
-        <div className="usa-file-input__instructions" aria-hidden>
+      <div className={targetWrapperClasses}>
+        {file && (
+          <div className="usa-file-input__preview-heading">
+            Selected file
+            <span className="usa-file-input__choose">Change file</span>
+          </div>
+        )}
+        <div className={instructionsClasses} aria-hidden>
           <span className="usa-file-input__drag-text">Drag file here or </span>
           <span className="usa-file-input__choose">choose from folder</span>
         </div>
+        {file && (
+          <div className="usa-file-input__preview" aria-hidden>
+            {file.name}
+          </div>
+        )}
         <div className="usa-file-input__box" />
+        {error && (
+          <div className="usa-file-input__accepted-files-message">
+            This is not a valid file type.
+          </div>
+        )}
         <input
           id={id}
           className="usa-file-input__input"
           type="file"
           name={name}
           accept={accept}
-          multiple={multiple}
+          // multiple={multiple}
           disabled={disabled}
           aria-describedby={ariaDescribedBy}
-          onChange={onChange}
+          onChange={handleChange}
           onBlur={onBlur}
         />
       </div>
