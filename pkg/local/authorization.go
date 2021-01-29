6699f8f585a217e7675d94c522b8a1380446b2f9
Nelz Carpentier <nelz@truss.works>
2021-01-29 15:51:56 -0800

    local authN does not do authZ

--- pkg/local/authorization.go
+++ pkg/local/authorization.go
@@ -2,6 +2,7 @@ package local
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"strings"
 
@@ -22,54 +23,85 @@ type DevUserConfig struct {
 	JobCodes []string `json:"jobCodes"`
 }
 
-func authorizeMiddleware(logger *zap.Logger, next http.Handler, testEUAID string) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		logger.Info("Using local authorization middleware")
+func headerToPrincipal(header string, defaultEUAID string) (authn.Principal, error) {
+	if len(header) == 0 {
+		return nil, fmt.Errorf("no Authentication header present")
+	}
+	tokenParts := strings.Split(header, "Bearer ")
+	if len(tokenParts) < 2 {
+		return nil, fmt.Errorf("invalid Bearer in auth header")
+	}
+	devUserConfigJSON := tokenParts[1]
+	if devUserConfigJSON == "" {
+		return nil, fmt.Errorf("empty dev user config JSON")
+	}
 
-		if len(r.Header["Authorization"]) == 0 {
-			logger.Info("No Authentication header present")
-			next.ServeHTTP(w, r)
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		tokenParts := strings.Split(r.Header["Authorization"][0], "Bearer ")
-		if len(tokenParts) < 2 {
-			logger.Error("invalid Bearer in auth header")
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		devUserConfigJSON := tokenParts[1]
-		if devUserConfigJSON == "" {
-			logger.Error("empty dev user config JSON")
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		config := DevUserConfig{}
-		if parseErr := json.Unmarshal([]byte(devUserConfigJSON), &config); parseErr != nil {
-			// Assume at this point that we've opted for Okta login on the frontend.
-			euaID := defaultTestEUAID
-			if testEUAID != "" {
-				euaID = testEUAID
-			}
-			logger.Info("Using local authorization middleware with Okta frontend login")
-			ctx := appcontext.WithPrincipal(r.Context(), &authn.EUAPrincipal{EUAID: euaID, JobCodeEASi: true, JobCodeGRT: true})
-			next.ServeHTTP(w, r.WithContext(ctx))
-			return
-		}
-
-		logger.Info("Using local authorization middleware and populating EUA ID and job codes")
-		ctx := appcontext.WithPrincipal(r.Context(), &authn.EUAPrincipal{
-			EUAID:       config.EUA,
+	config := DevUserConfig{}
+	if parseErr := json.Unmarshal([]byte(devUserConfigJSON), &config); parseErr != nil {
+		// if we can't parse the chunk of string after the "Bearer ",
+		// Assume at this point that we've opted for Okta login on the frontend.
+		// and therefore we inject a fully authorized Principal
+		return &authn.EUAPrincipal{
+			EUAID:       defaultEUAID,
 			JobCodeEASi: true,
-			JobCodeGRT:  swag.ContainsStrings(config.JobCodes, "EASI_D_GOVTEAM")})
-		next.ServeHTTP(w, r.WithContext(ctx))
+			JobCodeGRT:  true,
+		}, fmt.Errorf("default principal with Okta frontend login")
+	}
+
+	return &authn.EUAPrincipal{
+		EUAID:       config.EUA,
+		JobCodeEASi: true,
+		JobCodeGRT:  swag.ContainsStrings(config.JobCodes, "EASI_D_GOVTEAM"),
+	}, fmt.Errorf("populating EUA ID and job codes")
+}
+
+func authorizeMiddleware(next http.Handler, defaultEUAID string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+
+		// the AUTHENTICATION layer's responsibility is to answer "Do I
+		// know who this is?", and if it can figure that out, it puts the
+		// principal onto the context. Otherwise, it just lets the request
+		// continue on.
+		// (The responsibility to answer "is this requester allowed to
+		// make this request" is the responsibility of a later
+		// AUTHORIZATION layer)
+		p, err := headerToPrincipal(r.Header.Get("Authorization"), defaultEUAID)
+		if err != nil {
+			appcontext.ZLogger(ctx).Info("LOCAL AUTH MIDDLEWARE", zap.Error(err))
+		}
+		if p != nil {
+			// principal goes on the context
+			ctx = appcontext.WithPrincipal(ctx, p)
+
+			// decorate logs with the principal info
+			logger := appcontext.ZLogger(ctx).
+				With(zap.String("user", p.ID())).
+				With(zap.Bool("grt", p.AllowGRT()))
+			ctx = appcontext.WithLogger(ctx, logger)
+
+			// new principal and loggers now available to all the
+			// downstream parts of the chain via the decorated request
+			r = r.WithContext(ctx)
+		}
+		next.ServeHTTP(w, r)
 	})
 }
 
 // NewLocalAuthorizeMiddleware stubs out context info while ignoring remote authorization
 func NewLocalAuthorizeMiddleware(logger *zap.Logger, testEUAID string) func(http.Handler) http.Handler {
+	// we log here at ERROR level, because if this code is running
+	// in any of our deployed environments, we want to be
+	// VERY LOUD (i.e. send alerts) about it
+	logger.Error("Using LOCAL AUTHENTICATION middleware!!")
+
+	// calculate whatever EUA ID will be used when we fallback
+	euaID := defaultTestEUAID
+	if testEUAID != "" {
+		euaID = testEUAID
+	}
+
 	return func(next http.Handler) http.Handler {
-		return authorizeMiddleware(logger, next, testEUAID)
+		return authorizeMiddleware(next, euaID)
 	}
 }
