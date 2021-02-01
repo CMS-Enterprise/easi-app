a2e03a65ab7e58053d8121df62cbe0fe7b93fcb2
Isaac Garfinkle <isaac@truss.works>
2021-02-01 11:35:43 -0800

    Fix uuid unmarshaler, now errors correctly

--- pkg/graph/model/uuid.go
+++ pkg/graph/model/uuid.go
@@ -14,10 +14,13 @@ func MarshalUUID(id uuid.UUID) graphql.Marshaler {
 
 // UnmarshalUUID allows uuid to be unmarshalled by graphql
 func UnmarshalUUID(v interface{}) (uuid.UUID, error) {
-	if idAsString, ok := v.(string); ok {
-		if id, err := uuid.Parse(idAsString); err != nil {
-			return id, nil
-		}
+	idAsString, ok := v.(string)
+	if !ok {
+		return uuid.Nil, errors.New("id should be a valid UUID")
 	}
-	return uuid.Nil, errors.New("id should be a valid UUID")
+	id, err := uuid.Parse(idAsString)
+	if err != nil {
+		return uuid.Nil, err
+	}
+	return id, nil
 }
