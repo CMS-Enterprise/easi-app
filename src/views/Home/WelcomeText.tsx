5314ab40a54bf9c1fd4b7135737981e2996ddc90
Christopher Hui <christopher@truss.works>
2021-02-05 15:46:19 -0800

    Add plain info component

--- src/views/Home/WelcomeText.tsx
+++ src/views/Home/WelcomeText.tsx
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 import { useOktaAuth } from '@okta/okta-react';
 import { Link as UswdsLink } from '@trussworks/react-uswds';
 
+import PlainInfo from 'components/PlainInfo';
+
 const WelcomeText = () => {
   const { t } = useTranslation();
   const { authState } = useOktaAuth();
@@ -14,23 +16,18 @@ const WelcomeText = () => {
       <p className="line-height-body-5 font-body-lg text-light">
         {t('home:subtitle')}
       </p>
-      <div className="easi-home__info-wrapper">
-        <div className="easi-home__info-icon">
-          <i className="fa fa-info" />
-        </div>
-        <p className="line-height-body-5">
-          <Trans i18nKey="home:easiInfo">
-            zeroIndex
-            <a
-              href="https://share.cms.gov/Office/OIT/CIOCorner/Lists/Intake/NewForm.aspx"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              localeLink
-            </a>
-          </Trans>
-        </p>
-      </div>
+      <PlainInfo>
+        <Trans i18nKey="home:easiInfo">
+          zeroIndex
+          <a
+            href="https://share.cms.gov/Office/OIT/CIOCorner/Lists/Intake/NewForm.aspx"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            localeLink
+          </a>
+        </Trans>
+      </PlainInfo>
       {authState.isAuthenticated ? (
         <UswdsLink
           className="usa-button"
