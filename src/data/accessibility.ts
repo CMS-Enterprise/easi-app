e61f3ff3ec970e06bb4464336f5697231fea6236
Christopher Hui <christopher@truss.works>
2021-02-08 13:23:07 -0800

    wip add 508 request form

---
+++ src/data/accessibility.ts
@@ -0,0 +1,11 @@
+// Data for the Accessibility/508 process
+
+// eslint-disable-next-line
+export const initialAccessibilityRequestFormData = {
+  projectId: '',
+  businessOwner: {
+    name: '',
+    component: ''
+  },
+  lifecycleId: ''
+};
--- src/views/Accessibility/AccessibiltyRequest/Create/index.tsx
+++ src/views/Accessibility/AccessibiltyRequest/Create/index.tsx
@@ -1,34 +1,136 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
-import assert from 'assert';
-import CreateAccessibilityRequestQuery from 'queries/CreateAccessibilityRequestQuery';
-import * as yup from 'yup';
+import { useTranslation } from 'react-i18next';
+import { Button } from '@trussworks/react-uswds';
+import {
+  Field as FormikField,
+  Form as FormikForm,
+  Formik
+  // FormikProps
+} from 'formik';
 
-import { MutationField, MutationForm } from 'components/MutationForm';
-import { CreateAccessibilityRequestInput } from 'types/graphql-global-types';
-
-const CreateAccessibilityRequestFormSchema = yup.object({
-  name: yup
-    .string()
-    .trim()
-    .required('Enter a name for this request')
-    .default('')
-});
+import Footer from 'components/Footer';
+import Header from 'components/Header';
+import MainContent from 'components/MainContent';
+import PageWrapper from 'components/PageWrapper';
+import PlainInfo from 'components/PlainInfo';
+// import { ErrorAlert, ErrorAlertMessage } from 'components/shared/ErrorAlert';
+import FieldErrorMsg from 'components/shared/FieldErrorMsg';
+import FieldGroup from 'components/shared/FieldGroup';
+import HelpText from 'components/shared/HelpText';
+import Label from 'components/shared/Label';
+import { NavLink, SecondaryNav } from 'components/shared/SecondaryNav';
+import TextField from 'components/shared/TextField';
+import { initialAccessibilityRequestFormData } from 'data/accessibility';
+// import CreateAccessibilityRequestQuery from 'queries/CreateAccessibilityRequestQuery';
+import accessibilitySchema from 'validations/accessibilitySchema';
 
 const Create = () => {
-  // Move this into the MutationForm?
-  const req: CreateAccessibilityRequestInput = CreateAccessibilityRequestFormSchema.getDefaultFromShape();
-  assert(req);
+  const { t } = useTranslation('accessibility');
 
+  // return (
+  //   <div className="margin-left-3">
+  //     <MutationForm
+  //       mutation={CreateAccessibilityRequestQuery}
+  //       schema={CreateAccessibilityRequestFormSchema}
+  //     >
+  //       <MutationField name="name" label="Name" />
+  //     </MutationForm>
+  //   </div>
+  // );
   return (
-    <div className="margin-left-3">
-      <MutationForm
-        mutation={CreateAccessibilityRequestQuery}
-        schema={CreateAccessibilityRequestFormSchema}
-      >
-        <MutationField name="name" label="Name" />
-      </MutationForm>
-    </div>
+    <PageWrapper>
+      <Header />
+      <MainContent className="margin-bottom-5">
+        <SecondaryNav>
+          <NavLink to="/508/requests/new">
+            {t('tabs.accessibilityRequests')}
+          </NavLink>
+        </SecondaryNav>
+        <div className="grid-container">
+          {/* TODO: Use PageHeading component */}
+          <h1 className="margin-top-6 margin-bottom-5">Add a new request</h1>
+          <Formik
+            initialValues={initialAccessibilityRequestFormData}
+            onSubmit={() => {
+              console.log('submitting');
+            }}
+            validationSchema={accessibilitySchema.requestForm}
+            validateOnBlur={false}
+            validateOnChange={false}
+            validateOnMount={false}
+          >
+            {() => {
+              return (
+                <div className="margin-bottom-7">
+                  <FormikForm>
+                    <FieldGroup scrollElement="requester.name" error={false}>
+                      <Label htmlFor="508Request-BusinessOwnerName">
+                        Business Owner Name
+                      </Label>
+                      <FieldErrorMsg />
+                      <FormikField
+                        as={TextField}
+                        error={false}
+                        id="508Request-BusinessOwnerName"
+                        maxLength={50}
+                        name="businessOwner.name"
+                        disabled
+                      />
+                    </FieldGroup>
+
+                    <FieldGroup
+                      scrollElement="businessOwner.component"
+                      error={false}
+                    >
+                      <Label htmlFor="508Form-BusinessOwnerComponent">
+                        Business Owner Component
+                      </Label>
+                      <FieldErrorMsg />
+
+                      <FormikField
+                        as={TextField}
+                        key="508Form-BusinessOwnerComponent"
+                        name="businessOwner.component"
+                        disabled
+                      />
+                    </FieldGroup>
+                    <FieldGroup scrollElement="requestName" error={false}>
+                      <Label htmlFor="508Request-RequestName">
+                        Request Name
+                      </Label>
+                      <HelpText id="508Request-RequestName">
+                        This name will be shown on the Active requests page. For
+                        example, ACME 1.3
+                      </HelpText>
+                      <FieldErrorMsg />
+                      <FormikField
+                        as={TextField}
+                        error={false}
+                        id="508Request-RequestName"
+                        maxLength={50}
+                        name="requestName"
+                      />
+                    </FieldGroup>
+
+                    <div className="tablet:grid-col-8">
+                      <PlainInfo>
+                        A request for 508 testing will be added to the list of
+                        508 requests. An email will be sent to the Business
+                        Owner and the 508 team stating that a request has been
+                        added to the system.
+                      </PlainInfo>
+                    </div>
+                    <Button type="submit">Add a new request</Button>
+                  </FormikForm>
+                </div>
+              );
+            }}
+          </Formik>
+        </div>
+      </MainContent>
+      <Footer />
+    </PageWrapper>
   );
 };
 
