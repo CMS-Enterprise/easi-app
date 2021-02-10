13a92f546e6178939f53206ff8f5ae0ce6176ea4
Sara Bocciardi <sara@truss.works>
2021-02-10 11:56:08 -0800

    More table work.

--- src/views/Accessibility/AccessibiltyRequest/Documents/index.tsx
+++ src/views/Accessibility/AccessibiltyRequest/Documents/index.tsx
@@ -1,8 +1,8 @@
 import React, { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
-import { Link as UswdsLink } from '@trussworks/react-uswds';
-// import { useTable } from 'react-table';
+import { useTable } from 'react-table';
+import { Link as UswdsLink, Table } from '@trussworks/react-uswds';
 import { DateTime } from 'luxon';
 
 type Document = {
@@ -12,9 +12,13 @@ type Document = {
 
 type DocumentsListProps = {
   documents: Document[];
+  requestName: string;
 };
 
-const AccessibilityDocumentsList = ({ documents }: DocumentsListProps) => {
+const AccessibilityDocumentsList = ({
+  documents,
+  requestName
+}: DocumentsListProps) => {
   const { t } = useTranslation('accessibility');
 
   const columns: any = useMemo(() => {
@@ -43,26 +47,86 @@ const AccessibilityDocumentsList = ({ documents }: DocumentsListProps) => {
             >
               {t('documentTable.view')}
             </UswdsLink>
-            {/* eslint-disable-next-line prettier/prettier */}
-            { }
+            <span className="usa-sr-only">{row.original.name}</span>
             <UswdsLink
               asCustom={Link}
               to={`/some-508-request/${row.original.name}`}
+              className="margin-left-2"
             >
               {t('documentTable.remove')}
             </UswdsLink>
+            <span className="usa-sr-only">{row.original.name}</span>
           </>
         )
       }
     ];
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  console.log(columns);
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow
+  } = useTable({
+    columns,
+    data: documents,
+    documents,
+    initialState: {
+      sortBy: [{ id: 'uploadedAt', desc: true }]
+    }
+  });
 
   if (documents.length === 0) {
     return <span>{t('requestDetails.documents.none')}</span>;
   }
-  return <p>{JSON.stringify(documents)}</p>;
+
+  return (
+    <Table bordered={false} {...getTableProps()} fullWidth>
+      <caption className="usa-sr-only">
+        {`${t('documentTable.caption')} ${requestName}`}
+      </caption>
+      <thead>
+        {headerGroups.map(headerGroup => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <th style={{ whiteSpace: 'nowrap' }} scope="col">
+                {column.render('Header')}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map(row => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell, i) => {
+                if (i === 0) {
+                  return (
+                    <th
+                      {...cell.getCellProps()}
+                      scope="row"
+                      style={{ maxWidth: '16rem' }}
+                    >
+                      {cell.render('Cell')}
+                    </th>
+                  );
+                }
+                return (
+                  <td {...cell.getCellProps()} style={{ maxWidth: '16rem' }}>
+                    {cell.render('Cell')}
+                  </td>
+                );
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </Table>
+  );
 };
 
 export default AccessibilityDocumentsList;
