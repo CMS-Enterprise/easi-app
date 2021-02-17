120462924245c65435eaabb05e2972e382a3d4fd
Christopher Hui <christopher@truss.works>
2021-02-16 17:14:19 -0800

    move confirmation message to its own component

---
+++ src/components/ConfirmationMessage/index.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import ConfirmationMessage from './index';
+
+export default {
+  title: 'Confirmation Message',
+  component: ConfirmationMessage
+};
+
+export const Default = () => (
+  <ConfirmationMessage>
+    Procurement System Mega 4000 has been created.
+  </ConfirmationMessage>
+);
