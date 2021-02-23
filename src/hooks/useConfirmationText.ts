5b824f3a24774b3bdc27d41ed1bb4a9404ff5029
Christopher Hui <christopher@truss.works>
2021-02-23 09:38:31 -0800

    update setter name and add documentation

--- src/hooks/useConfirmationText.ts
+++ src/hooks/useConfirmationText.ts
@@ -1,14 +1,23 @@
 import { useEffect, useState } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
 
+// useConfirmationText is a hook used to get the confirmation text
+// passed through react router state.
+// const confirmationText = useConfirmationText();
+//
+// Two common ways to pass state through react-router (there are more)
+// history.push('/some/url', { confirmationText: 'Confirmed' });
+// or
+// const location = { pathname: '/some/url', state: { confirmationText: 'Confirmed' }};
+// <Link to={location} />
 function useConfirmationText() {
   const location = useLocation<any>();
   const history = useHistory();
-  const [confirmationText, setIsConfirmationText] = useState('');
+  const [confirmationText, setConfirmationText] = useState('');
 
   useEffect(() => {
     if (location.state && location.state.confirmationText) {
-      setIsConfirmationText(location.state.confirmationText);
+      setConfirmationText(location.state.confirmationText);
       history.replace({
         state: {}
       });
