dffb469e99b13d6a70c57b531b9ba3ecea05be47
Isaac Garfinkle <isaac@truss.works>
2021-02-24 14:46:18 -0800

    Fix some width issues

--- src/views/Accessibility/AccessibiltyRequest/Documents/New/index.tsx
+++ src/views/Accessibility/AccessibiltyRequest/Documents/New/index.tsx
@@ -20,6 +20,7 @@ import FileUpload from 'components/FileUpload';
 import Footer from 'components/Footer';
 import Header from 'components/Header';
 import MainContent from 'components/MainContent';
+import PageHeading from 'components/PageHeading';
 import PageWrapper from 'components/PageWrapper';
 import FieldErrorMsg from 'components/shared/FieldErrorMsg';
 import FieldGroup from 'components/shared/FieldGroup';
@@ -175,125 +176,139 @@ const New = () => {
               const { errors, setFieldValue, values } = formikProps;
               const flatErrors = flattenErrors(errors);
               return (
-                <Form onSubmit={formikProps.handleSubmit}>
-                  <h1>
+                <>
+                  <PageHeading>
                     Upload a document to {data?.accessibilityRequest?.name}
-                  </h1>
-                  <Label htmlFor="file-upload">
-                    Choose a document to upload
-                    <FileUpload
-                      id="file-upload"
-                      name="file-upload"
-                      accept=".pdf,.txt"
-                      onChange={onChange}
-                      onBlur={() => {}}
-                      ariaDescribedBy=""
-                    />
-                  </Label>
-                  {selectedFile && (
-                    <FieldGroup
-                      scrollElement="documentType.commonType"
-                      error={!!flatErrors['documentType.commonType']}
-                    >
-                      <fieldset className="usa-fieldset margin-top-4">
-                        <legend className="usa-label margin-bottom-1">
-                          What type of document are you uploading?
-                        </legend>
-
-                        <FieldErrorMsg>
-                          {flatErrors['documentType.commonType']}
-                        </FieldErrorMsg>
-                        {([
-                          'AWARDED_VPAT',
-                          'TEST_PLAN',
-                          'TESTING_VPAT',
-                          'TEST_RESULTS',
-                          'REMEDIATION_PLAN'
-                        ] as AccessibilityRequestDocumentCommonType[]).map(
-                          commonType => {
-                            return (
-                              <Field
-                                key={`FileUpload-CommonType${commonType}`}
-                                as={RadioField}
-                                checked={
-                                  values.documentType.commonType === commonType
-                                }
-                                id={`FileUpload-CommonType${commonType}`}
-                                name="documentType.commonType"
-                                label={getDocumentTypeLabel(commonType)}
-                                onChange={() => {
-                                  setFieldValue(
-                                    'documentType.commonType',
-                                    commonType
-                                  );
-                                  setFieldValue('documentType.otherType', '');
-                                }}
-                                value={commonType}
-                              />
-                            );
-                          }
-                        )}
-                        <Field
-                          as={RadioField}
-                          checked={values.documentType.commonType === 'OTHER'}
-                          id="FileUpload-CommonTypeOTHER"
-                          name="documentType.commonType"
-                          label={getDocumentTypeLabel(
-                            AccessibilityRequestDocumentCommonType.OTHER
-                          )}
-                          onChange={() => {
-                            setFieldValue('documentType.commonType', 'OTHER');
-                          }}
-                          value="OTHER"
+                  </PageHeading>
+                  <div className="grid-col-9">
+                    <Form onSubmit={formikProps.handleSubmit}>
+                      <Label htmlFor="file-upload">
+                        Choose a document to upload
+                        <FileUpload
+                          id="file-upload"
+                          name="file-upload"
+                          accept=".pdf,.txt"
+                          onChange={onChange}
+                          onBlur={() => {}}
+                          ariaDescribedBy=""
                         />
-                        {values.documentType.commonType === 'OTHER' && (
-                          <div className="width-card-lg margin-left-4 margin-bottom-1">
-                            <FieldGroup
-                              scrollElement="documentType.otherType"
-                              error={!!flatErrors['documentType.otherType']}
-                            >
-                              <Label
-                                htmlFor="FileUpload-OtherType"
-                                className="margin-bottom-1"
-                                style={{ marginTop: '0.5em' }}
-                              >
-                                Document name
-                              </Label>
-                              <FieldErrorMsg>
-                                {flatErrors['documentType.otherType']}
-                              </FieldErrorMsg>
-                              <Field
-                                as={TextField}
-                                error={!!flatErrors['documentType.otherType']}
-                                className="margin-top-0"
-                                id="DocumentType-OtherType"
-                                name="documentType.otherType"
-                              />
-                            </FieldGroup>
-                          </div>
-                        )}
-                      </fieldset>
-                    </FieldGroup>
-                  )}
-                  {selectedFile && (
-                    <div className="padding-top-2">
-                      <p>
-                        To keep CMS safe, documents are scanned for viruses
-                        after uploading. If something goes wrong, we&apos;ll let
-                        you know.
-                      </p>
-                      <Button
-                        type="submit"
-                        disabled={
-                          generateURLStatus.loading ||
-                          createDocumentStatus.loading
-                        }
-                      >
-                        Upload document
-                      </Button>
-                    </div>
-                  )}
-                </Form>
+                      </Label>
+                      {selectedFile && (
+                        <FieldGroup
+                          scrollElement="documentType.commonType"
+                          error={!!flatErrors['documentType.commonType']}
+                        >
+                          <fieldset className="usa-fieldset margin-top-4">
+                            <legend className="usa-label margin-bottom-1">
+                              What type of document are you uploading?
+                            </legend>
+                            <FieldErrorMsg>
+                              {flatErrors['documentType.commonType']}
+                            </FieldErrorMsg>
+                            {([
+                              'AWARDED_VPAT',
+                              'TEST_PLAN',
+                              'TESTING_VPAT',
+                              'TEST_RESULTS',
+                              'REMEDIATION_PLAN'
+                            ] as AccessibilityRequestDocumentCommonType[]).map(
+                              commonType => {
+                                return (
+                                  <Field
+                                    key={`FileUpload-CommonType${commonType}`}
+                                    as={RadioField}
+                                    checked={
+                                      values.documentType.commonType ===
+                                      commonType
+                                    }
+                                    id={`FileUpload-CommonType${commonType}`}
+                                    name="documentType.commonType"
+                                    label={getDocumentTypeLabel(commonType)}
+                                    onChange={() => {
+                                      setFieldValue(
+                                        'documentType.commonType',
+                                        commonType
+                                      );
+                                      setFieldValue(
+                                        'documentType.otherType',
+                                        ''
+                                      );
+                                    }}
+                                    value={commonType}
+                                  />
+                                );
+                              }
+                            )}
+                            <Field
+                              as={RadioField}
+                              checked={
+                                values.documentType.commonType === 'OTHER'
+                              }
+                              id="FileUpload-CommonTypeOTHER"
+                              name="documentType.commonType"
+                              label={getDocumentTypeLabel(
+                                AccessibilityRequestDocumentCommonType.OTHER
+                              )}
+                              onChange={() => {
+                                setFieldValue(
+                                  'documentType.commonType',
+                                  'OTHER'
+                                );
+                              }}
+                              value="OTHER"
+                            />
+                            {values.documentType.commonType === 'OTHER' && (
+                              <div className="width-card-lg margin-left-4 margin-bottom-1">
+                                <FieldGroup
+                                  scrollElement="documentType.otherType"
+                                  error={!!flatErrors['documentType.otherType']}
+                                >
+                                  <Label
+                                    htmlFor="FileUpload-OtherType"
+                                    className="margin-bottom-1"
+                                    style={{ marginTop: '0.5em' }}
+                                  >
+                                    Document name
+                                  </Label>
+                                  <FieldErrorMsg>
+                                    {flatErrors['documentType.otherType']}
+                                  </FieldErrorMsg>
+                                  <Field
+                                    as={TextField}
+                                    error={
+                                      !!flatErrors['documentType.otherType']
+                                    }
+                                    className="margin-top-0"
+                                    id="DocumentType-OtherType"
+                                    name="documentType.otherType"
+                                  />
+                                </FieldGroup>
+                              </div>
+                            )}
+                          </fieldset>
+                        </FieldGroup>
+                      )}
+                      {selectedFile && (
+                        <div className="padding-top-2">
+                          <p>
+                            To keep CMS safe, documents are scanned for viruses
+                            after uploading. If something goes wrong, we&apos;ll
+                            let you know.
+                          </p>
+                          <Button
+                            type="submit"
+                            disabled={
+                              generateURLStatus.loading ||
+                              createDocumentStatus.loading
+                            }
+                          >
+                            Upload document
+                          </Button>
+                        </div>
+                      )}
+                    </Form>
+                  </div>
+                </>
               );
             }}
           </Formik>
