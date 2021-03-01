eeba9aa871a5beeb1c44434f057c1641a2460a01
Christopher Hui <christopher@truss.works>
2021-03-01 08:55:47 -0800

    update 508 request table sort

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,6 +1,17 @@
 import { DateTime } from 'luxon';
 
-const formatDate = (date: string) =>
-  DateTime.fromISO(date).toFormat('MMMM d yyyy');
+const formatDate = (date: string | DateTime) => {
+  // ISO String
+  if (typeof date === 'string') {
+    return DateTime.fromISO(date).toFormat('MMMM d yyyy');
+  }
+
+  // luxon DateTime
+  if (date instanceof DateTime) {
+    return date.toFormat('MMMM d yyyy');
+  }
+
+  return '';
+};
 
 export default formatDate;
