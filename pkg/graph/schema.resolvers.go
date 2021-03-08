59c118a65eac0d61f09fbb68f9ba64b4c005e794
Sara Bocciardi <sara@truss.works>
2021-03-08 11:32:35 -0800

    Add enum for request type.

--- pkg/graph/schema.resolvers.go
+++ pkg/graph/schema.resolvers.go
@@ -372,10 +372,6 @@ func (r *systemIntakeResolver) RejectionReason(ctx context.Context, obj *models.
 	panic(fmt.Errorf("not implemented"))
 }
 
-func (r *systemIntakeResolver) RequestType(ctx context.Context, obj *models.SystemIntake) (string, error) {
-	panic(fmt.Errorf("not implemented"))
-}
-
 func (r *systemIntakeResolver) Solution(ctx context.Context, obj *models.SystemIntake) (*string, error) {
 	panic(fmt.Errorf("not implemented"))
 }
@@ -416,3 +412,13 @@ type accessibilityRequestDocumentResolver struct{ *Resolver }
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
 type systemIntakeResolver struct{ *Resolver }
+
+// !!! WARNING !!!
+// The code below was going to be deleted when updating resolvers. It has been copied here so you have
+// one last chance to move it out of harms way if you want. There are two reasons this happens:
+//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
+//    it when you're done.
+//  - You have helper methods in this file. Move them out to keep these resolver files clean.
+func (r *systemIntakeResolver) RequestType(ctx context.Context, obj *models.SystemIntake) (string, error) {
+	panic(fmt.Errorf("not implemented"))
+}
