c59e21c3194084257f2594509b0749822bd96e11
Isaac Garfinkle <isaac@truss.works>
2021-03-24 13:55:21 -0700

    remove deletable code

--- pkg/graph/schema.resolvers.go
+++ pkg/graph/schema.resolvers.go
@@ -763,13 +763,3 @@ type businessCaseResolver struct{ *Resolver }
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
 type systemIntakeResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//    it when you're done.
-//  - You have helper methods in this file. Move them out to keep these resolver files clean.
-func (r *queryResolver) GrtFeedbacks(ctx context.Context, intakeID uuid.UUID) ([]*models.GRTFeedback, error) {
-	panic(fmt.Errorf("not implemented"))
-}
