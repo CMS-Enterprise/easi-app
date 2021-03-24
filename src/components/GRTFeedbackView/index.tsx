950c9fcfcf2f1eea1a18d047c33610995af4f208
Isaac Garfinkle <isaac@truss.works>
2021-03-23 17:30:23 -0700

    separate out reused code

--- src/components/GRTFeedbackView/index.tsx
+++ src/components/GRTFeedbackView/index.tsx
@@ -12,43 +12,47 @@ type GRTFeedbackViewProps = {
 const GRTFeedbackView = ({ grtFeedbacks }: GRTFeedbackViewProps) => {
   const { t } = useTranslation('businessCase');
 
+  const feedbacksForGRB = grtFeedbacks.filter(
+    grtFeedback => grtFeedback.feedbackType === 'GRB'
+  );
+  const feedbacksForBusinessOwner = grtFeedbacks.filter(
+    grtFeedback => grtFeedback.feedbackType === 'BUSINESS_OWNER'
+  );
+
+  const formatGRTFeedback = (feedback: GRTFeedback) => {
+    return (
+      <div className="margin-bottom-3">
+        <div className="text-bold">
+          {DateTime.fromISO(feedback.createdAt).toLocaleString(
+            DateTime.DATE_MED
+          )}
+        </div>
+        <p className="margin-top-1 line-height-body-3">{feedback.feedback}</p>
+      </div>
+    );
+  };
+
   return (
     <>
       <h2 className="margin-bottom-3 margin-top-0">
         {t('grtFeedback.header')}
       </h2>
-      {grtFeedbacks.some(grtFeedback => grtFeedback.feedbackType === 'GRB') && (
+      {feedbacksForGRB.length > 0 && (
         <div>
           <h3 className="margin-bottom-1">{t('grtFeedback.grbSubhead')}</h3>
           <HelpText className="margin-bottom-2">
             {t('grtFeedback.grbHelpText')}
           </HelpText>
-          {grtFeedbacks
-            .filter(grtFeedback => grtFeedback.feedbackType === 'GRB')
+          {feedbacksForGRB
             .sort(
               (a, b) =>
                 DateTime.fromISO(a.createdAt).toMillis() -
                 DateTime.fromISO(b.createdAt).toMillis()
             )
-            .map(grtFeedback => {
-              return (
-                <div>
-                  <div className="text-bold">
-                    {DateTime.fromISO(grtFeedback.createdAt).toLocaleString(
-                      DateTime.DATE_MED
-                    )}
-                  </div>
-                  <p className="margin-bottom-3 margin-top-1 line-height-body-3">
-                    {grtFeedback.feedback}
-                  </p>
-                </div>
-              );
-            })}
+            .map(grtFeedback => formatGRTFeedback(grtFeedback))}
         </div>
       )}
-      {grtFeedbacks.some(
-        grtFeedback => grtFeedback.feedbackType === 'BUSINESS_OWNER'
-      ) && (
+      {feedbacksForBusinessOwner.length > 0 && (
         <div>
           <h3 className="margin-bottom-1">
             {t('grtFeedback.businessOwnerSubhead')}
@@ -56,29 +60,13 @@ const GRTFeedbackView = ({ grtFeedbacks }: GRTFeedbackViewProps) => {
           <HelpText className="margin-bottom-2">
             {t('grtFeedback.businessOwnerHelpText')}
           </HelpText>
-          {grtFeedbacks
-            .filter(
-              grtFeedback => grtFeedback.feedbackType === 'BUSINESS_OWNER'
-            )
+          {feedbacksForBusinessOwner
             .sort(
               (a, b) =>
                 DateTime.fromISO(a.createdAt).toMillis() -
                 DateTime.fromISO(b.createdAt).toMillis()
             )
-            .map(grtFeedback => {
-              return (
-                <div>
-                  <div className="text-bold">
-                    {DateTime.fromISO(grtFeedback.createdAt).toLocaleString(
-                      DateTime.DATE_MED
-                    )}
-                  </div>
-                  <p className="margin-bottom-3 margin-top-1 line-height-body-3">
-                    {grtFeedback.feedback}
-                  </p>
-                </div>
-              );
-            })}
+            .map(grtFeedback => formatGRTFeedback(grtFeedback))}
         </div>
       )}
     </>
