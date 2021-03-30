13afb2c35a9c94bc65c49f5643c927473f0fb758
Nelz Carpentier <nelz@truss.works>
2021-03-30 12:40:50 -0700

    EASI-1134 instantiate and use new client

--- pkg/server/routes.go
+++ pkg/server/routes.go
@@ -24,6 +24,7 @@ import (
 	"github.com/cmsgov/easi-app/pkg/appvalidation"
 	"github.com/cmsgov/easi-app/pkg/cedar/cedareasi"
 	"github.com/cmsgov/easi-app/pkg/cedar/cedarldap"
+	cedarintake "github.com/cmsgov/easi-app/pkg/cedar/intake"
 	"github.com/cmsgov/easi-app/pkg/email"
 	"github.com/cmsgov/easi-app/pkg/flags"
 	"github.com/cmsgov/easi-app/pkg/graph"
@@ -64,6 +65,18 @@ func (s *Server) routes(
 	}
 
 	// set up CEDAR client
+	publisher := cedarintake.NewClient(
+		s.Config.GetString(appconfig.CEDARAPIURL),
+		s.Config.GetString(appconfig.CEDARAPIKey),
+		ldClient,
+	)
+	if s.environment.Deployed() {
+		s.NewCEDARClientCheck()
+		if cerr := publisher.CheckConnection(context.Background()); cerr != nil {
+			s.logger.Info("Non-Fatal - Failed to connect to CEDAR Intake API on startup", zap.Error(cerr))
+		}
+	}
+
 	var cedarEasiClient cedareasi.Client = local.NewCedarEasiClient()
 	if !(s.environment.Local() || s.environment.Test()) {
 		// check we have all of the configs for CEDAR clients
@@ -78,6 +91,7 @@ func (s *Server) routes(
 			s.CheckCEDAREasiClientConnection(cedarEasiClient)
 		}
 	}
+	_ = cedarEasiClient
 
 	var cedarLDAPClient cedarldap.Client
 	cedarLDAPClient = cedarldap.NewTranslatedClient(
@@ -317,7 +331,12 @@ func (s *Server) routes(
 					serviceConfig,
 					services.NewAuthorizeUserIsIntakeRequester(),
 					store.UpdateSystemIntake,
-					cedarEasiClient.ValidateAndSubmitSystemIntake,
+					func(c context.Context, si *models.SystemIntake) (string, error) {
+						// quick adapter to retrofit the new interface to take the place
+						// of the old interface
+						err := publisher.PublishSnapshot(c, si, nil, nil, nil, nil)
+						return "", err
+					},
 					saveAction,
 					emailClient.SendSystemIntakeSubmissionEmail,
 				),
