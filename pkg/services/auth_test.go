62567099973aa374ba39c0f3c097bb76a65b519e
Isaac Garfinkle <isaac@truss.works>
2021-04-19 12:39:19 -0700

    Add auth tests

--- pkg/services/auth_test.go
+++ pkg/services/auth_test.go
@@ -126,7 +126,7 @@ func (s ServicesTestSuite) TestAuthorizeRequireGRTJobCode() {
 	}
 }
 
-func (s ServicesTestSuite) NewAuthorizeUserIsIntakeRequesterOrHasGRTJobCode() {
+func (s ServicesTestSuite) TestAuthorizeUserIsIntakeRequesterOrHasGRTJobCode() {
 	fnAuth := AuthorizeUserIsIntakeRequesterOrHasGRTJobCode
 	nonEASI := authn.EUAPrincipal{EUAID: "FAKE", JobCodeEASi: false, JobCodeGRT: false}
 	nonGRT := authn.EUAPrincipal{EUAID: "FAKE", JobCodeEASi: true, JobCodeGRT: false}
@@ -159,6 +159,53 @@ func (s ServicesTestSuite) NewAuthorizeUserIsIntakeRequesterOrHasGRTJobCode() {
 		},
 		"is grt, is not author": {
 			ctx:     appcontext.WithPrincipal(context.Background(), &yesGRT),
+			intake:  &models.SystemIntake{EUAUserID: null.StringFrom("NOPE")},
+			allowed: true,
+		},
+	}
+
+	for name, tc := range testCases {
+		s.Run(name, func() {
+			ok, err := fnAuth(tc.ctx, tc.intake)
+			s.NoError(err)
+			s.Equal(tc.allowed, ok)
+		})
+	}
+}
+
+func (s ServicesTestSuite) TestAuthorizeUserIsRequestOwnerOr508Team() {
+	fnAuth := AuthorizeUserIsRequestOwnerOr508Team
+	nonEASI := authn.EUAPrincipal{EUAID: "FAKE", JobCodeEASi: false, JobCodeGRT: false}
+	non508 := authn.EUAPrincipal{EUAID: "FAKE", JobCodeEASi: true, JobCode508Tester: false, JobCode508User: false}
+	yes508tester := authn.EUAPrincipal{EUAID: "FAKE", JobCodeEASi: true, JobCode508Tester: true, JobCode508User: false}
+
+	testCases := map[string]struct {
+		ctx     context.Context
+		intake  *models.SystemIntake
+		allowed bool
+	}{
+		"anonymous": {
+			ctx:     context.Background(),
+			intake:  &models.SystemIntake{},
+			allowed: false,
+		},
+		"non easi": {
+			ctx:     appcontext.WithPrincipal(context.Background(), &nonEASI),
+			intake:  &models.SystemIntake{},
+			allowed: false,
+		},
+		"is not 508, is not author": {
+			ctx:     appcontext.WithPrincipal(context.Background(), &non508),
+			intake:  &models.SystemIntake{EUAUserID: null.StringFrom("NOPE")},
+			allowed: false,
+		},
+		"is author, is not 508": {
+			ctx:     appcontext.WithPrincipal(context.Background(), &non508),
+			intake:  &models.SystemIntake{EUAUserID: null.StringFrom("FAKE")},
+			allowed: true,
+		},
+		"is 508, is not author": {
+			ctx:     appcontext.WithPrincipal(context.Background(), &yes508tester),
 			intake:  &models.SystemIntake{EUAUserID: null.StringFrom("NOPE")},
 			allowed: true,
 		},
