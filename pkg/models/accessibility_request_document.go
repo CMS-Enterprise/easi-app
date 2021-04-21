7f0dd46221bb6d2ffe9fdb2cf751152dad54aa66
Isaac Garfinkle <isaac@truss.works>
2021-04-21 14:32:46 -0700

    Rename archived to deleted

--- pkg/models/accessibility_request_document.go
+++ pkg/models/accessibility_request_document.go
@@ -93,5 +93,5 @@ type AccessibilityRequestDocument struct {
 	OtherType          string                                 `db:"other_type"`
 	CreatedAt          *time.Time                             `json:"createdAt" db:"created_at"`
 	UpdatedAt          *time.Time                             `json:"updatedAt" db:"updated_at"`
-	ArchivedAt         *time.Time                             `json:"archivedAt" db:"archived_at"`
+	DeletedAt          *time.Time                             `json:"deletedAt" db:"deleted_at"`
 }
