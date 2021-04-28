856e660fb760dc3a30eaae53510fabb18cb25022
Nick Downey <Nicholas.Downey@cms.hhs.gov>
2021-04-27 22:34:14 -0400

    Refactored 508 test date tests to support apollo client

--- src/components/TestDateCard/index.test.tsx
+++ src/components/TestDateCard/index.test.tsx
@@ -1,55 +1,61 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { MemoryRouter } from 'react-router-dom';
+import { MockedProvider } from '@apollo/client/testing';
+import { mount, shallow } from 'enzyme';
 import { DateTime } from 'luxon';
 
 import TestDateCard from 'components/TestDateCard';
 
-describe('The Test Date Card component', () => {
-  it('renders without crashing', () => {
-    shallow(
-      <TestDateCard
-        date={DateTime.local().toISO()}
-        type="INITIAL"
-        testIndex={1}
-        score={0}
-      />
-    );
-  });
-
-  describe('test score', () => {
-    it('renders score', () => {
-      const component = shallow(
+const renderComponent = (score: number | null) => {
+  return mount(
+    <MemoryRouter>
+      <MockedProvider>
         <TestDateCard
           date={DateTime.local().toISO()}
           type="INITIAL"
           testIndex={1}
-          score={1000}
+          score={score}
+          requestId="Request ID"
+          requestName="Initial Request"
+          id="ID"
+          refetchRequest={() => 0}
         />
-      );
-      expect(component.find('[data-testid="score"]').text()).toEqual('100.0%');
-    });
+      </MockedProvider>
+    </MemoryRouter>
+  );
+};
 
-    it('renders a score of 0', () => {
-      const component = shallow(
+describe('The Test Date Card component', () => {
+  it('renders without crashing', () => {
+    shallow(
+      <MockedProvider>
         <TestDateCard
           date={DateTime.local().toISO()}
           type="INITIAL"
           testIndex={1}
           score={0}
+          requestId="Request ID"
+          requestName="Initial Request"
+          id="ID"
+          refetchRequest={() => 0}
         />
-      );
+      </MockedProvider>
+    );
+  });
+
+  describe('test score', () => {
+    it('renders score', () => {
+      const component = renderComponent(1000);
+      expect(component.find('[data-testid="score"]').text()).toEqual('100.0%');
+    });
+
+    it('renders a score of 0', () => {
+      const component = renderComponent(0);
       expect(component.find('[data-testid="score"]').text()).toEqual('0%');
     });
 
     it('renders score not added', () => {
-      const component = shallow(
-        <TestDateCard
-          date={DateTime.local().toISO()}
-          type="INITIAL"
-          testIndex={1}
-          score={null}
-        />
-      );
+      const component = renderComponent(null);
       expect(component.find('[data-testid="score"]').text()).toEqual(
         'Score not added'
       );
