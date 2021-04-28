849707032adab4170cf3fddaa1e76e12edd4f69a
Christopher Hui <christopher@truss.works>
2021-04-28 10:49:01 -1000

    remove duplicate 508 types file

--- src/types/accessibility.ts
+++ src/types/accessibility.ts
@@ -7,3 +7,15 @@ export type AccessibilityRequestForm = {
   };
   requestName: string;
 };
+
+// Form for reviewer to add dates
+export type TestDateFormType = {
+  testType: 'INITIAL' | 'REMEDIATION' | null;
+  dateMonth: string;
+  dateYear: string;
+  dateDay: string;
+  score: {
+    isPresent: boolean | null;
+    value: string;
+  };
+};
