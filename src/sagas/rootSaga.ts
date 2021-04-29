635cd63122dadc2a7c9c7d4b4f1dd973aadd93f5
Isaac Garfinkle <isaac@truss.works>
2021-04-29 00:30:38 -0700

    Remove remnants of systems endpoint

--- src/sagas/rootSaga.ts
+++ src/sagas/rootSaga.ts
@@ -4,13 +4,11 @@ import actionSaga from 'sagas/actionSaga';
 import businessCaseSaga from 'sagas/businessCaseSaga';
 import businessCasesSaga from 'sagas/businessCasesSaga';
 import fileUploadSaga from 'sagas/fileSaga';
-import searchSaga from 'sagas/searchSaga';
 import systemIntakeSaga from 'sagas/systemIntakeSaga';
 import systemIntakesSaga from 'sagas/systemIntakesSaga';
 
 export default function* rootSaga() {
   yield all([
-    searchSaga(),
     systemIntakesSaga(),
     systemIntakeSaga(),
     businessCaseSaga(),
