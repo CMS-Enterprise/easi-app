d1b164c142f29b37638b88f8c9188ae1d110b495
Jim Benton <jim@truss.works>
2021-05-06 13:03:18 -0500

    Add docs for useMessage

--- src/hooks/useMessage.tsx
+++ src/hooks/useMessage.tsx
@@ -16,6 +16,8 @@ const MessageContext = createContext<
   | undefined
 >(undefined);
 
+// MessageProvider manages the state necessary for child components to
+// use the useMessage hook.
 const MessageProvider = ({ children }: { children: ReactNode }) => {
   const [queuedMessage, showMessageOnNextPage] = useState<string>();
   const [message, showMessage] = useState<string>();
@@ -44,6 +46,17 @@ const MessageProvider = ({ children }: { children: ReactNode }) => {
   );
 };
 
+// useMessage is a hook that provides the ability to show notifications to the
+// user. Components will use the hook to get access to two methods:
+//
+// showMessage: display a message to the user immediately. This is useful
+// when an interaction won't cause an immediate redirect to a new page.
+//
+// showMessageOnNextPage: schedule a message to be shown on the next
+// visited by the user. Pages are identified using their URL and therefore
+// messages won't appear until the URL changes.
+//
+// Currently there is only support for showing a single message.
 const useMessage = () => {
   const context = useContext(MessageContext);
   if (context === undefined) {
