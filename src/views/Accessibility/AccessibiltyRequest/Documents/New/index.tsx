9c73ad201b5b189b327cc166b2d188baf1c13808
Jim Benton <jim@truss.works>
2021-05-10 13:26:54 -0500

    Add additional context to document file input

--- src/views/Accessibility/AccessibiltyRequest/Documents/New/index.tsx
+++ src/views/Accessibility/AccessibiltyRequest/Documents/New/index.tsx
@@ -196,11 +196,19 @@ const New = () => {
                       as={FileUpload}
                       id="FileUpload-File"
                       name="file"
+                      ariaDescribedBy="FileUpload-Description"
                       onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
                         onChange(e);
                         setFieldValue('file', e.currentTarget?.files?.[0]);
                       }}
                     />
+                    <div
+                      id="FileUpload-Description"
+                      className="sr-only"
+                      tabIndex={-1}
+                    >
+                      Activate to select a file.
+                    </div>
                   </FieldGroup>
                   {values.file && (
                     <FieldGroup
