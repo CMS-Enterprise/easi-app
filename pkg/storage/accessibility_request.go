b732bbb3b48993802583a0686c58e65796373715
Sara Bocciardi <sara@truss.works>
2021-05-17 11:16:46 -0700

    Limit queries to the requests for a single user.

--- pkg/storage/accessibility_request.go
+++ pkg/storage/accessibility_request.go
@@ -94,6 +94,34 @@ func (s *Store) FetchAccessibilityRequests(ctx context.Context) ([]models.Access
 	return requests, nil
 }
 
+// FetchMyAccessibilityRequests queries the DB for an accessibility requests.
+// TODO implement cursor pagination
+func (s *Store) FetchMyAccessibilityRequests(ctx context.Context) ([]models.AccessibilityRequest, error) {
+	principal := appcontext.Principal(ctx)
+	EUAUserID := principal.ID()
+
+	requests := []models.AccessibilityRequest{}
+
+	requestsSQL := `SELECT * FROM accessibility_requests
+		WHERE deleted_at IS NULL
+		AND eua_user_id = $1
+	`
+
+	err := s.db.Select(&requests, requestsSQL, EUAUserID)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return requests, nil
+		}
+		appcontext.ZLogger(ctx).Error("Failed to fetch accessibility requests", zap.Error(err))
+		return nil, &apperrors.QueryError{
+			Err:       err,
+			Operation: apperrors.QueryFetch,
+		}
+	}
+
+	return requests, nil
+}
+
 // DeleteAccessibilityRequest marks an accessibility request as deleted
 func (s *Store) DeleteAccessibilityRequest(ctx context.Context, id uuid.UUID) error {
 	const archiveAccessibilityRequestSQL = `UPDATE accessibility_requests
