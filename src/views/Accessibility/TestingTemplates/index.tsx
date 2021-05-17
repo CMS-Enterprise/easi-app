8e742c57ab22ddae5dd6d7a410b082a826de9cdd
Kim Allen <kim@truss.works>
2021-05-17 15:27:56 -0700

    Pull sections out into variables for readability

--- src/views/Accessibility/TestingTemplates/index.tsx
+++ src/views/Accessibility/TestingTemplates/index.tsx
@@ -63,6 +63,101 @@ const TestingTemplates = () => {
       </ul>
     </div>
   );
+  const testPlanSection = (
+    <div>
+      <h2
+        id={`testingTemplates_${t('testingTemplates.testPlanSection.heading')}`}
+      >
+        {t('testingTemplates.testPlanSection.heading')}
+      </h2>
+      <p>{t('testingTemplates.testPlanSection.description')}</p>
+      <ul className="accessibility-testing-templates__test-plan-list">
+        {testPlanList.map(item => (
+          <li>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+  const remediationPlanSection = (
+    <div>
+      <h2
+        id={`testingTemplates_${t(
+          'testingTemplates.remediationPlanSection.heading'
+        )}`}
+      >
+        {t('testingTemplates.remediationPlanSection.heading')}
+      </h2>
+      <p>{t('testingTemplates.remediationPlanSection.description')}</p>
+      <ul className="accessibility-testing-templates__remediation-plan-list">
+        {remediationPlanList.map(item => (
+          <li>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+
+  const downloadVPAT = (
+    <div className="accessibility-testing-templates__downloadBox">
+      <h3 className="margin-top-0">
+        {t('testingTemplates.vpatSection.subSection.downloadVPAT.heading')}
+      </h3>
+      <p>
+        <Link
+          href="https://www.itic.org/policy/accessibility/vpat"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {t(
+            'testingTemplates.vpatSection.subSection.downloadVPAT.line1.linkText'
+          )}
+        </Link>
+        {` `}
+        {t(
+          'testingTemplates.vpatSection.subSection.downloadVPAT.line1.otherText'
+        )}
+      </p>
+      <p>
+        <i className="fa fa-exclamation-circle fa-2x" />
+        {` `}
+        {t('testingTemplates.vpatSection.subSection.downloadVPAT.line2.text')}
+      </p>
+      <p>
+        <Link
+          href="https://www.youtube.com/watch?v=kAkSV9xiJ1A"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {t(
+            'testingTemplates.vpatSection.subSection.downloadVPAT.line3.linkText'
+          )}
+        </Link>
+        {` `}
+        {t(
+          'testingTemplates.vpatSection.subSection.downloadVPAT.line3.otherText'
+        )}
+      </p>
+    </div>
+  );
+
+  const vpatSection = (
+    <div>
+      <h2 id={`testingTemplates_${t('testingTemplates.vpatSection.heading')}`}>
+        {t('testingTemplates.vpatSection.heading')}
+      </h2>
+      <p>{t('testingTemplates.vpatSection.description')}</p>
+      <h3>{t('testingTemplates.vpatSection.subSection.heading')}</h3>
+      <ul className="accessibility-testing-templates__vpat-list">
+        <li>{t('testingTemplates.vpatSection.subSection.item1.text')}</li>
+        {vpatConformanceLevels.map(level => (
+          <p>
+            <span className="text-bold">{level.name}</span> {level.description}
+          </p>
+        ))}
+        <li>{t('testingTemplates.vpatSection.subSection.item2.text')}</li>
+      </ul>
+      {downloadVPAT}
+    </div>
+  );
 
   return (
     <div className="grid-container accessibility-testing-templates">
@@ -80,102 +175,12 @@ const TestingTemplates = () => {
               {t('testingTemplates.heading')}
             </PageHeading>
             {tableOfContents}
-            <h2
-              id={`testingTemplates_${t(
-                'testingTemplates.vpatSection.heading'
-              )}`}
-            >
-              {t('testingTemplates.vpatSection.heading')}
-            </h2>
-            <p>{t('testingTemplates.vpatSection.description')}</p>
-            <h3>{t('testingTemplates.vpatSection.subSection.heading')}</h3>
-            <ul className="accessibility-testing-templates__vpat-list">
-              <li>{t('testingTemplates.vpatSection.subSection.item1.text')}</li>
-              {vpatConformanceLevels.map(level => (
-                <p>
-                  <span className="text-bold">{level.name}</span>{' '}
-                  {level.description}
-                </p>
-              ))}
-              <li>{t('testingTemplates.vpatSection.subSection.item2.text')}</li>
-            </ul>
-            <div className="accessibility-testing-templates__downloadBox">
-              <h3 className="margin-top-0">
-                {t(
-                  'testingTemplates.vpatSection.subSection.downloadVPAT.heading'
-                )}
-              </h3>
-              <p>
-                <Link
-                  href="https://www.itic.org/policy/accessibility/vpat"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {t(
-                    'testingTemplates.vpatSection.subSection.downloadVPAT.line1.linkText'
-                  )}
-                </Link>
-                {` `}
-                {t(
-                  'testingTemplates.vpatSection.subSection.downloadVPAT.line1.otherText'
-                )}
-              </p>
-              <p>
-                <i className="fa fa-exclamation-circle fa-2x" />
-                {` `}
-                {t(
-                  'testingTemplates.vpatSection.subSection.downloadVPAT.line2.text'
-                )}
-              </p>
-              <p>
-                <Link
-                  href="https://www.youtube.com/watch?v=kAkSV9xiJ1A"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {t(
-                    'testingTemplates.vpatSection.subSection.downloadVPAT.line3.linkText'
-                  )}
-                </Link>
-                {` `}
-                {t(
-                  'testingTemplates.vpatSection.subSection.downloadVPAT.line3.otherText'
-                )}
-              </p>
-            </div>
-            <div>
-              <h2
-                id={`testingTemplates_${t(
-                  'testingTemplates.testPlanSection.heading'
-                )}`}
-              >
-                {t('testingTemplates.testPlanSection.heading')}
-              </h2>
-              <p>{t('testingTemplates.testPlanSection.description')}</p>
-              <ul className="accessibility-testing-templates__test-plan-list">
-                {testPlanList.map(item => (
-                  <li>{item}</li>
-                ))}
-              </ul>
-            </div>
-            <div>
-              <h2
-                id={`testingTemplates_${t(
-                  'testingTemplates.remediationPlanSection.heading'
-                )}`}
-              >
-                {t('testingTemplates.remediationPlanSection.heading')}
-              </h2>
-              <p>{t('testingTemplates.remediationPlanSection.description')}</p>
-              <ul className="accessibility-testing-templates__remediation-plan-list">
-                {remediationPlanList.map(item => (
-                  <li>{item}</li>
-                ))}
-              </ul>
-            </div>
+            {vpatSection}
+            {testPlanSection}
+            {remediationPlanSection}
           </div>
         </div>
-        <div className="grid-col-3 accessibility-testing-templates__side-col">
+        <div className="grid-col-3 accessibility-testing-templates__sidebar">
           <div>
             <h4 className="text-bold">
               Need help? Contact the Section 508 team
