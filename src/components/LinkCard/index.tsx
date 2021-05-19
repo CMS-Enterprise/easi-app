03097abbf8c70a8cf552da57b06db5c3fd163858
Kim Allen <kim@truss.works>
2021-05-19 15:17:57 -0700

    Test for header and link

--- src/components/LinkCard/index.tsx
+++ src/components/LinkCard/index.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
+import { Link } from '@trussworks/react-uswds';
 
 type LinkCardProps = {
   children: React.ReactNode;
   className?: string;
   link: string;
   heading: React.ReactNode | string;
-};
+} & JSX.IntrinsicElements['div']; // what does this mean?
 
 const LinkCard = ({ children, className, link, heading }: LinkCardProps) => {
   return (
-    <div className={className}>
-      <h2>{heading}</h2>
+    <div className={`padding-top-2 padding-left-1 ${className}`}>
+      <h2>
+        <Link href={link}>{heading}</Link>
+      </h2>
       {children}
     </div>
   );
