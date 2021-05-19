74b99309c2b637ae30caf871c090304833ea4956
Kim Allen <kim@truss.works>
2021-05-19 14:21:19 -0700

    Add props to LinkCard component

--- src/components/LinkCard/index.test.tsx
+++ src/components/LinkCard/index.test.tsx
@@ -5,6 +5,15 @@ import LinkCard from './index';
 
 describe('LinkCard', () => {
   it('renders without crashing', () => {
-    shallow(<LinkCard />);
+    const props = {
+      link: 'https://test/page',
+      heading: 'I am your header'
+    };
+
+    shallow(
+      <LinkCard {...props}>
+        <div>Component, I am your child</div>
+      </LinkCard>
+    );
   });
 });
