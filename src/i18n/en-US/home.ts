0a6f436004c0d19fe4fee32e17762a2a6791dfe1
Kim Allen <kim@truss.works>
2021-05-19 17:56:11 -0700

    Clean up extra divs and transaltion

--- src/i18n/en-US/home.ts
+++ src/i18n/en-US/home.ts
@@ -2,7 +2,6 @@ const home = {
   title: 'Welcome to EASi',
   subtitle:
     'You can use EASi to go through the set of steps needed to get a Lifecycle ID from the Governance Review Board (GRB).',
-  startNow: 'Start now',
   signIn: 'Sign in to start',
   accessibility: {
     heading: '508 Requests',
