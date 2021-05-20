d8a241a50103612e2b76a992ce0992cbc55b4056
Sara Bocciardi <sara@truss.works>
2021-05-20 12:35:45 -0700

    Use i18n for main page, too.

--- src/i18n/en-US/home.ts
+++ src/i18n/en-US/home.ts
@@ -21,6 +21,7 @@ const home = {
     }
   },
   requestsTable: {
+    heading: 'My governance requests',
     headers: {
       name: 'Request name',
       type: 'Governance',
--- src/views/MyRequests/index.tsx
+++ src/views/MyRequests/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 import Footer from 'components/Footer';
 import Header from 'components/Header';
@@ -8,17 +9,21 @@ import PageWrapper from 'components/PageWrapper';
 
 import Table from './Table';
 
-const MyRequests = () => (
-  <PageWrapper>
-    <Header />
-    <MainContent className="margin-bottom-5">
-      <div className="grid-container">
-        <PageHeading>My Requests</PageHeading>
-        <Table />
-      </div>
-    </MainContent>
-    <Footer />
-  </PageWrapper>
-);
+const MyRequests = () => {
+  const { t } = useTranslation('home');
+
+  return (
+    <PageWrapper>
+      <Header />
+      <MainContent className="margin-bottom-5">
+        <div className="grid-container">
+          <PageHeading>{t('requestsTable.heading')}</PageHeading>
+          <Table />
+        </div>
+      </MainContent>
+      <Footer />
+    </PageWrapper>
+  );
+};
 
 export default MyRequests;
