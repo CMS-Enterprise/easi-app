066fcb3acf643eaa7cfb3b265b033913cd3ee3df
Kim Allen <kim@truss.works>
2021-05-21 16:12:49 -0700

    Create basic next steps component and test

---
+++ src/components/AccessibilityRequestNextStep/index.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import AccessibilityRequestNextStep from './index';
+
+describe('AccessibilityRequestNextStep', () => {
+  const wrapper = shallow(<AccessibilityRequestNextStep />);
+  it('renders without error', () => {
+    expect(wrapper.length).toEqual(1);
+  });
+
+  it('has expected contents', () => {
+    expect(
+      wrapper.find('h3').contains('Next step: Provide your documents')
+    ).toEqual(true);
+  });
+});
