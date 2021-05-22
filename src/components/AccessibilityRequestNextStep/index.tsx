44c03abae859df3b57221c1ab308b03e9515b3d0
Kim Allen <kim@truss.works>
2021-05-21 17:27:50 -0700

    Add i18n text and conditionals in render

--- src/components/AccessibilityRequestNextStep/index.tsx
+++ src/components/AccessibilityRequestNextStep/index.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import { Trans, useTranslation } from 'react-i18next';
+import { Link } from '@trussworks/react-uswds';
 
 const AccessibilityRequestNextStep = () => {
-  return <h3>Next step: Provide your documents</h3>;
+  const { t } = useTranslation('accessibility');
+
+  return (
+    <div className="margin-bottom-3">
+      <h3 className="margin-y-0">
+        {t('requestDetails.documents.noDocs.heading')}
+      </h3>
+      <p className="line-height-body-4">
+        <Trans i18nKey="accessibility:requestDetails.documents.noDocs.description">
+          indexZero
+          <Link
+            className="display-inline-block"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="/508/templates"
+          >
+            linkText
+          </Link>
+          indexOne
+        </Trans>
+      </p>
+    </div>
+  );
 };
 
 export default AccessibilityRequestNextStep;
