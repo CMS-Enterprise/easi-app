f2344d485c1498ba047624f318159dd6047df7ab
Sara Bocciardi <sara@truss.works>
2021-05-24 10:42:32 -0700

    Add breadcrumb to the page.

--- src/views/MyRequests/index.tsx
+++ src/views/MyRequests/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
 
+import BreadcrumbNav from 'components/BreadcrumbNav';
 import Footer from 'components/Footer';
 import Header from 'components/Header';
 import MainContent from 'components/MainContent';
@@ -16,7 +18,14 @@ const MyRequests = () => {
     <PageWrapper>
       <Header />
       <MainContent className="margin-bottom-5">
-        <div className="grid-container">
+        <div className="grid-container margin-y-2">
+          <BreadcrumbNav>
+            <li>
+              <Link to="/">{t('requestsTable.breadcrumb.home')}</Link>
+              <i className="fa fa-angle-right margin-x-05" aria-hidden />
+            </li>
+            <li>{t('requestsTable.breadcrumb.table')}</li>
+          </BreadcrumbNav>
           <PageHeading>{t('requestsTable.heading')}</PageHeading>
           <Table />
         </div>
