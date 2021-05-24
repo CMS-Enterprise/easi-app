f7784f2780e4fb809eb01c27aec598f82cfda678
Jim Benton <jim@truss.works>
2021-05-24 14:33:36 -0500

    Only load flags for logged in users.

--- src/views/FlagsWrapper/index.tsx
+++ src/views/FlagsWrapper/index.tsx
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { useQuery } from '@apollo/client';
+import { useOktaAuth } from '@okta/okta-react';
 import { asyncWithLDProvider } from 'launchdarkly-react-client-sdk';
 import GetCurrentUserQuery from 'queries/GetCurrentUserQuery';
 import { GetCurrentUser } from 'queries/types/GetCurrentUser';
 
-type FlagsWrapperProps = {
+type WrapperProps = {
   children: React.ReactNode;
 };
 
-const FlagsWrapper = ({ children }: FlagsWrapperProps) => {
+const UserTargetingWrapper = ({ children }: WrapperProps) => {
   // wrapping initial value in function to get around useState and setState thinking
   // the functional component is a function to be evaluated.
   const [LDProvider, setLDProvider] = useState<React.FunctionComponent>(
@@ -16,26 +17,16 @@ const FlagsWrapper = ({ children }: FlagsWrapperProps) => {
   );
 
   const { data } = useQuery<GetCurrentUser>(GetCurrentUserQuery);
-  let key = process.env.REACT_APP_LD_ENV_USER;
-  let hash = process.env.REACT_APP_LD_USER_HASH;
-  let anonymous = true;
-
-  if (data) {
-    key = data.currentUser?.launchDarkly.userKey;
-    hash = data.currentUser?.launchDarkly.signedHash;
-    anonymous = false;
-  }
 
   useEffect(() => {
     (async () => {
       const provider = await asyncWithLDProvider({
         clientSideID: process.env.REACT_APP_LD_CLIENT_ID as string,
         user: {
-          key,
-          anonymous
+          key: data?.currentUser?.launchDarkly.userKey
         },
         options: {
-          hash
+          hash: data?.currentUser?.launchDarkly.signedHash
         },
         flags: {
           sandbox: true,
@@ -49,11 +40,21 @@ const FlagsWrapper = ({ children }: FlagsWrapperProps) => {
           add508Request: false
         }
       });
+
       setLDProvider(() => provider);
     })();
-  }, [key, hash, anonymous]);
+  }, [data]);
 
   return <LDProvider>{children}</LDProvider>;
+};
+
+const FlagsWrapper = ({ children }: WrapperProps) => {
+  const { authState } = useOktaAuth();
+  const Container = authState.isAuthenticated
+    ? UserTargetingWrapper
+    : React.Fragment;
+
+  return <Container>{children}</Container>;
 };
 
 export default FlagsWrapper;
