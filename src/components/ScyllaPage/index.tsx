0a08b2fc79e72298a5862323310d7b21649ef324
Christopher Hui <christopher@truss.works>
2021-05-25 15:00:38 -0700

    remove ScyllaPage

--- src/components/ScyllaPage/index.tsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable react/prop-types */
-
-import React, { FunctionComponent } from 'react';
-
-const ScyllaPage: FunctionComponent = ({ children }) => {
-  return <div className="margin-2">{children}</div>;
-};
-
-export default ScyllaPage;
