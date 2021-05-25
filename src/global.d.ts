ded26edfb05a4aedec3fef00d4748f102cf750bc
Christopher Hui <christopher@truss.works>
2021-05-25 13:30:15 -0700

    add downloads for template page

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,6 +1,8 @@
 declare module 'react-axe';
 declare module '@okta/okta-signin-widget';
 declare module '@okta/okta-signin-widget/dist/js/okta-sign-in.min';
+declare module '*.doc';
+declare module '*.docx';
 
 interface Window {
   Cypress: any;
