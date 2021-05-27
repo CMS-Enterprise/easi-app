c83fb4d7468c0c4797aa8315c3cd00988a048635
Sara Bocciardi <sara@truss.works>
2021-05-27 11:05:23 -0700

    More content checking

--- cypress/integration/accessibility.spec.js
+++ cypress/integration/accessibility.spec.js
@@ -1,3 +1,5 @@
+import { formatDate } from '../../src/utils/date';
+
 describe('The System Intake Form', () => {
   before(() => {
     cy.login();
@@ -26,5 +28,29 @@ describe('The System Intake Form', () => {
     cy.contains('li', 'Home')
     cy.contains('li', 'TACO')
     cy.contains('.usa-alert--success', '508 testing request created. We have sent you a confirmation email.')
+    cy.contains('h1', 'TACO')
+    cy.contains('h2', 'Next step: Provide your documents')
+    cy.contains('.usa-button', 'Upload a document')
+    cy.get('.accessibility-request__side-nav').within(() => {
+      cy.contains('h2', 'Test Dates and Scores')
+      cy.get('.accessibility-request__other-details').within(() => {
+        cy.contains('dt', 'Submission date')
+        const dateString = formatDate(new Date().toISOString())
+        cy.contains('dl', dateString)
+
+        cy.contains('dt', 'Business Owner')
+        cy.contains('dl', 'Shane Clark, OIT')
+
+        cy.contains('dt', 'Project name')
+        cy.contains('dl', 'TACO')
+
+        cy.contains('dt', 'Lifecycle ID')
+        cy.contains('dl', '000000')
+      })
+
+      cy.contains('a', '508 testing templates (opens in a new tab)')
+      cy.contains('a', 'Steps involved in 508 testing (opens in a new tab)')
+      // cy.contains('button', 'Remove this request from EASi')
+    })
   });
 });
