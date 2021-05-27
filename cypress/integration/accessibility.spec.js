a53521b5f6c81d91a61d2a60f2bde2d660ee7ee1
Sara Bocciardi <sara@truss.works>
2021-05-27 14:25:22 -0700

    Rename and add URL check.

--- cypress/integration/accessibility.spec.js
+++ cypress/integration/accessibility.spec.js
@@ -1,23 +1,14 @@
 import { formatDate } from '../../src/utils/date';
 
-describe('The System Intake Form', () => {
-  before(() => {
+describe('Accessibility Requests', () => {
+  beforeEach(() => {
     cy.login();
     // TODO HACK
     cy.wait(1000);
     cy.saveLocalStorage();
   });
 
-//   beforeEach(() => {
-//     cy.server();
-//     cy.route('POST', '/api/v1/system_intake').as('postSystemIntake');
-//     cy.route('PUT', '/api/v1/system_intake').as('putSystemIntake');
-//     cy.restoreLocalStorage();
-//     cy.visit('/system/new');
-//   });
-
-  it('can create a request and see it on the homepage', () => {
-    // Contact Details
+  it('can create a request and see its details', () => {
     cy.visit('/508/requests/new');
     cy.contains('h1', 'Request 508 testing')
     cy.contains('h2', "Choose the application you'd like to test")
@@ -25,6 +16,9 @@ describe('The System Intake Form', () => {
       .type('TACO - 000000{enter}')
       .should('have.value', 'TACO - 000000');
     cy.contains('button', 'Send 508 testing request').click();
+    cy.location().should(loc => {
+      expect(loc.pathname).to.match(/\/508\/requests\/.{36}/)
+    })
     cy.contains('li', 'Home')
     cy.contains('li', 'TACO')
     cy.contains('.usa-alert--success', '508 testing request created. We have sent you a confirmation email.')
