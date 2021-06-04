c11da3775141ee92c53e4616aa3b56558ea87fbc
Isaac Garfinkle <isaac@truss.works>
2021-06-04 12:29:48 -0700

    change i18n

--- src/i18n/en-US/accessibility.ts
+++ src/i18n/en-US/accessibility.ts
@@ -260,7 +260,7 @@ const accessibility = {
       ],
       download: {
         heading: 'Download Test Plan template',
-        link: 'Download the Test Plan template as a MS Word doc'
+        link: 'Download the Test Plan template as a PDFc'
       }
     },
     remediationPlanSection: {
@@ -273,7 +273,7 @@ const accessibility = {
       ],
       download: {
         heading: 'Download Remediation Plan template',
-        link: 'Download the Remediation Plan template as a MS Word doc'
+        link: 'Download the Remediation Plan template as a PDF'
       }
     }
   }
