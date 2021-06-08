4d99c443044bdef2a22c8c1fa2f4f51a8953ebbf
jsclarridge <2491291+jsclarridge@users.noreply.github.com>
2021-06-07 18:56:25 -0700

    Add crud role to db

---
+++ migrations/V81__Add_crud_role.sql
@@ -0,0 +1,13 @@
+CREATE ROLE crud;
+
+-- Modify existing tables and sequences.
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO crud;
+REVOKE ALL PRIVILEGES ON TABLE flyway_schema_history FROM crud;
+GRANT USAGE, UPDATE ON ALL SEQUENCES IN SCHEMA public TO crud;
+
+-- Modify future tables and sequences.
+-- Do not include `FOR ROLE` in the following statements so that:
+-- 1. when this runs in RDS, it will apply to the role running migrations.
+-- 2. when this runs in Docker, it will apply to the `postgres` role.
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO crud;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT USAGE, UPDATE ON SEQUENCES TO crud;
