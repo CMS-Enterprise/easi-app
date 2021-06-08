fa8b0a4a9e406c052d8de24c17f8c948ddbfd0c3
Isaac Garfinkle <isaac@truss.works>
2021-06-08 15:40:34 -0700

    Fix test date card story

--- src/components/TestDateCard/index.stories.tsx
+++ src/components/TestDateCard/index.stories.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { Meta } from '@storybook/react';
 import { DateTime } from 'luxon';
 
 import TestDateCard from 'components/TestDateCard';
@@ -12,12 +14,21 @@ const Template = (args: any) => (
 export default {
   title: 'Test Date Card',
   component: TestDateCard,
+  decorators: [
+    Story => (
+      <MemoryRouter initialEntries={['/']}>
+        <Story />
+      </MemoryRouter>
+    )
+  ],
   parameters: {
     info: 'The Test Date Card component'
   },
   args: {
-    date: DateTime.local()
+    testDate: {
+      date: DateTime.local()
+    }
   }
-};
+} as Meta;
 
 export const Default = Template.bind({});
