bf2f205fc20e7ff44709f822e13eaaa0c89f5c16
jsclarridge <2491291+jsclarridge@users.noreply.github.com>
2021-06-09 19:32:04 -0700

    Add separate statement for user password

--- migrations/V84__Add_app_user.sql
+++ migrations/V84__Add_app_user.sql
@@ -1,11 +1,13 @@
 DO
 $do$
 BEGIN
-  CREATE USER app_user WITH PASSWORD '${appuserpassword}';
+  CREATE USER app_user;
   EXCEPTION WHEN DUPLICATE_OBJECT THEN
   RAISE NOTICE 'not creating user app_user -- it already exists';
 END
 $do$;
+
+ALTER USER app_user WITH PASSWORD '${appuserpassword}';
 
 -- Assign the crud role (create, read, update, delete) to the
 -- app_user
