592a723405e8bb7b718447549418fa6ac17d04d0
Christopher Hui <christopher@truss.works>
2021-06-10 12:36:56 -0700

    add larger margin for page loading

--- src/components/PageLoading/index.tsx
+++ src/components/PageLoading/index.tsx
@@ -6,10 +6,11 @@ import Spinner from 'components/Spinner';
 const PageLoading = () => {
   const { t } = useTranslation('general');
   return (
-    <div className="margin-y-7" data-testid="page-loading">
+    <div className="margin-y-10" data-testid="page-loading">
       <div className="text-center">
         <Spinner size="xl" aria-valuetext={t('pageLoading')} aria-busy />
       </div>
+
       <h1 className="margin-top-6 text-center">{t('pageLoading')}</h1>
     </div>
   );
