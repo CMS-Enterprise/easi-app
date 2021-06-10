0aabfac73b3a9d78d7109740407e06bdd835793c
Christopher Hui <christopher@truss.works>
2021-06-10 13:51:52 -0700

    inital test for 508 change request status view

--- src/views/Accessibility/ChangeRequestStatus/index.test.tsx
+++ src/views/Accessibility/ChangeRequestStatus/index.test.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter, Route } from 'react-router-dom';
 import { MockedProvider } from '@apollo/client/testing';
 import { render, waitFor } from '@testing-library/react';
 import GetAccessibilityRequestForStatusChange from 'queries/GetAccessibilityRequestForStatusChange';
+
+import { MessageProvider } from 'hooks/useMessage';
 
 import ChangeRequestStatus from './index';
 
@@ -28,11 +30,21 @@ describe('Update 508 request status page', () => {
       }
     }
   ];
-  xit('renders without errors', async () => {
+
+  it('renders without errors', async () => {
     const { getByTestId } = render(
-      <MemoryRouter>
+      <MemoryRouter
+        initialEntries={[
+          '/508/requests/26908e00-927c-4924-8133-119be7eb21a9/change-status'
+        ]}
+      >
         <MockedProvider mocks={mockQuery} addTypename={false}>
-          <ChangeRequestStatus />
+          <MessageProvider>
+            <Route
+              path="/508/requests/:accessibilityRequestId"
+              component={ChangeRequestStatus}
+            />
+          </MessageProvider>
         </MockedProvider>
       </MemoryRouter>
     );
