c98a42989779281c0bb86280efae668b51f93514
Christopher Hui <christopher@truss.works>
2021-06-09 20:17:14 -0700

    hook frontend and backend up to update 508 request status

--- src/components/PlainInfo/index.tsx
+++ src/components/PlainInfo/index.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
+import classnames from 'classnames';
 
 import './index.scss';
 
 type PlainInfoProps = {
+  className?: string;
   children: React.ReactNode;
+  small?: boolean;
 };
 
 /**
  * This is a custom "info" component inspired by USWDS's Alert component.
  * The main difference is that this is plain with no background.
  */
-const PlainInfo = ({ children }: PlainInfoProps) => {
+const PlainInfo = ({ className, children, small }: PlainInfoProps) => {
+  const classes = classnames('easi-plain-info', className);
+
+  const iconClasses = classnames('easi-plain-info__icon', {
+    'easi-plain-info__icon--small': small
+  });
+
   return (
-    <div className="easi-plain-info">
-      <div className="easi-plain-info__icon">
+    <div className={classes}>
+      <div className={iconClasses}>
         <i className="fa fa-info" />
       </div>
       <p className="line-height-body-5">{children}</p>
