9dc9427597e96137a12decb7941f6ae98878dafa
Isaac Garfinkle <isaac@truss.works>
2021-06-10 17:01:38 -0700

    storage methods

---
+++ pkg/models/accessibility_note.go
@@ -0,0 +1,15 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+// AccessibilityNote represents data about a status that has been set
+type AccessibilityNote struct {
+	ID        uuid.UUID `json:"id"`
+	Note      string
+	RequestID uuid.UUID  `json:"requestId" db:"request_id"`
+	CreatedAt *time.Time `json:"createdAt" db:"created_at"`
+}
