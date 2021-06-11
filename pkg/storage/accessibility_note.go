1c365d57304ede887c18d5eeb7932dd6652ffb26
Isaac Garfinkle <isaac@truss.works>
2021-06-10 17:01:38 -0700

    storage methods

---
+++ pkg/storage/accessibility_note.go
@@ -0,0 +1,57 @@
+package storage
+
+import (
+	"context"
+	"time"
+
+	"go.uber.org/zap"
+
+	"github.com/google/uuid"
+
+	"github.com/cmsgov/easi-app/pkg/appcontext"
+	"github.com/cmsgov/easi-app/pkg/models"
+)
+
+// CreateAccessibilityNote creates a note for a given accessibility request
+func (s *Store) CreateAccessibilityNote(ctx context.Context, note *models.AccessibilityNote) (*models.AccessibilityNote, error) {
+	note.ID = uuid.New()
+	now := time.Now()
+	note.CreatedAt = &now
+
+	const createAccessibilityNoteSQL = `
+			INSERT INTO accessibility_notes (
+				id,
+				request_id,
+				created_at,
+				note
+			)
+			VALUES (
+				:id,
+				:request_id,
+				:created_at,
+				:note
+			)`
+	_, err := s.db.NamedExec(
+		createAccessibilityNoteSQL,
+		note,
+	)
+	if err != nil {
+		appcontext.ZLogger(ctx).Error("Failed to create accessibility note", zap.Error(err))
+		return nil, err
+	}
+	return s.FetchAccessibilityNoteByID(ctx, note.ID)
+}
+
+// FetchAccessibilityNoteByID fetches an accessibility note by its ID
+func (s *Store) FetchAccessibilityNoteByID(ctx context.Context, id uuid.UUID) (*models.AccessibilityNote, error) {
+	var note models.AccessibilityNote
+	err := s.db.Get(
+		&note, "SELECT * FROM accessibility_request_status_records WHERE request_id=$1;",
+		id,
+	)
+	if err != nil {
+		appcontext.ZLogger(ctx).Error("Failed to fetch accessibility note", zap.Error(err))
+		return nil, err
+	}
+	return &note, nil
+}
