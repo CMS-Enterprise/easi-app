e879164cd252b44943a5b1f1a028c015858b59d9
Kim Allen <kim@truss.works>
2021-06-28 15:57:18 -0700

    Add 5 failing tests related to alerts

--- src/views/Accessibility/AccessibilityRequestDetailPageRTL.test.tsx
+++ src/views/Accessibility/AccessibilityRequestDetailPageRTL.test.tsx
@@ -4,13 +4,13 @@ import { MemoryRouter, Route } from 'react-router-dom';
 import { MockedProvider } from '@apollo/client/testing';
 import { render, screen, within } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import CreateAccessibilityRequestNote from 'queries/CreateAccessibilityRequestNoteQuery';
+import GetAccessibilityRequestAccessibilityTeamOnlyQuery from 'queries/GetAccessibilityRequestAccessibilityTeamOnlyQuery';
 import GetAccessibilityRequestQuery from 'queries/GetAccessibilityRequestQuery';
 import configureMockStore from 'redux-mock-store';
 
+import { ACCESSIBILITY_TESTER_DEV } from 'constants/jobCodes';
 import { MessageProvider } from 'hooks/useMessage';
-
-import { ACCESSIBILITY_TESTER_DEV } from '../../constants/jobCodes';
-import GetAccessibilityRequestAccessibilityTeamOnlyQuery from '../../queries/GetAccessibilityRequestAccessibilityTeamOnlyQuery';
 
 import AccessibilityRequestDetailPage from './AccessibilityRequestDetailPage';
 
@@ -145,7 +145,7 @@ describe('AccessibilityRequestDetailPage', () => {
 
       const { findByRole, getAllByRole, getByRole } = render(providers);
       expect(
-        await screen.findByRole('tab', {
+        await findByRole('tab', {
           name: 'Notes'
         })
       ).toBeInTheDocument();
@@ -161,6 +161,162 @@ describe('AccessibilityRequestDetailPage', () => {
 
       const [, notesList] = getAllByRole('list');
       expect(within(notesList).getAllByRole('listitem').length).toEqual(2);
+    });
+
+    describe('Add a note', () => {
+      it('shows a success message', async () => {
+        const createNoteMocks = [
+          testerMocks[0],
+          {
+            request: {
+              query: CreateAccessibilityRequestNote,
+              variables: {
+                requestID: 'a11yRequest123',
+                note: 'I am an important note',
+                shouldSendEmail: false
+              }
+            },
+            result: {
+              data: {
+                createAccessibilityRequestNote: {
+                  id: 'noteID2',
+                  createdAt: 'time',
+                  authorName: 'Common Human',
+                  note: 'This is such a success'
+                }
+              }
+            }
+          }
+        ];
+
+        const providers = buildProviders(
+          createNoteMocks,
+          testerStore,
+          <AccessibilityRequestDetailPage />
+        );
+        const { findByRole, getByRole } = render(providers);
+        expect(await findByRole('alert')).toBeInTheDocument();
+        const { getByText } = within(getByRole('alert'));
+        expect(await getByText('Success')).toBeInTheDocument();
+        expect(
+          await getByText('Note added to My Special Request')
+        ).toBeInTheDocument();
+      });
+
+      it('can navigate through note tab', async () => {
+        const providers = buildProviders(
+          testerMocks,
+          testerStore,
+          <AccessibilityRequestDetailPage />
+        );
+
+        const { getByRole, getByTestId } = render(providers);
+        const notesTab = getByTestId('Notes-tab');
+        const notesTabButton = getByTestId('Notes-tab-btn');
+        userEvent.click(notesTabButton);
+        expect(await notesTab).toHaveClass('easi-tabs__tab--selected');
+        expect(await notesTabButton).toHaveFocus();
+        userEvent.tab();
+        expect(getByTestId('Notes-panel')).toHaveFocus();
+        userEvent.tab();
+        expect(
+          await getByRole('button', { name: 'Skip to existing notes' })
+        ).toHaveFocus();
+        userEvent.tab();
+        const textBox = getByRole('textbox', { name: 'Note' });
+        expect(await textBox).toHaveFocus();
+        userEvent.type(textBox, 'This request has been tested');
+        userEvent.tab();
+        expect(await getByRole('checkbox')).toHaveFocus();
+        userEvent.tab();
+      });
+
+      it('shows an error alert when there is a validation error', async () => {
+        const providers = buildProviders(
+          testerMocks,
+          testerStore,
+          <AccessibilityRequestDetailPage />
+        );
+
+        const { getByRole } = render(providers);
+
+        getByRole('button', { name: 'Add note' }).click();
+        expect(await getByRole('alert')).toBeInTheDocument();
+      });
+
+      it('shows an error alert message for an internal server error', async () => {
+        const errorMocks = [
+          testerMocks[0],
+          {
+            request: {
+              query: CreateAccessibilityRequestNote,
+              variables: {
+                requestID: 'a11yRequest123',
+                note: 'I am an important note',
+                shouldSendEmail: false
+              }
+            },
+            result: {
+              errors: [
+                {
+                  message: 'Something went very wrong',
+                  path: ['createAccessibilityRequestNote']
+                }
+              ],
+              data: { createAccessibilityRequestNote: null }
+            }
+          }
+        ];
+
+        const providers = buildProviders(
+          errorMocks,
+          testerStore,
+          <AccessibilityRequestDetailPage />
+        );
+        const { findByRole, getByRole } = render(providers);
+        expect(await findByRole('alert')).toBeInTheDocument();
+        const { getByText } = within(getByRole('alert'));
+        expect(await getByText('There is a problem')).toBeInTheDocument();
+      });
+
+      it('shows an error message for userErrors returned from the server', async () => {
+        const errorMocks = [
+          testerMocks[0],
+          {
+            request: {
+              query: CreateAccessibilityRequestNote,
+              variables: {
+                requestID: 'a11yRequest123',
+                note: 'I am an important note',
+                shouldSendEmail: false
+              }
+            },
+            result: {
+              data: {
+                createAccessibilityRequestNote: {
+                  accessibilityRequestNote: null,
+                  userErrors: [
+                    {
+                      message: 'I am a validation error',
+                      path: ['createAccessibilityRequest']
+                    }
+                  ]
+                }
+              }
+            }
+          }
+        ];
+
+        const providers = buildProviders(
+          errorMocks,
+          testerStore,
+          <AccessibilityRequestDetailPage />
+        );
+        const { findByRole, getByRole } = render(providers);
+        expect(await findByRole('alert')).toBeInTheDocument();
+        const { getByText } = within(getByRole('alert'));
+        expect(await getByText('There is a problem')).toBeInTheDocument();
+      });
     });
   });
 });
