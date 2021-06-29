27858f80be4b9bc7ca4707246a97986c986bb149
Jim Benton <jim@truss.works>
2021-06-29 12:58:03 -0500

    Add view with migration

---
+++ migrations/V91__Create_Accessibility_Request_Status_View.sql
@@ -0,0 +1,11 @@
+CREATE VIEW accessibility_requests_and_statuses AS
+SELECT
+	DISTINCT ON (accessibility_requests.id)
+	accessibility_requests.*,
+	status AS current_status,
+	accessibility_request_status_records.created_at AS current_status_updated_at,
+	accessibility_request_status_records.eua_user_id AS current_status_eau_user_id
+FROM
+	accessibility_requests
+LEFT JOIN accessibility_request_status_records ON accessibility_request_status_records.request_id = accessibility_requests.id
+ORDER BY accessibility_requests.id, accessibility_request_status_records.created_at DESC;
