50cba80461cc6b04d35cd653d098a3c1e3ad2b99
Jim Benton <jim@truss.works>
2021-06-30 15:20:43 -0500

    Add failing tests from previous commit.

--- src/views/Accessibility/AccessibilityRequestDetailPage.test.tsx
+++ src/views/Accessibility/AccessibilityRequestDetailPage.test.tsx
@@ -8,19 +8,20 @@ import {
   waitForElementToBeRemoved,
   within
 } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import CreateAccessibilityRequestNote from 'queries/CreateAccessibilityRequestNoteQuery';
 import GetAccessibilityRequestAccessibilityTeamOnlyQuery from 'queries/GetAccessibilityRequestAccessibilityTeamOnlyQuery';
 import GetAccessibilityRequestQuery from 'queries/GetAccessibilityRequestQuery';
 import configureMockStore from 'redux-mock-store';
 
+import { ACCESSIBILITY_TESTER_DEV } from 'constants/jobCodes';
 import { MessageProvider } from 'hooks/useMessage';
-
-import { ACCESSIBILITY_TESTER_DEV } from '../../constants/jobCodes';
 
 import AccessibilityRequestDetailPage from './AccessibilityRequestDetailPage';
 
 describe('AccessibilityRequestDetailPage', () => {
   const mockStore = configureMockStore();
-  const store = mockStore({
+  const defaultStore = mockStore({
     auth: {
       euaId: 'AAAA'
     }
@@ -55,6 +56,22 @@ describe('AccessibilityRequestDetailPage', () => {
     }
   };
 
+  const buildProviders = (
+    mocks: any,
+    store: any,
+    children: React.ReactElement
+  ) => (
+    <MemoryRouter initialEntries={['/508/requests/a11yRequest123']}>
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <Provider store={store}>
+          <Route path="/508/requests/:accessibilityRequestId">
+            <MessageProvider>{children}</MessageProvider>
+          </Route>
+        </Provider>
+      </MockedProvider>
+    </MemoryRouter>
+  );
+
   it('renders without crashing', async () => {
     render(
       <MemoryRouter
@@ -62,7 +79,7 @@ describe('AccessibilityRequestDetailPage', () => {
       >
         <MockedProvider mocks={[default508RequestQuery]} addTypename={false}>
           <MessageProvider>
-            <Provider store={store}>
+            <Provider store={defaultStore}>
               <Route path="/508/requests/:accessibilityRequestId">
                 <AccessibilityRequestDetailPage />
               </Route>
@@ -157,11 +174,12 @@ describe('AccessibilityRequestDetailPage', () => {
         }
       }
     };
+
     it('renders Next step if no documents', async () => {
       render(
         <MemoryRouter initialEntries={['/508/requests/a11yRequest123']}>
           <MockedProvider mocks={[withoutDocsQuery]} addTypename={false}>
-            <Provider store={store}>
+            <Provider store={defaultStore}>
               <MessageProvider>
                 <Route path="/508/requests/:accessibilityRequestId">
                   <AccessibilityRequestDetailPage />
@@ -186,7 +204,7 @@ describe('AccessibilityRequestDetailPage', () => {
       render(
         <MemoryRouter initialEntries={['/508/requests/a11yRequest123']}>
           <MockedProvider mocks={[withDocsQuery]} addTypename={false}>
-            <Provider store={store}>
+            <Provider store={defaultStore}>
               <MessageProvider>
                 <Route path="/508/requests/:accessibilityRequestId">
                   <AccessibilityRequestDetailPage />
@@ -416,6 +434,158 @@ describe('AccessibilityRequestDetailPage', () => {
         screen.getByRole('button', { name: /Add note/i }).click();
 
         expect(await screen.findByRole('button', { name: /Enter a note/i }));
+      });
+    });
+
+    describe('Add a note', () => {
+      it('shows a success message', async () => {
+        const createNoteMocks = [
+          defaultQuery,
+          {
+            request: {
+              query: CreateAccessibilityRequestNote,
+              variables: {
+                requestID: 'a11yRequest123',
+                note: 'I am an important note',
+                shouldSendEmail: false
+              }
+            },
+            result: {
+              data: {
+                createAccessibilityRequestNote: {
+                  id: 'noteID2',
+                  createdAt: 'time',
+                  authorName: 'Common Human',
+                  note: 'This is such a success'
+                }
+              }
+            }
+          }
+        ];
+
+        const providers = buildProviders(
+          createNoteMocks,
+          testerStore,
+          <AccessibilityRequestDetailPage />
+        );
+        render(providers);
+        expect(await screen.findByRole('alert')).toBeInTheDocument();
+        const { getByText } = within(screen.getByRole('alert'));
+        expect(await getByText('Success')).toBeInTheDocument();
+        expect(
+          await getByText('Note added to My Special Request')
+        ).toBeInTheDocument();
+      });
+
+      it('can add a note', async () => {
+        const providers = buildProviders(
+          [defaultQuery],
+          testerStore,
+          <AccessibilityRequestDetailPage />
+        );
+
+        render(providers);
+        const notesTabButton = screen.getByTestId('Notes-tab-btn');
+        userEvent.click(notesTabButton);
+        userEvent.tab();
+        userEvent.tab();
+        expect(
+          screen.getByRole('button', { name: 'Skip to existing notes' })
+        ).toHaveFocus();
+        userEvent.tab();
+        const textBox = screen.getByRole('textbox', { name: 'Note' });
+        expect(textBox).toHaveFocus();
+        userEvent.type(textBox, 'This request has been tested');
+        userEvent.tab();
+        expect(screen.getByRole('checkbox')).toHaveFocus();
+        userEvent.tab();
+      });
+
+      it('shows an error alert when there is a validation error', async () => {
+        const providers = buildProviders(
+          [defaultQuery],
+          testerStore,
+          <AccessibilityRequestDetailPage />
+        );
+
+        render(providers);
+
+        screen.getByRole('button', { name: 'Add note' }).click();
+        expect(await screen.findByRole('alert')).toBeInTheDocument();
+      });
+
+      it('shows an error alert message for an internal server error', async () => {
+        const errorMocks = [
+          defaultQuery,
+          {
+            request: {
+              query: CreateAccessibilityRequestNote,
+              variables: {
+                requestID: 'a11yRequest123',
+                note: 'I am an important note',
+                shouldSendEmail: false
+              }
+            },
+            result: {
+              errors: [
+                {
+                  message: 'Something went very wrong',
+                  path: ['createAccessibilityRequestNote']
+                }
+              ],
+              data: { createAccessibilityRequestNote: null }
+            }
+          }
+        ];
+
+        const providers = buildProviders(
+          errorMocks,
+          testerStore,
+          <AccessibilityRequestDetailPage />
+        );
+        render(providers);
+        expect(await screen.findByRole('alert')).toBeInTheDocument();
+        const { getByText } = within(screen.getByRole('alert'));
+        expect(await getByText('There is a problem')).toBeInTheDocument();
+      });
+
+      it('shows an error message for userErrors returned from the server', async () => {
+        const errorMocks = [
+          defaultQuery,
+          {
+            request: {
+              query: CreateAccessibilityRequestNote,
+              variables: {
+                requestID: 'a11yRequest123',
+                note: 'I am an important note',
+                shouldSendEmail: false
+              }
+            },
+            result: {
+              data: {
+                createAccessibilityRequestNote: {
+                  accessibilityRequestNote: null,
+                  userErrors: [
+                    {
+                      message: 'I am a validation error',
+                      path: ['createAccessibilityRequest']
+                    }
+                  ]
+                }
+              }
+            }
+          }
+        ];
+
+        const providers = buildProviders(
+          errorMocks,
+          testerStore,
+          <AccessibilityRequestDetailPage />
+        );
+        render(providers);
+        expect(await screen.findByRole('alert')).toBeInTheDocument();
+        const { getByText } = within(screen.getByRole('alert'));
+        expect(await getByText('There is a problem')).toBeInTheDocument();
       });
     });
   });
