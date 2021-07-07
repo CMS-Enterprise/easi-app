65c3e6a65758fd3cab4386c7a30492727a1e4e9f
jsclarridge <2491291+jsclarridge@users.noreply.github.com>
2021-07-07 16:11:45 -0700

    Add okta test vars for e2e

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,10 +1,10 @@
 import { ACCESSIBILITY_ADMIN_DEV } from '../../src/constants/jobCodes'
 
 describe('Logging in', () => {
-  // it('logs in with okta', () => {
-  //   cy.login();
-  //   cy.location('pathname', { timeout: 20000 }).should('equal', '/');
-  // });
+  it('logs in with okta', () => {
+    cy.login();
+    cy.location('pathname', { timeout: 20000 }).should('equal', '/');
+  });
 
   it('logs in with local auth', () => {
     cy.localLogin({name: 'TEST', role: ACCESSIBILITY_ADMIN_DEV});
