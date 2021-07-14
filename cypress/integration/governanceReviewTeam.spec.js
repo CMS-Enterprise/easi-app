796fc86ed1bcf60ba83dc06b15d99055e87ce643
Isaac Garfinkle <isaac@truss.works>
2021-07-13 20:00:46 -0700

    Add cypress test for closing a request

--- cypress/integration/governanceReviewTeam.spec.js
+++ cypress/integration/governanceReviewTeam.spec.js
@@ -165,4 +165,37 @@ describe('Governance Review Team', () => {
       .siblings('dd')
       .contains('Next steps');
   });
+
+  it('can close a request', () => {
+    // Selecting name based on pre-seeded data
+    // Closable Request - 20cbcfbf-6459-4c96-943b-e76b83122dbf
+    cy.get('a').contains('Closable Request').click();
+    cy.get(
+      'a[href="/governance-review-team/20cbcfbf-6459-4c96-943b-e76b83122dbf/actions"]'
+    )
+      .contains('Take an action')
+      .click();
+
+    cy.get('button[data-testid="collapsable-link"]').click();
+    cy.get('#no-governance').check({ force: true }).should('be.checked');
+    cy.get('button[type="submit"]').click();
+
+    cy.get('#SubmitActionForm-Feedback')
+      .type('Feedback')
+      .should('have.value', 'Feedback');
+    cy.get('button[type="submit"]').click();
+
+    cy.get('[data-testid="grt-status"]').contains('Closed');
+
+    cy.visit('/');
+    cy.get('[data-testid="view-closed-intakes-btn"]').click();
+    cy.get(
+      'a[href="/governance-review-team/20cbcfbf-6459-4c96-943b-e76b83122dbf/intake-request"]'
+    )
+      .contains('Closable Request')
+      .parent()
+      .siblings()
+      .eq(3)
+      .should('contain', 'Closed');
+  });
 });
