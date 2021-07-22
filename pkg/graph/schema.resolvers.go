8a0725ec52debe80d707c8d7419c7b505abf628b
Sara Bocciardi <sara@truss.works>
2021-07-22 14:30:36 -0700

    Add new schema for contract details.

--- pkg/graph/schema.resolvers.go
+++ pkg/graph/schema.resolvers.go
@@ -6,6 +6,7 @@ package graph
 import (
 	"context"
 	"errors"
+	"fmt"
 	"net/url"
 	"strconv"
 	"time"
@@ -952,6 +953,10 @@ func (r *mutationResolver) UpdateSystemIntakeRequestDetails(ctx context.Context,
 	return &model.UpdateSystemIntakePayload{
 		SystemIntake: savedIntake,
 	}, err
+}
+
+func (r *mutationResolver) UpdateSystemIntakeContractDetails(ctx context.Context, input model.UpdateSystemIntakeContractDetailsInput) (*model.UpdateSystemIntakePayload, error) {
+	panic(fmt.Errorf("not implemented"))
 }
 
 func (r *queryResolver) AccessibilityRequest(ctx context.Context, id uuid.UUID) (*models.AccessibilityRequest, error) {
