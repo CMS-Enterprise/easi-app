9260100bec73eb24b870117118d0d0b504d65983
Christopher Hui <christopher@truss.works>
2021-08-11 16:34:09 -0700

    fix local nil error

--- pkg/local/email.go
+++ pkg/local/email.go
@@ -20,9 +20,14 @@ type Sender struct {
 
 // Send logs an email
 func (s Sender) Send(ctx context.Context, toAddress models.EmailAddress, ccAddress *models.EmailAddress, subject string, body string) error {
+	ccAddresses := ""
+	if ccAddress != nil {
+		ccAddresses = ccAddress.String()
+	}
+
 	appcontext.ZLogger(ctx).Info("Mock sending email",
 		zap.String("To", toAddress.String()),
-		zap.String("CC", ccAddress.String()),
+		zap.String("CC", ccAddresses),
 		zap.String("Subject", subject),
 		zap.String("Body", body),
 	)
