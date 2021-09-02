79c81c4c70b0387eec557db395d1818edff03191
Christopher Hui <christopher@truss.works>
2021-09-02 12:25:14 -0700

    update implementation because autoRenew actually does work, surprise

--- src/views/AuthenticationWrapper/index.tsx
+++ src/views/AuthenticationWrapper/index.tsx
@@ -19,11 +19,9 @@ const AuthenticationWrapper = ({ children }: ParentComponentProps) => {
     issuer: process.env.REACT_APP_OKTA_ISSUER,
     clientId: process.env.REACT_APP_OKTA_CLIENT_ID,
     redirectUri: process.env.REACT_APP_OKTA_REDIRECT_URI,
-    responseType: ['code'],
     tokenManager: {
-      autoRenew: false
-    },
-    pkce: true
+      autoRenew: true
+    }
   });
 
   const handleAuthRequiredRedirect = () => {
