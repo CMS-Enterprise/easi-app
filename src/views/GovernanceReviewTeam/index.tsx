96dbaa4486fb650564b613fefc47450162556b58
Jim Benton <jim@truss.works>
2021-09-17 13:35:55 -0500

    Remove redux from GovernanceReviewTeam

--- src/views/GovernanceReviewTeam/index.tsx
+++ src/views/GovernanceReviewTeam/index.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
+import { useOktaAuth } from '@okta/okta-react';
 import { useFlags } from 'launchdarkly-react-client-sdk';
 
 import PageLoading from 'components/PageLoading';
 import RequestRepository from 'components/RequestRepository';
-import { AppState } from 'reducers/rootReducer';
-import user from 'utils/user';
+import user, { EasiAuthState } from 'utils/user';
 import RequestOverview from 'views/GovernanceReviewTeam/RequestOverview';
 import NotFound from 'views/NotFound';
 
 const GovernanceReviewTeam = () => {
-  const userGroups = useSelector((state: AppState) => state.auth.groups);
-  const isUserSet = useSelector((state: AppState) => state.auth.isUserSet);
+  const { authState }: { authState: EasiAuthState | null } = useOktaAuth();
+  const userGroups = authState?.groups || [];
+  const isUserSet = authState?.isAuthenticated;
+
   const flags = useFlags();
 
   if (isUserSet) {
