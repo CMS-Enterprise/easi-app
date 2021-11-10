da7fd7dac2bc52a2bebfcc8e20517a0b02400930
ClayBenson94 <contact@claybenson.me>
2021-11-10 08:52:21 -0500

    Remove padding and genericize component

--- src/components/SystemProfileHealthCard/index.tsx
+++ src/components/SystemProfileHealthCard/index.tsx
@@ -1,43 +1,71 @@
 import React from 'react';
 import {
-  Button,
   Card,
   CardBody,
   CardFooter,
-  CardHeader
+  CardHeader,
+  Grid,
+  GridContainer
 } from '@trussworks/react-uswds';
+import classnames from 'classnames';
+
+import './index.scss';
 
 type SystemProfileHealthCardProps = {
   heading: string;
   body: React.ReactNode | string;
   footer: React.ReactNode | string;
+  status: 'success' | 'warning' | 'fail';
+  statusText: string;
 };
 
 export const SystemProfileHealthCard = (
   props: SystemProfileHealthCardProps
 ) => {
-  const { heading, body, footer } = props;
+  const { heading, body, footer, status, statusText } = props;
+
+  let iconColor = '';
+  let iconClassname = '';
+  switch (status) {
+    case 'success':
+      iconColor = 'green';
+      iconClassname = 'fa-check-circle';
+      break;
+    case 'warning':
+      iconColor = '#ff9321';
+      iconClassname = 'fa-exclamation-circle';
+      break;
+    case 'fail':
+      iconColor = 'red';
+      iconClassname = 'fa-times-circle';
+      break;
+    default:
+      iconColor = '#000000';
+      break;
+  }
 
   return (
     <div>
       <Card>
         <CardHeader>
-          <h2 className="usa-card__heading">
-            {heading || 'System Profile Health'}
-          </h2>
+          <h2 className="usa-card__heading">{heading}</h2>
         </CardHeader>
         <CardBody>
-          {body || (
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-              consectetur, nisi sed consectetur sagittis, nunc nisl consectetur
-              augue, eget tincidunt nisl nisi eu nisl.
-            </p>
-          )}
+          <GridContainer className="resetGridPadding">
+            <Grid row>
+              <Grid desktop={{ col: 9 }}>{body}</Grid>
+              <Grid desktop={{ col: 3 }} style={{ textAlign: 'center' }}>
+                <i
+                  style={{ color: iconColor }}
+                  className={classnames('fa', 'fa-5x', iconClassname)}
+                />
+                <br />
+                {statusText}
+              </Grid>
+            </Grid>
+          </GridContainer>
         </CardBody>
-        <CardFooter>
-          {footer || <Button type="button">Test Button</Button>}
-        </CardFooter>
+        <CardFooter>{footer}</CardFooter>
       </Card>
     </div>
   );
--- src/views/Sandbox/index.tsx
+++ src/views/Sandbox/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Button, Grid, GridContainer } from '@trussworks/react-uswds';
+import { Button } from '@trussworks/react-uswds';
 
 import Header from 'components/Header';
 import SystemProfileHealthCard from 'components/SystemProfileHealthCard';
@@ -21,87 +21,55 @@ const Sandbox = () => {
           <SystemProfileHealthCard
             heading="Authority to Operate (ATO)"
             body={
-              <GridContainer>
-                <Grid row>
-                  <Grid desktop={{ col: 9 }}>
-                    <p>
-                      The implementation of a Federal Government information
-                      system requires a formal Government Authorization to
-                      Operate (ATO) for infrastructure systems and/or all
-                      application systems developed, hosted and/or maintained on
-                      behalf of CMS
-                    </p>
-                    <a href="/sandbox">Learn more about ATO</a>
-                  </Grid>
-                  <Grid desktop={{ col: 3 }} style={{ textAlign: 'center' }}>
-                    <i
-                      style={{ color: 'green' }}
-                      className="fa fa-check-circle fa-5x"
-                    />
-                    <br />
-                    Good to go
-                  </Grid>
-                </Grid>
-              </GridContainer>
+              <>
+                <p>
+                  The implementation of a Federal Government information system
+                  requires a formal Government Authorization to Operate (ATO)
+                  for infrastructure systems and/or all application systems
+                  developed, hosted and/or maintained on behalf of CMS
+                </p>
+                <a href="/sandbox">Learn more about ATO</a>
+              </>
             }
             footer={<Button type="button">View ATO Information</Button>}
+            status="success"
+            statusText="Good to go"
           />
           <SystemProfileHealthCard
             heading="Section 508"
             body={
-              <GridContainer>
-                <Grid row>
-                  <Grid desktop={{ col: 9 }}>
-                    <p>
-                      Section 508 of the Rehabilitation Act (29 U.S.C. § 794d),
-                      as amended by the Workforce Investment Act of 1998 (P.L.
-                      105-220) requires federal agencies to develop, procure,
-                      maintain and use information and communications technology
-                      (ICT) that is accessible to people with disabilities -
-                      regardless of whether or not they work for the federal
-                      government.
-                    </p>
-                    <a href="/sandbox">Learn more about Section 508</a>
-                  </Grid>
-                  <Grid desktop={{ col: 3 }} style={{ textAlign: 'center' }}>
-                    <i
-                      style={{ color: '#ff9321' }}
-                      className="fa fa-exclamation-circle fa-5x"
-                    />
-                    <br />
-                    80% Coverage
-                  </Grid>
-                </Grid>
-              </GridContainer>
+              <>
+                <p>
+                  Section 508 of the Rehabilitation Act (29 U.S.C. § 794d), as
+                  amended by the Workforce Investment Act of 1998 (P.L. 105-220)
+                  requires federal agencies to develop, procure, maintain and
+                  use information and communications technology (ICT) that is
+                  accessible to people with disabilities - regardless of whether
+                  or not they work for the federal government.
+                </p>
+                <a href="/sandbox">Learn more about 508 Compliance</a>
+              </>
             }
             footer={
               <Button type="button">View 508 Compliance Information</Button>
             }
+            status="warning"
+            statusText="Not yet compliant (80%)"
           />
           <SystemProfileHealthCard
             heading="Technical Review Board (TRB)"
             body={
-              <GridContainer>
-                <Grid row>
-                  <Grid desktop={{ col: 9 }}>
-                    <p>
-                      The TRB provides oversight to ensure IT investments are
-                      consistent with CMS’s IT strategy.
-                    </p>
-                    <a href="/sandbox">Learn more about the TRB</a>
-                  </Grid>
-                  <Grid desktop={{ col: 3 }} style={{ textAlign: 'center' }}>
-                    <i
-                      style={{ color: 'red' }}
-                      className="fa fa-times-circle fa-5x"
-                    />
-                    <br />
-                    Need to Schedule
-                  </Grid>
-                </Grid>
-              </GridContainer>
+              <>
+                <p>
+                  The TRB provides oversight to ensure IT investments are
+                  consistent with CMS’s IT strategy.
+                </p>
+                <a href="/sandbox">Learn more about the TRB</a>
+              </>
             }
             footer={<Button type="button">View TRB Information</Button>}
+            status="fail"
+            statusText="Need to Schedule"
           />
         </div>
       </div>
