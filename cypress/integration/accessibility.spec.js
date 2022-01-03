fe46b386517c64fb998865e6d0430127e861a40a
Patrick Segura <patrick.segura@oddball.io>
2022-01-03 14:13:04 -0500

    Removed cypress check for a user dropdown button that was removed

--- cypress/integration/accessibility.spec.js
+++ cypress/integration/accessibility.spec.js
@@ -55,7 +55,6 @@ describe('Accessibility Requests', () => {
   it('has the correct page order when clicking through 508 team home page', () => {
     cy.localLogin({ name: 'A11Y', role: 'EASI_D_508_USER' });
 
-    cy.contains('button', 'User A11Y').click();
     cy.contains('button', 'Make 508 request').click();
     cy.location().should(loc => {
       expect(loc.pathname).to.eq('/508/making-a-request');
