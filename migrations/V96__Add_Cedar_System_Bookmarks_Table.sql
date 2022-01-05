681d72ac4549d2f530f46b549527430931824cc7
Jeremy Strang <jeremystrang@Jeremys-MBP.attlocal.net>
2022-01-05 09:52:05 -0500

    work on cedar system bookmarks

--- migrations/V96__Add_Cedar_System_Bookmarks_Table.sql
+++ migrations/V96__Add_Cedar_System_Bookmarks_Table.sql
@@ -1,4 +1,4 @@
-create table cedar_system_bookmark (
+create table cedar_system_bookmarks (
   eua_user_id text not null CHECK (eua_user_id ~ '^[A-Z0-9]{4}$'),
   cedar_system_id text not null,
   created_at timestamp with time zone,
--- pkg/storage/cedar_system_bookmark.go
+++ pkg/storage/cedar_system_bookmark.go
@@ -20,7 +20,7 @@ func (s *Store) CreateCedarSystemBookmark(ctx context.Context, cedarSystemBookma
 	cedarSystemBookmark.CreatedAt = &createAt
 	cedarSystemBookmark.EUAUserID = euaUserID
 	const createCedarSystemBookmarkSQL = `
-		INSERT INTO cedar_system_bookmark (
+		INSERT INTO cedar_system_bookmarks (
 			eua_user_id,
 			cedar_system_id,
 			created_at
@@ -50,7 +50,7 @@ func (s *Store) FetchCedarSystemBookmarks(ctx context.Context, id *string) ([]*m
 		euaUserID := appcontext.Principal(ctx).ID()
 		id = &euaUserID
 	}
-	err := s.db.SelectContext(ctx, &results, `SELECT * FROM cedar_system_bookmark WHERE eua_user_id=$1`, *id)
+	err := s.db.SelectContext(ctx, &results, `SELECT * FROM cedar_system_bookmarks WHERE eua_user_id=$1`, *id)
 
 	if err != nil && !errors.Is(err, sql.ErrNoRows) {
 		appcontext.ZLogger(ctx).Error("Failed to fetch cedar system bookmarks", zap.Error(err), zap.String("id", *id))
@@ -66,7 +66,7 @@ func (s *Store) FetchCedarSystemBookmarks(ctx context.Context, id *string) ([]*m
 // DeleteCedarSystemBookmark deletes (soft delete - set deleted_at field) an existing cedar system bookmark object in the database
 func (s *Store) DeleteCedarSystemBookmark(ctx context.Context, cedarSystemBookmark *models.CedarSystemBookmark) (*models.CedarSystemBookmark, error) {
 	const deleteCedarSystemBookmarkSQL = `
-		DELETE FROM cedar_system_bookmark
+		DELETE FROM cedar_system_bookmarks
 		WHERE eua_user_id.id = :id
 		AND cedar_system_id = :
 		RETURNING *`
