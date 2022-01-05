5a5579d1b35d1a67c0220e26f0ad67f815413154
Jeremy Strang <jeremystrang@Jeremys-MBP.attlocal.net>
2022-01-05 09:55:10 -0500

    fixed issue with cedar system bookmark schema

--- pkg/models/cedar_system_bookmark.go
+++ pkg/models/cedar_system_bookmark.go
@@ -9,6 +9,6 @@ import (
 // CedarSystemBookmark represents a cedar system that a user has bookmarked in the system repository
 type CedarSystemBookmark struct {
 	EUAUserID     string     `json:"euaUserId" db:"eua_user_id"`
-	CedarSystemID uuid.UUID  `json:"cedarSystemId" db:"cedar_system"`
+	CedarSystemID uuid.UUID  `json:"cedarSystemId" db:"cedar_system_id"`
 	CreatedAt     *time.Time `db:"created_at"`
 }
