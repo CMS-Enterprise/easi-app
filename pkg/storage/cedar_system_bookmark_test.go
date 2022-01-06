51140212f11d8befaefdbf2b868f06d787714a0c
Jeremy Strang <jeremystrang@Jeremys-MBP.attlocal.net>
2022-01-06 13:37:07 -0500

    added test to verify functionality for duplicate bookmarks

--- pkg/storage/cedar_system_bookmark_test.go
+++ pkg/storage/cedar_system_bookmark_test.go
@@ -2,8 +2,12 @@ package storage
 
 import (
 	"context"
+	"time"
 
 	"github.com/cmsgov/easi-app/pkg/models"
+	"github.com/cmsgov/easi-app/pkg/testhelpers"
+
+	"github.com/facebookgo/clock"
 )
 
 func (s StoreTestSuite) TestCreateCedarSystemBookmark() {
@@ -23,5 +27,30 @@ func (s StoreTestSuite) TestCreateCedarSystemBookmark() {
 		fetched, err := s.store.FetchCedarSystemBookmarks(ctx)
 		s.NoError(err)
 		s.True(len(fetched) > 0)
+	})
+}
+
+func (s StoreTestSuite) TestDuplicateCedarSystemBookmark() {
+	ctx := context.Background()
+
+	mockClock := clock.NewMock()
+	settableClock := testhelpers.SettableClock{Mock: mockClock}
+	s.store.clock = &settableClock
+
+	s.Run("create a new cedar system bookmark", func() {
+		cedarSystemID := "326-1556-2"
+		bookmark := models.CedarSystemBookmark{
+			EUAUserID:     "ANON",
+			CedarSystemID: cedarSystemID,
+		}
+		created, err := s.store.CreateCedarSystemBookmark(ctx, &bookmark)
+		createdAt := (*created.CreatedAt).Unix()
+		s.NoError(err)
+
+		mockClock.Add(time.Minute)
+
+		duplicate, err2 := s.store.CreateCedarSystemBookmark(ctx, &bookmark)
+		s.NoError(err2)
+		s.Greater((*duplicate.CreatedAt).Unix(), createdAt)
 	})
 }
