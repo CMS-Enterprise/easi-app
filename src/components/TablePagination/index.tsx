ed87482bd4743f53a248dab0bc8118c50d79e329
Patrick Segura <patrick.segura@oddball.io>
2022-01-10 16:57:45 -0500

    Main pagination component functions complete - still needs responsiveness and testing

--- src/components/TablePagination/index.tsx
+++ src/components/TablePagination/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import { UsePaginationInstanceProps, UsePaginationState } from 'react-table';
 import classnames from 'classnames';
 
@@ -8,6 +9,23 @@ type ReactTablePaginationProps = {
   className?: string;
 } & UsePaginationInstanceProps<{}> &
   UsePaginationState<{}>;
+
+const filterPages = (visiblePages: number[], totalPages: number) => {
+  return visiblePages.filter(page => page <= totalPages);
+};
+
+const getVisiblePages = (page: number, total: number) => {
+  if (total < 7) {
+    return filterPages([1, 2, 3, 4, 5, 6], total);
+  }
+  if (page % 5 >= 0 && page > 4 && page + 2 < total) {
+    return [1, page - 2, page - 1, page, page + 1, page + 2, total];
+  }
+  if (page % 5 >= 0 && page > 4 && page + 2 >= total) {
+    return [1, total - 3, total - 2, total - 1, total];
+  }
+  return [1, 2, 3, 4, 5, total];
+};
 
 const TablePagination = ({
   className,
@@ -22,74 +40,104 @@ const TablePagination = ({
   pageSize,
   setPageSize
 }: ReactTablePaginationProps) => {
-  const classNames = classnames('pagination', className);
+  const { t } = useTranslation('systemProfile');
+  const [visiblePages, setVisiblePages] = useState<number[]>([]);
+  const classNames = classnames('usa-pagination', className);
+
+  useEffect(() => {
+    setVisiblePages(getVisiblePages(pageIndex + 1, pageOptions.length));
+  }, [pageIndex, pageOptions.length]);
+
   return (
-    <div className={classNames} data-testid="table-pagination">
-      <button
-        type="button"
-        className="usa-button"
-        onClick={() => gotoPage(0)}
-        disabled={!canPreviousPage}
-      >
-        {'<<'}
-      </button>{' '}
-      <button
-        type="button"
-        className="usa-button"
-        onClick={() => previousPage()}
-        disabled={!canPreviousPage}
-      >
-        {'<'}
-      </button>{' '}
-      <button
-        type="button"
-        className="usa-button"
-        onClick={() => nextPage()}
-        disabled={!canNextPage}
-      >
-        {'>'}
-      </button>{' '}
-      <button
-        type="button"
-        className="usa-button"
-        onClick={() => gotoPage(pageCount - 1)}
-        disabled={!canNextPage}
-      >
-        {'>>'}
-      </button>{' '}
-      <span className="margin-left-1">
-        Page{' '}
-        <strong>
-          {pageIndex + 1} of {pageOptions.length}
-        </strong>{' '}
-      </span>
-      <span className="margin-left-1">
-        | Go to page:{' '}
-        <input
-          className="usa-input display-inline width-10 margin-left-1"
-          type="number"
-          defaultValue={pageIndex + 1}
-          onChange={e => {
-            const page: number = e.target.value
-              ? Number(e.target.value) - 1
-              : 0;
-            gotoPage(page);
-          }}
-        />
-      </span>{' '}
-      <select
-        className="usa-input display-inline width-15"
-        onBlur={e => {
-          setPageSize(Number(e.target.value));
-        }}
-      >
-        {[10, 20, 30, 40, 50].map(customPageSize => (
-          <option key={customPageSize} value={customPageSize}>
-            Show {customPageSize}
-          </option>
-        ))}
-      </select>
-    </div>
+    <nav
+      aria-label="Pagination"
+      className={classNames}
+      data-testid="table-pagination"
+    >
+      <ul className="usa-pagination__list">
+        {canPreviousPage && (
+          <li className="usa-pagination__item usa-pagination__arrow">
+            <button
+              type="button"
+              className="usa-pagination__link usa-pagination__previous-page"
+              aria-label="Previous page"
+              onClick={() => previousPage()}
+              disabled={!canPreviousPage}
+            >
+              <span className="usa-pagination__link-text">
+                {'< '}
+                {t('tableAndPagination:pagination.previous')}{' '}
+              </span>
+            </button>
+          </li>
+        )}
+        <li className="usa-pagination__item usa-pagination__page-no">
+          {visiblePages.map((page, index, array) => {
+            return (
+              <>
+                {array[index - 1] + 2 < page ? (
+                  <>
+                    <li
+                      className="usa-pagination__item usa-pagination__overflow"
+                      role="presentation"
+                    >
+                      <span> … </span>
+                    </li>
+                    <li className="usa-pagination__item">
+                      <button
+                        type="button"
+                        aria-label={`Page ${page}`}
+                        key={page}
+                        className={
+                          pageIndex + 1 === page
+                            ? 'usa-pagination__button usa-current'
+                            : 'usa-pagination__button'
+                        }
+                        onClick={() => gotoPage(page - 1)}
+                      >
+                        {page}
+                      </button>
+                    </li>
+                  </>
+                ) : (
+                  <li className="usa-pagination__item">
+                    <button
+                      type="button"
+                      aria-label={`Page ${page}`}
+                      key={page}
+                      className={
+                        pageIndex + 1 === page
+                          ? 'usa-pagination__button usa-current'
+                          : 'usa-pagination__button'
+                      }
+                      onClick={() => gotoPage(page - 1)}
+                    >
+                      {page}
+                    </button>
+                  </li>
+                )}
+              </>
+            );
+          })}
+        </li>
+        {canNextPage && (
+          <li className="usa-pagination__item usa-pagination__arrow">
+            <button
+              type="button"
+              className="usa-pagination__link usa-pagination__previous-page"
+              aria-label="Next page"
+              onClick={() => nextPage()}
+              disabled={!canNextPage}
+            >
+              <span className="usa-pagination__link-text">
+                {t('tableAndPagination:pagination.next')}
+                {' >'}
+              </span>
+            </button>
+          </li>
+        )}
+      </ul>
+    </nav>
   );
 };
 
