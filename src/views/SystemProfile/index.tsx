55cace6c0f8743072911182e80856e6fd7327e14
Patrick Segura <patrick.segura@oddball.io>
2022-01-26 12:39:23 -0500

    Resolve merge conflicts

--- src/views/SystemProfile/index.tsx
+++ src/views/SystemProfile/index.tsx
@@ -13,12 +13,9 @@ import {
 import classnames from 'classnames';
 
 import BookmarkCardIcon from 'components/BookmarkCard/BookmarkCardIcon';
-import Footer from 'components/Footer';
-import Header from 'components/Header';
 import UswdsReactLink from 'components/LinkWrapper';
 import MainContent from 'components/MainContent';
 import PageHeading from 'components/PageHeading';
-import PageWrapper from 'components/PageWrapper';
 import CollapsableLink from 'components/shared/CollapsableLink';
 import {
   DescriptionDefinition,
@@ -81,129 +78,154 @@ const SystemProfile = () => {
   }
 
   return (
-    <PageWrapper>
-      <Header />
-      <MainContent>
-        <div id="system-profile">
-          <SummaryBox heading="" className="system-profile__summary-box">
-            <Grid className="grid-container">
-              <BreadcrumbBar variant="wrap">
-                <Breadcrumb>
-                  <span>&larr; </span>
-                  <BreadcrumbLink asCustom={Link} to="/system-profile">
-                    <span>{t('singleSystem.summary.back')}</span>
-                  </BreadcrumbLink>
-                </Breadcrumb>
-              </BreadcrumbBar>
+    <MainContent>
+      <div id="system-profile">
+        <SummaryBox heading="" className="system-profile__summary-box">
+          <Grid className="grid-container">
+            <BreadcrumbBar variant="wrap">
+              <Breadcrumb>
+                <span>&larr; </span>
+                <BreadcrumbLink asCustom={Link} to="/system-profile">
+                  <span>{t('singleSystem.summary.back')}</span>
+                </BreadcrumbLink>
+              </Breadcrumb>
+            </BreadcrumbBar>
 
-              <PageHeading className="margin-top-2">
-                <div className="bookmark__title">
-                  <BookmarkCardIcon size="sm" className="margin-right-1" />{' '}
-                  <span>{systemInfo.name} </span>
-                  <span className="system-profile__acronym">
-                    ({systemInfo.acronym})
-                  </span>
-                </div>
-                <div className="system-profile__summary-body">
-                  <CollapsableLink
-                    className="margin-top-3"
-                    eyeIcon
-                    startOpen
-                    labelPosition="bottom"
-                    closeLabel={t('singleSystem.summary.hide')}
-                    styleLeftBar={false}
-                    id={t('singleSystem.id')}
-                    label={t('singleSystem.summary.expand')}
+            <PageHeading className="margin-top-2">
+              <div className="bookmark__title">
+                <BookmarkCardIcon size="sm" className="margin-right-1" />{' '}
+                <span>{systemInfo.name} </span>
+                <span className="system-profile__acronym">
+                  ({systemInfo.acronym})
+                </span>
+              </div>
+              <div className="system-profile__summary-body">
+                <CollapsableLink
+                  className="margin-top-3"
+                  eyeIcon
+                  startOpen
+                  labelPosition="bottom"
+                  closeLabel={t('singleSystem.summary.hide')}
+                  styleLeftBar={false}
+                  id={t('singleSystem.id')}
+                  label={t('singleSystem.summary.expand')}
+                >
+                  <DescriptionDefinition
+                    definition={systemInfo.description}
+                    className="margin-bottom-2"
+                  />
+                  <UswdsReactLink
+                    aria-label={t('singleSystem.summary.label')}
+                    className="line-height-body-5"
+                    to="/" // TODO: Get link from CEDAR?
+                    variant="external"
+                    target="_blank"
                   >
-                    <DescriptionDefinition
-                      definition={systemInfo.description}
-                      className="margin-bottom-2"
-                    />
-                    <UswdsReactLink
-                      aria-label={t('singleSystem.summary.label')}
-                      className="line-height-body-5"
-                      to="/" // TODO: Get link from CEDAR?
-                      variant="external"
-                      target="_blank"
-                    >
-                      {t('singleSystem.summary.view')} {systemInfo.name}
-                      <span aria-hidden>&nbsp;</span>
-                    </UswdsReactLink>
+                    {t('singleSystem.summary.view')} {systemInfo.name}
+                    <span aria-hidden>&nbsp;</span>
+                  </UswdsReactLink>
 
-                    {/* TODO: Map fields to CEDAR data */}
-                    <Grid row className="margin-top-3">
-                      <Grid desktop={{ col: 6 }} className="margin-bottom-2">
-                        <DescriptionDefinition
-                          definition={t('singleSystem.summary.subheader1')}
-                        />
-                        <DescriptionTerm
-                          className="system-profile__subheader"
-                          term={systemInfo.businessOwnerOrg || ''}
-                        />
-                      </Grid>
-                      <Grid desktop={{ col: 6 }} className="margin-bottom-2">
-                        <DescriptionDefinition
-                          definition={t('singleSystem.summary.subheader2')}
-                        />
-                        <DescriptionTerm
-                          className="system-profile__subheader"
-                          term={systemInfo.businessOwnerOrgComp || ''}
-                        />
-                      </Grid>
-                      <Grid desktop={{ col: 6 }} className="margin-bottom-2">
-                        <DescriptionDefinition
-                          definition={t('singleSystem.summary.subheader3')}
-                        />
-                        <DescriptionTerm
-                          className="system-profile__subheader"
-                          term={systemInfo.systemMaintainerOrg || ''}
-                        />
-                      </Grid>
-                      <Grid desktop={{ col: 6 }} className="margin-bottom-2">
-                        <DescriptionDefinition
-                          definition={t('singleSystem.summary.subheader4')}
-                        />
-                        <DescriptionTerm
-                          className="system-profile__subheader"
-                          term={systemInfo.id || ''}
-                        />
-                      </Grid>
+                  {/* TODO: Map fields to CEDAR data */}
+                  <Grid row className="margin-top-3">
+                    <Grid desktop={{ col: 6 }} className="margin-bottom-2">
+                      <DescriptionDefinition
+                        definition={t('singleSystem.summary.subheader1')}
+                      />
+                      <DescriptionTerm
+                        className="system-profile__subheader"
+                        term={systemInfo.businessOwnerOrg || ''}
+                      />
                     </Grid>
-                  </CollapsableLink>
-                </div>
-              </PageHeading>
-            </Grid>
-          </SummaryBox>
+                    <Grid desktop={{ col: 6 }} className="margin-bottom-2">
+                      <DescriptionDefinition
+                        definition={t('singleSystem.summary.subheader2')}
+                      />
+                      <DescriptionTerm
+                        className="system-profile__subheader"
+                        term={systemInfo.businessOwnerOrgComp || ''}
+                      />
+                    </Grid>
+                    <Grid desktop={{ col: 6 }} className="margin-bottom-2">
+                      <DescriptionDefinition
+                        definition={t('singleSystem.summary.subheader3')}
+                      />
+                      <DescriptionTerm
+                        className="system-profile__subheader"
+                        term={systemInfo.systemMaintainerOrg || ''}
+                      />
+                    </Grid>
+                    <Grid desktop={{ col: 6 }} className="margin-bottom-2">
+                      <DescriptionDefinition
+                        definition={t('singleSystem.summary.subheader4')}
+                      />
+                      <DescriptionTerm
+                        className="system-profile__subheader"
+                        term={systemInfo.id || ''}
+                      />
+                    </Grid>
+                  </Grid>
+                </CollapsableLink>
+              </div>
+            </PageHeading>
+          </Grid>
+        </SummaryBox>
 
-          {/* Button/Header to display when mobile/tablet */}
-          <div className="grid-container system-profile__nav">
-            <div
-              className={classnames('usa-overlay', {
-                'is-visible': isMobileSideNavExpanded
-              })}
-            />
-            <button
-              type="button"
-              className="usa-menu-btn easi-header__basic  system-profile__nav-button"
-              onClick={() => setIsMobileSideNavExpanded(true)}
-            >
-              <h3 className="padding-left-1">{t(`navigation.${subinfo}`)}</h3>
-              <span className="fa fa-bars" />
-            </button>
-          </div>
+        {/* Button/Header to display when mobile/tablet */}
+        <div className="grid-container system-profile__nav">
+          <div
+            className={classnames('usa-overlay', {
+              'is-visible': isMobileSideNavExpanded
+            })}
+          />
+          <button
+            type="button"
+            className="usa-menu-btn easi-header__basic  system-profile__nav-button"
+            onClick={() => setIsMobileSideNavExpanded(true)}
+          >
+            <h3 className="padding-left-1">{t(`navigation.${subinfo}`)}</h3>
+            <span className="fa fa-bars" />
+          </button>
+        </div>
 
-          <SectionWrapper className="margin-top-5 margin-bottom-5">
-            <Grid className="grid-container">
-              <Grid row>
-                <Grid desktop={{ col: 3 }}>
-                  {/* Side navigation for single system */}
-                  {!isMobile ? (
-                    <SideNav
+        <SectionWrapper className="margin-top-5 margin-bottom-5">
+          <Grid className="grid-container">
+            <Grid row>
+              <Grid desktop={{ col: 3 }}>
+                {/* Side navigation for single system */}
+                {!isMobile ? (
+                  <SideNav
+                    items={Object.keys(sideNavItems(systemInfo.id)).map(
+                      (key: string) => (
+                        <NavLink
+                          to={sideNavItems(systemInfo.id)[key].route}
+                          key={key}
+                          activeClassName="usa-current"
+                          className={classnames({
+                            'nav-group-border': sideNavItems(systemInfo.id)[key]
+                              .groupEnd
+                          })}
+                          exact
+                        >
+                          {t(`navigation.${key}`)}
+                        </NavLink>
+                      )
+                    )}
+                  />
+                ) : (
+                  <div ref={mobileSideNav} className={mobileSideNavClasses}>
+                    {/* Mobile Display */}
+                    <PrimaryNav
+                      onToggleMobileNav={() =>
+                        setIsMobileSideNavExpanded(false)
+                      }
+                      mobileExpanded={isMobileSideNavExpanded}
+                      aria-label="Side navigation"
                       items={Object.keys(sideNavItems(systemInfo.id)).map(
                         (key: string) => (
                           <NavLink
                             to={sideNavItems(systemInfo.id)[key].route}
                             key={key}
+                            onClick={() => setIsMobileSideNavExpanded(false)}
                             activeClassName="usa-current"
                             className={classnames({
                               'nav-group-border': sideNavItems(systemInfo.id)[
@@ -217,88 +239,58 @@ const SystemProfile = () => {
                         )
                       )}
                     />
-                  ) : (
-                    <div ref={mobileSideNav} className={mobileSideNavClasses}>
-                      {/* Mobile Display */}
-                      <PrimaryNav
-                        onToggleMobileNav={() =>
-                          setIsMobileSideNavExpanded(false)
-                        }
-                        mobileExpanded={isMobileSideNavExpanded}
-                        aria-label="Side navigation"
-                        items={Object.keys(sideNavItems(systemInfo.id)).map(
-                          (key: string) => (
-                            <NavLink
-                              to={sideNavItems(systemInfo.id)[key].route}
-                              key={key}
-                              onClick={() => setIsMobileSideNavExpanded(false)}
-                              activeClassName="usa-current"
-                              className={classnames({
-                                'nav-group-border': sideNavItems(systemInfo.id)[
-                                  key
-                                ].groupEnd
-                              })}
-                              exact
-                            >
-                              {t(`navigation.${key}`)}
-                            </NavLink>
-                          )
-                        )}
-                      />
-                    </div>
-                  )}
-                </Grid>
+                  </div>
+                )}
+              </Grid>
 
-                <Grid
-                  desktop={{ col: 6 }}
-                  className="padding-left-5 padding-right-5"
-                >
-                  {/* This renders the selected sidenav central component */}
-                  {sideNavItems(systemInfo.id)[subinfo || 'home'].component}
-                </Grid>
-                {/* Point of contact/ miscellaneous info */}
-                <Grid desktop={{ col: 3 }}>
-                  <div className="top-divider" />
-                  <p>{t('singleSystem.pointOfContact')}</p>
-                  <DescriptionTerm
-                    className="system-profile__subheader"
-                    term={systemInfo.businessOwnerOrgComp || ''}
-                  />
-                  <DescriptionDefinition
-                    definition={t('singleSystem.summary.subheader2')}
-                  />
-                  <p>
-                    <UswdsReactLink
-                      aria-label={t('singleSystem.sendEmail')}
-                      className="line-height-body-5"
-                      to="/" // TODO: Get link from CEDAR?
-                      variant="external"
-                      target="_blank"
-                    >
-                      {t('singleSystem.sendEmail')}
-                      <span aria-hidden>&nbsp;</span>
-                    </UswdsReactLink>
-                  </p>
-                  <p>
-                    <UswdsReactLink
-                      aria-label={t('singleSystem.moreContact')}
-                      className="line-height-body-5"
-                      to="/" // TODO: Get link from CEDAR?
-                      variant="external"
-                      target="_blank"
-                    >
-                      {t('singleSystem.moreContact')}
-                      <span aria-hidden>&nbsp;</span>
-                    </UswdsReactLink>
-                  </p>
-                </Grid>
+              <Grid
+                desktop={{ col: 6 }}
+                className="padding-left-5 padding-right-5"
+              >
+                {/* This renders the selected sidenav central component */}
+                {sideNavItems(systemInfo.id)[subinfo || 'home'].component}
+              </Grid>
+              {/* Point of contact/ miscellaneous info */}
+              <Grid desktop={{ col: 3 }}>
+                <div className="top-divider" />
+                <p>{t('singleSystem.pointOfContact')}</p>
+                <DescriptionTerm
+                  className="system-profile__subheader"
+                  term={systemInfo.businessOwnerOrgComp || ''}
+                />
+                <DescriptionDefinition
+                  definition={t('singleSystem.summary.subheader2')}
+                />
+                <p>
+                  <UswdsReactLink
+                    aria-label={t('singleSystem.sendEmail')}
+                    className="line-height-body-5"
+                    to="/" // TODO: Get link from CEDAR?
+                    variant="external"
+                    target="_blank"
+                  >
+                    {t('singleSystem.sendEmail')}
+                    <span aria-hidden>&nbsp;</span>
+                  </UswdsReactLink>
+                </p>
+                <p>
+                  <UswdsReactLink
+                    aria-label={t('singleSystem.moreContact')}
+                    className="line-height-body-5"
+                    to="/" // TODO: Get link from CEDAR?
+                    variant="external"
+                    target="_blank"
+                  >
+                    {t('singleSystem.moreContact')}
+                    <span aria-hidden>&nbsp;</span>
+                  </UswdsReactLink>
+                </p>
               </Grid>
             </Grid>
-          </SectionWrapper>
-        </div>
-      </MainContent>
-      <Footer />
-    </PageWrapper>
+          </Grid>
+        </SectionWrapper>
+      </div>
+    </MainContent>
   );
 };
 
