c1f78c44c3f3d923cab93a35bd5516dd2118b1a6
Dylan Sprague <dylan.sprague@oddball.io>
2022-01-28 17:03:32 -0500

    Adjust schema utility to have oneOf references to all object definitions

--- cmd/intake_schema_gen/main.go
+++ cmd/intake_schema_gen/main.go
@@ -1,49 +1,79 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"os"
+	"path/filepath"
+	"reflect"
 
 	"github.com/alecthomas/jsonschema"
 
 	"github.com/cmsgov/easi-app/pkg/cedar/intake/models"
 )
 
-type emptyStruct struct {
+// EASIObject represents all possible types of objects that can be sent to CEDAR from EASi
+type EASIObject struct {
 }
 
-func printSchema(schema *jsonschema.Schema) {
-	bytes, err := schema.MarshalJSON()
-	if err != nil {
-		fmt.Println("Error marshaling schema")
-	} else {
-		fmt.Println(string(bytes))
+func createRefTypeToDefinition(defName string) *jsonschema.Type {
+	return &jsonschema.Type{
+		Ref: fmt.Sprint("#/definitions/", defName),
 	}
 }
 
-func addDefinitionsToRootSchema(rootSchema *jsonschema.Schema, objectSchema *jsonschema.Schema) {
+func writeSchemaToFile(schema *jsonschema.Schema) {
+	schemaFileName := "cedar_intake_schema.json"
+	fullFileName := filepath.Join("pkg", "cedar", "intake", schemaFileName)
+
+	marshaledSchema, err := schema.MarshalJSON()
+	if err != nil {
+		fmt.Println("Error marshaling schema")
+		fmt.Println(err)
+		return
+	}
+
+	var prettyJSON bytes.Buffer
+	err = json.Indent(&prettyJSON, marshaledSchema, "", "  ")
+	if err != nil {
+		fmt.Println("Error pretty-printing JSON")
+		fmt.Println(err)
+		return
+	}
+
+	err = os.WriteFile(fullFileName, prettyJSON.Bytes(), 0600)
+	if err != nil {
+		fmt.Println("Error writing schema")
+		fmt.Println(err)
+	}
+}
+
+func addDefinitionsToRootSchema(rootSchema *jsonschema.Schema, objectSchema *jsonschema.Schema, rootObjectName string) {
+	refToRootObject := createRefTypeToDefinition(rootObjectName)
+	rootSchema.Definitions["EASIObject"].OneOf = append(rootSchema.Definitions["EASIObject"].OneOf, refToRootObject)
+
 	for objectName, objectType := range objectSchema.Definitions {
 		rootSchema.Definitions[objectName] = objectType
 	}
 }
 
 func main() {
-	rootSchema := jsonschema.Reflect(&emptyStruct{})
+	rootSchema := jsonschema.Reflect(&EASIObject{})
 
-	objectSchemas := []*jsonschema.Schema{
-		jsonschema.Reflect(&models.EASIAction{}),
-		jsonschema.Reflect(&models.EASIBizCase{}),
-		jsonschema.Reflect(&models.EASIBusinessSolution{}),
-		jsonschema.Reflect(&models.EASIGrtFeedback{}),
-		jsonschema.Reflect(&models.EASIIntake{}),
-		jsonschema.Reflect(&models.EASILifecycleCost{}),
-		jsonschema.Reflect(&models.EASINote{}),
+	objects := []interface{}{
+		models.EASIAction{},
+		models.EASIBizCase{},
+		models.EASIBusinessSolution{},
+		models.EASIGrtFeedback{},
+		models.EASIIntake{},
+		models.EASILifecycleCost{},
+		models.EASINote{},
 	}
 
-	for _, objectSchema := range objectSchemas {
-		addDefinitionsToRootSchema(rootSchema, objectSchema)
+	for _, object := range objects {
+		addDefinitionsToRootSchema(rootSchema, jsonschema.Reflect(object), reflect.TypeOf(object).Name())
 	}
 
-	// TODO - adjust rootSchema $ref
-
-	printSchema(rootSchema)
+	writeSchemaToFile(rootSchema)
 }
