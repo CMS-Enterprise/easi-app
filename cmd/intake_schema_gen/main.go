3591047da0849d51f634a9fecfc3352d1d422796
Dylan Sprague <dylan.sprague@oddball.io>
2022-02-03 18:37:25 -0500

    Clarify package name in schema generator Go script

--- cmd/intake_schema_gen/main.go
+++ cmd/intake_schema_gen/main.go
@@ -10,7 +10,7 @@ import (
 	"github.com/alecthomas/jsonschema"
 
 	"github.com/cmsgov/easi-app/pkg/cedar/intake"
-	"github.com/cmsgov/easi-app/pkg/cedar/intake/models"
+	intakemodels "github.com/cmsgov/easi-app/pkg/cedar/intake/models"
 )
 
 func generateSchema(object interface{}, version intake.SchemaVersion, filename string) {
@@ -47,12 +47,12 @@ func generateSchema(object interface{}, version intake.SchemaVersion, filename s
 // Note for future devs: the go/types package could be used to extract static type information on each file in that directory,
 // but there's no way to convert a types.Type to the reflect.Type that the jsonschema package could use.
 func main() {
-	generateSchema(models.EASIAction{}, intake.IntakeInputSchemaEASIActionV01, "easi_action.json")
+	generateSchema(intakemodels.EASIAction{}, intake.IntakeInputSchemaEASIActionV01, "easi_action.json")
 
 	// includes business solution and lifecycle cost as sub-items
-	generateSchema(models.EASIBizCase{}, intake.IntakeInputSchemaEASIBizCaseV01, "easi_business_case.json")
+	generateSchema(intakemodels.EASIBizCase{}, intake.IntakeInputSchemaEASIBizCaseV01, "easi_business_case.json")
 
-	generateSchema(models.EASIGrtFeedback{}, intake.IntakeInputSchemaEASIGrtFeedbackV01, "easi_grt_feedback.json")
-	generateSchema(models.EASIIntake{}, intake.IntakeInputSchemaEASIIntakeV01, "easi_system_intake.json")
-	generateSchema(models.EASINote{}, intake.IntakeInputSchemaEASINoteV01, "easi_note.json")
+	generateSchema(intakemodels.EASIGrtFeedback{}, intake.IntakeInputSchemaEASIGrtFeedbackV01, "easi_grt_feedback.json")
+	generateSchema(intakemodels.EASIIntake{}, intake.IntakeInputSchemaEASIIntakeV01, "easi_system_intake.json")
+	generateSchema(intakemodels.EASINote{}, intake.IntakeInputSchemaEASINoteV01, "easi_note.json")
 }
