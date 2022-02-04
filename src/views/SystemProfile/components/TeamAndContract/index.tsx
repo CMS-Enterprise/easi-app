8a6aaabe37f1b69240c4e33e6bcd09f24149fe53
adamodd <adam.suharja@oddball.io>
2022-02-04 15:59:03 -0500

    System team styles

--- src/views/SystemProfile/components/TeamAndContract/index.tsx
+++ src/views/SystemProfile/components/TeamAndContract/index.tsx
@@ -16,6 +16,8 @@ import Divider from 'components/shared/Divider';
 // import classnames from 'classnames';
 import { GetCedarSystems_cedarSystems as CedarSystemProps } from 'queries/types/GetCedarSystems';
 
+import './index.scss';
+
 type TeamAndContractProps = {
   className?: string;
   children?: React.ReactNode;
@@ -57,178 +59,202 @@ const SystemTeamAndContract = ({
 }: TeamAndContractProps) => {
   const { t } = useTranslation('systemProfile');
   return (
-    <GridContainer>
-      <Grid row gap>
-        <Grid desktop={{ col: 9 }}>
-          <PageHeading
-            headingLevel="h2"
-            className="margin-top-0 margin-bottom-4"
-          >
-            {t('teamAndContract.header.teamAndContract')}
-          </PageHeading>
-          <GridContainer className="padding-x-0">
-            <Grid row>
-              <Grid tablet={{ col: true }}>
-                <div>
-                  <strong>
-                    {t('teamAndContract.federalFullTimeEmployees')}
-                  </strong>
-                </div>
-                <div>5</div>
+    <div id="system-team-and-contract">
+      <GridContainer>
+        <Grid row gap>
+          <Grid desktop={{ col: 9 }}>
+            <PageHeading
+              headingLevel="h2"
+              className="margin-top-0 margin-bottom-4"
+            >
+              {t('teamAndContract.header.teamAndContract')}
+            </PageHeading>
+            <GridContainer className="padding-x-0">
+              <Grid row>
+                <Grid tablet={{ col: true }}>
+                  <div>
+                    <strong>
+                      {t('teamAndContract.federalFullTimeEmployees')}
+                    </strong>
+                  </div>
+                  <div>5</div>
+                </Grid>
+                <Grid tablet={{ col: true }}>
+                  <div>
+                    <strong>
+                      {t('teamAndContract.contractorFullTimeEmployees')}
+                    </strong>
+                  </div>
+                  <div>6</div>
+                </Grid>
               </Grid>
-              <Grid tablet={{ col: true }}>
-                <div>
-                  <strong>
-                    {t('teamAndContract.contractorFullTimeEmployees')}
-                  </strong>
-                </div>
-                <div>6</div>
-              </Grid>
-            </Grid>
-          </GridContainer>
-          <Divider className="margin-top-5" />
-          <PageHeading headingLevel="h2" className="margin-top-5 margin-top-4">
-            {t('teamAndContract.header.contractInformation')}
-          </PageHeading>
-          <CardGroup>
-            {vendorsData.map(vendor => {
-              return (
-                <Card className="grid-col-12 margin-bottom-2">
+            </GridContainer>
+            <Divider className="margin-top-5" />
+            <PageHeading
+              headingLevel="h2"
+              className="margin-top-5 margin-top-4"
+            >
+              {t('teamAndContract.header.contractInformation')}
+            </PageHeading>
+            <CardGroup>
+              {vendorsData.map(vendor => {
+                return (
+                  <Card className="grid-col-12 margin-bottom-2">
+                    <CardHeader className="padding-2 padding-bottom-0">
+                      <PageHeading
+                        headingLevel="h5"
+                        className="margin-y-0 font-sans-2xs text-normal"
+                      >
+                        {t('teamAndContract.vendors')}
+                      </PageHeading>
+                      <PageHeading
+                        headingLevel="h3"
+                        className="margin-y-0 line-height-sans-3"
+                      >
+                        {vendor.vendors.map(name => (
+                          <div>{name}</div>
+                        ))}
+                      </PageHeading>
+                      <div className="padding-top-2">
+                        <strong>
+                          {t('teamAndContract.contractAwardDate')}
+                        </strong>
+                      </div>
+                      <div>{vendor.contractAwardDate}</div>
+                      <Divider className="margin-y-2" />
+                    </CardHeader>
+                    <CardBody className="padding-2 padding-top-0">
+                      <PageHeading
+                        headingLevel="h5"
+                        className="margin-top-0 margin-bottom-1 font-sans-2xs text-normal"
+                      >
+                        {t('teamAndContract.periodOfPerformance')}
+                      </PageHeading>
+                      <GridContainer className="padding-x-0">
+                        <Grid row>
+                          <Grid col>
+                            <div>
+                              <strong>{t('teamAndContract.startDate')}</strong>
+                            </div>
+                            <div>{vendor.popStartDate}</div>
+                          </Grid>
+                          <Grid col>
+                            <div>
+                              <strong>{t('teamAndContract.endDate')}</strong>
+                            </div>
+                            <div>{vendor.popEndDate}</div>
+                          </Grid>
+                        </Grid>
+                      </GridContainer>
+                      <Divider className="margin-y-2" />
+                      <PageHeading
+                        headingLevel="h5"
+                        className="margin-y-0 font-sans-2xs text-normal"
+                      >
+                        {t('teamAndContract.contractNumber')}
+                      </PageHeading>
+                      <PageHeading headingLevel="h3" className="margin-y-0">
+                        {vendor.contractNumber}
+                      </PageHeading>
+                      <PageHeading
+                        headingLevel="h5"
+                        className="margin-bottom-0 margin-top-2 font-sans-2xs text-normal"
+                      >
+                        {t('teamAndContract.technologyFunctions')}
+                      </PageHeading>
+                      <div>
+                        {vendor.technologyFunctions.map(name => (
+                          <Tag
+                            key={name}
+                            className="system-profile__tag text-base-darker bg-base-lighter margin-bottom-1"
+                          >
+                            {name}
+                          </Tag>
+                        ))}
+                      </div>
+                      <PageHeading
+                        headingLevel="h5"
+                        className="margin-bottom-0 margin-top-2 font-sans-2xs text-normal"
+                      >
+                        {t('teamAndContract.assetsOrServices')}
+                      </PageHeading>
+                      <div>
+                        {vendor.assetsOrServices.map(name => (
+                          <Tag
+                            key={name}
+                            className="system-profile__tag text-base-darker bg-base-lighter margin-bottom-1"
+                          >
+                            {name}
+                          </Tag>
+                        ))}
+                      </div>
+                    </CardBody>
+                  </Card>
+                );
+              })}
+            </CardGroup>
+            <Divider className="margin-top-5" />
+            <PageHeading
+              headingLevel="h2"
+              className="margin-top-5 margin-bottom-4"
+            >
+              {t('teamAndContract.header.pointsOfContact')}
+            </PageHeading>
+            <CardGroup>
+              {contactListData.map(contact => (
+                <Card key={contact.id} className="grid-col-12 margin-bottom-2">
                   <CardHeader className="padding-2 padding-bottom-0">
-                    <PageHeading headingLevel="h5" className="margin-y-0">
-                      {t('teamAndContract.vendors')}
+                    <PageHeading
+                      headingLevel="h5"
+                      className="margin-y-0 font-sans-2xs text-normal"
+                    >
+                      {contact.role}
                     </PageHeading>
                     <PageHeading headingLevel="h3" className="margin-y-0">
-                      {vendor.vendors.map(name => (
-                        <div>{name}</div>
-                      ))}
+                      {contact.name}
                     </PageHeading>
                     <div>
-                      <strong>{t('teamAndContract.contractAwardDate')}</strong>
+                      <UswdsReactLink
+                        className="line-height-body-5"
+                        to={contact.email}
+                        variant="external"
+                        target="_blank"
+                      >
+                        {contact.email}
+                      </UswdsReactLink>
                     </div>
-                    <div>{vendor.contractAwardDate}</div>
                     <Divider className="margin-y-2" />
-                    <div />
                   </CardHeader>
                   <CardBody className="padding-2 padding-top-0">
-                    <PageHeading headingLevel="h5" className="margin-y-0">
-                      {t('teamAndContract.periodOfPerformance')}
-                    </PageHeading>
-                    <GridContainer className="padding-x-0">
-                      <Grid row>
-                        <Grid col>
-                          <div>
-                            <strong>{t('teamAndContract.startDate')}</strong>
-                          </div>
-                          <div>{vendor.popStartDate}</div>
-                        </Grid>
-                        <Grid col>
-                          <div>
-                            <strong>{t('teamAndContract.endDate')}</strong>
-                          </div>
-                          <div>{vendor.popEndDate}</div>
-                        </Grid>
-                      </Grid>
-                    </GridContainer>
-                    <Divider className="margin-y-2" />
-                    <PageHeading headingLevel="h5" className="margin-y-0">
-                      {t('teamAndContract.contractNumber')}
-                    </PageHeading>
-                    <PageHeading headingLevel="h3" className="margin-y-0">
-                      {vendor.contractNumber}
-                    </PageHeading>
-                    <PageHeading headingLevel="h5" className="margin-y-0">
-                      {t('teamAndContract.technologyFunctions')}
-                    </PageHeading>
-                    <div>
-                      {vendor.technologyFunctions.map(name => (
-                        <Tag
-                          key={name}
-                          className="system-profile__tag text-base-darker bg-base-lighter margin-bottom-1"
-                        >
-                          {name}
-                        </Tag>
-                      ))}
-                    </div>
-                    <PageHeading headingLevel="h5" className="margin-y-0">
-                      {t('teamAndContract.assetsOrServices')}
-                    </PageHeading>
-                    <div>
-                      {vendor.assetsOrServices.map(name => (
-                        <Tag
-                          key={name}
-                          className="system-profile__tag text-base-darker bg-base-lighter margin-bottom-1"
-                        >
-                          {name}
-                        </Tag>
-                      ))}
-                    </div>
+                    {contact.org}
                   </CardBody>
-                  <div />
                 </Card>
-              );
-            })}
-          </CardGroup>
-          <Divider className="margin-top-5" />
-          <PageHeading
-            headingLevel="h2"
-            className="margin-top-5 margin-bottom-4"
-          >
-            {t('teamAndContract.header.pointsOfContact')}
-          </PageHeading>
-          <CardGroup>
-            {contactListData.map(contact => (
-              <Card key={contact.id} className="grid-col-12 margin-bottom-2">
-                <CardHeader className="padding-2 padding-bottom-0">
-                  <PageHeading headingLevel="h5" className="margin-y-0">
-                    {contact.role}
-                  </PageHeading>
-                  <PageHeading headingLevel="h3" className="margin-y-0">
-                    {contact.name}
-                  </PageHeading>
-                  <div>
-                    <UswdsReactLink
-                      className="line-height-body-5"
-                      to={contact.email}
-                      variant="external"
-                      target="_blank"
-                    >
-                      {contact.email}
-                    </UswdsReactLink>
-                  </div>
-                  <Divider className="margin-y-2" />
-                  <div />
-                </CardHeader>
-                <CardBody className="padding-2 padding-top-0">
-                  {contact.org}
-                </CardBody>
-                <div />
-              </Card>
-            ))}
-          </CardGroup>
+              ))}
+            </CardGroup>
+          </Grid>
+          <Grid desktop={{ col: 3 }}>
+            <PageHeading headingLevel="h5" className="margin-y-0">
+              {t('teamAndContract.pointOfContact')}
+            </PageHeading>
+            <PageHeading headingLevel="h2" className="margin-y-0">
+              {pointOfContactData.name}
+            </PageHeading>
+            <div>{pointOfContactData.role}</div>
+            <div>
+              <UswdsReactLink
+                aria-label={t('teamAndContract.sendAnEmail')}
+                className="line-height-body-5"
+                to={pointOfContactData.email}
+                variant="external"
+                target="_blank"
+              >
+                {t('teamAndContract.sendAnEmail')}
+              </UswdsReactLink>
+            </div>
+          </Grid>
         </Grid>
-        <Grid desktop={{ col: 3 }}>
-          <PageHeading headingLevel="h5" className="margin-y-0">
-            {t('teamAndContract.pointOfContact')}
-          </PageHeading>
-          <PageHeading headingLevel="h2" className="margin-y-0">
-            {pointOfContactData.name}
-          </PageHeading>
-          <div>{pointOfContactData.role}</div>
-          <div>
-            <UswdsReactLink
-              aria-label={t('teamAndContract.sendAnEmail')}
-              className="line-height-body-5"
-              to={pointOfContactData.email}
-              variant="external"
-              target="_blank"
-            >
-              {t('teamAndContract.sendAnEmail')}
-            </UswdsReactLink>
-          </div>
-        </Grid>
-      </Grid>
-    </GridContainer>
+      </GridContainer>
+    </div>
   );
 };
 
