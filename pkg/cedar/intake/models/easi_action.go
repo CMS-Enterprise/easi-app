1822f064109cf0233efdc29ed695c9aa4e4b9dd7
Dylan Sprague <dylan.sprague@oddball.io>
2022-02-04 17:25:38 -0500

    Test commit for directly running gen_intake_schema as pre-commit hook

--- pkg/cedar/intake/models/easi_action.go
+++ pkg/cedar/intake/models/easi_action.go
@@ -2,7 +2,7 @@ package models
 
 // EASIAction represents an action taken by an EASi user on a system intake
 type EASIAction struct {
-	ActionType string `json:"actionType"`
+	ActionType string `json:"ACTIONTYPE"`
 	ActorEUA   string `json:"actorEUA"`
 	Feedback   string `json:"feedback"` // Feedback sent to requestor via email
 	IntakeID   string `json:"intakeId"`
