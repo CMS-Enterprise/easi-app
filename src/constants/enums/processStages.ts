6339a20edf2ca82031014a135fb85432db2817e8
Patrick Segura <patrick.segura@oddball.io>
2022-03-01 10:14:15 -0500

    Update processStages.ts

--- src/constants/enums/processStages.ts
+++ src/constants/enums/processStages.ts
@@ -8,10 +8,6 @@ const processStages = [
     name: 'I have done some initial research'
   },
   {
-    value: 'funded',
-    name: 'The project is already funded'
-  },
-  {
     value: 'inDevelopment',
     name: 'Initial development underway'
   },
