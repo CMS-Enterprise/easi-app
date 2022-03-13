cc740be000acd342493b259ae1cb62435ccfc45c
Gary Zhao <gary.j.zhao@gmail.com>
2022-03-12 20:35:42 -0500

    subNavItem array to dry up code

--- src/views/GovernanceReviewTeam/SubNav/SubNav.tsx
+++ src/views/GovernanceReviewTeam/SubNav/SubNav.tsx
@@ -1,16 +1,54 @@
 import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import { NavLink } from 'react-router-dom';
 import {
   IconArrowBack,
   IconExpandLess,
   IconExpandMore
 } from '@trussworks/react-uswds';
+import classnames from 'classnames';
 
-// import classNames from 'classnames';
 import './index.scss';
 
-const SubNav = () => {
+type SubNavProps = {
+  systemId: string;
+};
+
+const SubNav = ({ systemId }: SubNavProps) => {
+  const { t } = useTranslation('governanceReviewTeam');
   const [isSubNavOpen, setIsSubNavOpen] = useState<boolean>(false);
+
+  const subNavItems = [
+    {
+      to: `/governance-review-team/${systemId}/intake-request`,
+      text: 'general:intake'
+    },
+    {
+      to: `/governance-review-team/${systemId}/business-case`,
+      text: 'general:businessCase'
+    },
+    {
+      to: `/governance-review-team/${systemId}/decision`,
+      text: 'decision.title'
+    },
+    {
+      to: `/governance-review-team/${systemId}/lcid`,
+      text: 'lifecycleID.title',
+      groupEnd: true
+    },
+    {
+      to: `/governance-review-team/${systemId}/actions`,
+      text: 'actions'
+    },
+    {
+      to: `/governance-review-team/${systemId}/notes`,
+      text: 'notes.heading'
+    },
+    {
+      to: `/governance-review-team/${systemId}/dates`,
+      text: 'dates.heading'
+    }
+  ];
 
   return (
     <div className="easi-grt__subNav-accordion">
@@ -29,41 +67,31 @@ const SubNav = () => {
       {isSubNavOpen && (
         <nav className="easi-grt__subNav__list-container">
           <ul className="easi-grt__subNav__list subNav">
+            {subNavItems.map(subNavItem => (
+              <li
+                className={classnames({
+                  'subNav__item--group-border': subNavItem.groupEnd
+                })}
+              >
+                <NavLink
+                  to={subNavItem.to}
+                  key={subNavItem.to}
+                  className={classnames({
+                    'subNav--current': false
+                  })}
+                >
+                  {t(subNavItem.text)}
+                </NavLink>
+              </li>
+            ))}
             <li>
-              <NavLink
-                to="/governance-review-team/3/intake-request"
-                key="/governance-review-team/3/intake-request"
-              >
-                general:intake
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/governance-review-team/3/intake-request"
-                key="/governance-review-team/3/intake-request"
-                // TODO: Determine how this works
-                // activeClassName="subNav--current"
-                className="subNav--current"
-              >
-                general:intake
-              </NavLink>
-            </li>
-            <li className="subNav__item--group-border">
-              <NavLink
-                to="/governance-review-team/3/intake-request"
-                key="/governance-review-team/3/intake-request"
-              >
-                general:intake
-              </NavLink>
-            </li>
-            <li className="">
               <NavLink
                 to="/"
                 key="home"
                 className="display-flex flex-align-center"
               >
                 <IconArrowBack className="margin-right-1" aria-hidden />
-                Back to all requests
+                {t('back.allRequests')}
               </NavLink>
             </li>
           </ul>
