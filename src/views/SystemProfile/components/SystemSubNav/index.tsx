10255989f23c0d56f4483cc2a84feb70c7499b86
Gary Zhao <gary.j.zhao@gmail.com>
2022-03-15 14:41:04 -0400

    System Sub Navigation

---
+++ src/views/SystemProfile/components/SystemSubNav/index.tsx
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { NavLink } from 'react-router-dom';
+import { IconExpandLess, IconExpandMore } from '@trussworks/react-uswds';
+import classnames from 'classnames';
+
+import useCheckResponsiveScreen from 'hooks/checkMobile';
+
+import sideNavItems from '..';
+
+import './index.scss';
+
+type SystemSubNavProps = {
+  subinfo: string;
+  systemInfo: any;
+};
+
+const SystemSubNav = ({ subinfo, systemInfo }: SystemSubNavProps) => {
+  const { t } = useTranslation('systemProfile');
+  const isMobile = useCheckResponsiveScreen('tablet');
+  const [isAccordionOpen, setIsAccordionOpen] = useState<boolean>(false);
+
+  useEffect(() => {
+    // Fixes edge case: subnavigation remains open when user (when in small screen size) expands window to large size really fast (using window manager)
+    if (!isMobile) {
+      setIsAccordionOpen(false);
+    }
+  }, [isMobile]);
+
+  return (
+    <div className="easi-grt__subNav-accordion">
+      <button
+        type="button"
+        className="usa-menu-btn easi-header__basic width-full"
+        onClick={() => setIsAccordionOpen(!isAccordionOpen)}
+        aria-expanded={isAccordionOpen}
+        aria-controls="easi-grt__subNav"
+      >
+        <h3 className="padding-left-1">{t(`navigation.${subinfo}`)}</h3>
+        {!isAccordionOpen ? (
+          <IconExpandMore size={3} />
+        ) : (
+          <IconExpandLess size={3} />
+        )}
+      </button>
+      {isAccordionOpen && (
+        <div id="easi-grt__subNav" className="easi-grt__subNav__list-container">
+          <ul className="easi-grt__subNav__list subNav">
+            {Object.keys(sideNavItems(systemInfo)).map((key: string) => (
+              <li
+                key={key}
+                className={classnames({
+                  'subNav__item--group-border': sideNavItems(systemInfo)[key]
+                    .groupEnd
+                })}
+              >
+                <NavLink
+                  to={sideNavItems(systemInfo)[key].route}
+                  key={key}
+                  className={classnames({
+                    'subNav--current': key === subinfo
+                  })}
+                  onClick={() => setIsAccordionOpen(!isAccordionOpen)}
+                >
+                  {t(`navigation.${key}`)}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SystemSubNav;
