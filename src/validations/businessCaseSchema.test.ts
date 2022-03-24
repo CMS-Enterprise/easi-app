fecb4da7da838b82b87b93ba9b9fa9b383fad167
Gary Zhao <gary.j.zhao@gmail.com>
2022-03-24 15:14:37 -0400

    Added tests relavant to ticket

---
+++ src/validations/businessCaseSchema.test.ts
@@ -0,0 +1,34 @@
+import { BusinessCaseDraftValidationSchema } from './businessCaseSchema';
+
+describe('Draft Business Case schema validation', () => {
+  ['1234567890', '123-456-7890'].forEach(value => {
+    it(`accepts valid phone number: ${value}`, async () => {
+      const result = await BusinessCaseDraftValidationSchema.generalRequestInfo
+        .validateAt('requester.phoneNumber', {
+          requester: {
+            phoneNumber: '1234567890'
+          }
+        })
+        .catch((err: any) => {
+          return err;
+        });
+      expect(result.errors).toBeUndefined();
+    });
+  });
+  ['123456789', '123-456-789', 'asdf'].forEach(value => {
+    it(`throws error for the following values: ${value}`, async () => {
+      const result = await BusinessCaseDraftValidationSchema.generalRequestInfo
+        .validateAt('requester.phoneNumber', {
+          requester: {
+            phoneNumber: value
+          }
+        })
+        .catch((err: any) => {
+          return err;
+        });
+      expect(result.errors).toEqual([
+        'Enter the requester’s phone number like 1234567890 or 123-456-7890'
+      ]);
+    });
+  });
+});
