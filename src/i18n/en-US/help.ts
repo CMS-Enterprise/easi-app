4cdf00262a27951815f5e984c64566ec79f814bd
Gary Zhao <gary.j.zhao@gmail.com>
2022-04-05 14:51:15 -0400

    add copy to i18n.ts

--- src/i18n/en-US/help.ts
+++ src/i18n/en-US/help.ts
@@ -1,2 +1,38 @@
-const help = {};
+const help = {
+  heading: "We're here to help.",
+  subheading:
+    'Get assistance with your governance processes and requests, 508 testing, and system information.',
+  additionalContacts: {
+    heading: 'Additional contacts',
+    subheading:
+      'Still need help? Use the contact information below to contact the group you need.',
+    contacts: {
+      grAdmin: {
+        title: 'Governance Review Admin',
+        copy:
+          'The Governance Review Admin team can help with questions related to your IT Governance requests.',
+        email: 'IT_Governance@cms.hhs.gov'
+      },
+      section508: {
+        title: 'Section 508',
+        copy:
+          'The Section 508 team can help with questions related to your 508 testing requests, including those about COTS and GOTS products.',
+        email: 'CMS_Section508@cms.hhs.gov'
+      },
+      enterpriseArchitecture: {
+        title: 'Enterprise Architecture',
+        copy:
+          'The Enterprise Architecture team can help with questions related to your Enterprise Architecture needs.',
+        email: 'EnterpriseArchitecture@cms.hhs.gov'
+      },
+      itNavigator: {
+        title: 'IT Navigator',
+        copy:
+          'The IT Navigator team can help with questions related to choosing the type of IT Governance request that fits your needs. ',
+        email: 'EnterpriseArchitecture@cms.hhs.gov'
+      }
+    }
+  }
+};
+
 export default help;
