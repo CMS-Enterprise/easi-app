d22cc801b8323de865f5af1b598013e13f065d9a
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2022-04-08 09:48:27 -0500

    Added contacts to help translation file

--- src/i18n/en-US/help.ts
+++ src/i18n/en-US/help.ts
@@ -2,7 +2,48 @@ const help = {
   read: 'Read',
   relatedHelp: 'Related help articles',
   relatedDescription:
+<<<<<<< Updated upstream
     'Below are some additional help articles that you may find useful.'
+=======
+    'Below are some additional help articles that you may find useful.',
+  back: 'Back',
+  additionalContacts: {
+    heading: 'Additional contacts',
+    subheading:
+      'Still need help? Use the contact information below to contact the group you need.',
+    emailHeading: 'Email addresses',
+    contacts: {
+      govReview: {
+        title: 'Governance Review Admin',
+        type: 'IT Governance',
+        content:
+          'The Governance Review Admin team can help with questions related to your IT Governance requests.',
+        email: 'IT_Governance@cms.hhs.gov'
+      },
+      enterprise: {
+        title: 'Enterprise Architecture',
+        type: 'IT Governance',
+        content:
+          'The Enterprise Architecture team can help with questions related to your Enterprise Architecture needs.',
+        email: 'EnterpriseArchitecture@cms.hhs.gov'
+      },
+      itNavigator: {
+        title: 'IT Navigator',
+        type: 'IT Governance',
+        content:
+          'The IT Navigator team can help with questions related to choosing the type of IT Governance request that fits your needs.',
+        email: 'NavigatorInquiries@cms.hhs.gov'
+      },
+      section508: {
+        title: 'Section 508',
+        type: 'Section 508',
+        content:
+          'The Section 508 team can help with questions related to your 508 testing requests, including those about COTS and GOTS products.',
+        email: 'CMS_Section508@cms.hhs.gov'
+      }
+    }
+  }
+>>>>>>> Stashed changes
 };
 
 export default help;
