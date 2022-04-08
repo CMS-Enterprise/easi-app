108a037ebdc23368be9826236152a20af666e59a
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2022-04-08 09:49:18 -0500

    Added tests

---
+++ src/components/HelpContacts/index.test.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+
+import HelpContacts from './index';
+
+describe('HelpContacts', () => {
+  it('matches the snapshot', () => {
+    const { asFragment } = render(
+      <MemoryRouter>
+        <HelpContacts type="IT Governance" />
+      </MemoryRouter>
+    );
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('renders correct contacts when array is passed', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <HelpContacts type={['IT Governance', 'Section 508']} />
+      </MemoryRouter>
+    );
+
+    expect(getByText('Governance Review Admin')).toBeInTheDocument();
+    expect(getByText('Section 508')).toBeInTheDocument();
+  });
+});
