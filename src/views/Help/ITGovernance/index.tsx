eb0b92e8f3488de0d9f457af5b61e5125cd5bbba
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2022-04-11 09:20:16 -0500

    Added <HelpBreadcrumb>

--- src/views/Help/ITGovernance/index.tsx
+++ src/views/Help/ITGovernance/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Route, Switch, useHistory } from 'react-router-dom';
-import { Button, CardGroup, IconArrowBack } from '@trussworks/react-uswds';
+import { Route, Switch } from 'react-router-dom';
+import { CardGroup } from '@trussworks/react-uswds';
 
 import ArticleCard from 'components/ArticleCard';
+import HelpBreadcrumb from 'components/HelpBreadcrumb';
 import HelpContacts from 'components/HelpContacts';
 import HelpPageIntro from 'components/HelpPageIntro';
 import Divider from 'components/shared/Divider';
@@ -11,26 +12,11 @@ import Divider from 'components/shared/Divider';
 import itGovernanceArticles from './articles';
 import PrepareForGRT from './PrepareForGRT';
 
-const Breadcrumb = () => {
-  const history = useHistory();
-  const { t } = useTranslation('help');
-  return (
-    <Button
-      type="button"
-      unstyled
-      onClick={() => history.goBack()}
-      className="margin-top-7"
-    >
-      <IconArrowBack className="margin-right-05 margin-top-3px text-tbottom" />
-      {t('back')}
-    </Button>
-  );
-};
-
 const PageContent = () => {
   const { t } = useTranslation('help');
   return (
     <>
+      <HelpBreadcrumb type="Back" />
       <HelpPageIntro
         heading={t('itGovernance.heading')}
         subheading={t('itGovernance.subheading')}
@@ -49,7 +35,6 @@ const PageContent = () => {
 const ITGovernance = () => {
   return (
     <>
-      <Breadcrumb />
       <Switch>
         <Route
           path="/help/it-governance/prepare-for-grt"
