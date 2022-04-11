77da6d26d99ece8b4e5b2de5757b37556d0d0156
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2022-04-11 11:48:45 -0500

    Updated test

--- src/views/Help/ITGovernance/index.test.tsx
+++ src/views/Help/ITGovernance/index.test.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter, Route } from 'react-router-dom';
 import { render } from '@testing-library/react';
 
 import ITGovernance from './index';
 
-describe('ITGovernance', () => {
+describe('IT Governance Help', () => {
   it('matches the snapshot', () => {
     const { asFragment } = render(
-      <MemoryRouter>
-        <ITGovernance />
+      <MemoryRouter initialEntries={['/help/it-governance']}>
+        <Route path="/help/it-governance">
+          <ITGovernance />
+        </Route>
       </MemoryRouter>
     );
     expect(asFragment()).toMatchSnapshot();
