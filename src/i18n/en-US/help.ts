87209cc8e1850c8f98eb58cea8beca5c2173219d
Patrick Segura <95709965+patrickseguraoddball@users.noreply.github.com>
2022-04-11 07:20:57 -0700

    Easi 1897 - Help center - Section 508 (#1604)
    * <HelpBreadcrumb> component
    
    * <HelpTag> component
    
    * <HelpPageIntro> component
    
    * Added contacts to help translation file
    
    * <HelpContacts> component
    
    * Added tests
    
    * Updated breadcrumb margin and snapshot
    
    * Update translation file
    
    * Updated tag spacing
    
    * Updated snapshots
    
    * Init section 508 help work
    
    * General 508 help layout/work
    
    * Added snapshot testing to section 508
    
    * Adjusted text cutoff and updated snapshot
    
    * Removed snapshot write command
    
    Co-authored-by: Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>

--- src/i18n/en-US/help.ts
+++ src/i18n/en-US/help.ts
@@ -4,6 +4,12 @@ const help = {
   relatedDescription:
     'Below are some additional help articles that you may find useful.',
   back: 'Back',
+  section508: {
+    heading: 'Section 508',
+    subheading:
+      'Get assistance with Section 508 processes and requirements, such as 508 testing. To learn more about Section 508, please visit ',
+    website: 'www.section508.gov'
+  },
   additionalContacts: {
     heading: 'Additional contacts',
     subheading:
--- src/views/Help/Section508/articles.ts
+++ src/views/Help/Section508/articles.ts
@@ -5,7 +5,7 @@ const section508Articles: ArticleProps[] = [
   {
     route: '/section-508/steps-involved', // route for hitting rendered article component
     type: 'Section 508', // Used for tagging of article
-    translation: 'accessibility' // Should reference the translation used to index the title and description for cards
+    translation: 'stepsInvolved508' // Should reference the translation used to index the title and description for cards
   },
   {
     route: '/section-508/template-508', // route for hitting rendered article component
