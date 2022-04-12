2a6e770c919cdbbb1389024e1f2701a61f425e98
ClayBenson94 <contact@claybenson.me>
2022-04-12 10:47:09 -0400

    Modified 'As Is' removal migration

--- migrations/V104__Business_Case_As_Is_Changes.sql
+++ migrations/V104__Business_Case_As_Is_Changes.sql
@@ -1,28 +1,41 @@
---  Removing 'As Is' from lifecycle_cost_solution enum
-ALTER TYPE lifecycle_cost_solution 
-    RENAME TO lifecycle_cost_solution_temp;
-    
-CREATE TYPE lifecycle_cost_solution AS ENUM ('Preferred', 'A', 'B');
+/*
+    Removing 'As Is' from estimated_lifecycle_costs table and lifecycle_cost_solution enum
+*/
 
-ALTER TABLE estimated_lifecycle_costs
-    RENAME solution TO solution_temp;
+-- Delete old "As Is" rows from the table
+DELETE FROM estimated_lifecycle_costs WHERE solution = 'As Is';
 
-ALTER TABLE estimated_lifecycle_costs ADD solution lifecycle_cost_solution not null;
+-- Create a new type for the new temp column
+CREATE TYPE lifecycle_cost_solution_no_asis_temp AS ENUM ('Preferred', 'A', 'B');
 
-UPDATE estimated_lifecycle_costs SET solution = solution_temp::text::lifecycle_cost_solution;
+-- Create a new temp column
+ALTER TABLE estimated_lifecycle_costs ADD COLUMN solution_temp lifecycle_cost_solution_no_asis_temp;
 
-ALTER TABLE estimated_lifecycle_costs
-    DROP solution_temp;
+-- Copy the data from the old column to the new column
+UPDATE estimated_lifecycle_costs SET solution_temp = solution::text::lifecycle_cost_solution_no_asis_temp;
 
-DROP TYPE lifecycle_cost_solution_temp;
+-- Drop the old column
+ALTER TABLE estimated_lifecycle_costs DROP COLUMN solution;
+
+-- Drop the old type
+DROP TYPE lifecycle_cost_solution;
+
+-- Rename the temp type
+ALTER TYPE lifecycle_cost_solution_no_asis_temp RENAME TO lifecycle_cost_solution;
+
+-- Rename the temp column
+ALTER TABLE estimated_lifecycle_costs RENAME solution_temp TO solution;
+
+-- Add NOT NULL constraint
+ALTER TABLE estimated_lifecycle_costs ALTER COLUMN solution SET NOT NULL;
 
 -- Renaming as_is_summary and dropping all other 'As Is' columns
-ALTER TABLE business_cases 
+ALTER TABLE business_cases
     RENAME COLUMN as_is_summary TO current_solution_summary;
 
+-- Drop unused business_cases columns
 ALTER TABLE business_cases 
     DROP COLUMN as_is_title,
     DROP COLUMN as_is_pros,
     DROP COLUMN as_is_cons,
     DROP COLUMN as_is_cost_savings;
-   
