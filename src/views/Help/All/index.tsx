dc6faba853cbd059ff873c717aa21f190a5146f5
adamodd <adam.suharja@oddball.io>
2022-04-13 08:44:08 -0400

    All help articles

--- src/views/Help/All/index.tsx
+++ src/views/Help/All/index.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { CardGroup } from '@trussworks/react-uswds';
 
+import ArticleCard from 'components/ArticleCard';
+import HelpBreadcrumb from 'components/HelpBreadcrumb';
+import HelpPageIntro from 'components/HelpPageIntro';
 import MainContent from 'components/MainContent';
+import itGovernanceArticles from 'views/Help/ITGovernance/articles';
+import section508Articles from 'views/Help/Section508/articles';
 
 const AllHelp = () => {
+  const { t } = useTranslation('help');
+
+  const allArticles = itGovernanceArticles.concat(section508Articles);
+
   return (
-    <MainContent className="grid-container">
-      <>ALL HELP</>
+    <MainContent className="margin-bottom-10">
+      <HelpBreadcrumb type="Back" />
+      <HelpPageIntro heading={t('allHelpArticles')} />
+      <CardGroup className="margin-y-2">
+        {allArticles.map(article => (
+          <ArticleCard key={article.route} {...article} isLink />
+        ))}
+      </CardGroup>
     </MainContent>
   );
 };
