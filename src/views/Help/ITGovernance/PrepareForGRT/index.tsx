4a6d696271ad7cb8d3073088ca5755b438bf12c7
adamodd <adam.suharja@oddball.io>
2022-04-13 13:50:32 -0400

    Help PrepareForGrt MainContent wrap

--- src/views/Help/ITGovernance/PrepareForGRT/index.tsx
+++ src/views/Help/ITGovernance/PrepareForGRT/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import HelpBreadcrumb from 'components/HelpBreadcrumb';
+import MainContent from 'components/MainContent';
 import PrepareForGRTBase from 'components/PrepareForGRT';
 import RelatedArticles from 'components/RelatedArticles';
 
 const PrepareForGRT = () => {
   return (
-    <>
+    <MainContent>
       <div className="grid-container">
         <HelpBreadcrumb type="Close" />
         <PrepareForGRTBase helpMode />
@@ -14,7 +15,7 @@ const PrepareForGRT = () => {
       <div className="margin-top-7">
         <RelatedArticles type="IT Governance" />
       </div>
-    </>
+    </MainContent>
   );
 };
 
