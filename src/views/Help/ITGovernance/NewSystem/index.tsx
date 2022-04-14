31f99b47da80df6b8b800dd148d0d25ec1b43253
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2022-04-13 22:06:24 -0500

    Steps Involved layout

--- src/views/Help/ITGovernance/NewSystem/index.tsx
+++ src/views/Help/ITGovernance/NewSystem/index.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+import GovernanceOverviewContent from 'components/GovernanceOverview';
+import HelpBreadcrumb from 'components/HelpBreadcrumb';
+import HelpPageIntro from 'components/HelpPageIntro';
+import RelatedArticles from 'components/RelatedArticles';
 
 const NewSystem = () => {
-  return <></>;
+  const { t } = useTranslation('newSystem');
+  return (
+    <>
+      <div className="grid-container">
+        <HelpBreadcrumb type="Close" />
+        <HelpPageIntro
+          heading={t('newSystem:heading')}
+          subheading={t('newSystem:subheading')}
+          type="IT Governance"
+        />
+        <GovernanceOverviewContent helpArticle className="padding-bottom-3" />
+      </div>
+      <RelatedArticles type="IT Governance" />
+    </>
+  );
 };
 
 export default NewSystem;
