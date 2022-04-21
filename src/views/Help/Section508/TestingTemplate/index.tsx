77323df2eabfb11c565c95b3e7436b7d8fa270d5
Gary Zhao <gary.j.zhao@gmail.com>
2022-04-21 13:24:50 -0400

    Copy and Paste

--- src/views/Help/Section508/TestingTemplate/index.tsx
+++ src/views/Help/Section508/TestingTemplate/index.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 
-const TestingTemplates = () => {
+import HelpBreadcrumb from 'components/HelpBreadcrumb';
+import MainContent from 'components/MainContent';
+import RelatedArticles from 'components/RelatedArticles';
+import TestingTemplates from 'views/Accessibility/TestingTemplates';
+
+const PrepareForGRT = () => {
   return (
-    <div className="grid-container">
-      <>TestingTemplates</>
-    </div>
+    <>
+      <MainContent className="grid-container">
+        <HelpBreadcrumb type="Close tab" />
+        <TestingTemplates />
+      </MainContent>
+      <div className="margin-top-7">
+        <RelatedArticles type="Section 508" />
+      </div>
+    </>
   );
 };
 
-export default TestingTemplates;
+export default PrepareForGRT;
