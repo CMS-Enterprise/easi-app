2a7af17a040a715f37b0220c5bbaa0b5f6eac4fd
Gary Zhao <gary.j.zhao@gmail.com>
2022-04-21 14:49:17 -0400

    Add base component

--- src/views/Help/Section508/TestingTemplate/index.tsx
+++ src/views/Help/Section508/TestingTemplate/index.tsx
@@ -3,14 +3,14 @@ import React from 'react';
 import HelpBreadcrumb from 'components/HelpBreadcrumb';
 import MainContent from 'components/MainContent';
 import RelatedArticles from 'components/RelatedArticles';
-import TestingTemplates from 'views/Accessibility/TestingTemplates';
+import TestingTemplatesBase from 'components/TestingTemplates';
 
 const PrepareForGRT = () => {
   return (
     <>
       <MainContent className="grid-container">
         <HelpBreadcrumb type="Close tab" />
-        <TestingTemplates />
+        <TestingTemplatesBase helpArticle />
       </MainContent>
       <div className="margin-top-7">
         <RelatedArticles type="Section 508" />
