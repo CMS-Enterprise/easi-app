f3a89edf5ad03837f578e2c6e54be4f38211d8cf
Patrick Segura <95709965+patrickseguraoddball@users.noreply.github.com>
2022-04-28 07:13:35 -0700

    Commented out sample business case route and card.  Update snapshots (#1631)

--- src/views/Help/ITGovernance/articles.ts
+++ src/views/Help/ITGovernance/articles.ts
@@ -12,11 +12,12 @@ const itGovernanceArticles: ArticleProps[] = [
     type: 'IT Governance',
     translation: 'governanceReviewBoard'
   },
-  {
-    route: '/it-governance/sample-business-case',
-    type: 'IT Governance',
-    translation: 'sampleBusinessCase'
-  },
+  // TODO: Renable once sample business case article component completed
+  // {
+  //   route: '/it-governance/sample-business-case',
+  //   type: 'IT Governance',
+  //   translation: 'sampleBusinessCase'
+  // },
   {
     route: '/it-governance/steps-overview/new-system',
     type: 'IT Governance',
