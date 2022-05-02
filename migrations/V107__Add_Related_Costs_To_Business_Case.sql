11d6231e401aed1d28714d238690c9d284ea6e85
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2022-05-02 16:38:39 -0500

    Updated lifecycle_cost_phase enum

---
+++ migrations/V107__Add_Related_Costs_To_Business_Case.sql
@@ -0,0 +1,5 @@
+ALTER TYPE lifecycle_cost_phase ADD VALUE 'Help desk/call center';
+ALTER TYPE lifecycle_cost_phase ADD VALUE 'Software licenses';
+ALTER TYPE lifecycle_cost_phase ADD VALUE 'Planning, support, and professional services';
+ALTER TYPE lifecycle_cost_phase ADD VALUE 'Infrastructure';
+ALTER TYPE lifecycle_cost_phase ADD VALUE 'OIT services, tools, and pilots';
