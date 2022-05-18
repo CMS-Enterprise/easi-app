13dc0fd9a4051d3f6264f3c5b4a3a5a05455b49a
Jeremy Strang <95702411+jstrang-oddball@users.noreply.github.com>
2022-05-18 10:47:31 -0400

    made new system intake contacts columns not nullable

--- migrations/V107__Add_Role_and_Component_to_System_Intake_Contacts.sql
+++ migrations/V107__Add_Role_and_Component_to_System_Intake_Contacts.sql
@@ -1,2 +1,2 @@
-ALTER TABLE system_intake_contacts ADD COLUMN component text;
-ALTER TABLE system_intake_contacts ADD COLUMN role text;
+ALTER TABLE system_intake_contacts ADD COLUMN component text not null;
+ALTER TABLE system_intake_contacts ADD COLUMN role text not null;
