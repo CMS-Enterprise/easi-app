0a1e984c1c58620d9ac9ad9b4aa7df8c2daf931d
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2022-06-02 08:25:28 -0500

    Options checkbox

--- src/components/shared/MultiSelect/index.tsx
+++ src/components/shared/MultiSelect/index.tsx
@@ -5,11 +5,48 @@ import classNames from 'classnames';
 
 import './index.scss';
 
+type OptionType = {
+  label: string;
+  value: string | number;
+};
+
+type OptionsProps = {
+  options: OptionType[];
+  selected: OptionType[];
+  optionClick: (option: OptionType) => void;
+};
+
+const Options = ({ options, selected, optionClick }: OptionsProps) => {
+  const { t } = useTranslation();
+  return (
+    <ul className="easi-multiselect__options usa-list--unstyled padding-y-05 border-1px border-top-0 maxh-card overflow-scroll position-absolute right-0 left-0 z-top bg-white">
+      {options.map(option => (
+        <li
+          className="display-flex flex-align-center padding-y-05 padding-x-1"
+          key={option.value}
+          role="option"
+          aria-selected={selected.includes(option)}
+        >
+          <label>
+            <input
+              className="margin-right-1"
+              type="checkbox"
+              checked={selected.includes(option)}
+              onChange={() => optionClick(option)}
+            />
+            {t(option.label)}
+          </label>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
 type MultiSelectProps = {
   className?: string;
   id?: string;
   placeholder?: string;
-  options: string[];
+  options: OptionType[];
   selectedLabel?: string;
   tags?: boolean;
   disabled?: boolean;
@@ -24,17 +61,26 @@ export default function MultiSelect({
   tags = false,
   disabled = false
 }: MultiSelectProps) {
-  const [selected, setSelected] = useState<string[]>([]);
+  const [searchValue, setSearchValue] = useState('');
+
+  const [selected, setSelected] = useState<any[]>([]);
   const [active, setActive] = useState(false);
+
   const { t } = useTranslation();
-  const optionClick = (option: string) => {
+  const selectRef = useRef(null);
+
+  const optionClick = (option: OptionType) => {
     if (selected.includes(option)) {
       setSelected(selected.filter(selectedOption => selectedOption !== option));
     } else {
       setSelected([...selected, option]);
     }
   };
-  const selectRef = useRef(null);
+
+  const handleSearch = (value: string) => {
+    setSearchValue(value);
+  };
+
   useEffect(() => {
     function handleClickOutside(e) {
       if (selectRef.current && !selectRef.current.contains(e.target)) {
@@ -46,6 +92,7 @@ export default function MultiSelect({
       document.removeEventListener('mousedown', handleClickOutside);
     };
   }, [selectRef]);
+
   return (
     <div
       className={classNames(
@@ -54,36 +101,21 @@ export default function MultiSelect({
       )}
     >
       <div className="easi-multiselect__field" role="listbox" ref={selectRef}>
-        <div
-          className="usa-select maxw-none"
-          onClick={() => setActive(!active)}
-          onKeyPress={() => setActive(!active)}
-          role="button"
-          tabIndex={0}
-        >
-          {t(`${selected.length} selected`)}
+        <div className="usa-select maxw-none padding-0">
+          <input
+            type="search"
+            className="usa-input padding-1 height-full border-0"
+            value={searchValue}
+            onClick={() => setActive(true)}
+            onChange={e => handleSearch(e.target.value)}
+          />
         </div>
         {active && (
-          <ul className="easi-multiselect__options usa-list--unstyled padding-y-05 border-1px border-top-0 maxh-card overflow-scroll position-absolute right-0 left-0 z-top bg-white">
-            {options.map(option => (
-              <li
-                className="display-flex flex-align-center padding-y-05 padding-x-1"
-                key={option}
-                role="option"
-                aria-selected={selected.includes(option)}
-              >
-                <label>
-                  <input
-                    className="margin-right-1"
-                    type="checkbox"
-                    checked={selected.includes(option)}
-                    onChange={() => optionClick(option)}
-                  />
-                  {t(option)}
-                </label>
-              </li>
-            ))}
-          </ul>
+          <Options
+            options={options}
+            selected={selected}
+            optionClick={optionClick}
+          />
         )}
       </div>
       <div className="easi-multiselect__selected-list margin-top-3">
@@ -92,11 +124,11 @@ export default function MultiSelect({
           <ul className="usa-list--unstyled margin-top-1">
             {selected.map(option => (
               <li
-                key={option}
+                key={option.value}
                 className="display-flex flex-justify-start margin-y-05"
               >
                 <Tag className="bg-primary-lighter text-ink text-no-uppercase padding-y-1 padding-x-105 display-flex flex-align-center">
-                  {option}
+                  {option.label}
                   <IconClose
                     className="margin-left-1"
                     onClick={() => optionClick(option)}
