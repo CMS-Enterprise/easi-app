744a5de54cc7dcdd6e12a935ff25b9ea4bd3f8f8
adamodd <adam.suharja@oddball.io>
2022-06-22 15:31:10 -0400

    System profile jest tests
    System profile mock data
    Consolidate render tests into snapshot tests
    Tidy up render boilerplate

--- src/views/SystemProfile/components/SubSystems/index.test.tsx
+++ src/views/SystemProfile/components/SubSystems/index.test.tsx
@@ -1,51 +1,22 @@
 import React from 'react';
 import { MemoryRouter, Route } from 'react-router-dom';
-import { render, screen, waitFor } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
-import { mockSystemInfo, subSystems } from 'views/Sandbox/mockSystemData';
+import { getSystemProfileMockData } from 'data/mock/systemProfile';
 
 import SubSystems from './index';
 
-describe.skip('System Sub-systems subpage', () => {
-  it('renders without errors', async () => {
-    render(
-      <MemoryRouter initialEntries={['/systems/326-9-0/sub-systems']}>
-        <Route path="/systems/:systemId/:subinfo">
-          <SubSystems
-            system={{
-              ...mockSystemInfo[3],
-              subSystems
-            }}
-          />
-        </Route>
-      </MemoryRouter>
-    );
-
-    await waitFor(() => {
-      expect(
-        screen.getByText('Test Ocular Fiction Utensil')
-      ).toBeInTheDocument();
-      expect(
-        screen.getByText('Planned retirement: Q4 2022')
-      ).toBeInTheDocument();
-    });
-  });
+describe('System Sub-systems subpage', () => {
+  const systemProfileData = getSystemProfileMockData();
 
   it('matches snapshot', async () => {
-    const { asFragment } = render(
-      <MemoryRouter initialEntries={['/systems/326-9-0/sub-systems']}>
-        <Route path="/systems/:systemId/:subinfo">
-          <SubSystems
-            system={{
-              ...mockSystemInfo[3],
-              subSystems
-            }}
-          />
-        </Route>
+    const { asFragment, getByText } = render(
+      <MemoryRouter initialEntries={['/systems/000-100-0/sub-systems']}>
+        <SubSystems system={systemProfileData} />
       </MemoryRouter>
     );
-    await waitFor(() => {
-      expect(asFragment()).toMatchSnapshot();
-    });
+    expect(asFragment()).toMatchSnapshot();
+    expect(getByText('Test Ocular Fiction Utensil')).toBeInTheDocument();
+    expect(getByText('Planned retirement: Q4 2022')).toBeInTheDocument();
   });
 });
--- src/views/SystemProfile/index.test.tsx
+++ src/views/SystemProfile/index.test.tsx
@@ -1,64 +1,32 @@
 import React from 'react';
 import { MemoryRouter, Route } from 'react-router-dom';
 import { MockedProvider } from '@apollo/client/testing';
-import { fireEvent, render, waitFor } from '@testing-library/react';
+import {
+  fireEvent,
+  render,
+  waitFor,
+  waitForElementToBeRemoved
+} from '@testing-library/react';
 
-import GetCedarSystemQuery from 'queries/GetCedarSystemQuery';
-import GetSystemProfileQuery from 'queries/GetSystemProfileQuery';
-import { mockSystemInfo } from 'views/Sandbox/mockSystemData';
+import { query } from 'data/mock/systemProfile';
 
 import SystemProfile from './index';
 
-const mocks = [
-  {
-    request: {
-      query: GetCedarSystemQuery,
-      variables: {
-        id: '326-9-0'
-      }
-    },
-    result: {
-      data: {
-        cedarSystem: mockSystemInfo[3]
-      }
-    }
-  }
-];
-
-describe('The making a request page', () => {
-  /*
-  it('renders without errors', async () => {
-    render(
-      <MemoryRouter initialEntries={['/systems/326-9-0/tools-and-software']}>
-        <Route path="/systems/:systemId/:subinfo">
-          <MockedProvider mocks={mocks} addTypename={false}>
-            <SystemProfile />
-          </MockedProvider>
-        </Route>
-      </MemoryRouter>
-    );
-
-    await waitFor(() => {
-      expect(
-        screen.getByText('Medicare Beneficiary Contact Center')
-      ).toBeInTheDocument();
-    });
-  });
-  */
-
+describe('System Profile parent request', () => {
   it('matches snapshot', async () => {
-    const { asFragment } = render(
-      <MemoryRouter initialEntries={['/systems/326-9-0/tools-and-software']}>
+    const { asFragment, getByText, getAllByText, getByTestId } = render(
+      <MemoryRouter initialEntries={['/systems/000-100-0/home']}>
         <Route path="/systems/:systemId/:subinfo">
-          <MockedProvider mocks={mocks} addTypename={false}>
+          <MockedProvider mocks={[query]} addTypename={false}>
             <SystemProfile />
           </MockedProvider>
         </Route>
       </MemoryRouter>
     );
-    await waitFor(() => {
-      expect(asFragment()).toMatchSnapshot();
-    });
+    await waitForElementToBeRemoved(() => getByTestId('page-loading'));
+    expect(asFragment()).toMatchSnapshot();
+    expect(getByText('CMS.gov')).toBeInTheDocument();
+    expect(getAllByText('Jane Doe')[0]).toBeInTheDocument();
   });
 });
 
@@ -77,34 +45,7 @@ describe('System profile description is expandable', () => {
     });
   });
 
-  it.skip('shows read more & less', async () => {
-    const query = {
-      request: {
-        query: GetSystemProfileQuery,
-        variables: {
-          id: '000-100-0'
-        }
-      },
-      result: {
-        data: {
-          cedarAuthorityToOperate: [],
-          cedarThreat: [],
-          cedarSystemDetails: {
-            cedarSystem: {
-              id: '000-100-0',
-              name: 'Application Programming Interface Gateway',
-              description: '',
-              acronym: '',
-              status: '',
-              businessOwnerOrg: '',
-              businessOwnerOrgComp: '',
-              systemMaintainerOrg: '',
-              systemMaintainerOrgComp: ''
-            }
-          }
-        }
-      }
-    };
+  it('shows read more & less', async () => {
     const { getByText, findByTestId } = render(
       <MemoryRouter initialEntries={['/systems/000-100-0/home']}>
         <Route path="/systems/:systemId/:subinfo">
