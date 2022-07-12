7bf205dfc5f9d5e51c6537d1501a46f42feabffe
Jeremy Strang <jeremy.strang@oddball.io>
2022-07-12 18:25:05 -0400

    added email template for 'report a problem' email

--- pkg/email/help_report_a_problem.go
+++ pkg/email/help_report_a_problem.go
@@ -1,11 +1,39 @@
 package email
 
+import (
+	"bytes"
+	"context"
+
+	"github.com/cmsgov/easi-app/pkg/apperrors"
+)
+
 // SendReportAProblemEmailInput contains the data submitted by the user to the "report a problem" help form
 type SendReportAProblemEmailInput struct {
 	IsAnonymous            bool
+	ReporterName           string
+	ReporterEmail          string
 	CanBeContacted         bool
 	EasiService            string
 	WhatWereYouDoing       string
 	WhatWentWrong          string
 	HowSevereWasTheProblem string
+}
+
+// SendReportAProblemEmail sends an email to the EASI team containing a user's request for help
+func (c Client) SendReportAProblemEmail(ctx context.Context, input SendReportAProblemEmailInput) error {
+	subject := "Feedback for EASi System"
+
+	var b bytes.Buffer
+	err := c.templates.helpReportAProblem.Execute(&b, input)
+
+	if err != nil {
+		return &apperrors.NotificationError{Err: err, DestinationType: apperrors.DestinationTypeEmail}
+	}
+
+	err = c.sender.Send(ctx, c.config.EASIHelpEmail, nil, subject, b.String())
+	if err != nil {
+		return &apperrors.NotificationError{Err: err, DestinationType: apperrors.DestinationTypeEmail}
+	}
+
+	return nil
 }
