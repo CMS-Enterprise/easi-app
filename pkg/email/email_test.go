f228460628bc6c8f74b2be97e7387cfa1dc4a089
Dylan Sprague <dylan.sprague@oddball.io>
2022-07-24 13:44:52 -0400

    Test issue LCID email method with table-driven tests

--- pkg/email/email_test.go
+++ pkg/email/email_test.go
@@ -16,8 +16,9 @@ import (
 
 type EmailTestSuite struct {
 	suite.Suite
-	logger *zap.Logger
-	config Config
+	logger                      *zap.Logger
+	config                      Config
+	multipleRecipientsTestCases []multipleRecipientsTestCase
 }
 
 type mockSender struct {
@@ -80,22 +81,80 @@ func (c mockFailedTemplateCaller) Execute(wr io.Writer, data interface{}) error 
 	return errors.New("template caller had an error")
 }
 
+type multipleRecipientsTestCase struct {
+	recipients models.EmailNotificationRecipients
+}
+
 func TestEmailTestSuite(t *testing.T) {
 	logger := zap.NewNop()
 	config := testhelpers.NewConfig()
 
 	emailConfig := Config{
-		GRTEmail:          models.NewEmailAddress(config.GetString(appconfig.GRTEmailKey)),
+		GRTEmail:          models.NewEmailAddress("grt_email@cms.fake"),     // unique email address that can't get confused with ITInvestmentEmail
+		ITInvestmentEmail: models.NewEmailAddress("it_investment@cms.fake"), // unique email address that can't get confused with GRTEmail
 		URLHost:           config.GetString(appconfig.ClientHostKey),
 		URLScheme:         config.GetString(appconfig.ClientProtocolKey),
 		TemplateDirectory: config.GetString(appconfig.EmailTemplateDirectoryKey),
 	}
 
+	// construct list of test cases for emails to multiple recipients
+
+	// possible values - nil, non-nil empty slice, slice with one element, slice with multiple elements
+	possibleRegularRecipients := [][]models.EmailAddress{
+		nil,
+		{},
+		{"abcd@fake.email"},
+		{"efgh@fake.email", "ijkl@fake.email"},
+	}
+
+	multipleRecipientsTestCases := []multipleRecipientsTestCase{}
+	for _, possibleRegularRecipientList := range possibleRegularRecipients {
+		for _, shouldNotifyITGovernance := range []bool{true, false} {
+			for _, shouldNotifyITInvestment := range []bool{true, false} {
+				testCase := multipleRecipientsTestCase{
+					recipients: models.EmailNotificationRecipients{
+						RegularRecipientEmails:   possibleRegularRecipientList,
+						ShouldNotifyITGovernance: shouldNotifyITGovernance,
+						ShouldNotifyITInvestment: shouldNotifyITInvestment,
+					},
+				}
+				multipleRecipientsTestCases = append(multipleRecipientsTestCases, testCase)
+			}
+		}
+	}
+
 	sesTestSuite := &EmailTestSuite{
-		Suite:  suite.Suite{},
-		logger: logger,
-		config: emailConfig,
+		Suite:                       suite.Suite{},
+		logger:                      logger,
+		config:                      emailConfig,
+		multipleRecipientsTestCases: multipleRecipientsTestCases,
 	}
 
 	suite.Run(t, sesTestSuite)
+}
+
+// helper method - call this from test cases for individual email methods
+func (s *EmailTestSuite) runMultipleRecipientsTestAgainstAllTestCases(methodUnderTest func(client Client, recipients models.EmailNotificationRecipients) error) {
+	for _, testCase := range s.multipleRecipientsTestCases {
+		s.Run("Emails are only sent to the specified recipients", func() {
+			sender := mockMultipleRecipientsSender{}
+			client, err := NewClient(s.config, &sender)
+			s.NoError(err)
+
+			expectedRecipients := []models.EmailAddress{}
+			expectedRecipients = append(expectedRecipients, testCase.recipients.RegularRecipientEmails...)
+			if testCase.recipients.ShouldNotifyITGovernance {
+				expectedRecipients = append(expectedRecipients, s.config.GRTEmail)
+			}
+			if testCase.recipients.ShouldNotifyITInvestment {
+				expectedRecipients = append(expectedRecipients, s.config.ITInvestmentEmail)
+			}
+
+			err = methodUnderTest(client, testCase.recipients)
+			s.NoError(err)
+
+			actualRecipients := sender.AllToRecipients()
+			s.ElementsMatch(expectedRecipients, actualRecipients)
+		})
+	}
 }
