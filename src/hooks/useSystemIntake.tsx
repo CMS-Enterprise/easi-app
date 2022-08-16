e0151c33beea1c0912ab78d1606a7221bb65a2b3
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2022-08-16 10:20:02 -0500

    useSystemIntake custom hook

---
+++ src/hooks/useSystemIntake.tsx
@@ -0,0 +1,31 @@
+import { useMemo } from 'react';
+import { useQuery } from '@apollo/client';
+
+import GetSystemIntakeQuery from 'queries/GetSystemIntakeQuery';
+import {
+  GetSystemIntake,
+  GetSystemIntakeVariables
+} from 'queries/types/GetSystemIntake';
+
+export default function useSystemIntake(id: string) {
+  const { loading, data, refetch } = useQuery<
+    GetSystemIntake,
+    GetSystemIntakeVariables
+  >(GetSystemIntakeQuery, {
+    fetchPolicy: 'cache-first',
+    variables: {
+      id
+    }
+  });
+
+  const systemIntake = useMemo(() => {
+    if (!data?.systemIntake) return undefined;
+    return data?.systemIntake;
+  }, [data?.systemIntake]);
+
+  return {
+    systemIntake,
+    loading,
+    refetch
+  };
+}
