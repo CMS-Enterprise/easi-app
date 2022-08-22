f5cb8386ef2be9e02fec546f6ff99fb3f6955fa6
Dylan Sprague <92891039+DylanSpOddball@users.noreply.github.com>
2022-08-22 12:36:04 -0400

    Change Contract Vehicle field to Contract Number - backend changes (#1747)
    * Commenting out branch-name restriction hook temporarily
    
    * Add contract_number field/column on system intake model/DB table
    
    * Add contract number to GQL queries (not mutations)
    
    * Replace contract vehicle with contract number in GQL mutations (not queries)
    
    * Add tests for functionality around replacing contract vehicles with contract numbers
    
    * Add intakes with known UUIDs to db:seed data for testing EASI-1977 backend work
    
    * Hack in temporary support on frontend for updated GQL schema (Needed to pass Cypress test; NOT long-term solution)

--- pkg/graph/schema.resolvers.go
+++ pkg/graph/schema.resolvers.go
@@ -1640,7 +1640,9 @@ func (r *mutationResolver) UpdateSystemIntakeContractDetails(ctx context.Context
 		// set the fields to the values we receive
 		intake.ExistingContract = null.StringFromPtr(input.Contract.HasContract)
 		intake.Contractor = null.StringFromPtr(input.Contract.Contractor)
-		intake.ContractVehicle = null.StringFromPtr(input.Contract.Vehicle)
+		intake.ContractNumber = null.StringFromPtr(input.Contract.Number)
+		intake.ContractVehicle = null.StringFromPtr(nil) // blank this out in favor of the newer ContractNumber field (see EASI-1977)
+
 		if input.Contract.StartDate != nil {
 			intake.ContractStartDate = input.Contract.StartDate
 		}
@@ -1653,6 +1655,7 @@ func (r *mutationResolver) UpdateSystemIntakeContractDetails(ctx context.Context
 			if *input.Contract.HasContract == "NOT_STARTED" || *input.Contract.HasContract == "NOT_NEEDED" {
 				intake.Contractor = null.StringFromPtr(nil)
 				intake.ContractVehicle = null.StringFromPtr(nil)
+				intake.ContractNumber = null.StringFromPtr(nil)
 				intake.ContractStartDate = nil
 				intake.ContractEndDate = nil
 			}
@@ -2288,6 +2291,7 @@ func (r *systemIntakeResolver) Contract(ctx context.Context, obj *models.SystemI
 		HasContract: obj.ExistingContract.Ptr(),
 		StartDate:   &contractStart,
 		Vehicle:     obj.ContractVehicle.Ptr(),
+		Number:      obj.ContractNumber.Ptr(),
 	}, nil
 }
 
