9805a7c50b9fe13ad27fd0a08e5b10944775b1a8
Steven Wade <steven.wade@oddball.io>
2022-09-07 13:14:32 -0700

    Update BaseStructPreuUpdate comment to be more descriptive

--- pkg/graph/resolvers/base_struct.go
+++ pkg/graph/resolvers/base_struct.go
@@ -8,7 +8,9 @@ import (
 	"github.com/cmsgov/easi-app/pkg/storage"
 )
 
-// BaseStructPreUpdate applies incoming changes from to a TaskList Section, and validates it's status
+// BaseStructPreUpdate is a hook to call before updating a baseStruct object in the database it will
+// 1. Update the base struct meta data
+// 2. Apply changes from the changes object to the provided base struct if applyChanges == true
 func BaseStructPreUpdate(logger *zap.Logger, bs models.IBaseStruct, changes map[string]interface{}, principal authentication.Principal, store *storage.Store, applyChanges bool) error {
 	section := bs.GetBaseStruct()
 
