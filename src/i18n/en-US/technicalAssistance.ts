9bc9e432b432c24d542264b215ded215d1b5a41f
adamodd <adam.suharja@oddball.io>
2022-09-07 15:22:19 -0400

    Trb table stub

--- src/i18n/en-US/technicalAssistance.ts
+++ src/i18n/en-US/technicalAssistance.ts
@@ -1,6 +1,13 @@
 const technicalAssistance = {
   heading: 'Technical Assistance',
-  nextStep: 'Start a new request'
+  nextStep: 'Start a new request',
+  table: {
+    header: {
+      requestName: 'Request Name',
+      submissionDate: 'Submission date',
+      status: 'Status'
+    }
+  }
 };
 
 export default technicalAssistance;
--- src/views/TechnicalAssistance/index.tsx
+++ src/views/TechnicalAssistance/index.tsx
@@ -1,10 +1,38 @@
-import React from 'react';
+/**
+ * @file "Technical Assistance" (TRB) homepage.
+ * Created with reference to `src/views/MakingARequest`, `src/views/MyRequests/Table`.
+ */
+
+import React, { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useRouteMatch } from 'react-router-dom';
-import { Grid, Link } from '@trussworks/react-uswds';
+import {
+  Column,
+  useFilters,
+  useGlobalFilter,
+  usePagination,
+  useSortBy,
+  useTable
+} from 'react-table';
+import { Button, Link, Table } from '@trussworks/react-uswds';
 
 import MainContent from 'components/MainContent';
 import PageHeading from 'components/PageHeading';
+import GlobalClientFilter from 'components/TableFilter';
+import TablePageSize from 'components/TablePageSize';
+import TablePagination from 'components/TablePagination';
+import TableResults from 'components/TableResults';
+import {
+  currentTableSortDescription,
+  getColumnSortStatus,
+  getHeaderSortIcon
+} from 'utils/tableSort';
+
+interface TrbDataTodo {
+  name: string;
+  createdDts: string;
+  status: string;
+}
 
 type TechnicalAssistanceProps = {};
 
@@ -12,20 +40,180 @@ type TechnicalAssistanceProps = {};
 function TechnicalAssistance({}: TechnicalAssistanceProps) {
   const { t } = useTranslation('technicalAssistance');
   const { url } = useRouteMatch();
+
+  const columns = useMemo<Column<TrbDataTodo>[]>(() => {
+    return [
+      {
+        Header: t<string>('table.header.requestName'),
+        accessor: 'name'
+      },
+      {
+        Header: t<string>('table.header.submissionDate'),
+        accessor: 'createdDts'
+      },
+      {
+        Header: t<string>('table.header.status'),
+        accessor: 'status'
+      }
+    ];
+  }, [t]);
+
+  const data = useMemo<TrbDataTodo[]>(() => {
+    return [
+      {
+        name: 'foo',
+        createdDts: '2011-10-05T14:48:00.000Z',
+        status: 'OPEN'
+      },
+      {
+        name: 'bar',
+        createdDts: '2012-10-05T14:48:00.000Z',
+        status: 'OPEN'
+      },
+      {
+        name: 'baz',
+        createdDts: '2013-10-05T14:48:00.000Z',
+        status: 'CLOSED'
+      }
+    ];
+  }, []);
+
+  const {
+    canNextPage,
+    canPreviousPage,
+    getTableBodyProps,
+    getTableProps,
+    gotoPage,
+    headerGroups,
+    nextPage,
+    pageCount,
+    pageOptions,
+    prepareRow,
+    previousPage,
+    rows,
+    setGlobalFilter,
+    setPageSize,
+    state
+  } = useTable(
+    {
+      columns,
+      data,
+      autoResetSortBy: false,
+      autoResetPage: false,
+      initialState: {
+        sortBy: useMemo(() => [{ id: 'name', desc: false }], []),
+        pageIndex: 0,
+        pageSize: 10
+      }
+    },
+    useFilters,
+    useGlobalFilter,
+    useSortBy,
+    usePagination
+  );
+
   return (
     <MainContent className="technical-assistance grid-container">
-      <Grid row>
-        <Grid>
-          <PageHeading>{t('heading')}</PageHeading>
-          <Link
-            href={`${url}/request-type`}
-            className="usa-button"
-            variant="unstyled"
-          >
-            {t('nextStep')}
-          </Link>
-        </Grid>
-      </Grid>
+      <PageHeading>{t('heading')}</PageHeading>
+
+      <Link
+        href={`${url}/request-type`}
+        className="usa-button"
+        variant="unstyled"
+      >
+        {t('nextStep')}
+      </Link>
+
+      <div>
+        <GlobalClientFilter
+          setGlobalFilter={setGlobalFilter}
+          tableID={t('systemTable.id')}
+          tableName={t('systemTable.title')}
+          className="margin-bottom-4"
+        />
+        <TableResults
+          globalFilter={state.globalFilter}
+          pageIndex={state.pageIndex}
+          pageSize={state.pageSize}
+          filteredRowLength={rows.length}
+          rowLength={data.length}
+          className="margin-bottom-4"
+        />
+
+        <Table bordered={false} fullWidth scrollable {...getTableProps()}>
+          <thead>
+            {headerGroups.map(headerGroup => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column, index) => (
+                  <th
+                    {...column.getHeaderProps(column.getSortByToggleProps())}
+                    aria-sort={getColumnSortStatus(column)}
+                    scope="col"
+                    className="border-bottom-2px"
+                  >
+                    <Button
+                      type="button"
+                      unstyled
+                      className="width-full display-flex"
+                      {...column.getSortByToggleProps()}
+                    >
+                      <div className="flex-fill text-no-wrap">
+                        {column.render('Header')}
+                      </div>
+                      <div className="position-relative width-205 margin-left-05">
+                        {getHeaderSortIcon(column)}
+                      </div>
+                    </Button>
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {rows.map(row => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map((cell, index) => {
+                    return (
+                      <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+
+        <div className="grid-row grid-gap grid-gap-lg">
+          <TablePagination
+            gotoPage={gotoPage}
+            previousPage={previousPage}
+            nextPage={nextPage}
+            canNextPage={canNextPage}
+            pageIndex={state.pageIndex}
+            pageOptions={pageOptions}
+            canPreviousPage={canPreviousPage}
+            pageCount={pageCount}
+            pageSize={state.pageSize}
+            setPageSize={setPageSize}
+            page={[]}
+            className="desktop:grid-col-fill"
+          />
+          <TablePageSize
+            className="desktop:grid-col-auto"
+            pageSize={state.pageSize}
+            setPageSize={setPageSize}
+          />
+        </div>
+
+        <div
+          className="usa-sr-only usa-table__announcement-region"
+          aria-live="polite"
+        >
+          {currentTableSortDescription(headerGroups[0])}
+        </div>
+      </div>
     </MainContent>
   );
 }
