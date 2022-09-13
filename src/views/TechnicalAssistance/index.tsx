01e66d5231c1ef8dcfad071c3e8e68ed61c37e43
adamodd <adam.suharja@oddball.io>
2022-09-13 08:41:07 -0400

    Use route paths

--- src/views/TechnicalAssistance/index.tsx
+++ src/views/TechnicalAssistance/index.tsx
@@ -4,13 +4,13 @@ import { Route, Switch, useRouteMatch } from 'react-router-dom';
 import Homepage from './Homepage';
 
 function TechnicalAssistance() {
-  const { url } = useRouteMatch();
+  const { path } = useRouteMatch();
   return (
     <Switch>
-      <Route exact path={url}>
+      <Route exact path={path}>
         <Homepage />
       </Route>
-      <Route exact path={`${url}/request-type`}>
+      <Route exact path={`${path}/request-type`}>
         <>New Request wip</>
       </Route>
     </Switch>
