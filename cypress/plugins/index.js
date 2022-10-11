3cbd76a88ca6a969f14b965bfefe54f3b8a8e66b
Dylan Sprague <dylan.sprague@oddball.io>
2022-10-11 17:26:14 -0400

    Re-enable code-coverage on single Cypress spec file
    Should fail; intended to make sure md5 dependency is needed

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,6 +11,7 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 const cypressOTP = require('cypress-otp');
+const cypressCodeCovTask = require('@cypress/code-coverage/task');
 const wp = require('@cypress/webpack-preprocessor');
 
 module.exports = (on, config) => {
@@ -19,6 +20,7 @@ module.exports = (on, config) => {
   on('task', {
     generateOTP: cypressOTP
   });
+  cypressCodeCovTask(on, config);
 
   const options = {
     webpackOptions: {
