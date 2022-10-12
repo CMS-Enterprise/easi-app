367fc2ec907573a694ccecfd852bfb88af5dd27b
Jeremy Strang <jeremy.strang@oddball.io>
2022-10-11 23:25:18 -0400

    updated trb request form test to check collab group values after update

--- pkg/graph/resolvers/trb_request_form_test.go
+++ pkg/graph/resolvers/trb_request_form_test.go
@@ -17,18 +17,19 @@ func (s *ResolverSuite) TestCreateTRBRequestForm() {
 	s.NoError(err)
 
 	s.Run("create/update/fetch TRB request forms", func() {
+		initialCollabGroups := []models.TRBCollabGroupOption{
+			models.TRBCollabGroupOptionSecurity,
+			models.TRBCollabGroupOptionCloud,
+		}
 		form := map[string]interface{}{
 			"trbRequestId":             trbRequest.ID,
 			"needsAssistanceWith":      "All the things",
 			"hasSolutionInMind":        false,
 			"whereInProcess":           models.TRBWhereInProcessOptionUnknown,
 			"hasExpectedStartEndDates": false,
-			"collabGroups": []models.TRBCollabGroupOption{
-				models.TRBCollabGroupOptionSecurity,
-				models.TRBCollabGroupOptionCloud,
-			},
-			"component": "Taco Cart",
-			"createdBy": anonEua,
+			"collabGroups":             initialCollabGroups,
+			"component":                "Taco Cart",
+			"createdBy":                anonEua,
 		}
 		createdForm, err := CreateTRBRequestForm(ctx, s.testConfigs.Store, form)
 		s.NoError(err)
@@ -36,30 +37,36 @@ func (s *ResolverSuite) TestCreateTRBRequestForm() {
 		s.EqualValues(createdForm.HasSolutionInMind, form["hasSolutionInMind"])
 		s.EqualValues(createdForm.WhereInProcess, form["whereInProcess"])
 		s.EqualValues(createdForm.HasExpectedStartEndDates, form["hasExpectedStartEndDates"])
-		// s.EqualValues(createdForm.CollabGroups, form["collabGroups"])
+		s.EqualValues(createdForm.CollabGroups[0], initialCollabGroups[0])
+		s.EqualValues(createdForm.CollabGroups[1], initialCollabGroups[1])
+		s.EqualValues(len(createdForm.CollabGroups), 2)
 
 		createdForm.ModifiedBy = &anonEua
+		updatedCollabGroups := []models.TRBCollabGroupOption{
+			models.TRBCollabGroupOptionSecurity,
+			models.TRBCollabGroupOptionCloud,
+			models.TRBCollabGroupOptionPrivacyAdvisor,
+		}
 		formChanges := map[string]interface{}{
 			"trbRequestId":             trbRequest.ID,
 			"needsAssistanceWith":      "Some of the things",
 			"hasSolutionInMind":        true,
 			"whereInProcess":           models.TRBWhereInProcessOptionContractingWorkHasStarted,
 			"hasExpectedStartEndDates": true,
-			"collabGroups": []models.TRBCollabGroupOption{
-				models.TRBCollabGroupOptionSecurity,
-				models.TRBCollabGroupOptionCloud,
-				models.TRBCollabGroupOptionPrivacyAdvisor,
-			},
-			"component": "Taco Cart",
+			"collabGroups":             updatedCollabGroups,
+			"component":                "Taco Cart",
 		}
 		updatedForm, err := UpdateTRBRequestForm(ctx, s.testConfigs.Store, formChanges)
 		s.NoError(err)
 		s.EqualValues(updatedForm.ModifiedBy, &anonEua)
-		s.EqualValues(createdForm.NeedsAssistanceWith, form["needsAssistanceWith"])
-		s.EqualValues(createdForm.HasSolutionInMind, form["hasSolutionInMind"])
-		s.EqualValues(createdForm.WhereInProcess, form["whereInProcess"])
-		s.EqualValues(createdForm.HasExpectedStartEndDates, form["hasExpectedStartEndDates"])
-		// s.EqualValues(createdForm.CollabGroups, form["collabGroups"])
+		s.EqualValues(updatedForm.NeedsAssistanceWith, formChanges["needsAssistanceWith"])
+		s.EqualValues(updatedForm.HasSolutionInMind, formChanges["hasSolutionInMind"])
+		s.EqualValues(updatedForm.WhereInProcess, formChanges["whereInProcess"])
+		s.EqualValues(updatedForm.HasExpectedStartEndDates, formChanges["hasExpectedStartEndDates"])
+		s.EqualValues(updatedForm.CollabGroups[0], updatedCollabGroups[0])
+		s.EqualValues(updatedForm.CollabGroups[1], updatedCollabGroups[1])
+		s.EqualValues(updatedForm.CollabGroups[2], updatedCollabGroups[2])
+		s.EqualValues(len(updatedForm.CollabGroups), 3)
 
 		fetched, err := GetTRBRequestFormByTRBRequestID(ctx, s.testConfigs.Store, trbRequest.ID)
 		s.NoError(err)
