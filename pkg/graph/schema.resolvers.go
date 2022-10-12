41c100aeef789cb69144e66c3f11288a8fdb4c05
Jeremy Strang <jeremy.strang@oddball.io>
2022-10-11 23:17:12 -0400

    removed commented out code

--- pkg/graph/schema.resolvers.go
+++ pkg/graph/schema.resolvers.go
@@ -1910,37 +1910,11 @@ func (r *mutationResolver) DeleteTRBRequestAttendee(ctx context.Context, id uuid
 
 // CreateTRBRequestForm is the resolver for the createTRBRequestForm field.
 func (r *mutationResolver) CreateTRBRequestForm(ctx context.Context, input map[string]interface{}) (*models.TRBRequestForm, error) {
-	// collabGroups := make([]string, len(input.CollabGroups))
-	// for _, cg := range input.CollabGroups {
-	// 	collabGroups = append(collabGroups, string(cg))
-	// }
-	// return resolvers.CreateTRBRequestForm(ctx, r.store, &models.TRBRequestForm{
-	// 	TRBRequestID:             input.TrbRequestID,
-	// 	Component:                input.Component,
-	// 	NeedsAssistanceWith:      input.NeedsAssistanceWith,
-	// 	HasSolutionInMind:        input.HasSolutionInMind,
-	// 	WhereInProcess:           input.WhereInProcess,
-	// 	HasExpectedStartEndDates: input.HasExpectedStartEndDates,
-	// 	CollabGroups:             pq.StringArray(collabGroups),
-	// })
 	return resolvers.CreateTRBRequestForm(ctx, r.store, input)
 }
 
 // UpdateTRBRequestForm is the resolver for the updateTRBRequestForm field.
 func (r *mutationResolver) UpdateTRBRequestForm(ctx context.Context, input map[string]interface{}) (*models.TRBRequestForm, error) {
-	// collabGroups := make([]string, len(input.CollabGroups))
-	// for _, cg := range input.CollabGroups {
-	// 	collabGroups = append(collabGroups, string(cg))
-	// }
-	// form := &models.TRBRequestForm{
-	// 	TRBRequestID:             input.TrbRequestID,
-	// 	Component:                input.Component,
-	// 	NeedsAssistanceWith:      input.NeedsAssistanceWith,
-	// 	HasSolutionInMind:        input.HasSolutionInMind,
-	// 	WhereInProcess:           input.WhereInProcess,
-	// 	HasExpectedStartEndDates: input.HasExpectedStartEndDates,
-	// 	CollabGroups:             pq.StringArray(collabGroups),
-	// }
 	return resolvers.UpdateTRBRequestForm(ctx, r.store, input)
 }
 
