de6f2e17e8d8fc46041d0ddb9ee4b401f4174a28
Dylan Sprague <dylan.sprague@oddball.io>
2022-12-29 16:01:50 -0500

    First drafts of SQL table, Go model for TRB admin notes

---
+++ migrations/V125__Add_TRB_Admin_Notes.sql
@@ -0,0 +1,23 @@
+CREATE TYPE trb_admin_note_category AS ENUM (
+  'GENERAL_REQUEST',
+  'INITIAL_REQUEST_FORM',
+  'SUPPORTING_DOCUMENTS',
+  'CONSULT_SESSION',
+  'ADVICE_LETTER'
+);
+
+CREATE TABLE trb_admin_notes (
+    -- PK, FK
+    id UUID PRIMARY KEY NOT NULL,
+    trb_request_id uuid NOT NULL REFERENCES trb_request(id),
+
+    -- general metadata
+    created_by TEXT NOT NULL CHECK (created_by ~ '^[A-Z0-9]{4}$'),
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    modified_by TEXT CHECK (modified_by ~ '^[A-Z0-9]{4}$'),
+    modified_at TIMESTAMP WITH TIME ZONE,
+
+    -- note-specific fields
+    category trb_admin_note_category NOT NULL,
+    note_text TEXT NOT NULL
+);
---
+++ pkg/models/trb_admin_note.go
@@ -0,0 +1,23 @@
+package models
+
+import "github.com/google/uuid"
+
+// TRBAdminNoteCategory is an enumeration of the possible categories of a TRBAdminNote
+type TRBAdminNoteCategory string
+
+// These are the possible categories for a TRB admin note
+const (
+	TRBAdminNoteCategoryGeneralRequest      TRBAdminNoteCategory = "GENERAL_REQUEST"
+	TRBAdminNoteCategoryInitialRequestForm  TRBAdminNoteCategory = "INITIAL_REQUEST_FORM"
+	TRBAdminNoteCategorySupportingDocuments TRBAdminNoteCategory = "SUPPORTING_DOCUMENTS"
+	TRBAdminNoteCategoryConsultSession      TRBAdminNoteCategory = "CONSULT_SESSION"
+	TRBAdminNoteCategoryAdviceLetter        TRBAdminNoteCategory = "ADVICE_LETTER"
+)
+
+// TRBAdminNote represents the data for a note attached to a TRB request by an admin
+type TRBAdminNote struct {
+	baseStruct
+	TRBRequestID uuid.UUID            `json:"trbRequestId" db:"trb_request_id"`
+	Category     TRBAdminNoteCategory `json:"category" db:"category"`
+	NoteText     string               `json:"noteText" db:"note_text"`
+}
