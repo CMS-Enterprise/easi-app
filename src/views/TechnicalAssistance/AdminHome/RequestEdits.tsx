cb410d63faa41982077635f4ffad27bd5895c3c7
adamodd <adam.suharja@oddball.io>
2023-02-15 10:56:37 -0500

    Request edits form submission

--- src/views/TechnicalAssistance/AdminHome/RequestEdits.tsx
+++ src/views/TechnicalAssistance/AdminHome/RequestEdits.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Controller, useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
+import { useParams } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import {
   Button,
@@ -20,13 +21,17 @@ import {
   CreateTrbRequestFeedback,
   CreateTrbRequestFeedbackVariables
 } from 'queries/types/CreateTrbRequestFeedback';
+import { TRBFeedbackAction } from 'types/graphql-global-types';
 
 function RequestEdits() {
   const { t } = useTranslation('technicalAssistance');
+
+  const { id, activePage } = useParams<{ id: string; activePage: string }>();
+
   const {
     control,
     handleSubmit,
-    formState: { isSubmitting }
+    formState: { isDirty, isSubmitting }
   } = useForm({
     defaultValues: {
       feedbackMessage: ''
@@ -42,21 +47,44 @@ function RequestEdits() {
   return (
     <GridContainer className="width-full">
       <Grid row>
-        <PageHeading>{t('actionRequestEdits.heading')}</PageHeading>
-        <div>{t('actionRequestEdits.description')}</div>
+        <PageHeading className="margin-bottom-0">
+          {t('actionRequestEdits.heading')}
+        </PageHeading>
+        <div className="line-height-body-5 font-body-lg text-light">
+          {t('actionRequestEdits.description')}
+        </div>
       </Grid>
       <Grid row gap>
         <Grid tablet={{ col: 12 }} desktop={{ col: 6 }}>
           <Form
             onSubmit={handleSubmit(formData => {
               // console.log('submit', formData);
-              // mutate({ variables: {} })
-              //   .then(result => {})
-              //   .catch(error => {});
+              mutate({
+                variables: {
+                  input: {
+                    trbRequestId: id,
+                    feedbackMessage: formData.feedbackMessage,
+                    copyTrbMailbox: false, // todo
+                    notifyEuaIds: ['ABCD'], // todo
+                    action: TRBFeedbackAction.REQUEST_EDITS
+                  }
+                }
+              })
+                .then(result => {
+                  // eslint-disable-next-line no-console
+                  console.log('success', result);
+                })
+                // eslint-disable-next-line no-shadow
+                .catch(error => {
+                  // eslint-disable-next-line no-console
+                  console.log('error', error);
+                });
             })}
             className="maxw-full"
           >
-            <div>{t('actionRequestEdits.fieldsMarkedRequired')}</div>
+            <div className="margin-top-1">
+              {t('actionRequestEdits.fieldsMarkedRequired')}
+            </div>
             <Controller
               name="feedbackMessage"
               control={control}
@@ -86,9 +114,13 @@ function RequestEdits() {
             />
             <h3>{t('actionRequestEdits.notificationTitle')}</h3>
             <div>{t('actionRequestEdits.notificationDescription')}</div>
-            {/* cedar contacts */}
+            {/* todo cedar contacts */}
             <div>
-              <Button type="submit" className="" disabled={isSubmitting}>
+              <Button
+                type="submit"
+                className=""
+                disabled={!isDirty || isSubmitting}
+              >
                 {t('actionRequestEdits.submit')}
               </Button>
             </div>
@@ -96,7 +128,7 @@ function RequestEdits() {
         </Grid>
       </Grid>
       <div>
-        <UswdsReactLink to="">
+        <UswdsReactLink to={`/trb/${id}/${activePage}`}>
           {t('actionRequestEdits.cancelAndReturn')}
         </UswdsReactLink>
       </div>
