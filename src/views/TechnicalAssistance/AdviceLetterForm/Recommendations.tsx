dd3fcc0de28ec377ded3ef1c7daa20e80d0ef984
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2023-03-02 13:07:41 -0600

    Remove recommendations work
    Remove recommendations work - this will be completed in an additional PR

--- src/views/TechnicalAssistance/AdviceLetterForm/Recommendations.tsx
+++ src/views/TechnicalAssistance/AdviceLetterForm/Recommendations.tsx
@@ -1,20 +1,11 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Route, Switch, useHistory, useRouteMatch } from 'react-router-dom';
-import { useMutation } from '@apollo/client';
-import { Alert, Button, ButtonGroup } from '@trussworks/react-uswds';
+import { useHistory } from 'react-router-dom';
+import { Alert, Button } from '@trussworks/react-uswds';
 
-import UswdsReactLink from 'components/LinkWrapper';
-import { DeleteTrbRecommendationQuery } from 'queries/TrbAdviceLetterQueries';
-import {
-  DeleteTRBRecommendation,
-  DeleteTRBRecommendationVariables
-} from 'queries/types/DeleteTRBRecommendation';
 import { GetTrbAdviceLetter_trbRequest_adviceLetter_recommendations as TRBRecommendation } from 'queries/types/GetTrbAdviceLetter';
 
 import Pager from '../RequestForm/Pager';
-
-import RecommendationsForm from './RecommendationsForm';
 
 export type RecommendationsProps = {
   trbRequestId: string;
@@ -26,117 +17,59 @@ const Recommendations = ({
   recommendations
 }: RecommendationsProps) => {
   const { t } = useTranslation('technicalAssistance');
-  const { path, url } = useRouteMatch();
   const history = useHistory();
 
   /** Whether recommendations have been added to the request */
   const hasRecommendations: boolean = recommendations.length > 0;
 
-  const [deleteRecommendation] = useMutation<
-    DeleteTRBRecommendation,
-    DeleteTRBRecommendationVariables
-  >(DeleteTrbRecommendationQuery);
-
-  console.log('recommendations', recommendations);
-
   return (
-    <Switch>
-      {/* Recommendations Form */}
-      <Route exact path={`${path}/form`}>
-        <RecommendationsForm trbRequestId={trbRequestId} />
-      </Route>
+    <div>
+      {/* Add recommendation button */}
+      <Button
+        className="margin-top-5 margin-bottom-1"
+        type="button"
+        onClick={() => null}
+      >
+        {t('adviceLetterForm.addRecommendation')}
+      </Button>
 
-      {/* Recommendations form step */}
-      <Route exact path={`${path}`}>
-        {/* Add recommendation button */}
-        <Button
-          className="margin-top-5 margin-bottom-1"
-          type="button"
-          onClick={() => history.push(`${url}/form`)}
-        >
-          {t('adviceLetterForm.addRecommendation')}
-        </Button>
+      {
+        /* No recommendations message */
+        !hasRecommendations ? (
+          <Alert type="info" slim>
+            {t('adviceLetterForm.noRecommendations')}
+          </Alert>
+        ) : (
+          /* Recommendations list */
+          <ul>
+            {recommendations.map(({ title, id }) => (
+              <li key={id}>{title}</li>
+            ))}
+          </ul>
+        )
+      }
 
-        {
-          /* No recommendations message */
-          !hasRecommendations ? (
-            <Alert type="info" slim>
-              {t('adviceLetterForm.noRecommendations')}
-            </Alert>
-          ) : (
-            /* Recommendations list */
-            <ul className="usa-list usa-list--unstyled margin-bottom-2">
-              {recommendations.map(({ title, recommendation, links, id }) => {
-                return (
-                  <li key={id}>
-                    <h4 className="margin-bottom-0">{title}</h4>
-                    <p className="margin-top-05 margin-bottom-0">
-                      {recommendation}
-                    </p>
-                    {
-                      /* Links list */
-                      links && (
-                        <ul className="usa-list usa-list--unstyled">
-                          {links.map((link, index) => (
-                            // eslint-disable-next-line react/no-array-index-key
-                            <li key={`${id}.${index}`}>
-                              <UswdsReactLink to={link} variant="external">
-                                {link}
-                              </UswdsReactLink>
-                            </li>
-                          ))}
-                        </ul>
-                      )
-                    }
-                    <ButtonGroup>
-                      {/* Edit */}
-                      <Button type="button" unstyled>
-                        {t('adviceLetterForm.editRecommendation')}
-                      </Button>
-                      {/* Remove */}
-                      <Button
-                        type="button"
-                        className="text-secondary margin-left-1"
-                        onClick={() =>
-                          deleteRecommendation({ variables: { id } })
-                        }
-                        unstyled
-                      >
-                        {t('adviceLetterForm.removeRecommendation')}
-                      </Button>
-                    </ButtonGroup>
-                  </li>
-                );
-              })}
-            </ul>
-          )
-        }
-
-        {/* Form pager buttons */}
-        <Pager
-          className="margin-top-4"
-          back={{
-            outline: true,
-            onClick: () => {
-              history.push(`/trb/${trbRequestId}/advice/summary`);
-            }
-          }}
-          next={{
-            // TODO: disabled logic
-            // disabled: isSubmitting,
-            text: hasRecommendations
-              ? t('button.next')
-              : t('adviceLetterForm.continueWithoutAdding'),
-            onClick: () =>
-              history.push(`/trb/${trbRequestId}/advice/next-steps`),
-            outline: !hasRecommendations
-          }}
-          taskListUrl={`/trb/${trbRequestId}/request`}
-          saveExitText={t('adviceLetterForm.returnToRequest')}
-          border={false}
-        />
-      </Route>
-    </Switch>
+      {/* Form pager buttons */}
+      <Pager
+        className="margin-top-4"
+        back={{
+          outline: true,
+          onClick: () => {
+            history.push(`/trb/${trbRequestId}/advice/summary`);
+          }
+        }}
+        next={{
+          text: hasRecommendations
+            ? t('button.next')
+            : t('adviceLetterForm.continueWithoutAdding'),
+          onClick: () => history.push(`/trb/${trbRequestId}/advice/next-steps`),
+          outline: !hasRecommendations
+        }}
+        taskListUrl={`/trb/${trbRequestId}/request`}
+        saveExitText={t('adviceLetterForm.returnToRequest')}
+        border={false}
+      />
+    </div>
   );
 };
 
