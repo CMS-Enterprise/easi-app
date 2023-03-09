a9f93165058b282add2c25f750fad1d2c9b050ae
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2023-03-09 14:25:33 -0600

    Recommendations form

---
+++ src/views/TechnicalAssistance/AdviceLetterForm/LinksArrayField/Index.tsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import {
+  Control,
+  Controller,
+  useFieldArray,
+  UseFormWatch
+} from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
+import { Button, FormGroup, IconAdd, TextInput } from '@trussworks/react-uswds';
+
+import Label from 'components/shared/Label';
+import { AdviceLetterRecommendationFields } from 'types/technicalAssistance';
+
+type LinksArrayFieldProps = {
+  control: Control<AdviceLetterRecommendationFields>;
+  watch: UseFormWatch<AdviceLetterRecommendationFields>;
+};
+
+/**
+ * TRB Recommendation links field using React Hook Forms useFieldArray hook
+ */
+export default function LinkArrayField({
+  control,
+  watch
+}: LinksArrayFieldProps) {
+  const { t } = useTranslation('technicalAssistance');
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: 'links'
+  });
+
+  /** Field value - updates on input change */
+  const links = watch('links');
+
+  return (
+    <div id="trbLinksField">
+      {/** Field inputs */}
+      <ul className="usa-list usa-list--unstyled">
+        {fields.map((item, index) => {
+          return (
+            <li key={item.id}>
+              <Label
+                htmlFor={`links.${index}.link`}
+                className="text-normal margin-top-3"
+              >
+                {t('Link')}
+              </Label>
+              <FormGroup className="margin-top-1 display-flex flex-align-center">
+                {/** Link input */}
+                <Controller
+                  name={`links.${index}.link`}
+                  control={control}
+                  render={({ field, fieldState: { error } }) => {
+                    return (
+                      <TextInput
+                        className="margin-top-0"
+                        type="text"
+                        id={`link.${item.id}`}
+                        {...field}
+                        ref={null}
+                      />
+                    );
+                  }}
+                />
+                {/** Remove button */}
+                <Button
+                  type="button"
+                  className="text-secondary margin-top-0 margin-left-2"
+                  onClick={() => remove(index)}
+                  unstyled
+                >
+                  {t('Remove')}
+                </Button>
+              </FormGroup>
+            </li>
+          );
+        })}
+      </ul>
+
+      {/** Add link button */}
+      <Button
+        type="button"
+        className="display-flex flex-align-center"
+        onClick={() => {
+          append({ link: '' });
+        }}
+        disabled={links?.length === 0}
+        unstyled
+      >
+        <IconAdd className="margin-right-05" />
+        {t(
+          fields.length === 0
+            ? 'adviceLetterForm.addResourceLink'
+            : 'adviceLetterForm.addAnotherResourceLink'
+        )}
+      </Button>
+    </div>
+  );
+}
--- src/views/TechnicalAssistance/AdviceLetterForm/Recommendations.tsx
+++ src/views/TechnicalAssistance/AdviceLetterForm/Recommendations.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { useHistory } from 'react-router-dom';
-import { Alert, Button } from '@trussworks/react-uswds';
+import { Route, Switch, useHistory, useRouteMatch } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import { Alert, Button, ButtonGroup } from '@trussworks/react-uswds';
 
+import UswdsReactLink from 'components/LinkWrapper';
+import { DeleteTrbRecommendationQuery } from 'queries/TrbAdviceLetterQueries';
+import {
+  DeleteTRBRecommendation,
+  DeleteTRBRecommendationVariables
+} from 'queries/types/DeleteTRBRecommendation';
+import { GetTrbAdviceLetter_trbRequest_adviceLetter_recommendations as TRBRecommendation } from 'queries/types/GetTrbAdviceLetter';
 import { StepComponentProps } from 'types/technicalAssistance';
 
 import Pager from '../RequestForm/Pager';
+
+import RecommendationsForm from './RecommendationsForm';
+
+export type RecommendationsProps = {
+  trbRequestId: string;
+  recommendations: TRBRecommendation[];
+};
 
 const Recommendations = ({
   trbRequestId,
@@ -15,6 +30,7 @@ const Recommendations = ({
   setFormAlert
 }: StepComponentProps) => {
   const { t } = useTranslation('technicalAssistance');
+  const { path, url } = useRouteMatch();
   const history = useHistory();
 
   const { recommendations } = adviceLetter;
@@ -22,54 +38,111 @@ const Recommendations = ({
   /** Whether recommendations have been added to the request */
   const hasRecommendations: boolean = recommendations.length > 0;
 
+  const [deleteRecommendation] = useMutation<
+    DeleteTRBRecommendation,
+    DeleteTRBRecommendationVariables
+  >(DeleteTrbRecommendationQuery);
+
   return (
-    <div id="trbAdviceRecommendations">
-      {/* Add recommendation button */}
-      <Button
-        className="margin-top-5 margin-bottom-1"
-        type="button"
-        onClick={() => null}
-      >
-        {t('adviceLetterForm.addRecommendation')}
-      </Button>
+    <Switch>
+      {/* Recommendations Form */}
+      <Route exact path={`${path}/form`}>
+        <RecommendationsForm trbRequestId={trbRequestId} />
+      </Route>
 
-      {
-        /* No recommendations message */
-        !hasRecommendations ? (
-          <Alert type="info" slim>
-            {t('adviceLetterForm.noRecommendations')}
-          </Alert>
-        ) : (
-          /* Recommendations list */
-          <ul>
-            {recommendations.map(({ title, id }) => (
-              <li key={id}>{title}</li>
-            ))}
-          </ul>
-        )
-      }
+      {/* Recommendations form step */}
+      <Route exact path={`${path}`}>
+        <div id="trbAdviceRecommendations">
+          {/* Add recommendation button */}
+          <Button
+            className="margin-top-5 margin-bottom-1"
+            type="button"
+            onClick={() => history.push(`${url}/form`)}
+          >
+            {t('adviceLetterForm.addRecommendation')}
+          </Button>
 
-      {/* Form pager buttons */}
-      <Pager
-        className="margin-top-4"
-        back={{
-          outline: true,
-          onClick: () => {
-            history.push(`/trb/${trbRequestId}/advice/summary`);
+          {
+            /* No recommendations message */
+            !hasRecommendations ? (
+              <Alert type="info" slim>
+                {t('adviceLetterForm.noRecommendations')}
+              </Alert>
+            ) : (
+              /* Recommendations list */
+              <ul className="usa-list usa-list--unstyled margin-bottom-2">
+                {recommendations.map(({ title, recommendation, links, id }) => {
+                  return (
+                    <li key={id}>
+                      <h4 className="margin-bottom-0">{title}</h4>
+                      <p className="margin-top-05 margin-bottom-0">
+                        {recommendation}
+                      </p>
+                      {
+                        /* Links list */
+                        links && (
+                          <ul className="usa-list usa-list--unstyled">
+                            {links.map((link, index) => (
+                              // eslint-disable-next-line react/no-array-index-key
+                              <li key={`${id}.${index}`}>
+                                <UswdsReactLink to={link} variant="external">
+                                  {link}
+                                </UswdsReactLink>
+                              </li>
+                            ))}
+                          </ul>
+                        )
+                      }
+                      <ButtonGroup>
+                        {/* Edit */}
+                        <Button type="button" unstyled>
+                          {t('adviceLetterForm.editRecommendation')}
+                        </Button>
+                        {/* Remove */}
+                        <Button
+                          type="button"
+                          className="text-secondary margin-left-1"
+                          onClick={() =>
+                            deleteRecommendation({ variables: { id } })
+                          }
+                          unstyled
+                        >
+                          {t('adviceLetterForm.removeRecommendation')}
+                        </Button>
+                      </ButtonGroup>
+                    </li>
+                  );
+                })}
+              </ul>
+            )
           }
-        }}
-        next={{
-          text: hasRecommendations
-            ? t('button.next')
-            : t('adviceLetterForm.continueWithoutAdding'),
-          onClick: () => history.push(`/trb/${trbRequestId}/advice/next-steps`),
-          outline: !hasRecommendations
-        }}
-        taskListUrl={`/trb/${trbRequestId}/request`}
-        saveExitText={t('adviceLetterForm.returnToRequest')}
-        border={false}
-      />
-    </div>
+
+          {/* Form pager buttons */}
+          <Pager
+            className="margin-top-4"
+            back={{
+              outline: true,
+              onClick: () => {
+                history.push(`/trb/${trbRequestId}/advice/summary`);
+              }
+            }}
+            next={{
+              // TODO: disabled logic
+              // disabled: isSubmitting,
+              text: hasRecommendations
+                ? t('button.next')
+                : t('adviceLetterForm.continueWithoutAdding'),
+              onClick: () =>
+                history.push(`/trb/${trbRequestId}/advice/next-steps`),
+              outline: !hasRecommendations
+            }}
+            taskListUrl={`/trb/${trbRequestId}/request`}
+            saveExitText={t('adviceLetterForm.returnToRequest')}
+            border={false}
+          />
+        </div>
+      </Route>
+    </Switch>
   );
 };
 
