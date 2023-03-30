f7cade083ce910ae711f4365d3bfb734a1bf84d8
ClayBenson94 <clay.benson@oddball.io>
2023-03-30 09:08:00 -0400

    Move csv cleaning to a util, write tests, use in TRB

--- src/data/systemIntake.ts
+++ src/data/systemIntake.ts
@@ -6,6 +6,7 @@ import {
   GovernanceCollaborationTeam,
   SystemIntakeForm
 } from 'types/systemIntake';
+import { cleanCSVData } from 'utils/csv';
 import { formatContractDate, formatDateLocal, parseAsUTC } from 'utils/date';
 // On the frontend, the field is now "requestName", but the backend API
 // has it as "projectName". This was an update from design.
@@ -295,42 +296,6 @@ export const convertIntakeToCSV = (
     decidedAt: intake.decidedAt,
     archivedAt: intake.archivedAt
   });
-};
-
-// Sanitize data for CSV export by escaping characters that could cause Excel DDE issues.
-// https://github.com/react-csv/react-csv/issues/156#issuecomment-517017565
-// https://jiraent.cms.gov/browse/EASI-2717
-export const cleanCSVData = (data: any): any => {
-  /** safety check because typeof null === 'object' */
-  if (data === null) {
-    return null;
-  }
-
-  /** if the child is an array, recursively sanitize the data */
-  if (Array.isArray(data)) {
-    return data.map(eachValue => {
-      return cleanCSVData(eachValue);
-    });
-  }
-
-  /** if the child is an object,  recursively clean the values of each key */
-  if (typeof data === 'object') {
-    return Object.keys(data).reduce((acc: any, eachKey) => {
-      acc[eachKey] = cleanCSVData(data[eachKey]);
-      return acc;
-    }, {});
-  }
-
-  /** if it's a boolean or number, no need to sanitize */
-  if (typeof data === 'boolean' || typeof data === 'number') {
-    return data;
-  }
-
-  /**
-   * fairly confident this should be typecast as a string by now
-   * basically, just chuck a backslash in front of any + or = characters
-   */
-  return (data as string).replace(/[+|=]/g, match => `\\${match}`);
 };
 
 /**
