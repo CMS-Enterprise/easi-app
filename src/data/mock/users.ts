745d6d0ff8102a80f82c3a8cb2a763ca75a0aaee
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2023-04-17 15:27:31 -0500

    Mock users array

---
+++ src/data/mock/users.ts
@@ -0,0 +1,383 @@
+/**
+ * Array containing all mock users for testing purposes
+ */
+export const users = [
+  {
+    __typename: 'UserInfo',
+    commonName: 'Ally Anderson',
+    email: 'ally.anderson@local.fake',
+    euaUserId: 'A11Y'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Adeline Aarons',
+    email: 'adeline.aarons@local.fake',
+    euaUserId: 'ABCD'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Audrey Abrams',
+    email: 'audrey.abrams@local.fake',
+    euaUserId: 'ADMI'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Aaron Adams',
+    email: 'aaron.adams@local.fake',
+    euaUserId: 'ADMN'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Avis Anderson',
+    email: 'avis.anderson@local.fake',
+    euaUserId: 'ATSI'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Otilia Abbott',
+    email: 'otilia.abbott@local.fake',
+    euaUserId: 'AX0Q'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Theo Crooks',
+    email: 'theo.crooks@local.fake',
+    euaUserId: 'CJRW'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Charlie Campbell',
+    email: 'charlie.campbell@local.fake',
+    euaUserId: 'CMSU'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Yasmine Dare',
+    email: 'yasmine.dare@local.fake',
+    euaUserId: 'D2AC'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Marjory Doyle',
+    email: 'marjory.doyle@local.fake',
+    euaUserId: 'D7R3'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Litzy Emard',
+    email: 'litzy.emard@local.fake',
+    euaUserId: 'ER3Z'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Mckayla Fritsch',
+    email: 'mckayla.fritsch@local.fake',
+    euaUserId: 'FAUI'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Hilbert Gislason',
+    email: 'hilbert.gislason@local.fake',
+    euaUserId: 'G4A7'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Delphia Green',
+    email: 'delphia.green@local.fake',
+    euaUserId: 'GBRG'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Hellen Grimes',
+    email: 'hellen.grimes@local.fake',
+    euaUserId: 'GFRY'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Leatha Gorczany',
+    email: 'leatha.gorczany@local.fake',
+    euaUserId: 'GP87'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Gary Gordon',
+    email: 'gary.gordon@local.fake',
+    euaUserId: 'GRTB'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Adrianna Gottlieb',
+    email: 'adrianna.gottlieb@local.fake',
+    euaUserId: 'GT98'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Kenna Gerhold',
+    email: 'kenna.gerhold@local.fake',
+    euaUserId: 'GZP4'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Lucinda Hansen',
+    email: 'lucinda.hansen@local.fake',
+    euaUserId: 'H2KQ'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Alyce Haag',
+    email: 'alyce.haag@local.fake',
+    euaUserId: 'HBGM'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Savanna Hyatt',
+    email: 'savanna.hyatt@local.fake',
+    euaUserId: 'HCNK'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Cecelia Hahn',
+    email: 'cecelia.hahn@local.fake',
+    euaUserId: 'HGDS'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Doyle Heller',
+    email: 'doyle.heller@local.fake',
+    euaUserId: 'HIV3'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Eldred Hammes',
+    email: 'eldred.hammes@local.fake',
+    euaUserId: 'HY0W'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Karianne Hickle',
+    email: 'karianne.hickle@local.fake',
+    euaUserId: 'HYG2'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Brooks Johnson',
+    email: 'brooks.johnson@local.fake',
+    euaUserId: 'J3C8'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Dawn Jaskolski',
+    email: 'dawn.jaskolski@local.fake',
+    euaUserId: 'JG1B'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Anabelle Jerde',
+    email: 'anabelle.jerde@local.fake',
+    euaUserId: 'JTTC'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Nat Krajcik',
+    email: 'nat.krajcik@local.fake',
+    euaUserId: 'K0AM'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Lance Konopelski',
+    email: 'lance.konopelski@local.fake',
+    euaUserId: 'K0LR'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Bernhard Koss',
+    email: 'bernhard.koss@local.fake',
+    euaUserId: 'K9W1'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Deonte Kassulke',
+    email: 'deonte.kassulke@local.fake',
+    euaUserId: 'KDYZ'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Cosmo Kramer',
+    email: 'cosmo.kramer@local.fake',
+    euaUserId: 'KR14'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Isobel Koelpin',
+    email: 'isobel.koelpin@local.fake',
+    euaUserId: 'KT77'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Kennedy Kuhic',
+    email: 'kennedy.kuhic@local.fake',
+    euaUserId: 'KVB3'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Annetta Lockman',
+    email: 'annetta.lockman@local.fake',
+    euaUserId: 'LW40'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Aurelie Morar',
+    email: 'aurelie.morar@local.fake',
+    euaUserId: 'MN3Q'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Gust Murray',
+    email: 'gust.murray@local.fake',
+    euaUserId: 'MR92'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Desmond Nolan',
+    email: 'desmond.nolan@local.fake',
+    euaUserId: 'N60U'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: "Hallie O'Hara",
+    email: 'hallie.ohara@local.fake',
+    euaUserId: 'OQYV'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Rudolph Pagac',
+    email: 'rudolph.pagac@local.fake',
+    euaUserId: 'POJG'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Laverne Roberts',
+    email: 'laverne.roberts@local.fake',
+    euaUserId: 'R0EI'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Catherine Rice',
+    email: 'catherine.rice@local.fake',
+    euaUserId: 'RH4V'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Elva Ruecker',
+    email: 'elva.ruecker@local.fake',
+    euaUserId: 'RP20'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Lauriane Stoltenberg',
+    email: 'lauriane.stoltenberg@local.fake',
+    euaUserId: 'S3W0'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Jerry Seinfeld',
+    email: 'jerry.seinfeld@local.fake',
+    euaUserId: 'SF13'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Alexander Stark',
+    email: 'alexander.stark@local.fake',
+    euaUserId: 'SKZO'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Isidro Swaniawski',
+    email: 'isidro.swaniawski@local.fake',
+    euaUserId: 'SM7H'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Caden Schmeler',
+    email: 'caden.schmeler@local.fake',
+    euaUserId: 'SPJW'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Earnest Torp',
+    email: 'earnest.torp@local.fake',
+    euaUserId: 'TD4Z'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Terry Thompson',
+    email: 'terry.thompson@local.fake',
+    euaUserId: 'TEST'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Waylon Tromp',
+    email: 'waylon.tromp@local.fake',
+    euaUserId: 'TWAW'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Palma Towne',
+    email: 'palma.towne@local.fake',
+    euaUserId: 'TX4A'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'User One',
+    email: 'user.one@local.fake',
+    euaUserId: 'USR1'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'User Two',
+    email: 'user.two@local.fake',
+    euaUserId: 'USR2'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'User Three',
+    email: 'user.three@local.fake',
+    euaUserId: 'USR3'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'User Four',
+    email: 'user.four@local.fake',
+    euaUserId: 'USR4'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'User Five',
+    email: 'user.five@local.fake',
+    euaUserId: 'USR5'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Zechariah Wyman',
+    email: 'zechariah.wyman@local.fake',
+    euaUserId: 'W1I4'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Rolando Weber',
+    email: 'rolando.weber@local.fake',
+    euaUserId: 'WNZ3'
+  },
+  {
+    __typename: 'UserInfo',
+    commonName: 'Kayla Zulauf',
+    email: 'kayla.zulauf@local.fake',
+    euaUserId: 'ZOCN'
+  }
+] as const;
+
+export type MockUserInfo = typeof users[number];
+export type MockEuaUserId = MockUserInfo['euaUserId'];
+export type MockUserName = MockUserInfo['commonName'];
+
+export default users;
