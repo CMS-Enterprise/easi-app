ce7e2b4585e93d9835ac175bd290e3f7e8618089
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2023-04-19 16:41:22 -0500

    MockedQuery utility type

--- src/types/util.ts
+++ src/types/util.ts
@@ -1,5 +1,25 @@
+import { DocumentNode, FetchResult, GraphQLRequest } from '@apollo/client';
+import { MockedResponse } from '@apollo/react-testing';
+
 export type NonNullableProps<T> = {
   [P in keyof T]: NonNullable<T[P]>;
 };
 
 export type FormFieldProps<T> = NonNullableProps<Required<T>>;
+
+/** Extends `GraphQLRequest` to optionally enforce stricter typing on `variables` property */
+interface MockedRequest<
+  TVariables extends Record<string, any> = Record<string, any>
+> extends GraphQLRequest {
+  query: DocumentNode;
+  variables: TVariables;
+}
+
+/** Extends `MockedResponse` to optionally enforce stricter typing on `request` and `result` properties */
+export interface MockedQuery<
+  TData extends Record<string, any> = Record<string, any>,
+  TVariables extends Record<string, any> = Record<string, any>
+> extends MockedResponse<TData> {
+  request: MockedRequest<TVariables>;
+  result: FetchResult<TData>;
+}
