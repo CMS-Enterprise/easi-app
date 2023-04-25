ca45f014549022739c0a4a7ad65fe03795d5d511
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2023-04-25 12:40:50 -0500

    Add ReviewAdviceLetter to form step

--- src/views/TechnicalAssistance/AdviceLetterForm/InternalReview.tsx
+++ src/views/TechnicalAssistance/AdviceLetterForm/InternalReview.tsx
@@ -1,16 +1,41 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useHistory } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
 
+import PageLoading from 'components/PageLoading';
+import { GetTrbAdviceLetterQuery } from 'queries/TrbAdviceLetterQueries';
+import {
+  GetTrbAdviceLetter,
+  GetTrbAdviceLetterVariables
+} from 'queries/types/GetTrbAdviceLetter';
+import { NotFoundPartial } from 'views/NotFound';
+
+import ReviewAdviceLetter from '../AdminHome/components/ReviewAdviceLetter';
 import Pager from '../RequestForm/Pager';
 
 const InternalReview = ({ trbRequestId }: { trbRequestId: string }) => {
   const { t } = useTranslation('technicalAssistance');
   const history = useHistory();
 
+  const { data, loading } = useQuery<
+    GetTrbAdviceLetter,
+    GetTrbAdviceLetterVariables
+  >(GetTrbAdviceLetterQuery, {
+    variables: {
+      id: trbRequestId
+    }
+  });
+
+  const adviceLetter = data?.trbRequest?.adviceLetter;
+
+  if (loading) return <PageLoading />;
+  if (!adviceLetter) return <NotFoundPartial />;
+
   return (
     <div id="trbAdviceInternalReview">
       {/* Internal Review */}
+      <ReviewAdviceLetter adviceLetter={adviceLetter} />
       {/** Form pager buttons */}
       <Pager
         className="margin-top-4"
