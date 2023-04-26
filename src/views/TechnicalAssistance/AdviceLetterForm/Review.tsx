4d1833c3ec1aa7a2c8f3d415689bbcd85d644213
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2023-04-25 21:11:42 -0500

    Submit action EmailRecipientFields

--- src/views/TechnicalAssistance/AdviceLetterForm/Review.tsx
+++ src/views/TechnicalAssistance/AdviceLetterForm/Review.tsx
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { FormProvider, useForm } from 'react-hook-form';
 import { Trans, useTranslation } from 'react-i18next';
 import { useHistory } from 'react-router-dom';
-import { Accordion } from '@trussworks/react-uswds';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Accordion, Form } from '@trussworks/react-uswds';
 
+import EmailRecipientFields from 'components/EmailRecipientFields';
 import Divider from 'components/shared/Divider';
 import useCacheQuery from 'hooks/useCacheQuery';
+import useTRBAttendees from 'hooks/useTRBAttendees';
 import GetTrbAdminNotesQuery from 'queries/GetTrbAdminNotesQuery';
 import {
   GetTrbAdminNotes,
   GetTrbAdminNotes_trbRequest_adminNotes as AdminNote,
   GetTrbAdminNotesVariables
 } from 'queries/types/GetTrbAdminNotes';
-import { StepComponentProps } from 'types/technicalAssistance';
+import {
+  StepComponentProps,
+  TrbRecipientFields
+} from 'types/technicalAssistance';
 import { formatDateLocal } from 'utils/date';
+import { trbActionSchema } from 'validations/trbRequestSchema';
 
 import ReviewAdviceLetter from '../AdminHome/components/ReviewAdviceLetter';
 import Pager from '../RequestForm/Pager';
@@ -39,9 +47,27 @@ const Review = ({
 
   const notes: AdminNote[] = data?.trbRequest?.adminNotes || [];
 
-  // useEffect(() => {
-  //   setIsStepSubmitting(isSubmitting);
-  // }, [setIsStepSubmitting, isSubmitting]);
+  const {
+    data: { attendees, requester },
+    createAttendee
+  } = useTRBAttendees(trbRequestId);
+
+  const actionForm = useForm<TrbRecipientFields>({
+    resolver: yupResolver(trbActionSchema()),
+    defaultValues: {
+      copyTrbMailbox: true,
+      notifyEuaIds: []
+    }
+  });
+
+  const {
+    handleSubmit,
+    formState: { isSubmitting }
+  } = actionForm;
+
+  useEffect(() => {
+    setIsStepSubmitting(isSubmitting);
+  }, [setIsStepSubmitting, isSubmitting]);
 
   return (
     <div id="trbAdviceReview" className="margin-top-5">
@@ -76,25 +102,49 @@ const Review = ({
 
       <Divider />
 
-      {/** Form pager buttons */}
-      <Pager
-        className="margin-top-4"
-        back={{
-          outline: true,
-          onClick: () =>
-            history.push(`/trb/${trbRequestId}/advice/internal-review`)
-        }}
-        next={{
-          text: 'Send',
-          onClick: () => {
-            // TODO: Submit
-            history.push(`/trb/${trbRequestId}/request`);
-          }
-        }}
-        taskListUrl={`/trb/${trbRequestId}/request`}
-        saveExitText={t('adviceLetterForm.returnToRequest')}
-        border={false}
-      />
+      {/* <h3>{t('actionRequestEdits.notificationTitle')}</h3> */}
+
+      <Form
+        onSubmit={handleSubmit(formData => null)}
+        className="maxw-full margin-bottom-205 tablet:grid-col-12 desktop:grid-col-6"
+      >
+        <FormProvider {...actionForm}>
+          <EmailRecipientFields
+            requester={requester}
+            contacts={attendees}
+            mailboxes={[
+              {
+                key: 'copyTrbMailbox',
+                label: t('emailRecipientFields.copyTrbMailbox')
+              }
+            ]}
+            createContact={contact =>
+              createAttendee({ ...contact, trbRequestId })
+            }
+          />
+        </FormProvider>
+
+        {/** Form pager buttons */}
+        <Pager
+          className="margin-top-4"
+          back={{
+            outline: true,
+            onClick: () =>
+              history.push(`/trb/${trbRequestId}/advice/internal-review`)
+          }}
+          next={{
+            type: 'submit',
+            text: 'Send'
+            // onClick: () => {
+            //   // TODO: Submit
+            //   history.push(`/trb/${trbRequestId}/request`);
+            // }
+          }}
+          taskListUrl={`/trb/${trbRequestId}/request`}
+          saveExitText={t('adviceLetterForm.returnToRequest')}
+          border={false}
+        />
+      </Form>
     </div>
   );
 };
