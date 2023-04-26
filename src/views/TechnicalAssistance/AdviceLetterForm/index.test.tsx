6ce2b46a58a7b230000cf97f0517de1b9ecba2a4
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2023-04-26 00:59:37 -0500

    Internal review snapshot

--- src/views/TechnicalAssistance/AdviceLetterForm/index.test.tsx
+++ src/views/TechnicalAssistance/AdviceLetterForm/index.test.tsx
@@ -160,7 +160,7 @@ describe('TRB Advice Letter Form', () => {
     expect(linkInput).toHaveValue(linkText);
   });
 
-  it.only('renders the Next Steps form', async () => {
+  it('renders the Next Steps form', async () => {
     const { findByRole, getByRole } = renderForm('next-steps');
 
     const nextStepsInput = await findByRole('textbox', {
@@ -183,5 +183,17 @@ describe('TRB Advice Letter Form', () => {
     // Check that followup point input is hidden when followup radio field is false
     userEvent.click(getByRole('radio', { name: 'Not necessary' }));
     expect(followupPointInput).not.toBeInTheDocument();
+  });
+
+  it('renders the Internal Review step', async () => {
+    const { asFragment, getByRole } = renderForm('internal-review');
+
+    await waitForPageLoad();
+
+    // Check for review needed alert
+    const alert = getByRole('heading', { name: 'Internal TRB review needed' });
+    expect(alert).toBeInTheDocument();
+
+    expect(asFragment()).toMatchSnapshot();
   });
 });
