69e4124b60d1a7bbe43fdbb577f8b1909939d516
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2023-05-05 15:37:30 -0500

    <IconLink> and <IconButton> components

---
+++ src/components/shared/IconButton/index.tsx
@@ -0,0 +1,55 @@
+import React, { ReactElement } from 'react';
+import { Button } from '@trussworks/react-uswds';
+import classNames from 'classnames';
+
+type IconProps = {
+  focusable?: boolean;
+  role?: string;
+  size?: 3 | 4 | 5 | 6 | 7 | 8 | 9;
+  className?: string;
+} & JSX.IntrinsicElements['svg'];
+
+type IconButtonProps = {
+  icon: ReactElement<IconProps>;
+  iconPosition?: 'before' | 'after';
+  type: 'button' | 'submit' | 'reset';
+  children: React.ReactNode;
+  secondary?: boolean;
+  base?: boolean;
+  accentStyle?: 'cool' | 'warm';
+  outline?: boolean;
+  inverse?: boolean;
+  size?: 'big' | 'small';
+  unstyled?: boolean;
+} & JSX.IntrinsicElements['button'];
+
+/**
+ * Link with icon
+ */
+const IconButton = ({
+  icon,
+  iconPosition = 'before',
+  children,
+  className,
+  ...defaultProps
+}: IconButtonProps) => {
+  const linkIcon = React.cloneElement(icon, {
+    className: classNames(
+      icon.props.className,
+      `margin-${iconPosition === 'before' ? 'right' : 'left'}-1`
+    )
+  });
+
+  return (
+    <Button
+      className={classNames('display-flex flex-align-center', className)}
+      {...defaultProps}
+    >
+      {iconPosition === 'before' && linkIcon}
+      {children}
+      {iconPosition === 'after' && linkIcon}
+    </Button>
+  );
+};
+
+export default IconButton;
