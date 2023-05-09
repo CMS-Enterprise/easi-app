b9c94d24bf2b629ee9c71737ea11a0cbedbf7ea6
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2023-05-09 15:28:09 -0500

    GetCedarRoleTypes query

--- src/queries/SetRolesForUserOnSystemQuery.ts
+++
@@ -1,7 +0,0 @@
-import { gql } from '@apollo/client';
-
-export default gql`
-  mutation SetRolesForUserOnSystem($input: SetRolesForUserOnSystemInput!) {
-    setRolesForUserOnSystem(input: $input)
-  }
-`;
