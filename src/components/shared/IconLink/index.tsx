f6331ffda563c4a8338ad52b66973dab224f33e4
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2023-05-15 10:02:41 -0500

    [EASI-2439] - System profile edit team (#1999)
    * Edit team page and form
    
    * Add edit team to routes
    
    * Employee fields
    
    * GetSystemProfileTeam query
    
    * Form default values
    
    * <IconLink> and <IconButton> components
    
    * Return to system profile links
    
    * Merge function for cedarSystemDetails
    
    * Add roles to team query
    
    * CedarRole query fragment
    
    * Team members section
    
    * Team member card footer buttons
    
    * Link to edit form
    
    * Add team member link
    
    * Add system name to query
    
    * Breadcrumbs
    
    * <EditPageCallout> component
    
    * Responsiveness
    
    * EditPageCallout tests
    
    * <EditTeam> tests
    
    * Fix system profile test and snapshot
    
    * Test for team card edit and remove buttons
    
    * Refactor routes
    
    * Remove unused query
    
    * Update tests
    
    * Hide contractor and federal employee fields
    
    * Hide edit page callouts until EASI-2447 is done
    
    * Change form route to 'team-member'
    
    * Removed unnecessary snapshot
    
    The snapshot in this test was flaky due to the loading spinner within the request form.
    
    We're already testing that the alert text exists, so the snapshot isn't necessary. The request form already has a snapshot in another test file.
    
    ---------
    
    Co-authored-by: ClayBenson94 <clay.benson@oddball.io>

---
+++ src/components/shared/IconLink/index.tsx
@@ -0,0 +1,55 @@
+import React, { ReactElement } from 'react';
+import classNames from 'classnames';
+
+import UswdsReactLink from 'components/LinkWrapper';
+
+type IconProps = {
+  focusable?: boolean;
+  role?: string;
+  size?: 3 | 4 | 5 | 6 | 7 | 8 | 9;
+  className?: string;
+} & JSX.IntrinsicElements['svg'];
+
+type IconLinkProps = {
+  icon: ReactElement<IconProps>;
+  iconPosition?: 'before' | 'after';
+  to: string | object;
+  variant?: 'external' | 'unstyled' | 'nav';
+  className?: string;
+  target?: '_blank';
+  rel?: 'noopener noreferrer';
+  'data-testid'?: string;
+  children: React.ReactNode | string;
+  onClick?: (e: React.MouseEvent<HTMLElement>) => void;
+};
+
+/**
+ * Link with icon
+ */
+const IconLink = ({
+  icon,
+  iconPosition = 'before',
+  className,
+  children,
+  ...defaultProps
+}: IconLinkProps) => {
+  const linkIcon = React.cloneElement(icon, {
+    className: classNames(
+      icon.props.className,
+      `margin-${iconPosition === 'before' ? 'right' : 'left'}-1`
+    )
+  });
+
+  return (
+    <UswdsReactLink
+      className={classNames('display-flex flex-align-center', className)}
+      {...defaultProps}
+    >
+      {iconPosition === 'before' && linkIcon}
+      {children}
+      {iconPosition === 'after' && linkIcon}
+    </UswdsReactLink>
+  );
+};
+
+export default IconLink;
