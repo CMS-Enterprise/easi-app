57ae459eb7c4da5d36d6a070293ea7b064a6081b
Patrick Segura <95709965+patrickseguraoddball@users.noreply.github.com>
2023-05-17 13:45:21 -0400

    [EASI-2944] - Action alert for admin IRF state = draft/ready (#2013)
    * Updated/added alert for IRF in draft state for admin
    
    * Updated unit test
    
    ---------
    
    Co-authored-by: Clay Benson <clay.benson@oddball.io>

--- src/i18n/en-US/technicalAssistance.ts
+++ src/i18n/en-US/technicalAssistance.ts
@@ -918,7 +918,9 @@ const technicalAssistance = {
     docInfoPlural:
       'There are <bold>{{docCount}}</bold> additional documents uploaded as a part of this request.',
     reviewInitialRequest:
-      'Please review the initial request form before setting a date and time in EASi.',
+      'Please wait until the initial request form is submitted, then review the form before setting a date and time in EASi.',
+    requestInDraftAlt:
+      'This request is still in a draft state. You will receive an email notification once the requester has submitted it for review.',
     viewDocs: 'View documents',
     open: 'Open',
     closed: 'Closed',
