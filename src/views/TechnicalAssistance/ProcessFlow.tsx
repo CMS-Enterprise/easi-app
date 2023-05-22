0175a3493e75debb06b15614ba763c194e71d194
Patrick Segura <patrick.segura@oddball.io>
2023-05-22 15:11:59 -0400

    Added new TRb article and linked in task list

--- src/views/TechnicalAssistance/ProcessFlow.tsx
+++ src/views/TechnicalAssistance/ProcessFlow.tsx
@@ -2,13 +2,7 @@ import React, { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Redirect, useHistory, useLocation } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
-import {
-  Button,
-  GridContainer,
-  ProcessList,
-  ProcessListHeading,
-  ProcessListItem
-} from '@trussworks/react-uswds';
+import { Button, GridContainer } from '@trussworks/react-uswds';
 
 import UswdsReactLink from 'components/LinkWrapper';
 import PageHeading from 'components/PageHeading';
@@ -18,6 +12,7 @@ import {
   CreateTrbRequestVariables
 } from 'queries/types/CreateTrbRequest';
 import { TRBRequestType } from 'types/graphql-global-types';
+import { StepsInProcessContent } from 'views/Help/TechnicalReviewBoard/StepsInProcess';
 
 import Breadcrumbs from './Breadcrumbs';
 
@@ -46,12 +41,6 @@ function ProcessFlow() {
   // Redirect to start if `requestType` isn't set
   if (!requestType) return <Redirect to="/trb/start" />;
 
-  const infoText = t<Record<string, string[]>>('steps.info', {
-    returnObjects: true
-  });
-  const stepsText = t<Record<string, string | string[]>[]>('steps.list', {
-    returnObjects: true
-  });
   const requestTypeText = t<Record<string, { heading: string }>>(
     'requestType.type',
     {
@@ -80,75 +69,9 @@ function ProcessFlow() {
           </UswdsReactLink>
         </span>
       </div>
-      <div className="margin-top-2 font-body-lg line-height-body-5 text-light">
-        {t('steps.description')}
-      </div>
-      <div className="margin-top-4 padding-2 line-height-body-5 bg-gray-5">
-        <div>{infoText.text[0]}</div>
-        <ul className="margin-y-1 list-style-middot-inner">
-          <li>{infoText.list[0]}</li>
-          <li>{infoText.list[1]}</li>
-          <li>{infoText.list[2]}</li>
-        </ul>
-        <div>{infoText.text[1]}</div>
-      </div>
-      <div>
-        <h2 className="margin-top-6 margin-bottom-5">
-          {t('steps.stepsInTheProcess')}
-        </h2>
-        <ProcessList className="padding-top-0 grid-col-8">
-          <ProcessListItem className="maxw-none">
-            <ProcessListHeading type="h3">
-              {stepsText[0].heading}
-            </ProcessListHeading>
-            <p>{stepsText[0].text[0]}</p>
-            <p>{stepsText[0].text[1]}</p>
-          </ProcessListItem>
-          <ProcessListItem className="maxw-none">
-            <ProcessListHeading type="h3">
-              {stepsText[1].heading}
-            </ProcessListHeading>
-            <p>{stepsText[1].text[0]}</p>
-          </ProcessListItem>
-          <ProcessListItem className="maxw-none">
-            <ProcessListHeading type="h3">
-              {stepsText[2].heading}
-            </ProcessListHeading>
-            <p className="margin-bottom-0">{stepsText[2].text[0]}</p>
-            <ul className="list-style-middot">
-              <li>{stepsText[2].list[0]}</li>
-              <li>{stepsText[2].list[1]}</li>
-              <li>{stepsText[2].list[2]}</li>
-            </ul>
-            <p>
-              <UswdsReactLink
-                to="/help/trb/prepare-consult-meeting"
-                target="_blank"
-              >
-                {stepsText[2].text[1]}
-              </UswdsReactLink>
-            </p>
-          </ProcessListItem>
-          <ProcessListItem className="maxw-none">
-            <ProcessListHeading type="h3">
-              {stepsText[3].heading}
-            </ProcessListHeading>
-            <p className="margin-bottom-0">{stepsText[3].text[0]}</p>
-            <ul className="list-style-middot">
-              <li>{stepsText[3].list[0]}</li>
-              <li>{stepsText[3].list[1]}</li>
-              <li>{stepsText[3].list[2]}</li>
-            </ul>
-            <p>{stepsText[3].text[1]}</p>
-          </ProcessListItem>
-          <ProcessListItem className="maxw-none">
-            <ProcessListHeading type="h3">
-              {stepsText[4].heading}
-            </ProcessListHeading>
-            <p>{stepsText[4].text[0]}</p>
-          </ProcessListItem>
-        </ProcessList>
-      </div>
+
+      <StepsInProcessContent />
+
       <div className="margin-top-1">
         <UswdsReactLink
           to="/trb/start"
