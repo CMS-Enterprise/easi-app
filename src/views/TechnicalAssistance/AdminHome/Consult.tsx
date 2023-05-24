1ce82abd4379db006f0b123bf3532b563d0554ad
Patrick Segura <patrick.segura@oddball.io>
2023-05-24 12:05:14 -0400

    Updated return route after consult

--- src/views/TechnicalAssistance/AdminHome/Consult.tsx
+++ src/views/TechnicalAssistance/AdminHome/Consult.tsx
@@ -46,9 +46,8 @@ interface ConsultFields extends TrbRecipientFields {
 function Consult() {
   const { t } = useTranslation('technicalAssistance');
 
-  const { id, activePage } = useParams<{
+  const { id } = useParams<{
     id: string;
-    activePage: string;
     action: 'schedule-consult';
   }>();
   const history = useHistory();
@@ -60,7 +59,7 @@ function Consult() {
     createAttendee
   } = useTRBAttendees(id);
 
-  const requestUrl = `/trb/${id}/${activePage}`;
+  const requestUrl = `/trb/${id}/request`;
 
   const [mutate, mutationResult] = useMutation<
     UpdateTrbRequestConsultMeeting,
