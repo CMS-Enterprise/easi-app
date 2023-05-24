5561408dfd85cb38218374908860d09353b8f445
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2023-05-24 09:26:13 -0500

    [NOREF] Upgrade react hook forms (#2025)
    * Upgrade React Hook Forms
    
    * Fix FormProvider type error
    
    `<FormProvider>` throws "type instantiation is excessively deep and possibly infinite" error if no fields type is provided to both `useForm` and `FormProvider`
    
    * Refactor Basic form step submit
    
    Refactor to handle errors within handleSubmit
    
    * Subject Areas error handling
    
    * Attendees error handling
    
    * Advice letter form error handling
    
    * Remove unneccesary error handling

--- src/views/TechnicalAssistance/RequestForm/Attendees.tsx
+++ src/views/TechnicalAssistance/RequestForm/Attendees.tsx
@@ -161,53 +161,39 @@ function Attendees({
       handleSubmit(
         // Validation passed
         async formData => {
-          // Submit the input only if there are changes
-          if (isDirty && requester.id) {
-            const { component, role } = formData;
-            // Update requester
-            await updateAttendee({
-              id: requester.id,
-              component: component || '',
-              role: role as PersonRole
-            })
-              // Refresh the RequestForm parent request query
-              // to update things like `stepsCompleted`
-              .then(() => refetchRequest && refetchRequest())
-              .catch(() => {
-                throw new Error(t<string>('attendees.alerts.error'));
-              });
-          }
-        },
-        // Validation did not pass
-        () => {
-          // Need to throw from this error handler so that the promise is rejected
-          throw new Error(t<string>('attendees.alerts.invalidForm'));
-        }
-      )()
-        // Wait for submit to finish before continuing.
-        // This essentially makes sure any effects like
-        // `setIsStepSubmitting` are called before unmount.
-        .then(
-          () => {
+          try {
+            // Submit the input only if there are changes
+            if (isDirty && requester.id) {
+              const { component, role } = formData;
+              // Update requester
+              await updateAttendee({
+                id: requester.id,
+                component: component || '',
+                role: role as PersonRole
+              })
+                // Refresh the RequestForm parent request query
+                // to update things like `stepsCompleted`
+                .then(() => refetchRequest?.());
+            }
             callback?.();
-          },
-          err => {
-            if (err instanceof ApolloError) {
+          } catch (e) {
+            if (e instanceof ApolloError) {
               setFormAlert({
                 type: 'error',
                 message: t<string>('attendees.alerts.error')
               });
             }
           }
-        ),
+        }
+      )(),
     [
-      t,
       handleSubmit,
       isDirty,
       refetchRequest,
-      setFormAlert,
       requester,
-      updateAttendee
+      updateAttendee,
+      setFormAlert,
+      t
     ]
   );
 
