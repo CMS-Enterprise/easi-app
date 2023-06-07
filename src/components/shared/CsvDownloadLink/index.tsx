3d31ee178d2c11a09d8711dc4e3708360f38b4b5
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2023-06-07 14:24:02 -0500

    [EASI-2991] Admin home view switcher (#2059)
    * <AdminHome> component
    
    * Select view dropdown
    
    * Add tables to AdminHome
    
    * TRB home table updates
    
    - Removed title and description text
    - UX updates
    
    * Update <CsvDownloadLink> props
    
    Updated props to match <CSVLink> component props
    
    * IT Gov admin table updates
    
    - Removed breadcrumbs
    - UX updates
    
    * Save view selection to local storage
    
    * Responsiveness
    
    * Fix failed test
    
    * Unit test
    
    * Fix unit test local storage bug
    
    * Cleanup
    
    * Fix adminView initial state bug
    
    * Cypress test fixes
    
    * Fix pr out of sync

--- src/components/shared/CsvDownloadLink/index.tsx
+++ src/components/shared/CsvDownloadLink/index.tsx
@@ -3,26 +3,33 @@ import { CSVLink } from 'react-csv';
 import { IconFileDownload } from '@trussworks/react-uswds';
 import classnames from 'classnames';
 
+interface CsvLinkProps {
+  data: string | object[];
+  headers?:
+    | {
+        label: string;
+        key: string;
+      }[]
+    | string[];
+  enclosingCharacter?: string;
+  separator?: string;
+  filename?: string;
+  uFEFF?: boolean;
+  asyncOnClick?: boolean;
+  className?: string;
+  children?: React.ReactNode;
+}
+
 /**
  * Uswds link wrapper of `CSVLink` with `IconFileDownload`.
  */
 export default function CsvDownloadLink({
   children,
-  data,
-  filename,
-  className
-}: {
-  children?: React.ReactNode;
-  data: any;
-  filename?: string;
-  className?: string;
-}) {
+  className,
+  ...props
+}: CsvLinkProps) {
   return (
-    <CSVLink
-      className={classnames('usa-link', className)}
-      data={data}
-      filename={filename}
-    >
+    <CSVLink className={classnames('usa-link', className)} {...props}>
       <IconFileDownload className="text-middle margin-right-1" />
       {children}
     </CSVLink>
