648b343b8c1049b4d88ce93e511537ff5805bb48
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2023-06-13 10:33:53 -0500

    Add documents to system intake query

--- src/queries/SystemIntakeDocumentQueries.ts
+++ src/queries/SystemIntakeDocumentQueries.ts
@@ -1,6 +1,6 @@
 import { gql } from '@apollo/client';
 
-const SystemIntakeDocument = gql`
+export const SystemIntakeDocument = gql`
   fragment SystemIntakeDocument on SystemIntakeDocument {
     documentType {
       commonType
