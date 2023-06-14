880283e23809d1314eabe7cf00ac153a815ba92b
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2023-06-14 10:04:30 -0500

    [EASI-2447] System profile add/remove contact (#2063)
    * <TeamMemberForm> text and initial layout
    
    * <TeamContactCard> displayRoles prop and styling
    
    * Add contact card to edit roles form
    
    * SetRolesForUserOnSystem mutation
    
    * GetCedarRoleTypes query
    
    * Name field
    
    * Role multiselect field
    
    * Field default values
    
    * Update roles field name
    
    * Fix breadcrumbs
    
    * Form submit disabled state
    
    * Form loading state
    
    * Form validation
    
    * Basic form unit test
    
    * Make edit page callout visible
    
    * Return and update query on success
    
    * Remove team member
    
    * Fix alert close button alignment
    
    * Success/Error message functionality
    
    * Add/edit alert messages
    
    * Add loading spinner to form button
    
    * Remove team member modal
    
    * Unit test fixes
    
    * Revert "Make edit page callout visible"
    
    This reverts commit 4c1e4de2aada592842543c947b37453ee21f453c.

--- src/types/graphql-global-types.ts
+++ src/types/graphql-global-types.ts
@@ -610,6 +610,12 @@ export interface SendTRBAdviceLetterInput {
   notifyEuaIds: string[];
 }
 
+export interface SetRolesForUserOnSystemInput {
+  cedarSystemID: string;
+  euaUserId: string;
+  desiredRoleTypeIDs: string[];
+}
+
 /**
  * Input to submit an intake for review
  */
