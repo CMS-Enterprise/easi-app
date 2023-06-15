5416f5e67429db12c9e45e02577004c2eab886eb
Dylan Sprague <dylan.sprague@oddball.io>
2023-06-15 13:29:25 -0400

    Create SQL table for general governance_request_feedback

---
+++ migrations/V143__Create_Governance_Request_Feedback.sql
@@ -0,0 +1,17 @@
+CREATE TABLE governance_request_feedback (
+    -- PK
+    id UUID PRIMARY KEY NOT NULL,
+
+    -- FK
+    intake_id UUID NOT NULL REFERENCES system_intakes(id),
+
+    -- required fields
+    feedback TEXT NOT NULL,
+
+    -- general metadata
+    -- use created_by to denote who the feedback is from
+    created_by TEXT NOT NULL CHECK (created_by ~ '^[A-Z0-9]{4}$'),
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    modified_by TEXT CHECK (modified_by ~ '^[A-Z0-9]{4}$'),
+    modified_at TIMESTAMP WITH TIME ZONE
+);
