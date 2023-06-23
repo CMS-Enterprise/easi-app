a78e6cf558df0ec96088034b289e28117544a25f
Dylan Sprague <dylan.sprague@oddball.io>
2023-06-23 17:11:49 -0400

    Add SQL-based constraint for source/target combinations using FK to table of valid combinations

--- migrations/V143__Create_Governance_Request_Feedback.sql
+++ migrations/V143__Create_Governance_Request_Feedback.sql
@@ -10,6 +10,21 @@ CREATE TYPE feedback_target_form AS ENUM (
     'FINAL_BUSINESS_CASE'
 );
 
+-- list of the only valid source_action/target_form combinations, so we can validate governance_request_feedback data
+CREATE TABLE feedback_valid_source_target_combinations (
+	source_action feedback_source_action NOT NULL,
+    target_form feedback_target_form NOT NULL,
+    PRIMARY KEY (source_action, target_form)
+);
+
+INSERT INTO feedback_valid_source_target_combinations (source_action, target_form)
+VALUES
+	('PROGRESS_TO_NEW_STEP', 'NO_TARGET_PROVIDED'),
+	('REQUEST_EDITS', 'INTAKE_REQUEST'),
+	('REQUEST_EDITS', 'DRAFT_BUSINESS_CASE'),
+	('REQUEST_EDITS', 'FINAL_BUSINESS_CASE')
+;
+
 CREATE TABLE governance_request_feedback (
     -- PK
     id UUID PRIMARY KEY NOT NULL,
@@ -27,5 +42,8 @@ CREATE TABLE governance_request_feedback (
     created_by TEXT NOT NULL CHECK (created_by ~ '^[A-Z0-9]{4}$'),
     created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     modified_by TEXT CHECK (modified_by ~ '^[A-Z0-9]{4}$'),
-    modified_at TIMESTAMP WITH TIME ZONE
+    modified_at TIMESTAMP WITH TIME ZONE,
+
+    -- validation for (source_action, target_form) combination
+    FOREIGN KEY (source_action, target_form) REFERENCES feedback_valid_source_target_combinations (source_action, target_form)
 );
