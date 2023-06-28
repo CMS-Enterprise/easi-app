dc6f666d41b02da5c866d84b3ae198e5852178ae
adamodd <adam.suharja@oddball.io>
2023-06-28 15:28:36 -0700

    Gov task list initial state from query data

--- src/views/GovernanceTaskList/index.test.tsx
+++ src/views/GovernanceTaskList/index.test.tsx
@@ -1,23 +1,64 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { MockedProvider } from '@apollo/client/testing';
-import { render } from '@testing-library/react';
+import { MemoryRouter, Route } from 'react-router-dom';
+import {
+  render,
+  waitForElementToBeRemoved,
+  within
+} from '@testing-library/react';
 import i18next from 'i18next';
+
+import GetGovernanceTaskListQuery from 'queries/GetGovernanceTaskListQuery';
+import VerboseMockedProvider from 'utils/testing/VerboseMockedProvider';
 
 import GovernanceTaskList from '.';
 
 describe('Governance Task List', () => {
-  it('renders a request task list', async () => {
-    const { getByRole } = render(
-      <MemoryRouter>
-        <MockedProvider>
-          <GovernanceTaskList />
-        </MockedProvider>
+  const id = '80950153-4a66-4881-b728-f4cc701ff926';
+
+  it('renders a request task list at the initial state', async () => {
+    const { getByRole, getByTestId } = render(
+      <MemoryRouter initialEntries={[`/governance-task-list/${id}`]}>
+        <VerboseMockedProvider
+          mocks={[
+            {
+              request: {
+                query: GetGovernanceTaskListQuery,
+                variables: {
+                  id
+                }
+              },
+              result: {
+                data: {
+                  systemIntake: {
+                    id,
+                    itGovTaskStatuses: {
+                      intakeFormStatus: 'READY',
+                      feedbackFromInitialReviewStatus: 'CANT_START',
+                      decisionAndNextStepsStatus: 'CANT_START',
+                      bizCaseDraftStatus: 'CANT_START',
+                      grtMeetingStatus: 'CANT_START',
+                      bizCaseFinalStatus: 'CANT_START',
+                      grbMeetingStatus: 'CANT_START',
+                      __typename: 'ITGovTaskStatuses'
+                    },
+                    __typename: 'SystemIntake'
+                  }
+                }
+              }
+            }
+          ]}
+        >
+          <Route path="/governance-task-list/:systemId">
+            <GovernanceTaskList />
+          </Route>
+        </VerboseMockedProvider>
       </MemoryRouter>
     );
 
+    await waitForElementToBeRemoved(() => getByTestId('page-loading'));
+
     // Header
-    getByRole('heading', {
+    await getByRole('heading', {
       level: 1,
       name: i18next.t<string>('itGov:taskList.heading')
     });
@@ -40,6 +81,17 @@ describe('Governance Task List', () => {
     getByRole('heading', {
       level: 3,
       name: i18next.t<string>('itGov:taskList.steps.0.title')
+    });
+
+    // The task list initial state is: Step 1: Ready to start
+    const step1 = getByTestId('fill-out-the-intake-request-form');
+    expect(within(step1).getByTestId('task-list-task-tag')).toHaveTextContent(
+      i18next.t<string>('taskList:taskStatus.READY')
+    );
+
+    // Step 1: Ready to start should show the Start button
+    within(step1).getByRole('link', {
+      name: i18next.t<string>('itGov:button.start')
     });
   });
 });
--- src/views/GovernanceTaskList/index.tsx
+++ src/views/GovernanceTaskList/index.tsx
@@ -8,6 +8,7 @@ import { kebabCase } from 'lodash';
 import UswdsReactLink from 'components/LinkWrapper';
 import MainContent from 'components/MainContent';
 import PageHeading from 'components/PageHeading';
+import PageLoading from 'components/PageLoading';
 import TaskListItem, {
   TaskListContainer,
   TaskListDescription
@@ -18,14 +19,14 @@ import {
   GetGovernanceTaskListVariables
 } from 'queries/types/GetGovernanceTaskList';
 import { ITGovIntakeFormStatus } from 'types/graphql-global-types';
+import NotFound from 'views/NotFound';
 import Breadcrumbs from 'views/TechnicalAssistance/Breadcrumbs';
 
 function GovernanceTaskList() {
   const { systemId } = useParams<{ systemId: string }>();
   const { t } = useTranslation('itGov');
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { data } = useQuery<
+  const { data, loading, error } = useQuery<
     GetGovernanceTaskList,
     GetGovernanceTaskListVariables
   >(GetGovernanceTaskListQuery, {
@@ -33,6 +34,10 @@ function GovernanceTaskList() {
       id: systemId
     }
   });
+
+  if (error) {
+    return <NotFound />;
+  }
 
   return (
     <MainContent className="margin-bottom-5 desktop:margin-bottom-10">
@@ -46,154 +51,162 @@ function GovernanceTaskList() {
           ]}
         />
 
-        <Grid row gap className="margin-top-6">
-          <Grid tablet={{ col: 9 }}>
-            <PageHeading className="margin-y-0">
-              {t('taskList.heading')}
-            </PageHeading>
+        {loading && <PageLoading />}
 
-            <div className="line-height-body-4">
-              <div className="font-body-lg line-height-body-5 text-light">
-                {t('taskList.description')}
-              </div>
+        {!loading && !!data && (
+          <Grid row gap className="margin-top-6">
+            <Grid tablet={{ col: 9 }}>
+              <PageHeading className="margin-y-0">
+                {t('taskList.heading')}
+              </PageHeading>
 
-              <TaskListContainer className="margin-top-4">
-                {/* 1. Fill out the Intake Request form */}
-                <TaskListItem
-                  heading={t('taskList.steps.0.title')}
-                  status={ITGovIntakeFormStatus.READY}
-                  testId={kebabCase(t('taskList.steps.0.title'))}
-                >
-                  <TaskListDescription>
-                    <p>{t('taskList.steps.0.description')}</p>
-                  </TaskListDescription>
-                  <UswdsReactLink
-                    variant="unstyled"
-                    className="usa-button"
-                    to="./"
+              <div className="line-height-body-4">
+                <div className="font-body-lg line-height-body-5 text-light">
+                  {t('taskList.description')}
+                </div>
+
+                <TaskListContainer className="margin-top-4">
+                  {/* 1. Fill out the Intake Request form */}
+                  <TaskListItem
+                    heading={t('taskList.steps.0.title')}
+                    status={ITGovIntakeFormStatus.READY}
+                    testId={kebabCase(t('taskList.steps.0.title'))}
                   >
-                    {t('button.start')}
+                    <TaskListDescription>
+                      <p>{t('taskList.steps.0.description')}</p>
+                    </TaskListDescription>
+
+                    {data.systemIntake?.itGovTaskStatuses.intakeFormStatus ===
+                      ITGovIntakeFormStatus.READY && (
+                      <UswdsReactLink
+                        variant="unstyled"
+                        className="usa-button"
+                        to="./"
+                      >
+                        {t('button.start')}
+                      </UswdsReactLink>
+                    )}
+                  </TaskListItem>
+
+                  {/* 2. Feedback from initial review */}
+                  <TaskListItem
+                    heading={t('taskList.steps.1.title')}
+                    status="CANNOT_START_YET"
+                    testId={kebabCase(t('taskList.steps.1.title'))}
+                  >
+                    <TaskListDescription>
+                      <p>{t('taskList.steps.1.description')}</p>
+                      <Alert type="info" slim className="margin-bottom-0">
+                        <Trans i18nKey="itGov:taskList.steps.1.info" />
+                      </Alert>
+                    </TaskListDescription>
+                  </TaskListItem>
+
+                  {/* 3. Prepare a draft Business Case */}
+                  <TaskListItem
+                    heading={t('taskList.steps.2.title')}
+                    status="CANNOT_START_YET"
+                    testId={kebabCase(t('taskList.steps.2.title'))}
+                  >
+                    <TaskListDescription>
+                      <p>{t('taskList.steps.2.description')}</p>
+                    </TaskListDescription>
+                  </TaskListItem>
+
+                  {/* 4. Attend the GRT meeting */}
+                  <TaskListItem
+                    heading={t('taskList.steps.3.title')}
+                    status="CANNOT_START_YET"
+                    testId={kebabCase(t('taskList.steps.3.title'))}
+                  >
+                    <TaskListDescription>
+                      <p>{t('taskList.steps.3.description')}</p>
+                      <div className="margin-top-2">
+                        <UswdsReactLink to="./">
+                          {t('taskList.steps.3.link')}
+                        </UswdsReactLink>
+                      </div>
+                    </TaskListDescription>
+                  </TaskListItem>
+
+                  {/* 5. Submit your Business Case for final approval */}
+                  <TaskListItem
+                    heading={t('taskList.steps.4.title')}
+                    status="CANNOT_START_YET"
+                    testId={kebabCase(t('taskList.steps.4.title'))}
+                  >
+                    <TaskListDescription>
+                      <p>{t('taskList.steps.4.description')}</p>
+                    </TaskListDescription>
+                  </TaskListItem>
+
+                  {/* 6. Attend the GRB meeting */}
+                  <TaskListItem
+                    heading={t('taskList.steps.5.title')}
+                    status="CANNOT_START_YET"
+                    testId={kebabCase(t('taskList.steps.5.title'))}
+                  >
+                    <TaskListDescription>
+                      <p>{t('taskList.steps.5.description')}</p>
+                      <div className="margin-top-2">
+                        <UswdsReactLink to="./">
+                          {t('taskList.steps.5.link')}
+                        </UswdsReactLink>
+                      </div>
+                    </TaskListDescription>
+                  </TaskListItem>
+
+                  {/* 7. Decision and next steps */}
+                  <TaskListItem
+                    heading={t('taskList.steps.6.title')}
+                    status="CANNOT_START_YET"
+                    testId={kebabCase(t('taskList.steps.6.title'))}
+                  >
+                    <TaskListDescription>
+                      <p>{t('taskList.steps.6.description')}</p>
+                    </TaskListDescription>
+                  </TaskListItem>
+                </TaskListContainer>
+              </div>
+            </Grid>
+
+            {/* Sidebar */}
+            <Grid tablet={{ col: 3 }}>
+              <div className="line-height-body-4 padding-top-3 border-top border-top-width-05 border-primary-lighter">
+                <div>
+                  <UswdsReactLink to="/system/making-a-request">
+                    {t('button.saveAndExit')}
                   </UswdsReactLink>
-                </TaskListItem>
+                </div>
+                <div className="margin-top-1">
+                  <Button type="button" unstyled className="text-error">
+                    {t('button.removeYourRequest')}
+                  </Button>
+                </div>
 
-                {/* 2. Feedback from initial review */}
-                <TaskListItem
-                  heading={t('taskList.steps.1.title')}
-                  status="CANNOT_START_YET"
-                  testId={kebabCase(t('taskList.steps.1.title'))}
-                >
-                  <TaskListDescription>
-                    <p>{t('taskList.steps.1.description')}</p>
-                    <Alert type="info" slim className="margin-bottom-0">
-                      <Trans i18nKey="itGov:taskList.steps.1.info" />
-                    </Alert>
-                  </TaskListDescription>
-                </TaskListItem>
-
-                {/* 3. Prepare a draft Business Case */}
-                <TaskListItem
-                  heading={t('taskList.steps.2.title')}
-                  status="CANNOT_START_YET"
-                  testId={kebabCase(t('taskList.steps.2.title'))}
-                >
-                  <TaskListDescription>
-                    <p>{t('taskList.steps.2.description')}</p>
-                  </TaskListDescription>
-                </TaskListItem>
-
-                {/* 4. Attend the GRT meeting */}
-                <TaskListItem
-                  heading={t('taskList.steps.3.title')}
-                  status="CANNOT_START_YET"
-                  testId={kebabCase(t('taskList.steps.3.title'))}
-                >
-                  <TaskListDescription>
-                    <p>{t('taskList.steps.3.description')}</p>
-                    <div className="margin-top-2">
-                      <UswdsReactLink to="./">
-                        {t('taskList.steps.3.link')}
-                      </UswdsReactLink>
-                    </div>
-                  </TaskListDescription>
-                </TaskListItem>
-
-                {/* 5. Submit your Business Case for final approval */}
-                <TaskListItem
-                  heading={t('taskList.steps.4.title')}
-                  status="CANNOT_START_YET"
-                  testId={kebabCase(t('taskList.steps.4.title'))}
-                >
-                  <TaskListDescription>
-                    <p>{t('taskList.steps.4.description')}</p>
-                  </TaskListDescription>
-                </TaskListItem>
-
-                {/* 6. Attend the GRB meeting */}
-                <TaskListItem
-                  heading={t('taskList.steps.5.title')}
-                  status="CANNOT_START_YET"
-                  testId={kebabCase(t('taskList.steps.5.title'))}
-                >
-                  <TaskListDescription>
-                    <p>{t('taskList.steps.5.description')}</p>
-                    <div className="margin-top-2">
-                      <UswdsReactLink to="./">
-                        {t('taskList.steps.5.link')}
-                      </UswdsReactLink>
-                    </div>
-                  </TaskListDescription>
-                </TaskListItem>
-
-                {/* 7. Decision and next steps */}
-                <TaskListItem
-                  heading={t('taskList.steps.6.title')}
-                  status="CANNOT_START_YET"
-                  testId={kebabCase(t('taskList.steps.6.title'))}
-                >
-                  <TaskListDescription>
-                    <p>{t('taskList.steps.6.description')}</p>
-                  </TaskListDescription>
-                </TaskListItem>
-              </TaskListContainer>
-            </div>
+                <h4 className="line-height-body-2 margin-top-3 margin-bottom-1">
+                  {t('taskList.help')}
+                </h4>
+                <div className="margin-top-1">
+                  <UswdsReactLink
+                    to="/help/it-governance/steps-overview/new-system"
+                    target="_blank"
+                  >
+                    {t('taskList.stepsInvolved')}
+                  </UswdsReactLink>
+                </div>
+                <div className="margin-top-1">
+                  <UswdsReactLink
+                    to="/help/it-governance/sample-business-case"
+                    target="_blank"
+                  >
+                    {t('taskList.sampleBusinessCase')}
+                  </UswdsReactLink>
+                </div>
+              </div>
+            </Grid>
           </Grid>
-
-          {/* Sidebar */}
-          <Grid tablet={{ col: 3 }}>
-            <div className="line-height-body-4 padding-top-3 border-top border-top-width-05 border-primary-lighter">
-              <div>
-                <UswdsReactLink to="/system/making-a-request">
-                  {t('button.saveAndExit')}
-                </UswdsReactLink>
-              </div>
-              <div className="margin-top-1">
-                <Button type="button" unstyled className="text-error">
-                  {t('button.removeYourRequest')}
-                </Button>
-              </div>
-
-              <h4 className="line-height-body-2 margin-top-3 margin-bottom-1">
-                {t('taskList.help')}
-              </h4>
-              <div className="margin-top-1">
-                <UswdsReactLink
-                  to="/help/it-governance/steps-overview/new-system"
-                  target="_blank"
-                >
-                  {t('taskList.stepsInvolved')}
-                </UswdsReactLink>
-              </div>
-              <div className="margin-top-1">
-                <UswdsReactLink
-                  to="/help/it-governance/sample-business-case"
-                  target="_blank"
-                >
-                  {t('taskList.sampleBusinessCase')}
-                </UswdsReactLink>
-              </div>
-            </div>
-          </Grid>
-        </Grid>
+        )}
       </GridContainer>
     </MainContent>
   );
