011b4d1cdb4eedb1ed4fd8e5cfccc2ae271b212a
adamodd <97050498+adamodd@users.noreply.github.com>
2023-07-05 13:16:11 -0700

    IT Gov v2 Task List View (initial task list state) (#2082)
    * feat: update task status to be one level, no admin section needed
    
    * chore: remove unneeded whitespace
    
    * chore: remove unnecessary extra character in status abbreviations
    
    * GovernanceTaskListV1
    
    * GovernanceTaskList v2 file
    LD flagged v2 route component
    
    * GetGovernanceTaskListQuery
    
    * Next `itGov` text
    
    * Governance Task List jest stub
    
    * Add `ITGovIntakeStatus` to `TaskStatus`
    
    * GovernanceTaskList stub
    Breadcrumbs
    Grid and sidebar
    TaskListItem
    
    * Static initial state of the Governance Task List
    
    * Set `FlagsWrapper#itGovV2Enabled` to false for ci e2e
    
    * Add `id` to gov task list query
    
    * Gov task list initial state from query data
    
    * Move up the flags.itGovV2Enabled to switch routes
    
    * ITGov task status types in TaskStatus
    
    * TaskListItem statuses from query data
    
    * `<p>` semantics
    
    * Column space
    
    * `task-list__item--na` applied from `CANT_START`
    
    * Undo commit mistake
    
    * Use shared/Alert
    
    ---------
    
    Co-authored-by: Steven Wade <steven.wade@oddball.io>
    Co-authored-by: Steven Wade <99294987+StevenWadeOddball@users.noreply.github.com>

---
+++ src/queries/GetGovernanceTaskListQuery.ts
@@ -0,0 +1,18 @@
+import { gql } from '@apollo/client';
+
+export default gql`
+  query GetGovernanceTaskList($id: UUID!) {
+    systemIntake(id: $id) {
+      id
+      itGovTaskStatuses {
+        intakeFormStatus
+        feedbackFromInitialReviewStatus
+        decisionAndNextStepsStatus
+        bizCaseDraftStatus
+        grtMeetingStatus
+        bizCaseFinalStatus
+        grbMeetingStatus
+      }
+    }
+  }
+`;
