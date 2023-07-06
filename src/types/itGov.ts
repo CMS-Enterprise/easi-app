43776d29cf57443a1820c45f55ebc5654ff3ff0d
adamodd <adam.suharja@oddball.io>
2023-07-06 15:43:35 -0700

    `ItGovTaskStatuses` type

---
+++ src/types/itGov.ts
@@ -0,0 +1,2 @@
+// eslint-disable-next-line camelcase, import/prefer-default-export
+export type { GetGovernanceTaskList_systemIntake_itGovTaskStatuses as ItGovTaskStatuses } from 'queries/types/GetGovernanceTaskList';
