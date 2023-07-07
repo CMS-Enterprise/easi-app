a8d7c559b82be689b4b371e4f1873c1addffcba4
Dylan Sprague <92891039+DylanSpOddball@users.noreply.github.com>
2023-07-07 12:38:52 -0400

    [EASI-3047] Generate presigned URLs for TRB attached docs on-demand (#2095)
    * Log file URL to console
    
    * Download file on button click
    
    * Remove console.log() calls
    
    * Fix up styling for button for downloading files
    
    * Remove commented-out code, unneeded code
    
    * Fix unit test code, snapshot
    
    * Update comment
    
    * Use showMessage() to display errors that happen when trying to view a document
    
    * Condense error handling for getDocumentUrls()
    
    * Remove unnecessary code block
    
    * Add MessageProvider to TRB Initial Request Form unit test
    
    * Polish error message when failing to view a file
    
    * Add MessageProvider to another unit test

---
+++ src/queries/GetTrbRequestDocumentUrlsQuery.ts
@@ -0,0 +1,13 @@
+import { gql } from '@apollo/client';
+
+export default gql`
+  query GetTrbRequestDocumentUrls($id: UUID!) {
+    trbRequest(id: $id) {
+      id
+      documents {
+        id
+        url
+      }
+    }
+  }
+`;
