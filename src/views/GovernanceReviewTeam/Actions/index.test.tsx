4c0883f7f4a6cc16c852886c6227930afa05831d
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2023-07-24 15:56:29 -0500

    Unit test updates

--- src/views/GovernanceReviewTeam/Actions/index.test.tsx
+++ src/views/GovernanceReviewTeam/Actions/index.test.tsx
@@ -3,18 +3,47 @@ import { MemoryRouter, Route } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-import { systemIntake } from 'data/mock/systemIntake';
+import {
+  getSystemIntakeContactsQuery,
+  getSystemIntakeQuery,
+  requester,
+  systemIntake
+} from 'data/mock/systemIntake';
+import { MockedQuery } from 'types/util';
+import VerboseMockedProvider from 'utils/testing/VerboseMockedProvider';
 
-import ChooseAction from '.';
+import Actions from '.';
+
+const renderActionPage = ({
+  mocks,
+  action
+}: {
+  mocks: MockedQuery[];
+  action?: string;
+}) => {
+  return render(
+    <VerboseMockedProvider mocks={mocks}>
+      <MemoryRouter
+        initialEntries={[
+          `/governance-review-team/${systemIntake.id}/actions/${action}`
+        ]}
+      >
+        <Route path={[`/governance-review-team/:systemId/actions/:subPage?`]}>
+          <Actions systemIntake={systemIntake} />
+        </Route>
+      </MemoryRouter>
+    </VerboseMockedProvider>
+  );
+};
 
 describe('IT Gov Actions', () => {
   it('Renders options and selects action', async () => {
-    const { getByRole, getByText, getByTitle, findByRole } = render(
+    const { getByRole, getByText, getByTitle } = render(
       <MemoryRouter
         initialEntries={[`/governance-review-team/${systemIntake.id}/actions`]}
       >
         <Route path={[`/governance-review-team/:systemId/actions/:action?`]}>
-          <ChooseAction systemIntake={systemIntake} />
+          <Actions systemIntake={systemIntake} />
         </Route>
       </MemoryRouter>
     );
@@ -36,14 +65,23 @@ describe('IT Gov Actions', () => {
     // Check that selecting action and submitting form takes user to correct route
     const requestEdits = getByTitle('Request edits to a form');
     userEvent.click(requestEdits);
-    expect(requestEdits).toBeChecked();
+  });
 
-    userEvent.click(getByRole('button', { name: 'Continue' }));
+  it('Renders request edits action', async () => {
+    const { getByRole, findByRole } = renderActionPage({
+      action: 'request-edits',
+      mocks: [getSystemIntakeQuery, getSystemIntakeContactsQuery]
+    });
 
     expect(
-      await findByRole('heading', {
-        name: 'Action: request edits'
-      })
+      await findByRole('heading', { name: 'Action: request edits' })
     ).toBeInTheDocument();
+
+    // Requester is default recipient
+    expect(
+      getByRole('checkbox', {
+        name: `${requester.commonName}, ${requester.component} (Requester)`
+      })
+    ).toBeChecked();
   });
 });
