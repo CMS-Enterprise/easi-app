27ba596b3ef0290f98d5b0bdddfe033d3fb1ccc3
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2023-08-02 03:39:15 -0500

    Change build output folder

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,8 @@ import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'build'
+  },
   plugins: [react(), viteTsconfigPaths(), svgrPlugin()]
 });
