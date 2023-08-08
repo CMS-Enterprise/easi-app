b36d69da1d493571b2e6ac5cfb70f4fd14c23374
Steven Wade <steven.wade@oddball.io>
2023-08-08 09:41:48 -0700

    feat: implement status calculation for GRB meeting

--- pkg/graph/resolvers/it_gov_task_statuses.go
+++ pkg/graph/resolvers/it_gov_task_statuses.go
@@ -100,7 +100,28 @@ func BizCaseFinalStatus(intake *models.SystemIntake) models.ITGovFinalBusinessCa
 
 // GrbMeetingStatus calculates the ITGovGRBStatus for the GrbMeeting section for the system intake task list for the requester view
 func GrbMeetingStatus(intake *models.SystemIntake) (models.ITGovGRBStatus, error) {
-	return models.ITGGRBSCantStart, nil
+	if intake.GRBDate != nil { // status depends on if there is a date scheduled or not
+		if intake.GRBDate.After(time.Now()) { // Meeting has not happened
+			return models.ITGGRBSScheduled, nil
+		}
+		if intake.Step == models.SystemIntakeStepGRBMEETING { //if the step is GRB meeting, status is awaiting decision
+			return models.ITGGRBSAwaitingDecision, nil
+		}
+		return models.ITGGRBSCompleted, nil // if the step is not GRB meeting, the status is completed
+	}
+
+	switch intake.Step {
+	case models.SystemIntakeStepINITIALFORM, models.SystemIntakeStepDRAFTBIZCASE, models.SystemIntakeStepGRTMEETING, models.SystemIntakeStepFINALBIZCASE: // Any step before GRB should show can't start
+		return models.ITGGRBSCantStart, nil
+
+	case models.SystemIntakeStepGRBMEETING: // If at the GRB step, show ready to schedule
+		return models.ITGGRBSReadyToSchedule, nil
+
+	case models.SystemIntakeStepDECISION: // If after GRB step, show that it was not needed (skipped)
+		return models.ITGGRBSNotNeeded, nil
+	default: //This is included to be explicit. This should not technically happen in normal use, but it is technically possible as the type is a type alias for string. It will also provide an error if a new state is added and not handled.
+		return "", apperrors.NewInvalidEnumError(fmt.Errorf("intake has an invalid value for its intake form step"), intake.Step, "SystemIntakeStep")
+	}
 }
 
 // DecisionAndNextStepsStatus calculates the ITGovDecisionStatus for the Decisions section for the system intake task list for the requester view
