9cd1c6575365f89904f740f72015dceb0ecbc24e
Steven Wade <steven.wade@oddball.io>
2023-08-11 11:44:06 -0700

    feat: update SystemIntakeUpdateContractDetails to be in the resolvers package

--- pkg/graph/resolvers/system_intake.go
+++ pkg/graph/resolvers/system_intake.go
@@ -10,6 +10,8 @@ import (
 	"github.com/cmsgov/easi-app/pkg/storage"
 )
 
+// TODO: thes calls could largely be combined in a more general call to Update the System Intake. It would rely on a similar approach that was taken in TRB
+
 // SystemIntakeUpdate takes a UpdateSystemIntakeRequestDetailsInput struct and updates the database with the provided information.
 // It also updates the request form state to show in progress, unless the state was EDITS_REQUESTED
 func SystemIntakeUpdate(ctx context.Context, store *storage.Store, fetchCedarSystem func(context.Context, string) (*models.CedarSystem, error), input model.UpdateSystemIntakeRequestDetailsInput) (*model.UpdateSystemIntakePayload, error) {
@@ -101,6 +103,100 @@ func SystemIntakeUpdateContactDetails(ctx context.Context, store *storage.Store,
 		intake.TRBCollaboratorName = null.StringFromPtr(nil)
 		intake.OITSecurityCollaboratorName = null.StringFromPtr(nil)
 		intake.EACollaboratorName = null.StringFromPtr(nil)
+	}
+
+	savedIntake, err := store.UpdateSystemIntake(ctx, intake)
+	return &model.UpdateSystemIntakePayload{
+		SystemIntake: savedIntake,
+	}, err
+}
+
+// SystemIntakeUpdateContractDetails updates specific contract information about a system intake
+// It also updates the request form state to show in progress, unless the state was EDITS_REQUESTED
+func SystemIntakeUpdateContractDetails(ctx context.Context, store *storage.Store, input model.UpdateSystemIntakeContractDetailsInput) (*model.UpdateSystemIntakePayload, error) {
+	intake, err := store.FetchSystemIntakeByID(ctx, input.ID)
+	if err != nil {
+		return nil, err
+	}
+	//TODO: move to helper function? Maybe in it's own package?
+	if intake.RequestFormState != models.SIRFSEditsRequested {
+		// If the request is in request edits, it should stay that way until submitted.
+		//If submitted and edited again, it should change to in progress.
+		intake.RequestFormState = models.SIRFSInProgress
+	}
+
+	if input.FundingSources != nil && input.FundingSources.FundingSources != nil {
+		intake.ExistingFunding = null.BoolFromPtr(input.FundingSources.ExistingFunding)
+		if intake.ExistingFunding.ValueOrZero() {
+			fundingSources := make([]*models.SystemIntakeFundingSource, 0, len(input.FundingSources.FundingSources))
+			for _, fundingSourceInput := range input.FundingSources.FundingSources {
+				fundingSources = append(fundingSources, &models.SystemIntakeFundingSource{
+					SystemIntakeID: intake.ID,
+					Source:         null.StringFromPtr(fundingSourceInput.Source),
+					FundingNumber:  null.StringFromPtr(fundingSourceInput.FundingNumber),
+				})
+			}
+
+			_, err = store.UpdateSystemIntakeFundingSources(ctx, input.ID, fundingSources)
+
+			if err != nil {
+				return nil, err
+			}
+		} else {
+			// Delete existing funding source records
+			_, err = store.UpdateSystemIntakeFundingSources(ctx, input.ID, nil)
+
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+
+	if input.Costs != nil {
+		intake.CostIncreaseAmount = null.StringFromPtr(input.Costs.ExpectedIncreaseAmount)
+		intake.CostIncrease = null.StringFromPtr(input.Costs.IsExpectingIncrease)
+
+		if input.Costs.IsExpectingIncrease != nil {
+			if *input.Costs.IsExpectingIncrease == "YES" {
+				intake.CostIncreaseAmount = null.StringFromPtr(input.Costs.ExpectedIncreaseAmount)
+				intake.CostIncrease = null.StringFromPtr(input.Costs.IsExpectingIncrease)
+			}
+			if *input.Costs.IsExpectingIncrease != "YES" {
+				intake.CostIncreaseAmount = null.StringFromPtr(nil)
+				intake.CostIncrease = null.StringFromPtr(input.Costs.IsExpectingIncrease)
+			}
+		}
+	}
+
+	if input.AnnualSpending != nil {
+		intake.CurrentAnnualSpending = null.StringFromPtr(input.AnnualSpending.CurrentAnnualSpending)
+		intake.PlannedYearOneSpending = null.StringFromPtr(input.AnnualSpending.PlannedYearOneSpending)
+	}
+
+	if input.Contract != nil {
+		// set the fields to the values we receive
+		intake.ExistingContract = null.StringFromPtr(input.Contract.HasContract)
+		intake.Contractor = null.StringFromPtr(input.Contract.Contractor)
+		intake.ContractNumber = null.StringFromPtr(input.Contract.Number)
+		intake.ContractVehicle = null.StringFromPtr(nil) // blank this out in favor of the newer ContractNumber field (see EASI-1977)
+
+		if input.Contract.StartDate != nil {
+			intake.ContractStartDate = input.Contract.StartDate
+		}
+		if input.Contract.EndDate != nil {
+			intake.ContractEndDate = input.Contract.EndDate
+		}
+
+		// in case hasContract has changed, clear the other fields
+		if input.Contract.HasContract != nil {
+			if *input.Contract.HasContract == "NOT_STARTED" || *input.Contract.HasContract == "NOT_NEEDED" {
+				intake.Contractor = null.StringFromPtr(nil)
+				intake.ContractVehicle = null.StringFromPtr(nil)
+				intake.ContractNumber = null.StringFromPtr(nil)
+				intake.ContractStartDate = nil
+				intake.ContractEndDate = nil
+			}
+		}
 	}
 
 	savedIntake, err := store.UpdateSystemIntake(ctx, intake)
