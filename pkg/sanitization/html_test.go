522d7d87b7a198afb6032cf585c5b6fcb1d8f05b
Steven Wade <steven.wade@oddball.io>
2023-08-22 15:34:35 -0700

    feat: add more sanitization test logic

--- pkg/sanitization/html_test.go
+++ pkg/sanitization/html_test.go
@@ -12,9 +12,20 @@ type testSanitizeHTMLType struct {
 	expectedOutput string
 }
 
+func TestDevelopmentHTML(t *testing.T) {
+	testCase := testSanitizeHTMLType{
+		testCase:       "Development",
+		input:          `<a href="https://dev.easi.cms.gov/">Valid link</a>`,
+		expectedOutput: `<a href="https://dev.easi.cms.gov/" rel="nofollow noopener" target="_blank">Valid link</a>`,
+	}
+	actualOutput := SanitizeHTML(testCase.input)
+	assert.EqualValues(t, testCase.expectedOutput, actualOutput)
+
+}
 func TestSanitizeHTML(t *testing.T) {
 
-	// TODO: add more test cases of expected filtered out results
+	// TODO: add more test cases of expected filtered out results.
+	// TODO: use escape string notation instead of escaping the response? ``
 
 	sanitizeHTMLTests := []testSanitizeHTMLType{
 		{
@@ -26,6 +37,16 @@ func TestSanitizeHTML(t *testing.T) {
 			testCase:       "Sanitize HTML will not modify plain text",
 			input:          "This is my test string",
 			expectedOutput: "This is my test string",
+		},
+		{
+			testCase:       "Sanitize HTML will remove allow href except on A tags",
+			input:          "<a href=\"https://dev.easi.cms.gov/\" rel=\"nofollow\">Valid link</a> <p href=\"https://dev.easi.cms.gov/\" rel=\"nofollow\">InValid link</p>",
+			expectedOutput: "<a href=\"https://dev.easi.cms.gov/\" rel=\"nofollow noopener\" target=\"_blank\">Valid link</a> <p>InValid link</p>",
+		},
+		{
+			testCase:       "Sanitize HTML will add security to rel attribute", //TODO, should we consider including a noreferrer value as well?
+			input:          `<a href="https://dev.easi.cms.gov/">Valid link</a>`,
+			expectedOutput: `<a href="https://dev.easi.cms.gov/" rel="nofollow noopener" target="_blank">Valid link</a>`,
 		},
 	}
 
