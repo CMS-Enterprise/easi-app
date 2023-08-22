a3852a0da79a0cfa7ebeb9b153fc842f306dae49
Steven Wade <steven.wade@oddball.io>
2023-08-22 15:20:13 -0700

    feat: table driven unit tests.

--- pkg/sanitization/html_test.go
+++ pkg/sanitization/html_test.go
@@ -6,14 +6,34 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type testSanitizeHTMLType struct {
+	testCase       string
+	input          string
+	expectedOutput string
+}
+
 func TestSanitizeHTML(t *testing.T) {
 
 	// TODO: add more test cases of expected filtered out results
-	rawInput := "<p><strong>Notification email</strong></p><p>A <a href=\"asdasd\" target=\"_blank\">notification</a> email will be sent to the requester when you complete this action. If you would like, you may also send a copy to the TRB mailbox and/or to any additional attendees.</p><p>Choose recipients</p><ol><li><p>asd</p></li><li><p>asd</p></li><li><p>asdewg</p></li></ol><ul><li><p>Hello</p></li><li><p><em>;l;lk;kl;</em></p></li></ul><script>This is a script that will be sanitized</script>"
-	expectedOutput := "<p><strong>Notification email</strong></p><p>A <a href=\"asdasd\" rel=\"nofollow\">notification</a> email will be sent to the requester when you complete this action. If you would like, you may also send a copy to the TRB mailbox and/or to any additional attendees.</p><p>Choose recipients</p><ol><li><p>asd</p></li><li><p>asd</p></li><li><p>asdewg</p></li></ol><ul><li><p>Hello</p></li><li><p><em>;l;lk;kl;</em></p></li></ul>"
 
-	actualOutput := SanitizeHTML(rawInput)
+	sanitizeHTMLTests := []testSanitizeHTMLType{
+		{
+			testCase:       "Sanitize HTML will remove script tags",
+			input:          "<p><strong>Notification email</strong></p><script>This is a script that will be sanitized</script>",
+			expectedOutput: "<p><strong>Notification email</strong></p>",
+		},
+		{
+			testCase:       "Sanitize HTML will not modify plain text",
+			input:          "This is my test string",
+			expectedOutput: "This is my test string",
+		},
+	}
 
-	assert.EqualValues(t, expectedOutput, actualOutput)
+	for _, test := range sanitizeHTMLTests {
+		t.Run(test.testCase, func(t *testing.T) {
+			actualOutput := SanitizeHTML(test.input)
+			assert.EqualValues(t, test.expectedOutput, actualOutput)
+		})
+	}
 
 }
