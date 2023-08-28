0a24c8da9805e184c6a606bdf5375b6f182f283d
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2023-08-28 14:20:11 -0500

    Remove Jest canvas mock

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,7 +3,7 @@ import { configure } from 'enzyme';
 
 import 'regenerator-runtime/runtime';
 import '@testing-library/jest-dom';
-import 'jest-canvas-mock';
+// import 'jest-canvas-mock';
 import './i18n';
 
 configure({ adapter: new Adapter() });
