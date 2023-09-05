6072a0e2bb0554951d350c765cf4707d1e59c3b9
ClayBenson94 <clay.benson@oddball.io>
2023-09-05 09:21:32 -0400

    Fix references to REACT_APP_ env vars that have not yet been converted to use VITE_

--- scripts/build_static.sh
+++ scripts/build_static.sh
@@ -6,9 +6,9 @@ set -eu -o pipefail
 case "$APP_ENV" in
   "test")
     EASI_URL="http://easi:8080"
-    export REACT_APP_OKTA_DOMAIN="https://test.idp.idm.cms.gov"
-    export REACT_APP_OKTA_REDIRECT_URI="http://localhost:3000/implicit/callback"
-    export REACT_APP_LOCAL_AUTH_ENABLED=true
+    export VITE_OKTA_DOMAIN="https://test.idp.idm.cms.gov"
+    export VITE_OKTA_REDIRECT_URI="http://localhost:3000/implicit/callback"
+    export VITE_LOCAL_AUTH_ENABLED=true
     ;;
   "dev")
     EASI_URL="https://dev.easi.cms.gov"
@@ -38,9 +38,9 @@ export VITE_OKTA_REDIRECT_URI="${EASI_URL}/implicit/callback"
 export VITE_API_ADDRESS="${EASI_URL}/api/v1"
 export VITE_GRAPHQL_ADDRESS="${EASI_URL}/api/graph/query"
 
-# Only set REACT_APP_OKTA_REDIRECT_URI if APP_ENV is not "test"
+# Only set VITE_OKTA_REDIRECT_URI if APP_ENV is not "test"
 if [ "$APP_ENV" != "test" ]; then
-    export REACT_APP_OKTA_REDIRECT_URI="${EASI_URL}/implicit/callback"
+    export VITE_OKTA_REDIRECT_URI="${EASI_URL}/implicit/callback"
 fi
 
 # Build the application
