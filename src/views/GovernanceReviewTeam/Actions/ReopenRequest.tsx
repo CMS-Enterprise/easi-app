ad0818cdda9ef2d4bb59ef18f933612f1cf5a96d
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2023-09-11 16:42:27 -0500

    Resolution form components

---
+++ src/views/GovernanceReviewTeam/Actions/ReopenRequest.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { FormProvider, useForm } from 'react-hook-form';
+
+import ActionForm, { SystemIntakeActionFields } from './ActionForm';
+
+const ReopenRequest = ({ systemIntakeId }: { systemIntakeId: string }) => {
+  const form = useForm<SystemIntakeActionFields>();
+
+  const { handleSubmit } = form;
+
+  const submit = handleSubmit(() => null);
+
+  return (
+    <FormProvider<SystemIntakeActionFields> {...form}>
+      <ActionForm systemIntakeId={systemIntakeId} onSubmit={submit}>
+        {/* Action fields here */}
+      </ActionForm>
+    </FormProvider>
+  );
+};
+
+export default ReopenRequest;
