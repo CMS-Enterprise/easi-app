e3fe198021b598f56aeb70c82a4a900a94d629c6
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2023-09-12 14:04:07 -0500

    Update Link component type

--- src/views/GovernanceReviewTeam/Actions/components/ResolutionBox.tsx
+++ src/views/GovernanceReviewTeam/Actions/components/ResolutionBox.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Link } from '@trussworks/react-uswds';
+import { Link } from 'react-router-dom';
 
 type ResolutionBoxProps = {
   title: string;
@@ -17,7 +17,7 @@ const ResolutionBox = ({ title, systemIntakeId }: ResolutionBoxProps) => {
     <div className="margin-top-3 margin-bottom-105 bg-base-lightest tablet:grid-col-6 padding-3">
       <p className="margin-0">{t('resolutions.selectedResolution')}</p>
       <h3 className="margin-top-05 margin-bottom-105">{t(title)}</h3>
-      <Link href={`/governance-review-team/${systemIntakeId}/resolutions`}>
+      <Link to={`/governance-review-team/${systemIntakeId}/resolutions`}>
         {t('resolutions.changeResolution')}
       </Link>
     </div>
