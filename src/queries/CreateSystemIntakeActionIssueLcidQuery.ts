0da61fa40a8564992315fb38fa998b04bf3ad8ea
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2023-09-14 11:24:41 -0500

    Issue LCID mutation

---
+++ src/queries/CreateSystemIntakeActionIssueLcidQuery.ts
@@ -0,0 +1,13 @@
+import { gql } from '@apollo/client';
+
+export default gql`
+  mutation CreateSystemIntakeActionIssueLcid(
+    $input: SystemIntakeIssueLCIDInput!
+  ) {
+    createSystemIntakeActionIssueLCID(input: $input) {
+      systemIntake {
+        id
+      }
+    }
+  }
+`;
---
+++ src/queries/types/CreateSystemIntakeActionIssueLcid.ts
@@ -0,0 +1,28 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+import { SystemIntakeIssueLCIDInput } from "./../../types/graphql-global-types";
+
+// ====================================================
+// GraphQL mutation operation: CreateSystemIntakeActionIssueLcid
+// ====================================================
+
+export interface CreateSystemIntakeActionIssueLcid_createSystemIntakeActionIssueLCID_systemIntake {
+  __typename: "SystemIntake";
+  id: UUID;
+}
+
+export interface CreateSystemIntakeActionIssueLcid_createSystemIntakeActionIssueLCID {
+  __typename: "UpdateSystemIntakePayload";
+  systemIntake: CreateSystemIntakeActionIssueLcid_createSystemIntakeActionIssueLCID_systemIntake | null;
+}
+
+export interface CreateSystemIntakeActionIssueLcid {
+  createSystemIntakeActionIssueLCID: CreateSystemIntakeActionIssueLcid_createSystemIntakeActionIssueLCID | null;
+}
+
+export interface CreateSystemIntakeActionIssueLcidVariables {
+  input: SystemIntakeIssueLCIDInput;
+}
