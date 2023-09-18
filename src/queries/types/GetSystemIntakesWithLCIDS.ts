504db758119974a78ab4942575f9948fea6546e2
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2023-09-18 14:55:49 -0500

    Add lcid info to GetSystemIntakesWithLCIDS query

--- src/queries/types/GetSystemIntakesWithLCIDS.ts
+++ src/queries/types/GetSystemIntakesWithLCIDS.ts
@@ -3,6 +3,8 @@
 // @generated
 // This file was automatically generated and should not be edited.
 
+import { SystemIntakeTRBFollowUp } from "./../../types/graphql-global-types";
+
 // ====================================================
 // GraphQL query operation: GetSystemIntakesWithLCIDS
 // ====================================================
@@ -12,6 +14,11 @@ export interface GetSystemIntakesWithLCIDS_systemIntakesWithLcids {
   id: UUID;
   lcid: string | null;
   requestName: string | null;
+  lcidExpiresAt: Time | null;
+  lcidScope: HTML | null;
+  decisionNextSteps: HTML | null;
+  trbFollowUpRecommendation: SystemIntakeTRBFollowUp | null;
+  lcidCostBaseline: string | null;
 }
 
 export interface GetSystemIntakesWithLCIDS {
