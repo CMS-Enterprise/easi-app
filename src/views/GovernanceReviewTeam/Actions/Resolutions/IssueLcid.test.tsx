76703084f21c2bb080e4e19e0873ad7d6ef6e21b
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2023-09-19 09:56:41 -0500

    Confirmation modal unit test

--- src/views/GovernanceReviewTeam/Actions/Resolutions/IssueLcid.test.tsx
+++ src/views/GovernanceReviewTeam/Actions/Resolutions/IssueLcid.test.tsx
@@ -17,6 +17,8 @@ import {
 } from 'types/graphql-global-types';
 import VerboseMockedProvider from 'utils/testing/VerboseMockedProvider';
 
+import { EditsRequestedContext } from '..';
+
 import IssueLcid from './IssueLcid';
 
 describe('Issue LCID form', async () => {
@@ -73,5 +75,74 @@ describe('Issue LCID form', async () => {
     expect(
       screen.getByRole('textbox', { name: 'Project cost baseline' })
     ).toHaveValue(systemIntakeWithLcid.lcidCostBaseline);
+  });
+
+  it('Displays confirmation modal when edits are requested', async () => {
+    render(
+      <VerboseMockedProvider
+        mocks={[
+          getSystemIntakeContactsQuery,
+          getSystemIntakeQuery,
+          getSystemIntakesWithLcidsQuery
+        ]}
+      >
+        <MemoryRouter>
+          <MessageProvider>
+            <EditsRequestedContext.Provider value="intakeRequest">
+              <IssueLcid
+                systemIntakeId={systemIntake.id}
+                state={SystemIntakeState.OPEN}
+                decisionState={SystemIntakeDecisionState.NO_DECISION}
+              />
+            </EditsRequestedContext.Provider>
+          </MessageProvider>
+        </MemoryRouter>
+      </VerboseMockedProvider>
+    );
+
+    await screen.findByText('Issue a Life Cycle ID');
+
+    userEvent.click(
+      screen.getByRole('radio', {
+        name: 'Generate a new Life Cycle ID'
+      })
+    );
+
+    userEvent.type(
+      screen.getByRole('textbox', { name: 'Expiration Date *' }),
+      '01/01/2025'
+    );
+
+    userEvent.type(
+      screen.getByRole('textbox', { name: 'Scope of Life Cycle ID *' }),
+      'Test scope'
+    );
+
+    userEvent.type(
+      screen.getByRole('textbox', { name: 'Next Steps *' }),
+      'Test next steps'
+    );
+
+    userEvent.click(
+      screen.getByRole('radio', {
+        name: 'No, they may if they wish but it’s not necessary'
+      })
+    );
+
+    userEvent.click(
+      screen.getByRole('button', { name: 'Complete action without email' })
+    );
+
+    // Check for modal
+
+    const modalTitle = await screen.findByText(
+      'Are you sure you want to complete this action?'
+    );
+    expect(modalTitle).toBeInTheDocument();
+
+    const modalText = screen.getByText(
+      'You previously requested that the team make changes to their intake request form. Completing this decision action will remove the “Edits requested” status from that form, and the requester will no longer be able to make any changes.'
+    );
+    expect(modalText).toBeInTheDocument();
   });
 });
