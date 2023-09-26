5322891a61f42939531785f518db34ad374d5db0
Patrick Segura <patrick.segura@oddball.io>
2023-09-26 14:37:25 -0500

    Used Truss SiteAlert

--- src/components/ServiceAlert/index.tsx
+++ src/components/ServiceAlert/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useLocation } from 'react-router-dom';
-import { Grid, GridContainer, IconError } from '@trussworks/react-uswds';
+import { Grid, GridContainer, SiteAlert } from '@trussworks/react-uswds';
 import classNames from 'classnames';
 
 import MainContent from 'components/MainContent';
@@ -39,20 +39,18 @@ const ServiceAlert = ({
     <MainContent className={classNames('bg-emergency', className)}>
       <GridContainer
         className={classNames({
-          'service-alert__landing padding-x-4': landing
+          'service-alert__landing': landing,
+          'padding-x-0': !landing
         })}
       >
-        <Grid desktop={{ col: 12 }}>
-          <div className="bg-emergency display-flex text-white padding-y-2 line-height-mono-4">
-            <div className="margin-right-2 width-4 service-alert__icon">
-              <IconError size={4} />
-            </div>
-            <div>
-              <h3 className="margin-0">{t(`${translationKey}.heading`)}</h3>
-
-              <p className="margin-y-0">{t(`${translationKey}.content`)}</p>
-            </div>
-          </div>
+        <Grid desktop={{ col: 12 }} className="padding-y-1">
+          <SiteAlert
+            variant="emergency"
+            slim
+            heading={t(`${translationKey}.heading`)}
+          >
+            <p className="margin-y-0">{t(`${translationKey}.content`)}</p>
+          </SiteAlert>
         </Grid>
       </GridContainer>
     </MainContent>
