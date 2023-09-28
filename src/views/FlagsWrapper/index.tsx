437d2e0e617b04e53268610302cac6662d88349e
Patrick Segura <patrick.segura@oddball.io>
2023-09-28 10:03:48 -0500

    Remove LD wrapper from unauthenticated

--- src/views/FlagsWrapper/index.tsx
+++ src/views/FlagsWrapper/index.tsx
@@ -1,10 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useQuery } from '@apollo/client';
 import { useOktaAuth } from '@okta/okta-react';
-import {
-  AsyncProviderConfig,
-  asyncWithLDProvider
-} from 'launchdarkly-react-client-sdk';
+import { asyncWithLDProvider } from 'launchdarkly-react-client-sdk';
 
 import GetCurrentUserQuery from 'queries/GetCurrentUserQuery';
 import { GetCurrentUser } from 'queries/types/GetCurrentUser';
@@ -13,7 +10,7 @@ type WrapperProps = {
   children: React.ReactNode;
 };
 
-const LDWrapper = ({ children }: WrapperProps) => {
+const UserTargetingWrapper = ({ children }: WrapperProps) => {
   // wrapping initial value in function to get around useState and setState thinking
   // the functional component is a function to be evaluated.
   const [LDProvider, setLDProvider] = useState<React.FunctionComponent>(
@@ -23,39 +20,36 @@ const LDWrapper = ({ children }: WrapperProps) => {
   const { data } = useQuery<GetCurrentUser>(GetCurrentUserQuery);
 
   useEffect(() => {
-    const ldConfig: AsyncProviderConfig = {
-      clientSideID: import.meta.env.VITE_LD_CLIENT_ID as string,
-      flags: {
-        sandbox: true,
-        downgradeGovTeam: false,
-        downgrade508User: false,
-        downgrade508Tester: false,
-        downgradeTrbAdmin: false,
-        itGovV2Enabled: false,
-        systemProfile: true,
-        systemProfileHiddenFields: false,
-        cedar508Requests: false,
-        technicalAssistance: true,
-        hide508Workflow: true,
-        serviceAlertEnabled: false
-      }
-    };
-
     if (data) {
-      ldConfig.context = {
-        kind: 'user',
-        key: data?.currentUser?.launchDarkly.userKey
-      };
-      ldConfig.options = {
-        hash: data?.currentUser?.launchDarkly.signedHash
-      };
+      (async () => {
+        const provider = await asyncWithLDProvider({
+          clientSideID: import.meta.env.VITE_LD_CLIENT_ID as string,
+          context: {
+            kind: 'user',
+            key: data?.currentUser?.launchDarkly.userKey
+          },
+          options: {
+            hash: data?.currentUser?.launchDarkly.signedHash
+          },
+          flags: {
+            sandbox: true,
+            downgradeGovTeam: false,
+            downgrade508User: false,
+            downgrade508Tester: false,
+            downgradeTrbAdmin: false,
+            itGovV2Enabled: false,
+            systemProfile: true,
+            systemProfileHiddenFields: false,
+            cedar508Requests: false,
+            technicalAssistance: true,
+            hide508Workflow: true,
+            serviceAlertEnabled: false
+          }
+        });
+
+        setLDProvider(() => () => provider({ children }));
+      })();
     }
-
-    (async () => {
-      const provider = await asyncWithLDProvider(ldConfig);
-
-      setLDProvider(() => () => provider({ children }));
-    })();
   }, [data, children]);
 
   return <LDProvider>{children}</LDProvider>;
@@ -63,7 +57,9 @@ const LDWrapper = ({ children }: WrapperProps) => {
 
 const FlagsWrapper = ({ children }: WrapperProps) => {
   const { authState } = useOktaAuth();
-  const Container = authState?.isAuthenticated ? LDWrapper : LDWrapper;
+  const Container = authState?.isAuthenticated
+    ? UserTargetingWrapper
+    : React.Fragment;
 
   return <Container>{children}</Container>;
 };
