91d956e1bea358063b926e3d904f06384c9b5a1e
ClayBenson94 <clay.benson@oddball.io>
2023-10-12 14:54:58 -0400

    Hotfix for setting systemintake status when taking progress to new step action

--- src/views/BusinessCase/Review/index.tsx
+++ src/views/BusinessCase/Review/index.tsx
@@ -20,6 +20,8 @@ const Review = ({ businessCase }: ReviewProps) => {
   const history = useHistory();
   const dispatch = useDispatch();
   const isSubmitting = useSelector((state: AppState) => state.action.isPosting);
+  // TODO: This should use systemIntake.SystemIntakeStep=FINAL_BUSINESS_CASE instead of the overall system intake status=FINAL_BUSINESS_CASE
+  // This means this cmoponent will probably need that step passed in as a prop (or the entire intake, whichever makes more sense)
   const actionType =
     businessCase.systemIntakeStatus === 'BIZ_CASE_FINAL_NEEDED'
       ? 'SUBMIT_FINAL_BIZ_CASE'
