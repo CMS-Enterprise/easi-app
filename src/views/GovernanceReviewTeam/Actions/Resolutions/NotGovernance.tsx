07847b138bebb9e6a73b4ef01713e96ffa83f520
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2023-10-17 14:24:55 -0500

    [EASI-3124] Not it gov request action (#2244)
    * Not IT Gov Request mutation
    
    * Todo comment to remove v1 mutation
    
    * Update fields type
    
    * Add mutation to submit function
    
    * Translation text
    
    * Success message
    
    * Fix translation text for NOT_GOVERNANCE decision
    
    * Add reason field to form
    
    * Modal if edits requested
    
    * File structure fix - delete query file
    
    * File structure fix - add renamed query file

--- src/views/GovernanceReviewTeam/Actions/Resolutions/NotGovernance.tsx
+++ src/views/GovernanceReviewTeam/Actions/Resolutions/NotGovernance.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
-import { FormProvider, useForm } from 'react-hook-form';
+import React, { useContext } from 'react';
+import { Controller, FormProvider, useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
+import { useMutation } from '@apollo/client';
+import { FormGroup } from '@trussworks/react-uswds';
+
+import HelpText from 'components/shared/HelpText';
+import Label from 'components/shared/Label';
+import TextAreaField from 'components/shared/TextAreaField';
+import CreateSystemIntakeActionNotITGovRequestQuery from 'queries/CreateSystemIntakeActionNotITGovRequestQuery';
+import {
+  CreateSystemIntakeActionNotITGovRequest,
+  CreateSystemIntakeActionNotITGovRequestVariables
+} from 'queries/types/CreateSystemIntakeActionNotITGovRequest';
+import { SystemIntakeNotITGovReqInput } from 'types/graphql-global-types';
+import { NonNullableProps } from 'types/util';
 
 import ActionForm, { SystemIntakeActionFields } from '../components/ActionForm';
+import { EditsRequestedContext } from '..';
 
 import ResolutionTitleBox from './ResolutionTitleBox';
 import { ResolutionProps } from '.';
 
-interface NotGovernanceFields extends SystemIntakeActionFields {}
+type NotGovernanceFields = NonNullableProps<
+  Omit<SystemIntakeNotITGovReqInput, 'systemIntakeID'> &
+    SystemIntakeActionFields
+>;
 
 const NotGovernance = ({
   systemIntakeId,
@@ -17,22 +34,40 @@ const NotGovernance = ({
   const { t } = useTranslation('action');
   const form = useForm<NotGovernanceFields>();
 
+  const { control } = form;
+
+  const [mutate] = useMutation<
+    CreateSystemIntakeActionNotITGovRequest,
+    CreateSystemIntakeActionNotITGovRequestVariables
+  >(CreateSystemIntakeActionNotITGovRequestQuery, {
+    refetchQueries: ['GetSystemIntake']
+  });
+
+  /** Edits requested form key for confirmation modal */
+  const editsRequestedKey = useContext(EditsRequestedContext);
+
   /**
-   * Submit handler containing mutation logic
+   * Mark as not IT Gov request on form submit
    *
    * Error and success handling is done in `<ActionForm>`
    */
-  const onSubmit = async (formData: NotGovernanceFields) => {
-    // Execute mutation here
-    // mutate(formData);
-  };
+  const onSubmit = async (formData: NotGovernanceFields) =>
+    mutate({
+      variables: {
+        input: {
+          systemIntakeID: systemIntakeId,
+          ...formData
+        }
+      }
+    });
 
   return (
     <FormProvider<NotGovernanceFields> {...form}>
       <ActionForm
         systemIntakeId={systemIntakeId}
-        successMessage=""
+        successMessage={t('notItGovRequest.success')}
         onSubmit={onSubmit}
+        requiredFields={false}
         title={
           <ResolutionTitleBox
             title={t('resolutions.summary.notItRequest')}
@@ -41,8 +76,37 @@ const NotGovernance = ({
             decisionState={decisionState}
           />
         }
+        // Show confirmation modal if edits have been requested
+        modal={
+          editsRequestedKey && {
+            title: t('decisionModal.title'),
+            content: t('decisionModal.content', {
+              action: t(`decisionModal.${editsRequestedKey}`)
+            })
+          }
+        }
       >
-        {/* Action fields here */}
+        <Controller
+          name="reason"
+          control={control}
+          render={({ field: { ref, ...field } }) => (
+            <FormGroup>
+              <Label htmlFor={field.name} className="text-normal">
+                {t('notItGovRequest.reason')}
+              </Label>
+              <HelpText className="margin-top-1">
+                {t('notItGovRequest.reasonHelpText')}
+              </HelpText>
+              <TextAreaField
+                {...field}
+                id={field.name}
+                value={field.value || ''}
+                size="sm"
+                characterCounter={false}
+              />
+            </FormGroup>
+          )}
+        />
       </ActionForm>
     </FormProvider>
   );
