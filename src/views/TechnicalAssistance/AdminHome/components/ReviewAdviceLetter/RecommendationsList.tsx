1136afa033c6f95fbde2285329453339c8b61306
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2023-10-17 11:13:16 -0500

    Sort functionality

--- src/views/TechnicalAssistance/AdminHome/components/ReviewAdviceLetter/RecommendationsList.tsx
+++ src/views/TechnicalAssistance/AdminHome/components/ReviewAdviceLetter/RecommendationsList.tsx
@@ -50,25 +50,52 @@ export default function RecommendationsList({
 }: RecommendationsListProps) {
   const { t } = useTranslation('technicalAssistance');
 
-  const [updateOrder] = useMutation<
-    UpdateTrbRecommendationOrder,
-    UpdateTrbRecommendationOrderVariables
-  >(UpdateTrbRecommendationOrderQuery);
-
   const [
     recommendationToRemove,
     setRecommendationToRemove
   ] = useState<TRBRecommendation | null>(null);
 
-  const sort = (id: string) => {
+  // Sorted recommendations array
+  const [sortedRecommendations, setSortedRecommendations] = useState(
+    recommendations
+  );
+
+  const [updateOrder] = useMutation<
+    UpdateTrbRecommendationOrder,
+    UpdateTrbRecommendationOrderVariables
+  >(UpdateTrbRecommendationOrderQuery);
+
+  /** Sort recommendations and update sortedRecommendations state */
+  const sort = (id: string, newIndex: number) => {
+    /** Updated sort order array */
+    const newOrder: string[] = sortedRecommendations
+      // Get just rec IDs
+      .map(rec => rec.id)
+      // Filter out rec ID to be sorted
+      .filter(value => value !== id);
+
+    // Insert rec ID at new index
+    newOrder.splice(newIndex, 0, id);
+
     updateOrder({
       variables: {
         input: {
           trbRequestId,
-          newOrder: recommendations.map(rec => rec.id)
+          newOrder
         }
       }
-    });
+    })
+      .then(response => {
+        const updatedRecommendations =
+          response?.data?.updateTRBAdviceLetterRecommendationOrder;
+
+        // Update sortedRecommendations state with new order
+        if (updatedRecommendations) {
+          setSortedRecommendations(updatedRecommendations);
+        }
+      })
+      // TODO: Error handling
+      .catch(() => null);
   };
 
   return (
@@ -91,14 +118,14 @@ export default function RecommendationsList({
         </RemoveRecommendationModal>
       )}
 
-      {recommendations.length > 0 && editable && (
+      {sortedRecommendations.length > 0 && editable && (
         <Alert type="info" slim className="margin-bottom-4">
           {t('adviceLetterForm.reorderRecommendations')}
         </Alert>
       )}
 
       <ul className={classNames('usa-list usa-list--unstyled', className)}>
-        {recommendations.map(recommendation => {
+        {sortedRecommendations.map((recommendation, index) => {
           const {
             title,
             id,
@@ -113,7 +140,7 @@ export default function RecommendationsList({
                   {/* TODO: reorder button accessibility */}
                   <Button
                     type="button"
-                    onClick={() => sort(id)}
+                    onClick={() => sort(id, index - 1)}
                     className="height-3"
                     unstyled
                   >
@@ -123,7 +150,7 @@ export default function RecommendationsList({
                   {/* TODO: reorder button accessibility */}
                   <Button
                     type="button"
-                    onClick={() => sort(id)}
+                    onClick={() => sort(id, index + 1)}
                     className="height-3"
                     unstyled
                   >
