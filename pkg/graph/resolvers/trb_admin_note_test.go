0aee1732672723aaf58633ab871dcd8a95bdeb93
Dylan Sprague <dylan.sprague@oddball.io>
2023-10-21 16:04:42 -0400

    Add integration test for creating notes in Initial Request Form category

--- pkg/graph/resolvers/trb_admin_note_test.go
+++ pkg/graph/resolvers/trb_admin_note_test.go
@@ -25,7 +25,6 @@ func (s *ResolverSuite) TestCreateTRBAdminNoteGeneralRequest() {
 			TrbRequestID: trbRequest.ID,
 			NoteText:     "test TRB admin note - general request",
 		}
-
 		createdNote, err := CreateTRBAdminNoteGeneralRequest(ctx, store, input)
 		s.NoError(err)
 		s.NotNil(createdNote)
@@ -34,7 +33,7 @@ func (s *ResolverSuite) TestCreateTRBAdminNoteGeneralRequest() {
 		s.EqualValues(models.TRBAdminNoteCategoryGeneralRequest, createdNote.Category)
 		s.EqualValues(input.NoteText, createdNote.NoteText)
 
-		// all these should be null because they don't apply to notes in the General Request category
+		// all these should be null because they don't apply to General Request notes
 		s.Nil(createdNote.AppliesToBasicRequestDetails.Ptr())
 		s.Nil(createdNote.AppliesToSubjectAreas.Ptr())
 		s.Nil(createdNote.AppliesToAttendees.Ptr())
@@ -44,7 +43,45 @@ func (s *ResolverSuite) TestCreateTRBAdminNoteGeneralRequest() {
 }
 
 func (s *ResolverSuite) TestCreateTRBAdminNoteInitialRequestForm() {
-	// TODO - implement
+	ctx := s.testConfigs.Context
+	store := s.testConfigs.Store
+
+	s.Run("Creating Admin Note with Initial Request Form category works", func() {
+		// set up request
+		trbRequest, err := CreateTRBRequest(ctx, models.TRBTFormalReview, store)
+		s.NoError(err)
+		s.NotNil(trbRequest)
+
+		// create admin note
+		input := model.CreateTRBAdminNoteInitialRequestFormInput{
+			TrbRequestID:                 trbRequest.ID,
+			NoteText:                     "test TRB admin note - initial request form",
+			AppliesToBasicRequestDetails: true,
+			AppliesToSubjectAreas:        true,
+			AppliesToAttendees:           false,
+		}
+		createdNote, err := CreateTRBAdminNoteInitialRequestForm(ctx, store, input)
+		s.NoError(err)
+		s.NotNil(createdNote)
+
+		// check that createdNote has the right field values
+		s.EqualValues(models.TRBAdminNoteCategoryInitialRequestForm, createdNote.Category)
+		s.EqualValues(input.NoteText, createdNote.NoteText)
+
+		// these three fields should be non-null - they should _always_ have values for Initial Request Form notes
+		s.NotNil(createdNote.AppliesToBasicRequestDetails.Ptr())
+		s.NotNil(createdNote.AppliesToSubjectAreas.Ptr())
+		s.NotNil(createdNote.AppliesToAttendees.Ptr())
+
+		// check that they have the correct values, as well
+		s.EqualValues(input.AppliesToBasicRequestDetails, createdNote.AppliesToBasicRequestDetails.Bool)
+		s.EqualValues(input.AppliesToSubjectAreas, createdNote.AppliesToSubjectAreas.Bool)
+		s.EqualValues(input.AppliesToAttendees, createdNote.AppliesToAttendees.Bool)
+
+		// these should be null because they don't apply to Initial Request Form notes
+		s.Nil(createdNote.AppliesToMeetingSummary.Ptr())
+		s.Nil(createdNote.AppliesToNextSteps.Ptr())
+	})
 }
 
 func (s *ResolverSuite) TestCreateTRBAdminNoteSupportingDocuments() {
@@ -81,7 +118,6 @@ func (s *ResolverSuite) TestCreateTRBAdminNoteSupportingDocuments() {
 				documentID,
 			},
 		}
-
 		createdNote, err := CreateTRBAdminNoteSupportingDocuments(ctx, store, input)
 		s.NoError(err)
 		s.NotNil(createdNote)
@@ -90,7 +126,7 @@ func (s *ResolverSuite) TestCreateTRBAdminNoteSupportingDocuments() {
 		s.EqualValues(models.TRBAdminNoteCategorySupportingDocuments, createdNote.Category)
 		s.EqualValues(input.NoteText, createdNote.NoteText)
 
-		// all these should be null because they don't apply to notes in the Supporting Documents category
+		// all these should be null because they don't apply to Supporting Documents notes
 		s.Nil(createdNote.AppliesToBasicRequestDetails.Ptr())
 		s.Nil(createdNote.AppliesToSubjectAreas.Ptr())
 		s.Nil(createdNote.AppliesToAttendees.Ptr())
@@ -127,6 +163,7 @@ func (s *ResolverSuite) TestCreateTRBAdminNoteSupportingDocuments() {
 		documentID := createdDoc.ID
 
 		// try to create an admin note referencing the document
+		// should fail due to database constraints, because the referenced document belongs to a different TRB request
 		input := model.CreateTRBAdminNoteSupportingDocumentsInput{
 			TrbRequestID: trbRequestForNote.ID,
 			NoteText:     "test TRB admin note - supporting documents",
@@ -134,8 +171,6 @@ func (s *ResolverSuite) TestCreateTRBAdminNoteSupportingDocuments() {
 				documentID,
 			},
 		}
-
-		// should fail due to database constraints, because the referenced document belongs to a different TRB request
 		_, err = CreateTRBAdminNoteSupportingDocuments(ctx, store, input)
 		s.Error(err)
 	})
@@ -156,7 +191,6 @@ func (s *ResolverSuite) TestCreateTRBAdminNoteConsultSession() {
 			TrbRequestID: trbRequest.ID,
 			NoteText:     "test TRB admin note - consult session",
 		}
-
 		createdNote, err := CreateTRBAdminNoteConsultSession(ctx, store, input)
 		s.NoError(err)
 		s.NotNil(createdNote)
@@ -165,7 +199,7 @@ func (s *ResolverSuite) TestCreateTRBAdminNoteConsultSession() {
 		s.EqualValues(models.TRBAdminNoteCategoryConsultSession, createdNote.Category)
 		s.EqualValues(input.NoteText, createdNote.NoteText)
 
-		// all these should be null because they don't apply to notes in the Consult Session category
+		// all these should be null because they don't apply to Consult Session notes
 		s.Nil(createdNote.AppliesToBasicRequestDetails.Ptr())
 		s.Nil(createdNote.AppliesToSubjectAreas.Ptr())
 		s.Nil(createdNote.AppliesToAttendees.Ptr())
