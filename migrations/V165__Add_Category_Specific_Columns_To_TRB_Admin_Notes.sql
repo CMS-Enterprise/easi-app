465305fb9b2f6b2227a9352197526dc23b973ca4
Dylan Sprague <dylan.sprague@oddball.io>
2023-10-25 16:26:09 -0400

    Add SQL constraints on trb_admin_notes table for specific categories

--- migrations/V165__Add_Category_Specific_Columns_To_TRB_Admin_Notes.sql
+++ migrations/V165__Add_Category_Specific_Columns_To_TRB_Admin_Notes.sql
@@ -1,5 +1,3 @@
--- TODO - upgrade migration number when I'm finished testing
-
 ALTER TABLE trb_admin_notes
   -- columns for Initial Request Form category
   ADD COLUMN applies_to_basic_request_details BOOLEAN,
@@ -9,8 +7,6 @@ ALTER TABLE trb_admin_notes
   -- columns for Advice Letter category
   ADD COLUMN applies_to_meeting_summary BOOLEAN,
   ADD COLUMN applies_to_next_steps BOOLEAN;
-
--- TODO - category-specific constraints
 
 -- populate columns for existing admin notes
 UPDATE trb_admin_notes
@@ -25,3 +21,47 @@ SET
   applies_to_meeting_summary = FALSE,
   applies_to_next_steps = FALSE
 WHERE category = 'ADVICE_LETTER';
+
+-- add constraints so that only notes of the correct category can set category-specific columns
+
+ALTER TABLE trb_admin_notes
+  ADD CONSTRAINT only_initial_request_notes_set_columns
+  CHECK (
+    category = 'INITIAL_REQUEST_FORM' OR
+    (
+      applies_to_basic_request_details IS NULL AND
+      applies_to_subject_areas IS NULL AND
+      applies_to_attendees IS NULL
+    )
+  );
+
+ALTER TABLE trb_admin_notes
+  ADD CONSTRAINT initial_request_notes_set_all_columns
+  CHECK (
+    category != 'INITIAL_REQUEST_FORM' OR
+    (
+      applies_to_basic_request_details IS NOT NULL AND
+      applies_to_subject_areas IS NOT NULL AND
+      applies_to_attendees IS NOT NULL
+    )
+  );
+
+ALTER TABLE trb_admin_notes
+  ADD CONSTRAINT only_advice_letter_notes_set_columns
+  CHECK (
+    category = 'ADVICE_LETTER' OR
+    (
+      applies_to_meeting_summary IS NULL AND
+      applies_to_next_steps IS NULL
+    )
+  );
+
+ALTER TABLE trb_admin_notes
+  ADD CONSTRAINT advice_letter_notes_set_all_columns
+  CHECK (
+    category != 'ADVICE_LETTER' OR
+    (
+      applies_to_meeting_summary IS NOT NULL AND
+      applies_to_next_steps IS NOT NULL
+    )
+  );
