9709e90e780c47189283778b03c6c132ffbadef8
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2023-10-30 15:39:42 -0500

    Add mutation to form

--- src/views/GovernanceReviewTeam/Actions/Resolutions/CloseRequest.tsx
+++ src/views/GovernanceReviewTeam/Actions/Resolutions/CloseRequest.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
+import { useMutation } from '@apollo/client';
 
+import CreateSystemIntakeActionCloseRequestQuery from 'queries/CreateSystemIntakeActionCloseRequestQuery';
+import {
+  CreateSystemIntakeActionCloseRequest,
+  CreateSystemIntakeActionCloseRequestVariables
+} from 'queries/types/CreateSystemIntakeActionCloseRequest';
 import { SystemIntakeCloseRequestInput } from 'types/graphql-global-types';
 import { NonNullableProps } from 'types/util';
 
@@ -25,15 +31,27 @@ const CloseRequest = ({
   const { t } = useTranslation('action');
   const form = useForm<CloseRequestFields>();
 
+  const [closeRequest] = useMutation<
+    CreateSystemIntakeActionCloseRequest,
+    CreateSystemIntakeActionCloseRequestVariables
+  >(CreateSystemIntakeActionCloseRequestQuery, {
+    refetchQueries: ['GetSystemIntake']
+  });
+
   /**
-   * Submit handler containing mutation logic
+   * Close request on form submit
    *
    * Error and success handling is done in `<ActionForm>`
    */
-  const onSubmit = async (formData: CloseRequestFields) => {
-    // Execute mutation here
-    // mutate(formData);
-  };
+  const onSubmit = async (formData: CloseRequestFields) =>
+    closeRequest({
+      variables: {
+        input: {
+          systemIntakeID: systemIntakeId,
+          ...formData
+        }
+      }
+    });
 
   return (
     <FormProvider<CloseRequestFields> {...form}>
