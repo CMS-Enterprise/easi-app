949b7cca77a851979a2ee22c522822d6871026f2
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2023-10-31 13:52:26 -0500

    Add mutation to form submit

--- src/views/GovernanceReviewTeam/Actions/ProgressToNewStep.tsx
+++ src/views/GovernanceReviewTeam/Actions/ProgressToNewStep.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
+import { useMutation } from '@apollo/client';
+
+import CreateSystemIntakeActionProgressToNewStepQuery from 'queries/CreateSystemIntakeActionProgressToNewStepQuery';
+import {
+  CreateSystemIntakeActionProgressToNewStep,
+  CreateSystemIntakeActionProgressToNewStepVariables
+} from 'queries/types/CreateSystemIntakeActionProgressToNewStep';
+import { SystemIntakeProgressToNewStepsInput } from 'types/graphql-global-types';
+import { NonNullableProps } from 'types/util';
 
 import ActionForm, { SystemIntakeActionFields } from './components/ActionForm';
 
-interface ProgressToNewStepFields extends SystemIntakeActionFields {}
+type ProgressToNewStepFields = NonNullableProps<
+  Omit<SystemIntakeProgressToNewStepsInput, 'systemIntakeID'> &
+    SystemIntakeActionFields
+>;
 
 const ProgressToNewStep = ({ systemIntakeId }: { systemIntakeId: string }) => {
   const { t } = useTranslation('action');
+
+  const [mutate] = useMutation<
+    CreateSystemIntakeActionProgressToNewStep,
+    CreateSystemIntakeActionProgressToNewStepVariables
+  >(CreateSystemIntakeActionProgressToNewStepQuery, {
+    refetchQueries: ['GetSystemIntake']
+  });
 
   const form = useForm<ProgressToNewStepFields>();
 
@@ -16,10 +35,15 @@ const ProgressToNewStep = ({ systemIntakeId }: { systemIntakeId: string }) => {
    *
    * Error and success handling is done in `<ActionForm>`
    */
-  const onSubmit = async (formData: ProgressToNewStepFields) => {
-    // Execute mutation here
-    // mutate(formData);
-  };
+  const onSubmit = async (formData: ProgressToNewStepFields) =>
+    mutate({
+      variables: {
+        input: {
+          systemIntakeID: systemIntakeId,
+          ...formData
+        }
+      }
+    });
 
   return (
     <FormProvider<ProgressToNewStepFields> {...form}>
