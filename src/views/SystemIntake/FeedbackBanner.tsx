6423b5108a31c0df6db0f45e42c04d211494a105
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2023-11-07 09:17:15 -0600

    Warning icon responsiveness

--- src/views/SystemIntake/FeedbackBanner.tsx
+++ src/views/SystemIntake/FeedbackBanner.tsx
@@ -5,7 +5,7 @@ import { GridContainer, IconWarning, Link } from '@trussworks/react-uswds';
 const FeedbackBanner = () => {
   const { t } = useTranslation('intake');
   return (
-    <div className="bg-error-lighter padding-y-2">
+    <div className="system-intake__feedback-banner bg-error-lighter padding-y-2">
       <GridContainer>
         <p className="margin-top-0 margin-bottom-2 display-flex line-height-body-5">
           <IconWarning className="text-error-dark margin-right-1" size={3} />
