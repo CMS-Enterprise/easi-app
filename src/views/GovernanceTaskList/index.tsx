a62f5fba21ac058b915c29d9c86897156c32e583
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2023-12-04 11:18:23 -0600

    Archive request functionality

--- src/views/GovernanceTaskList/index.tsx
+++ src/views/GovernanceTaskList/index.tsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Trans, useTranslation } from 'react-i18next';
-import { useParams } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useHistory, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
-import { Button, Grid, GridContainer, Link } from '@trussworks/react-uswds';
+import {
+  Button,
+  ButtonGroup,
+  Grid,
+  GridContainer,
+  Link,
+  ModalHeading
+} from '@trussworks/react-uswds';
 
 import UswdsReactLink from 'components/LinkWrapper';
 import MainContent from 'components/MainContent';
+import Modal from 'components/Modal';
 import PageHeading from 'components/PageHeading';
 import PageLoading from 'components/PageLoading';
 import Alert from 'components/shared/Alert';
 import { TaskListContainer } from 'components/TaskList';
 import { IT_GOV_EMAIL } from 'constants/externalUrls';
+import useMessage from 'hooks/useMessage';
 import GetGovernanceTaskListQuery from 'queries/GetGovernanceTaskListQuery';
 import {
   GetGovernanceTaskList,
@@ -20,6 +30,7 @@ import {
   ITGovDecisionStatus,
   ITGovIntakeFormStatus
 } from 'types/graphql-global-types';
+import { archiveSystemIntake } from 'types/routines';
 import NotFound from 'views/NotFound';
 import Breadcrumbs from 'views/TechnicalAssistance/Breadcrumbs';
 
@@ -34,6 +45,12 @@ import GovTaskIntakeForm from './GovTaskIntakeForm';
 function GovernanceTaskList() {
   const { systemId } = useParams<{ systemId: string }>();
   const { t } = useTranslation('itGov');
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  const { showMessageOnNextPage } = useMessage();
+
+  const [isModalOpen, setModalOpen] = useState<boolean>(false);
 
   const { data, loading, error } = useQuery<
     GetGovernanceTaskList,
@@ -45,6 +62,19 @@ function GovernanceTaskList() {
   });
 
   const systemIntake = data?.systemIntake;
+  const requestName = systemIntake?.requestName;
+
+  const archiveIntake = () => {
+    const redirect = () => {
+      const message = t('taskList:withdraw_modal.confirmationText', {
+        context: requestName ? 'name' : 'noName',
+        requestName
+      });
+      showMessageOnNextPage(message, { type: 'success' });
+      history.push('/');
+    };
+    dispatch(archiveSystemIntake({ intakeId: systemId, redirect }));
+  };
 
   if (error) {
     return <NotFound />;
@@ -151,7 +181,12 @@ function GovernanceTaskList() {
                   ITGovIntakeFormStatus.IN_PROGRESS
                 ].includes(systemIntake.itGovTaskStatuses.intakeFormStatus) && (
                   <div className="margin-top-1">
-                    <Button type="button" unstyled className="text-error">
+                    <Button
+                      type="button"
+                      className="text-error"
+                      onClick={() => setModalOpen(true)}
+                      unstyled
+                    >
                       {t('button.removeYourRequest')}
                     </Button>
                   </div>
@@ -181,6 +216,31 @@ function GovernanceTaskList() {
           </Grid>
         )}
       </GridContainer>
+
+      {/* Remove request modal */}
+      <Modal isOpen={isModalOpen} closeModal={() => setModalOpen(false)}>
+        <ModalHeading>
+          {t('taskList:withdraw_modal:header', {
+            requestName: requestName || 'this request'
+          })}
+        </ModalHeading>
+
+        <p>{t('taskList:withdraw_modal:warning')}</p>
+
+        <ButtonGroup>
+          <Button
+            className="margin-right-1"
+            type="button"
+            onClick={archiveIntake}
+          >
+            {t('taskList:withdraw_modal:confirm')}
+          </Button>
+
+          <Button type="button" unstyled onClick={() => setModalOpen(false)}>
+            {t('taskList:withdraw_modal:cancel')}
+          </Button>
+        </ButtonGroup>
+      </Modal>
     </MainContent>
   );
 }
