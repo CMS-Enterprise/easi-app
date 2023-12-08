7d4957d9af38cdbcdeec4044be1a05dcdb2fee74
ClayBenson94 <clay.benson@oddball.io>
2023-12-08 09:31:15 -0500

    Add migration for NOT NULL costs and dropping NULL costs

---
+++ migrations/V168__Drop_Null_Lifecycle_Costs.sql
@@ -0,0 +1,6 @@
+/*
+ * We shouldn't be adding entries to this table with NULL costs (which we were, prior to https://github.com/CMSgov/easi-app/pull/2313)
+ * This migration aims to drop all `NULL` costs from this table and modify the table's constraints to prevent this from happening again.
+ */
+DELETE FROM estimated_lifecycle_costs WHERE cost IS NULL;
+ALTER TABLE estimated_lifecycle_costs ALTER COLUMN cost NOT NULL;
