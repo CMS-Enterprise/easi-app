fba07c0e4d1430cfc1060e93749052acbaedc446
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2023-12-07 23:54:40 -0600

    GRT admin breadcrumb text

--- src/i18n/en-US/articles/governanceReviewTeam.ts
+++ src/i18n/en-US/articles/governanceReviewTeam.ts
@@ -245,6 +245,7 @@ const governanceReviewTeam = {
     feedbackMoved:
       'Feedback and recommendations have moved! Use the Feedback tab in the navigation to the left to view feedback and recommendations that have been sent to the requester and project team.'
   },
+  governanceRequestDetails: 'Governance request details',
   actions: 'Actions',
   requestType: 'Request type',
   status: {
