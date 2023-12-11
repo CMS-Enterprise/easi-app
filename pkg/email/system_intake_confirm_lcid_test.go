ff9b95b3710ca9b6b27a04a39cee917b870636a9
Lee Warrick <lee.warrick@oddball.io>
2023-12-11 17:53:56 -0500

    add optional syntax to confirm and issue templates for cost baseline, refactor tests

--- pkg/email/system_intake_confirm_lcid_test.go
+++ pkg/email/system_intake_confirm_lcid_test.go
@@ -48,6 +48,73 @@ func (s *EmailTestSuite) TestIntakeConfirmLCIDNotification() {
 		ShouldNotifyITGovernance: false,
 		ShouldNotifyITInvestment: false,
 	}
+	getExpectedEmail := func(
+		lifecycleCostBaseline *string,
+		issuedAt *time.Time,
+		trbRecommendation models.SystemIntakeTRBFollowUp,
+		additionalInfo *models.HTML,
+	) string {
+		var lifecycleCostBaselineStr string
+		var additionalInfoStr string
+		var issuedAtStr string
+		if issuedAt != nil {
+			issuedAtStr = fmt.Sprintf("\n<strong>Original date issued:</strong> %s<br>", issuedAt.Format("01/02/2006"))
+		}
+		if lifecycleCostBaseline != nil {
+			lifecycleCostBaselineStr = fmt.Sprintf("\n<strong>Project Cost Baseline:</strong> %s<br>", *lifecycleCostBaseline)
+		}
+		if additionalInfo != nil {
+			additionalInfoStr = fmt.Sprintf(
+				"<hr><p><strong>Additional information from the Governance Team:</strong></p><p>%s</p>",
+				additionalInfo.ToTemplate(),
+			)
+		}
+		return fmt.Sprintf(`<h1 style="margin-bottom: 0.5rem;">EASi</h1>
+
+<span style="font-size:15px; line-height: 18px; color: #71767A">Easy Access to System Information</span>
+
+<p>The Governance Team has confirmed a Life Cycle ID (LCID) for %s</p>
+
+<p><strong>Lifecycle ID:</strong> %s<br>%s
+<strong>Expiration date:</strong> %s<br>
+<strong>Scope:</strong> %s<br>%s
+<strong>Next steps:</strong> %s</p>
+
+<p>Technical Review Board (TRB) Consultation:<br>
+As a part of your next steps, the Governance Team %s.</p>
+
+<p>View this closed request in EASi:
+<ul>
+<li>The person who initially submitted this request, %s, may <a href="%s">click here</a> to view the request task list.</li>
+<li>Governance Team members may <a href="%s">click here</a> to view the request details.</li>
+<li>Others should contact %s or the Governance Team for more information about this request.</li>
+</ul></p>
+
+If you have questions, please contact the Governance Team at <a href="mailto:%s">%s</a>.
+
+%s
+<hr>
+
+<p>Depending on the request, the Governance Team may follow up with this project team at a later date.</p>
+`,
+			requestName,
+			lifecycleID,
+			issuedAtStr,
+			expiresAt.Format("01/02/2006"),
+			*lifecycleScope.StringPointer(),
+			lifecycleCostBaselineStr,
+			*decisionNextSteps.StringPointer(),
+			trbRecommendationMap[trbRecommendation],
+			requester,
+			requestLink,
+			adminLink,
+			requester,
+			ITGovInboxAddress,
+			ITGovInboxAddress,
+			additionalInfoStr,
+		)
+
+	}
 	client, err := NewClient(s.config, &sender)
 	s.NoError(err)
 	err = client.SystemIntake.SendConfirmLCIDNotification(
@@ -69,52 +136,8 @@ func (s *EmailTestSuite) TestIntakeConfirmLCIDNotification() {
 	expectedSubject := fmt.Sprintf("A Life Cycle ID (%s) has been confirmed for %s", lifecycleID, requestName)
 	s.Equal(expectedSubject, sender.subject)
 
-	expectedEmail := fmt.Sprintf(`<h1 style="margin-bottom: 0.5rem;">EASi</h1>
+	expectedEmail := getExpectedEmail(&lifecycleCostBaseline, &issuedAt, models.TRBFRNotRecommended, additionalInfo)
 
-<span style="font-size:15px; line-height: 18px; color: #71767A">Easy Access to System Information</span>
-
-<p>The Governance Team has confirmed a Life Cycle ID (LCID) for %s</p>
-
-<p><strong>Lifecycle ID:</strong> %s<br>
-<strong>Original date issued:</strong> %s<br>
-<strong>Expiration date:</strong> %s<br>
-<strong>Scope:</strong> %s<br>
-<strong>Project Cost Baseline:</strong> %s<br>
-<strong>Next steps:</strong> %s</p>
-
-<p>Technical Review Board (TRB) Consultation:<br>
-As a part of your next steps, the Governance Team %s.</p>
-
-<p>View this closed request in EASi:
-<ul>
-<li>The person who initially submitted this request, %s, may <a href="%s">click here</a> to view the request task list.</li>
-<li>Governance Team members may <a href="%s">click here</a> to view the request details.</li>
-<li>Others should contact %s or the Governance Team for more information about this request.</li>
-</ul></p>
-
-If you have questions, please contact the Governance Team at <a href="mailto:%s">%s</a>.
-
-<hr><p><strong>Additional information from the Governance Team:</strong></p><p>%s</p>
-<hr>
-
-<p>Depending on the request, the Governance Team may follow up with this project team at a later date.</p>
-`,
-		requestName,
-		lifecycleID,
-		issuedAt.Format("01/02/2006"),
-		expiresAt.Format("01/02/2006"),
-		*lifecycleScope.StringPointer(),
-		lifecycleCostBaseline,
-		*decisionNextSteps.StringPointer(),
-		"does not think a TRB consult is necessary",
-		requester,
-		requestLink,
-		adminLink,
-		requester,
-		ITGovInboxAddress,
-		ITGovInboxAddress,
-		*additionalInfo.StringPointer(),
-	)
 	s.Equal(expectedEmail, sender.body)
 	s.Run("Recipient is correct", func() {
 		allRecipients := []models.EmailAddress{
@@ -139,49 +162,7 @@ If you have questions, please contact the Governance Team at <a href="mailto:%s"
 		nil,
 	)
 	s.NoError(err)
-	expectedEmail = fmt.Sprintf(`<h1 style="margin-bottom: 0.5rem;">EASi</h1>
-
-<span style="font-size:15px; line-height: 18px; color: #71767A">Easy Access to System Information</span>
-
-<p>The Governance Team has confirmed a Life Cycle ID (LCID) for %s</p>
-
-<p><strong>Lifecycle ID:</strong> %s<br>
-<strong>Expiration date:</strong> %s<br>
-<strong>Scope:</strong> %s<br>
-<strong>Project Cost Baseline:</strong> %s<br>
-<strong>Next steps:</strong> %s</p>
-
-<p>Technical Review Board (TRB) Consultation:<br>
-As a part of your next steps, the Governance Team %s.</p>
-
-<p>View this closed request in EASi:
-<ul>
-<li>The person who initially submitted this request, %s, may <a href="%s">click here</a> to view the request task list.</li>
-<li>Governance Team members may <a href="%s">click here</a> to view the request details.</li>
-<li>Others should contact %s or the Governance Team for more information about this request.</li>
-</ul></p>
-
-If you have questions, please contact the Governance Team at <a href="mailto:%s">%s</a>.
-
-
-<hr>
-
-<p>Depending on the request, the Governance Team may follow up with this project team at a later date.</p>
-`,
-		requestName,
-		lifecycleID,
-		expiresAt.Format("01/02/2006"),
-		*lifecycleScope.StringPointer(),
-		lifecycleCostBaseline,
-		*decisionNextSteps.StringPointer(),
-		"does not think a TRB consult is necessary",
-		requester,
-		requestLink,
-		adminLink,
-		requester,
-		ITGovInboxAddress,
-		ITGovInboxAddress,
-	)
+	expectedEmail = getExpectedEmail(&lifecycleCostBaseline, nil, models.TRBFRNotRecommended, nil)
 
 	s.Run("Should omit additional info and issuedAt if absent", func() {
 		s.Equal(expectedEmail, sender.body)
@@ -204,58 +185,9 @@ If you have questions, please contact the Governance Team at <a href="mailto:%s"
 			additionalInfo,
 		)
 		s.NoError(err)
-		expectedEmail := fmt.Sprintf(`<h1 style="margin-bottom: 0.5rem;">EASi</h1>
-
-<span style="font-size:15px; line-height: 18px; color: #71767A">Easy Access to System Information</span>
-
-<p>The Governance Team has confirmed a Life Cycle ID (LCID) for %s</p>
-
-<p><strong>Lifecycle ID:</strong> %s<br>
-<strong>Original date issued:</strong> %s<br>
-<strong>Expiration date:</strong> %s<br>
-<strong>Scope:</strong> %s<br>
-<strong>Project Cost Baseline:</strong> %s<br>
-<strong>Next steps:</strong> %s</p>
-
-<p>Technical Review Board (TRB) Consultation:<br>
-As a part of your next steps, the Governance Team %s.</p>
-
-<p>View this closed request in EASi:
-<ul>
-<li>The person who initially submitted this request, %s, may <a href="%s">click here</a> to view the request task list.</li>
-<li>Governance Team members may <a href="%s">click here</a> to view the request details.</li>
-<li>Others should contact %s or the Governance Team for more information about this request.</li>
-</ul></p>
-
-If you have questions, please contact the Governance Team at <a href="mailto:%s">%s</a>.
-
-<hr><p><strong>Additional information from the Governance Team:</strong></p><p>%s</p>
-<hr>
-
-<p>Depending on the request, the Governance Team may follow up with this project team at a later date.</p>
-`,
-			requestName,
-			lifecycleID,
-			issuedAt.Format("01/02/2006"),
-			expiresAt.Format("01/02/2006"),
-			*lifecycleScope.StringPointer(),
-			lifecycleCostBaseline,
-			*decisionNextSteps.StringPointer(),
-			emailText,
-			requester,
-			requestLink,
-			adminLink,
-			requester,
-			ITGovInboxAddress,
-			ITGovInboxAddress,
-			*additionalInfo.StringPointer(),
-		)
-		s.Equal(expectedEmail, sender.body)
+		expectedEmail := getExpectedEmail(&lifecycleCostBaseline, &issuedAt, modelEnum, additionalInfo)
 		s.Run(fmt.Sprintf("%s should become %s in email body", modelEnum, emailText), func() {
-			allRecipients := []models.EmailAddress{
-				recipient,
-			}
-			s.ElementsMatch(sender.toAddresses, allRecipients)
+			s.Equal(expectedEmail, sender.body)
 		})
 
 	}
--- pkg/email/system_intake_issue_lcid_test.go
+++ pkg/email/system_intake_issue_lcid_test.go
@@ -48,6 +48,69 @@ func (s *EmailTestSuite) TestIntakeIssueLCIDNotification() {
 		ShouldNotifyITGovernance: false,
 		ShouldNotifyITInvestment: false,
 	}
+	getExpectedEmail := func(
+		lifecycleCostBaseline *string,
+		trbRecommendation models.SystemIntakeTRBFollowUp,
+		additionalInfo *models.HTML,
+	) string {
+		var lifecycleCostBaselineStr string
+		var additionalInfoStr string
+		if lifecycleCostBaseline != nil {
+			lifecycleCostBaselineStr = fmt.Sprintf("\n<strong>Project Cost Baseline:</strong> %s<br>", *lifecycleCostBaseline)
+		}
+		if additionalInfo != nil {
+			additionalInfoStr = fmt.Sprintf(
+				"<hr><p><strong>Additional information from the Governance Team:</strong></p><p>%s</p>",
+				additionalInfo.ToTemplate(),
+			)
+		}
+		return fmt.Sprintf(`<h1 style="margin-bottom: 0.5rem;">EASi</h1>
+
+<span style="font-size:15px; line-height: 18px; color: #71767A">Easy Access to System Information</span>
+
+<p>The Governance Team has issued a Life Cycle ID (LCID) for %s</p>
+
+<p><strong>Lifecycle ID:</strong> %s<br>
+<strong>Date issued:</strong> %s<br>
+<strong>Expiration date:</strong> %s<br>
+<strong>Scope:</strong> %s<br>%s
+<strong>Next steps:</strong> %s</p>
+
+<p>Technical Review Board (TRB) Consultation:<br>
+As a part of your next steps, the Governance Team %s.</p>
+
+<p>View this closed request in EASi:
+<ul>
+<li>The person who initially submitted this request, %s, may <a href="%s">click here</a> to view the request task list.</li>
+<li>Governance Team members may <a href="%s">click here</a> to view the request details.</li>
+<li>Others should contact %s or the Governance Team for more information about this request.</li>
+</ul></p>
+
+If you have questions, please contact the Governance Team at <a href="mailto:%s">%s</a>.
+
+%s
+<hr>
+
+<p>Depending on the request, the Governance Team may follow up with this project team at a later date.</p>
+`,
+			requestName,
+			lifecycleID,
+			lifecycleIssuedAt.Format("01/02/2006"),
+			expiresAt.Format("01/02/2006"),
+			*lifecycleScope.StringPointer(),
+			lifecycleCostBaselineStr,
+			*decisionNextSteps.StringPointer(),
+			trbRecommendationMap[trbRecommendation],
+			requester,
+			requestLink,
+			adminLink,
+			requester,
+			ITGovInboxAddress,
+			ITGovInboxAddress,
+			additionalInfoStr,
+		)
+
+	}
 	client, err := NewClient(s.config, &sender)
 	s.NoError(err)
 	err = client.SystemIntake.SendIssueLCIDNotification(
@@ -69,52 +132,7 @@ func (s *EmailTestSuite) TestIntakeIssueLCIDNotification() {
 	expectedSubject := fmt.Sprintf("A Life Cycle ID (%s) has been issued for %s", lifecycleID, requestName)
 	s.Equal(expectedSubject, sender.subject)
 
-	expectedEmail := fmt.Sprintf(`<h1 style="margin-bottom: 0.5rem;">EASi</h1>
-
-<span style="font-size:15px; line-height: 18px; color: #71767A">Easy Access to System Information</span>
-
-<p>The Governance Team has issued a Life Cycle ID (LCID) for %s</p>
-
-<p><strong>Lifecycle ID:</strong> %s<br>
-<strong>Date issued:</strong> %s<br>
-<strong>Expiration date:</strong> %s<br>
-<strong>Scope:</strong> %s<br>
-<strong>Project Cost Baseline:</strong> %s<br>
-<strong>Next steps:</strong> %s</p>
-
-<p>Technical Review Board (TRB) Consultation:<br>
-As a part of your next steps, the Governance Team %s.</p>
-
-<p>View this closed request in EASi:
-<ul>
-<li>The person who initially submitted this request, %s, may <a href="%s">click here</a> to view the request task list.</li>
-<li>Governance Team members may <a href="%s">click here</a> to view the request details.</li>
-<li>Others should contact %s or the Governance Team for more information about this request.</li>
-</ul></p>
-
-If you have questions, please contact the Governance Team at <a href="mailto:%s">%s</a>.
-
-<hr><p><strong>Additional information from the Governance Team:</strong></p><p>%s</p>
-<hr>
-
-<p>Depending on the request, the Governance Team may follow up with this project team at a later date.</p>
-`,
-		requestName,
-		lifecycleID,
-		lifecycleIssuedAt.Format("01/02/2006"),
-		expiresAt.Format("01/02/2006"),
-		*lifecycleScope.StringPointer(),
-		lifecycleCostBaseline,
-		*decisionNextSteps.StringPointer(),
-		"does not think a TRB consult is necessary",
-		requester,
-		requestLink,
-		adminLink,
-		requester,
-		ITGovInboxAddress,
-		ITGovInboxAddress,
-		*additionalInfo.StringPointer(),
-	)
+	expectedEmail := getExpectedEmail(&lifecycleCostBaseline, models.TRBFRNotRecommended, additionalInfo)
 	s.Equal(expectedEmail, sender.body)
 	s.Run("Recipient is correct", func() {
 		allRecipients := []models.EmailAddress{
@@ -139,53 +157,31 @@ If you have questions, please contact the Governance Team at <a href="mailto:%s"
 		nil,
 	)
 	s.NoError(err)
-	expectedEmail = fmt.Sprintf(`<h1 style="margin-bottom: 0.5rem;">EASi</h1>
-
-<span style="font-size:15px; line-height: 18px; color: #71767A">Easy Access to System Information</span>
-
-<p>The Governance Team has issued a Life Cycle ID (LCID) for %s</p>
-
-<p><strong>Lifecycle ID:</strong> %s<br>
-<strong>Date issued:</strong> %s<br>
-<strong>Expiration date:</strong> %s<br>
-<strong>Scope:</strong> %s<br>
-<strong>Project Cost Baseline:</strong> %s<br>
-<strong>Next steps:</strong> %s</p>
-
-<p>Technical Review Board (TRB) Consultation:<br>
-As a part of your next steps, the Governance Team %s.</p>
-
-<p>View this closed request in EASi:
-<ul>
-<li>The person who initially submitted this request, %s, may <a href="%s">click here</a> to view the request task list.</li>
-<li>Governance Team members may <a href="%s">click here</a> to view the request details.</li>
-<li>Others should contact %s or the Governance Team for more information about this request.</li>
-</ul></p>
-
-If you have questions, please contact the Governance Team at <a href="mailto:%s">%s</a>.
-
-
-<hr>
-
-<p>Depending on the request, the Governance Team may follow up with this project team at a later date.</p>
-`,
-		requestName,
-		lifecycleID,
-		lifecycleIssuedAt.Format("01/02/2006"),
-		expiresAt.Format("01/02/2006"),
-		*lifecycleScope.StringPointer(),
-		lifecycleCostBaseline,
-		*decisionNextSteps.StringPointer(),
-		"does not think a TRB consult is necessary",
-		requester,
-		requestLink,
-		adminLink,
-		requester,
-		ITGovInboxAddress,
-		ITGovInboxAddress,
-	)
+	expectedEmail = getExpectedEmail(&lifecycleCostBaseline, models.TRBFRNotRecommended, nil)
 
 	s.Run("Should omit additional info if absent", func() {
+		s.Equal(expectedEmail, sender.body)
+	})
+
+	err = client.SystemIntake.SendIssueLCIDNotification(
+		ctx,
+		recipients,
+		intakeID,
+		requestName,
+		lifecycleID,
+		lifecycleIssuedAt,
+		&expiresAt,
+		*lifecycleScope,
+		nil,
+		*decisionNextSteps,
+		models.TRBFRNotRecommended,
+		requester,
+		additionalInfo,
+	)
+	s.NoError(err)
+	expectedEmail = getExpectedEmail(nil, models.TRBFRNotRecommended, additionalInfo)
+
+	s.Run("Should omit lifecycle cost baseline if absent", func() {
 		s.Equal(expectedEmail, sender.body)
 	})
 
@@ -206,60 +202,9 @@ If you have questions, please contact the Governance Team at <a href="mailto:%s"
 			additionalInfo,
 		)
 		s.NoError(err)
-		expectedEmail := fmt.Sprintf(`<h1 style="margin-bottom: 0.5rem;">EASi</h1>
-
-<span style="font-size:15px; line-height: 18px; color: #71767A">Easy Access to System Information</span>
-
-<p>The Governance Team has issued a Life Cycle ID (LCID) for %s</p>
-
-<p><strong>Lifecycle ID:</strong> %s<br>
-<strong>Date issued:</strong> %s<br>
-<strong>Expiration date:</strong> %s<br>
-<strong>Scope:</strong> %s<br>
-<strong>Project Cost Baseline:</strong> %s<br>
-<strong>Next steps:</strong> %s</p>
-
-<p>Technical Review Board (TRB) Consultation:<br>
-As a part of your next steps, the Governance Team %s.</p>
-
-<p>View this closed request in EASi:
-<ul>
-<li>The person who initially submitted this request, %s, may <a href="%s">click here</a> to view the request task list.</li>
-<li>Governance Team members may <a href="%s">click here</a> to view the request details.</li>
-<li>Others should contact %s or the Governance Team for more information about this request.</li>
-</ul></p>
-
-If you have questions, please contact the Governance Team at <a href="mailto:%s">%s</a>.
-
-<hr><p><strong>Additional information from the Governance Team:</strong></p><p>%s</p>
-<hr>
-
-<p>Depending on the request, the Governance Team may follow up with this project team at a later date.</p>
-`,
-			requestName,
-			lifecycleID,
-			lifecycleIssuedAt.Format("01/02/2006"),
-			expiresAt.Format("01/02/2006"),
-			*lifecycleScope.StringPointer(),
-			lifecycleCostBaseline,
-			*decisionNextSteps.StringPointer(),
-			emailText,
-			requester,
-			requestLink,
-			adminLink,
-			requester,
-			ITGovInboxAddress,
-			ITGovInboxAddress,
-			*additionalInfo.StringPointer(),
-		)
-		s.Equal(expectedEmail, sender.body)
+		expectedEmail := getExpectedEmail(&lifecycleCostBaseline, modelEnum, additionalInfo)
 		s.Run(fmt.Sprintf("%s should become %s in email body", modelEnum, emailText), func() {
-			allRecipients := []models.EmailAddress{
-				recipient,
-			}
-			s.ElementsMatch(sender.toAddresses, allRecipients)
+			s.Equal(expectedEmail, sender.body)
 		})
-
 	}
-
 }
