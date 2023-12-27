b30d776c31205344d2226990b3817dbc927fbeb6
Steven Wade <steven.wade@oddball.io>
2023-12-27 14:06:12 -0800

    feat: initialize db and query for unique usernames from the database

--- cmd/populate_user_table/main.go
+++ cmd/populate_user_table/main.go
@@ -1,7 +1,41 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "accountUploader",
+	Short: "accountUploader is utility for querying and generating user account references",
+	Long:  "accountUploader is utility for querying and generating user account references",
+}
+
+func init() {
+	// rootCmd.AddCommand(serveCmd)
+	// rootCmd.AddCommand(testCmd)
+}
+func execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
 // TODO: https://github.com/CMSgov/mint-app/tree/00e01f91fd8e7e624c54c25d3b3f62d0a8a388d4/cmd/backfill is a good reference point
 func main() {
+	config := viper.New()
+	config.AutomaticEnv()
+	uploader := NewUploader(config)
+	_ = rootCmd //TODO, do we even want to bother with cobra?
+	userNames, err := uploader.QueryUsernames()
+	if err != nil {
+		fmt.Print(err)
+	}
+	fmt.Print(userNames)
 
 	/*
 		Steps
--- cmd/populate_user_table/uploader.go
+++ cmd/populate_user_table/uploader.go
@@ -3,49 +3,74 @@ package main
 import (
 	"fmt"
 
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/jmoiron/sqlx"
 	"github.com/spf13/viper"
 	"go.uber.org/zap"
 
 	"github.com/cmsgov/easi-app/pkg/appconfig"
 	"github.com/cmsgov/easi-app/pkg/storage"
-	"github.com/cmsgov/easi-app/pkg/upload"
 
-	ld "gopkg.in/launchdarkly/go-server-sdk.v5"
+	_ "embed"
 )
+
+//go:embed SQL/get_all_usernames.sql
+var getAllUserNamesSQL string
 
 // Uploader handles functionality for uploading data to the DB
 type Uploader struct {
 	Store  storage.Store
+	DB     *sqlx.DB
 	Logger zap.Logger
 }
 
 // NewUploader instantiates an Uploader
-func NewUploader() *Uploader { //TODO make this more configurable if needed
-	config := viper.New()
-	config.AutomaticEnv()
+func NewUploader(config *viper.Viper) *Uploader { //TODO make this more configurable if needed
+	// config := viper.New()
+	// config.AutomaticEnv()
 
-	store, logger, _ := getResolverDependencies(config)
+	db, logger := getResolverDependencies(config)
 	return &Uploader{
-		Store:  *store,
+		// Store:  *store,
+		DB:     db,
 		Logger: *logger,
 	}
+}
+
+func (u *Uploader) QueryUsernames() ([]string, error) {
+	usernames := []string{}
+	// stmt, err := u.DB.PrepareNamed(getAllUserNamesSQL)
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// arg := struct{}{}
+
+	// err = stmt.Select(&usernames, arg) //TODO verify if nil works here or not
+	err := u.DB.Select(&usernames, getAllUserNamesSQL)
+
+	if err != nil {
+		return nil, fmt.Errorf(" unable to query usernames %w", err)
+
+	}
+
+	return usernames, nil
+
 }
 
 // getResolverDependencies takes a Viper config and returns a Store and Logger object to be used
 // by various resolver functions.
 func getResolverDependencies(config *viper.Viper) (
-	*storage.Store,
+	*sqlx.DB,
 	*zap.Logger,
-	*upload.S3Client, //TODO is this needed?
 ) {
 	// Create the logger
 	logger := zap.NewNop()
 
-	// Create LD Client, which is required for creating the store
-	ldClient, err := ld.MakeCustomClient("fake", ld.Config{Offline: true}, 0)
-	if err != nil {
-		panic(err)
-	}
+	// // Create LD Client, which is required for creating the store
+	// ldClient, err := ld.MakeCustomClient("fake", ld.Config{Offline: true}, 0)
+	// if err != nil {
+	// 	panic(err)
+	// }
 
 	// Create the DB Config & Store
 	dbConfig := storage.DBConfig{
@@ -57,20 +82,51 @@ func getResolverDependencies(config *viper.Viper) (
 		SSLMode:        config.GetString(appconfig.DBSSLModeConfigKey),
 		MaxConnections: config.GetInt(appconfig.DBMaxConnections),
 	}
-	store, err := storage.NewStore(dbConfig, ldClient)
+	// store, err := storage.NewStore(dbConfig, ldClient)
+	// if err != nil {
+	// 	fmt.Printf("Failed to get new database: %v", err)
+	// 	panic(err)
+
+	db, err := newDB(dbConfig)
 	if err != nil {
 		fmt.Printf("Failed to get new database: %v", err)
 		panic(err)
 	}
 
-	// Create S3 client
-	s3Cfg := upload.Config{
-		Bucket:  config.GetString(appconfig.AWSS3FileUploadBucket),
-		Region:  config.GetString(appconfig.AWSRegion),
-		IsLocal: true,
+	return db, logger
+}
+
+func newDB(config storage.DBConfig) (*sqlx.DB, error) {
+
+	var db *sqlx.DB
+	var err error
+	if config.UseIAM {
+		// Connect using the IAM DB package
+		sess := session.Must(session.NewSession())
+		db = newConnectionPoolWithIam(sess, config)
+		err = db.Ping()
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		// Connect via normal user/pass
+		dataSourceName := fmt.Sprintf(
+			"host=%s port=%s user=%s "+
+				"password=%s dbname=%s sslmode=%s",
+			config.Host,
+			config.Port,
+			config.Username,
+			config.Password,
+			config.Database,
+			config.SSLMode,
+		)
+
+		db, err = sqlx.Connect("postgres", dataSourceName)
+		if err != nil {
+			return nil, err
+		}
 	}
 
-	s3Client := upload.NewS3Client(s3Cfg)
-
-	return store, logger, &s3Client
+	db.SetMaxOpenConns(config.MaxConnections)
+	return db, nil
 }
