0eebd5d652d861d7fcab6795b52e7cacf8e43176
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2024-01-04 08:08:47 -0600

    Undo skipping tests
    Fix skipped tests from merging main into branch

--- cypress/e2e/systemIntake.cy.js
+++ cypress/e2e/systemIntake.cy.js
@@ -1,6 +1,6 @@
 import cmsGovernanceTeams from '../../src/constants/enums/cmsGovernanceTeams';
 
-describe.skip('The System Intake Form', () => {
+describe('The System Intake Form', () => {
   beforeEach(() => {
     cy.localLogin({ name: 'E2E1' });
 
