da3f965a237d3e61107e0692ea8d597799b606df
Steven Wade <steven.wade@oddball.io>
2024-01-16 11:11:35 -0800

    feat: add unnest(notify_eua_ids) to command SQL for querying EUAIDs

--- cmd/populate_user_table/sql/get_all_usernames.sql
+++ cmd/populate_user_table/sql/get_all_usernames.sql
@@ -12,18 +12,12 @@ FROM actions
 UNION
 
  
-  
   -- Sub query for business_cases table
-  SELECT username
-  FROM (
-    SELECT eua_user_id AS username
-    FROM business_cases
-    -- UNION
-    -- SELECT eua_user_id AS username
-    -- FROM business_cases
-  ) AS business_cases_usernames
-  
-  UNION
+
+SELECT eua_user_id AS username
+FROM business_cases
+
+UNION
 
 -- Sub query for cedar_system_bookmarks table
 SELECT eua_user_id AS username
@@ -72,120 +66,122 @@ SELECT modified_by AS username
 FROM trb_admin_notes
 UNION
 
-  -- Sub query for trb_admin_notes_trb_admin_note_recommendations_links table
-  SELECT created_by AS username
-  FROM trb_admin_notes_trb_admin_note_recommendations_links
-  UNION
-  SELECT modified_by AS username
-  FROM trb_admin_notes_trb_admin_note_recommendations_links
-  
-  UNION
-  
-  -- Sub query for trb_admin_notes_trb_request_documents_links table
-  SELECT created_by AS username
-  FROM trb_admin_notes_trb_request_documents_links
-  UNION
-  SELECT modified_by AS username
-  FROM trb_admin_notes_trb_request_documents_links
-  
-  UNION
-  
-  -- Sub query for trb_advice_letter_recommendations table
-  SELECT created_by AS username
-  FROM trb_advice_letter_recommendations
-  UNION
-  SELECT modified_by AS username
-  FROM trb_advice_letter_recommendations
-  
-  UNION
-  
-  -- Sub query for trb_advice_letters table
-  SELECT created_by AS username
-  FROM trb_advice_letters
-  UNION
-  SELECT modified_by AS username
-  FROM trb_advice_letters
-  
-  UNION
-  
-  -- Sub query for trb_lead_options table
-  SELECT created_by AS username
-  FROM trb_lead_options
-  UNION
-  SELECT modified_by AS username
-  FROM trb_lead_options
-  UNION
-  SELECT eua_user_id AS username
-  FROM trb_lead_options
-  
-  UNION
-  
-  -- Sub query for trb_request table
-  SELECT created_by AS username
-  FROM trb_request
-  UNION
-  SELECT modified_by AS username
-  FROM trb_request
-  UNION
-  SELECT trb_lead AS username
-  FROM trb_request
-  
-  UNION
-  
-  -- Sub query for trb_request_attendees table
-  SELECT created_by AS username
-  FROM trb_request_attendees
-  UNION
-  SELECT modified_by AS username
-  FROM trb_request_attendees
-  UNION
-  SELECT eua_user_id AS username
-  FROM trb_request_attendees
-  
-  UNION
-  
-  -- Sub query for trb_request_documents table
-  SELECT created_by AS username
-  FROM trb_request_documents
-  UNION
-  SELECT modified_by AS username
-  FROM trb_request_documents
-  
-  UNION
-  
-  -- Sub query for trb_request_feedback table
-  SELECT created_by AS username
-  FROM trb_request_feedback
-  UNION
-  SELECT modified_by AS username
-  FROM trb_request_feedback
-  -- TODO: EASI-3341 column notify_eua_ids is an array of EUAIDs, we probably should do a call of unnest and add it to the union select
-  
-  UNION
-  
-  -- Sub query for trb_request_forms table
-  SELECT created_by AS username
-  FROM trb_request_forms
-  UNION
-  SELECT modified_by AS username
-  FROM trb_request_forms
-  
-  UNION
-  
-  -- Sub query for trb_request_funding_sources table
-  SELECT created_by AS username
-  FROM trb_request_funding_sources
-  UNION
-  SELECT modified_by AS username
-  FROM trb_request_funding_sources
-  
-  UNION
-  
-  -- Sub query for trb_request_system_intakes table
-  SELECT created_by AS username
-  FROM trb_request_system_intakes
-  UNION
-  SELECT modified_by AS username
-  FROM trb_request_system_intakes
+-- Sub query for trb_admin_notes_trb_admin_note_recommendations_links table
+SELECT created_by AS username
+FROM trb_admin_notes_trb_admin_note_recommendations_links
+UNION
+SELECT modified_by AS username
+FROM trb_admin_notes_trb_admin_note_recommendations_links
+
+UNION
+
+-- Sub query for trb_admin_notes_trb_request_documents_links table
+SELECT created_by AS username
+FROM trb_admin_notes_trb_request_documents_links
+UNION
+SELECT modified_by AS username
+FROM trb_admin_notes_trb_request_documents_links
+
+UNION
+
+-- Sub query for trb_advice_letter_recommendations table
+SELECT created_by AS username
+FROM trb_advice_letter_recommendations
+UNION
+SELECT modified_by AS username
+FROM trb_advice_letter_recommendations
+
+UNION
+
+-- Sub query for trb_advice_letters table
+SELECT created_by AS username
+FROM trb_advice_letters
+UNION
+SELECT modified_by AS username
+FROM trb_advice_letters
+
+UNION
+
+-- Sub query for trb_lead_options table
+SELECT created_by AS username
+FROM trb_lead_options
+UNION
+SELECT modified_by AS username
+FROM trb_lead_options
+UNION
+SELECT eua_user_id AS username
+FROM trb_lead_options
+
+UNION
+
+-- Sub query for trb_request table
+SELECT created_by AS username
+FROM trb_request
+UNION
+SELECT modified_by AS username
+FROM trb_request
+UNION
+SELECT trb_lead AS username
+FROM trb_request
+
+UNION
+
+-- Sub query for trb_request_attendees table
+SELECT created_by AS username
+FROM trb_request_attendees
+UNION
+SELECT modified_by AS username
+FROM trb_request_attendees
+UNION
+SELECT eua_user_id AS username
+FROM trb_request_attendees
+
+UNION
+
+-- Sub query for trb_request_documents table
+SELECT created_by AS username
+FROM trb_request_documents
+UNION
+SELECT modified_by AS username
+FROM trb_request_documents
+
+UNION
+
+-- Sub query for trb_request_feedback table
+SELECT unnest(notify_eua_ids) AS username
+FROM trb_request_feedback
+UNION
+SELECT created_by AS username
+FROM trb_request_feedback
+UNION
+SELECT modified_by AS username
+FROM trb_request_feedback
+
+UNION
+
+-- Sub query for trb_request_forms table
+SELECT created_by AS username
+FROM trb_request_forms
+UNION
+SELECT modified_by AS username
+FROM trb_request_forms
+
+UNION
+
+-- Sub query for trb_request_funding_sources table
+SELECT created_by AS username
+FROM trb_request_funding_sources
+UNION
+SELECT modified_by AS username
+FROM trb_request_funding_sources
+
+UNION
+
+-- Sub query for trb_request_system_intakes table
+SELECT created_by AS username
+FROM trb_request_system_intakes
+UNION
+SELECT modified_by AS username
+FROM trb_request_system_intakes
 ) AS combined_usernames 
 WHERE username IS NOT NULL;
