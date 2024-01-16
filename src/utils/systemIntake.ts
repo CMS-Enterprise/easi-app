56a4e984522cf691c0cb04a23f4ab7edff568c5e
adamodd <97050498+adamodd@users.noreply.github.com>
2024-01-16 09:35:28 -0800

    EASI-3783 v1 references on Requester home page (#2374)
    * Rm requester status column flag
    
    * Cleanup more col refs
    
    * Rm old i18n#intake:statusMap, utils/systemIntake#translateStatus

--- src/utils/systemIntake.ts
+++ src/utils/systemIntake.ts
@@ -4,8 +4,7 @@ import cmsDivisionsAndOffices from 'constants/enums/cmsDivisionsAndOffices';
 import {
   closedIntakeStatusesV1,
   openIntakeStatusesV1,
-  RequestType,
-  SystemIntakeStatusV1
+  RequestType
 } from 'types/systemIntake';
 
 // NOTE: these utility functions take strings rather than more restricted types so they can operate on values coming from GraphQL queries,
@@ -71,28 +70,4 @@ export const getAcronymForComponent = (componentToTranslate: string) => {
 
   // TODO: what do we return if not found? (should be impossible)
   return component ? component.acronym : 'Other';
-};
-
-/**
- * Translate the API status enum to a human readable string
- * @param statusEnum - intake status represented by enum value
- */
-export const translateStatus = (
-  statusEnum: SystemIntakeStatusV1,
-  lcid: string | null
-) => {
-  let statusTranslation = '';
-
-  if (statusEnum === 'LCID_ISSUED') {
-    // if status is LCID_ISSUED, translate from enum to i18n and append LCID
-    // Display not available message if LCID is null (usually due to bad SharePoint data migration)
-    statusTranslation = `${i18next.t(`intake:statusMap.${statusEnum}`)}: ${
-      lcid || 'ID Not Available'
-    }`;
-  } else {
-    // if not just translate from enum to i18n
-    statusTranslation = i18next.t(`intake:statusMap.${statusEnum}`);
-  }
-
-  return statusTranslation;
 };
