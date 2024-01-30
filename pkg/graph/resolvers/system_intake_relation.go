3f6dcd11e43ae9f3bb52288906144f0337ccef8e
ClayBenson94 <clay.benson@oddball.io>
2024-01-30 13:31:22 -0500

    Add new resolvers and tests

--- pkg/graph/resolvers/system_intake_relation.go
+++ pkg/graph/resolvers/system_intake_relation.go
@@ -40,3 +40,61 @@ func SetSystemIntakeRelationExistingService(
 		return updatedIntake, nil
 	})
 }
+
+// SetSystemIntakeRelationNewSystem effectively clears the relationship between a system intake
+// and any previously set list of CEDAR Systems and contract/service names. It also
+// sets any contract number relationships.
+func SetSystemIntakeRelationNewSystem(
+	ctx context.Context,
+	store *storage.Store,
+	input *model.SetSystemIntakeRelationNewSystemInput,
+) (*models.SystemIntake, error) {
+	return sqlutils.WithTransaction[models.SystemIntake](store, func(tx *sqlx.Tx) (*models.SystemIntake, error) {
+		// Fetch intake by ID
+		intake, err := store.FetchSystemIntakeByID(ctx, input.SystemIntakeID)
+		if err != nil {
+			return nil, err
+		}
+
+		// Clear contract name
+		intake.ContractName = null.NewString("", false)
+		updatedIntake, err := store.UpdateSystemIntake(ctx, intake)
+		if err != nil {
+			return nil, err
+		}
+
+		// TODO: STORE -> Delete CEDAR system relationships
+		// TODO: STORE -> Delete & recreate contract number relationships
+
+		return updatedIntake, nil
+	})
+}
+
+// SetSystemIntakeRelationExistingSystem sets the relationship between a system intake and
+// a list of CEDAR Systems by setting an array of CEDAR System IDs, clearing any data about
+// contract/service names, and setting any contract number relationships.
+func SetSystemIntakeRelationExistingSystem(
+	ctx context.Context,
+	store *storage.Store,
+	input *model.SetSystemIntakeRelationExistingSystemInput,
+) (*models.SystemIntake, error) {
+	return sqlutils.WithTransaction[models.SystemIntake](store, func(tx *sqlx.Tx) (*models.SystemIntake, error) {
+		// Fetch intake by ID
+		intake, err := store.FetchSystemIntakeByID(ctx, input.SystemIntakeID)
+		if err != nil {
+			return nil, err
+		}
+
+		// Clear contract name
+		intake.ContractName = null.NewString("", false)
+		updatedIntake, err := store.UpdateSystemIntake(ctx, intake)
+		if err != nil {
+			return nil, err
+		}
+
+		// TODO: STORE -> Add CEDAR system relationships
+		// TODO: STORE -> Delete & recreate contract number relationships
+
+		return updatedIntake, nil
+	})
+}
--- pkg/graph/resolvers/system_intake_relation_test.go
+++ pkg/graph/resolvers/system_intake_relation_test.go
@@ -3,9 +3,80 @@ package resolvers
 import (
 	"time"
 
+	"github.com/guregu/null"
+
 	"github.com/cmsgov/easi-app/pkg/graph/model"
 	"github.com/cmsgov/easi-app/pkg/models"
 )
+
+func (suite *ResolverSuite) TestSetSystemIntakeRelationNewSystem() {
+	ctx := suite.testConfigs.Context
+	store := suite.testConfigs.Store
+
+	submittedAt := time.Now()
+
+	// Create an inital intake that has an existing contract name
+	openIntake, err := store.CreateSystemIntake(ctx, &models.SystemIntake{
+		State:        models.SystemIntakeStateOPEN,
+		Status:       models.SystemIntakeStatusINTAKESUBMITTED,
+		RequestType:  models.SystemIntakeRequestTypeNEW,
+		SubmittedAt:  &submittedAt,
+		ContractName: null.StringFrom("My Test Contract Name"),
+	})
+	suite.NoError(err)
+	suite.NotNil(openIntake)
+
+	// Set the "new system" relationship
+	input := &model.SetSystemIntakeRelationNewSystemInput{
+		SystemIntakeID: openIntake.ID,
+		// TODO: ContractNumbers
+	}
+	updatedIntake, err := SetSystemIntakeRelationNewSystem(ctx, store, input)
+	suite.NoError(err)
+
+	// Once fully implemented, we'll need to add tests that ensure the following
+	// 1. TODO - Removal of CEDAR System ID relationships that might be existing
+	// 2. TODO - That contract number relationships are deleted and recreated properly
+	//
+	// These may best be tested by other tests, or can be added to this test
+
+	// Ensure the contract name was deleted properly
+	suite.True(updatedIntake.ContractName.IsZero())
+}
+func (suite *ResolverSuite) TestSetSystemIntakeRelationExistingSystem() {
+	ctx := suite.testConfigs.Context
+	store := suite.testConfigs.Store
+
+	submittedAt := time.Now()
+
+	// Create an inital intake that has an existing contract name
+	openIntake, err := store.CreateSystemIntake(ctx, &models.SystemIntake{
+		State:        models.SystemIntakeStateOPEN,
+		Status:       models.SystemIntakeStatusINTAKESUBMITTED,
+		RequestType:  models.SystemIntakeRequestTypeNEW,
+		SubmittedAt:  &submittedAt,
+		ContractName: null.StringFrom("My Test Contract Name"),
+	})
+	suite.NoError(err)
+	suite.NotNil(openIntake)
+
+	// Set the "new system" relationship
+	input := &model.SetSystemIntakeRelationExistingSystemInput{
+		SystemIntakeID: openIntake.ID,
+		// TODO: ContractNumbers
+	}
+	updatedIntake, err := SetSystemIntakeRelationExistingSystem(ctx, store, input)
+	suite.NoError(err)
+
+	// Once fully implemented, we'll need to add tests that ensure the following
+	// 1. TODO - Setting of CEDAR System ID relationships that might be existing
+	// 2. TODO - That contract number relationships are deleted and recreated properly
+	//
+	// These may best be tested by other tests, or can be added to this test
+
+	// Ensure the contract name was deleted properly
+	suite.True(updatedIntake.ContractName.IsZero())
+}
 
 func (suite *ResolverSuite) TestSetSystemIntakeRelationExistingService() {
 	ctx := suite.testConfigs.Context
