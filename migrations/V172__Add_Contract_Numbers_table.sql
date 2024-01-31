0fef19a68d71e0badc4822235d5e3fd0e0501f42
Sam Lawson <samlawson@Sams-MBP.lan>
2024-01-31 10:43:16 -0800

    remove commented sql

--- migrations/V172__Add_Contract_Numbers_table.sql
+++ migrations/V172__Add_Contract_Numbers_table.sql
@@ -1,7 +1,6 @@
 CREATE TABLE IF NOT EXISTS system_intake_contract_numbers (
     id UUID PRIMARY KEY NOT NULL,
     intake_id UUID REFERENCES system_intakes(id) ON DELETE CASCADE,
-    -- trb_id UUID REFERENCES trb_request(id) ON DELETE CASCADE,
     contract_number ZERO_STRING NOT NULL,
     created_by TEXT NOT NULL CHECK (created_by ~ '^[A-Z0-9]{4}$'),
     created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -13,15 +12,3 @@ CREATE TABLE IF NOT EXISTS system_intake_contract_numbers (
 CREATE UNIQUE INDEX IF NOT EXISTS
     contract_number_intake_id_unique_idx ON contract_numbers
         USING btree (intake_id, contract_number);
-
--- allow one link between a unique trb id and a unique contract number
--- CREATE UNIQUE INDEX IF NOT EXISTS
---     contract_number_trb_id_unique_idx ON contract_numbers
---         USING btree (trb_id, contract_number)
---             WHERE trb_id IS NOT NULL;
-
--- ALTER TABLE IF EXISTS contract_numbers ADD CONSTRAINT exactly_one_fkey check (
---     ((intake_id IS NOT NULL) AND (trb_id IS NULL))
---     OR
---     ((intake_id IS NULL) AND (trb_id IS NOT NULL))
--- );
