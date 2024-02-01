3f633c98234ff7dd5bc012a5fccbde28a8834fb5
Sam Lawson <samlawson@Sams-MBP.lan>
2024-02-01 14:24:04 -0800

    remove call to `String()`

--- cmd/devdata/trb_request.go
+++ cmd/devdata/trb_request.go
@@ -582,7 +582,7 @@ func (s *seederConfig) addAdviceLetter(ctx context.Context, trb *models.TRBReque
 	}
 
 	adviceLetterChanges := map[string]interface{}{
-		"trbRequestId":          trb.ID.String(),
+		"trbRequestId":          trb.ID,
 		"meetingSummary":        "Talked about stuff",
 		"isFollowupRecommended": isFollowUpRequested,
 	}
