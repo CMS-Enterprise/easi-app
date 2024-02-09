0b490ed9cc331e4e237007e8c5002f73e8fae623
Sam Lawson <samlawson@Sams-MBP.lan>
2024-02-08 18:06:24 -0800

    fix sql stmt, add tests

--- pkg/sqlqueries/SQL/system_intake_contract_number/delete.sql
+++ pkg/sqlqueries/SQL/system_intake_contract_number/delete.sql
@@ -1,3 +1,3 @@
 DELETE FROM system_intake_contract_numbers
-WHERE contract_number NOT IN ($1)
+WHERE contract_number <> ALL($1)
 AND system_intake_id = $2;
--- pkg/storage/system_intake_contract_number_test.go
+++ pkg/storage/system_intake_contract_number_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"strings"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/jmoiron/sqlx"
@@ -21,6 +22,7 @@ func (s *StoreTestSuite) TestLinkSystemIntakeContractNumbers() {
 		contract1 = "1"
 		contract2 = "2"
 		contract3 = "3"
+		contract4 = "4"
 	)
 
 	var createdIDs []uuid.UUID
@@ -54,16 +56,73 @@ func (s *StoreTestSuite) TestLinkSystemIntakeContractNumbers() {
 			s.NoError(err)
 		}
 
-		params := formatParamTableJSON("system_intake_id", createdIDs)
-
-		data, err := s.store.SystemIntakeContractNumbersBySystemIntakeIDLOADER(ctx, params)
+		data, err := s.store.SystemIntakeContractNumbersBySystemIntakeIDLOADER(ctx, formatParamTableJSON("system_intake_id", createdIDs))
 		s.NoError(err)
 
 		for _, systemIntakeID := range createdIDs {
-			contactsFound, ok := data[systemIntakeID.String()]
+			contractsFound, ok := data[systemIntakeID.String()]
 			s.True(ok)
-			s.Len(contactsFound, 3)
+			s.Len(contractsFound, 3)
+
+			var (
+				found1 bool
+				found2 bool
+				found3 bool
+			)
+
+			for _, contract := range contractsFound {
+				if contract.ContractNumber == contract1 {
+					found1 = true
+				}
+
+				if contract.ContractNumber == contract2 {
+					found2 = true
+				}
+
+				if contract.ContractNumber == contract3 {
+					found3 = true
+				}
+			}
+
+			s.True(found1)
+			s.True(found2)
+			s.True(found3)
 		}
+
+		// now, we can add contract 4 to one of the system intakes and verify that the created_at dates for the first three remain unchanged
+		_, err = sqlutils.WithTransaction[any](s.db, func(tx *sqlx.Tx) (*any, error) {
+			s.NoError(s.store.SetSystemIntakeContractNumbers(ctx, tx, createdIDs[0], append(contractNumbers, contract4)))
+			return nil, nil
+		})
+		s.NoError(err)
+
+		data, err = s.store.SystemIntakeContractNumbersBySystemIntakeIDLOADER(ctx, formatParamTableJSON("system_intake_id", []uuid.UUID{createdIDs[0]}))
+		s.NoError(err)
+		contractsFound, ok := data[createdIDs[0].String()]
+		s.True(ok)
+		s.Len(contractsFound, 4)
+
+		var (
+			firstThreeContractsTime time.Time
+			fourthContractTime      time.Time
+		)
+
+		for _, contract := range contractsFound {
+			if contract.ContractNumber == contract1 ||
+				contract.ContractNumber == contract2 ||
+				contract.ContractNumber == contract3 {
+				firstThreeContractsTime = contract.CreatedAt
+			}
+
+			if contract.ContractNumber == contract4 {
+				fourthContractTime = contract.CreatedAt
+			}
+		}
+
+		s.False(firstThreeContractsTime.IsZero())
+		s.False(fourthContractTime.IsZero())
+
+		s.True(fourthContractTime.After(firstThreeContractsTime))
 
 		_, err = s.db.ExecContext(ctx, "DELETE FROM system_intakes WHERE id = ANY($1)", pq.Array(createdIDs))
 		s.NoError(err)
