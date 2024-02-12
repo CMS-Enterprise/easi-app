eedef16126384892685aaae1f8ac7dca3b13373a
samoddball <sam.lawson@oddball.io>
2024-02-12 12:11:15 -0800

    remove unneeded change

--- src/views/BusinessCase/AlternativeSolution/AlternativeSolutionB.test.tsx
+++ src/views/BusinessCase/AlternativeSolution/AlternativeSolutionB.test.tsx
@@ -58,7 +58,8 @@ describe('Business case alternative b solution', () => {
         id: '75746af8-9a9b-4558-a375-cf9848eb2b0d',
         systemIntakeId: '34ded286-02fa-4457-b1a5-0fc6ec00ecf5',
         alternativeB: {
-          ...defaultProposedSolution
+          ...defaultProposedSolution,
+          title: 'Alt B'
         }
       },
       isLoading: false,
