c26241a3158b95790a9e0d11715d7e7a6039f25a
ClayBenson94 <clay.benson@oddball.io>
2024-02-19 14:04:38 -0500

    Update migration to rename and re-use request relation type

--- migrations/V175__Add_TRB_Request_Contract_Name.sql
+++ migrations/V175__Add_TRB_Request_Contract_Name.sql
@@ -1,11 +1,7 @@
+-- Now that we're using this enum in TRB requests, we need to rename it to be more generic
+ALTER TYPE system_intake_relation_type RENAME TO request_relation_type
+ALTER TABLE trb_request ADD COLUMN system_relation_type request_relation_type;
+
 -- Using a NULLable ZERO_STRING here so that you either have a valid non-empty string
 -- or an explicitly NULL value (i.e. not "")
 ALTER TABLE trb_request ADD COLUMN contract_name ZERO_STRING;
-
-CREATE TYPE trb_request_relation_type AS ENUM (
-  'NEW_SYSTEM',
-  'EXISTING_SYSTEM',
-  'EXISTING_SERVICE'
-);
-
-ALTER TABLE trb_request ADD COLUMN system_relation_type trb_request_relation_type;
