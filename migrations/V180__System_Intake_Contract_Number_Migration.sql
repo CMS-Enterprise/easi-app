b1da44f872db351dd7e3c4541c90e4338934fac5
samoddball <sam.lawson@oddball.io>
2024-02-28 13:58:20 -0800

    fix spacing

--- migrations/V180__System_Intake_Contract_Number_Migration.sql
+++ migrations/V180__System_Intake_Contract_Number_Migration.sql
@@ -1,37 +1,37 @@
 INSERT INTO system_intake_contract_numbers
 (
-	id,
+    id,
     system_intake_id,
-    contract_number, 
-    created_by, 
-    created_at, 
+    contract_number,
+    created_by,
+    created_at,
     modified_by,
     modified_at
 )
 SELECT
-	gen_random_uuid(),
-    si.id, 
-    si.contract_number, 
-	(SELECT 
+    gen_random_uuid(),
+    si.id,
+    si.contract_number,
+    (SELECT
         CASE
-	        WHEN 
+            WHEN
                 si.eua_user_id IS NULL
-            THEN 
+            THEN
                 '00000001-0001-0001-0001-000000000001' -- EASI SYSTEM USER
-	        ELSE 
+            ELSE
                 (SELECT ua.id FROM user_account ua WHERE ua.username = si.eua_user_id)
 	    END),
     si.created_at,
-	(SELECT 
+    (SELECT
         CASE
-	        WHEN 
+            WHEN
                 si.eua_user_id IS NULL
-            THEN 
+            THEN
                 '00000001-0001-0001-0001-000000000001' -- EASI SYSTEM USER
-	        ELSE 
+            ELSE
                 (SELECT ua.id FROM user_account ua WHERE ua.username = si.eua_user_id)
-	    END),
+        END),
     si.updated_at
 FROM system_intakes si
-WHERE si.contract_number IS NOT NULL 
+WHERE si.contract_number IS NOT NULL
 AND LENGTH(TRIM(si.contract_number)) > 0; -- skip contract numbers that are only whitespace
