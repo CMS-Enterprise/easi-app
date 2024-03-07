733fd5494c282527968b08a219f51480808b1b8a
Azeez Salu <azeez.salu@oddball.io>
2024-03-07 03:40:20 -0500

    updated the scan wait time

--- scripts/check_ecr_findings.sh
+++ scripts/check_ecr_findings.sh
@@ -9,9 +9,22 @@ image_tag="$2"
 # Start the image scan
 aws ecr start-image-scan --repository-name "$repository_name" --image-id imageTag="$image_tag"
 
-# Wait for the scan to complete 
+# Wait for the scan to complete
 echo "Waiting for scan to complete..."
-sleep 60 # To discuss sleep time with Clay 
+while true; do
+    scanStatus=$(aws ecr describe-image-scan-findings --repository-name "$repository_name" --image-id imageTag="$image_tag" | jq -r '.imageScanStatus.status')
+    
+    if [[ "$scanStatus" == "COMPLETE" ]]; then
+        echo "Scan complete"
+        break
+    elif [[ "$scanStatus" == "FAILED" ]]; then
+        echo "Scan failed"
+        exit 1
+    else
+        echo "Scan in progress..."
+        sleep 10
+    fi
+done
 
 # Retrieve the scan findings and parse the total findings
 totalFindings=$(aws ecr describe-image-scan-findings --repository-name "$repository_name" --image-id imageTag="$image_tag" | jq '.imageScanFindings.findingSeverityCounts | add')
