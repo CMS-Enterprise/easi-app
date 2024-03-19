e9f8815acb202c8ec5c274cd5cd9add46944cbee
samoddball <sam.lawson@oddball.io>
2024-03-19 14:42:28 -0700

    update tests - we can discard result when we get an error

--- pkg/storage/transactions_test.go
+++ pkg/storage/transactions_test.go
@@ -2,13 +2,15 @@ package storage
 
 import (
 	"context"
-	"fmt"
+	"errors"
 
 	"github.com/jmoiron/sqlx"
 
 	"github.com/cmsgov/easi-app/pkg/models"
 	"github.com/cmsgov/easi-app/pkg/sqlutils"
 )
+
+var errArtifical = errors.New("this is an artificial error to ensure that the transaction rolls back")
 
 func (suite *StoreTestSuite) TestWithTransaction() {
 
@@ -37,27 +39,26 @@ func (suite *StoreTestSuite) TestWithTransaction() {
 	})
 
 	suite.Run("Errors will rollback a transaction", func() {
-		newTRB, err := sqlutils.WithTransactionRet[*models.TRBRequest](ctx, suite.store, func(tx *sqlx.Tx) (*models.TRBRequest, error) {
+		err := sqlutils.WithTransaction(ctx, suite.store, func(tx *sqlx.Tx) error {
 
 			trb := models.NewTRBRequest(anonEua)
 			trb.Type = models.TRBTNeedHelp
 			trb.State = models.TRBRequestStateOpen
 
-			createdTRB, err := suite.store.CreateTRBRequest(ctx, tx, trb)
+			_, err := suite.store.CreateTRBRequest(ctx, tx, trb)
 			if err != nil {
-				return nil, err
+				return err
 			}
-			return createdTRB, fmt.Errorf("this is an artificial error to ensure that the transaction rolls back")
+			return errArtifical
 
 		})
-		suite.Error(err)
-		suite.Nil(newTRB)
+		suite.ErrorIs(err, errArtifical)
 	})
 
 	suite.Run("With Transaction can also perform discrete db actions not directly part of the transaction", func() {
 		//NOTE: this is not behavior we should expect in production code.
 		var trbGlobal *models.TRBRequest
-		newTRB, err := sqlutils.WithTransactionRet[*models.TRBRequest](ctx, suite.store, func(tx *sqlx.Tx) (*models.TRBRequest, error) {
+		err := sqlutils.WithTransaction(ctx, suite.store, func(tx *sqlx.Tx) error {
 
 			trb := models.NewTRBRequest(anonEua)
 			trb.Type = models.TRBTNeedHelp
@@ -65,19 +66,17 @@ func (suite *StoreTestSuite) TestWithTransaction() {
 
 			createdTRB, err := suite.store.CreateTRBRequest(ctx, suite.store, trb) //Call the method on the store itself, so it is automatically created
 			if err != nil {
-				return nil, err
+				return err
 			}
 			trbGlobal = createdTRB
-			return createdTRB, fmt.Errorf("this is an artificial error to ensure that the transaction rolls back")
+			return errArtifical
 
 		})
-		suite.Error(err)
-		suite.Nil(newTRB) //if there is an error, WithTransaction returns nil
+		suite.ErrorIs(err, errArtifical)
 
 		trbRet, err := suite.store.GetTRBRequestByID(ctx, trbGlobal.ID) // If the transaction was commited, we should get the plan
 		suite.NoError(err)
 		suite.NotNil(trbRet)
-
 	})
 
 }
