a9998ca9712df02e19bf2146f95fa4714655b1e6
ClayBenson94 <clay.benson@oddball.io>
2024-03-25 10:03:29 -0400

    Modify 2000 wait to 1500

--- cypress/e2e/governanceReviewTeam.cy.js
+++ cypress/e2e/governanceReviewTeam.cy.js
@@ -561,7 +561,7 @@ describe('Governance Review Team', () => {
 
     cy.get('#react-select-IntakeForm-ContactCommonName-input')
       .type('Aaron A')
-      .wait(2000)
+      .wait(1500)
       .type('{downArrow}{enter}')
       .should('have.value', 'Aaron Adams, ADMN (aaron.adams@local.fake)');
 
