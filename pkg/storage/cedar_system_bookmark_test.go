d0a37021de53ee9f6e47f9088d1bd5ae57a1d1a2
samoddball <156127704+samoddball@users.noreply.github.com>
2024-05-10 16:39:33 -0700

    Easi 4116/bookmarked sys (#2572)
    * first stab at this - only done for a single route for creating POC
    
    * (WIP) building out loader for testing
    
    * (WIP) figuring out graphql + dataloaders
    
    * more dataloader + store work
    
    * fix some bugs, get test working
    
    * handle nil map better
    
    * patch up test. we may not need `EUAUserID` field on `CedarSystemBookmark` - digging
    
    * don't overwrite a given EUA when creating a bookmark
    
    * working on creating a bookmark input type and a response type, where the input does not have `euaUserId`
    
    * whitespace
    
    * remove CreatedAt
    
    * remove ptr wrapper
    
    * remove commented out import
    
    * update postman collection for `isBookmarked` and clean up url var
    
    * ss through EUA from resolver
    
    * update postman collection

--- pkg/storage/cedar_system_bookmark_test.go
+++ pkg/storage/cedar_system_bookmark_test.go
@@ -2,8 +2,13 @@ package storage
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"time"
 
+	"github.com/google/uuid"
+
+	"github.com/cmsgov/easi-app/pkg/appcontext"
 	"github.com/cmsgov/easi-app/pkg/models"
 	"github.com/cmsgov/easi-app/pkg/testhelpers"
 
@@ -44,13 +49,50 @@ func (s *StoreTestSuite) TestDuplicateCedarSystemBookmark() {
 			CedarSystemID: cedarSystemID,
 		}
 		created, err := s.store.CreateCedarSystemBookmark(ctx, &bookmark)
-		createdAt := (*created.CreatedAt).Unix()
+		createdAt := created.CreatedAt.Unix()
 		s.NoError(err)
 
 		mockClock.Add(time.Minute)
 
 		duplicate, err2 := s.store.CreateCedarSystemBookmark(ctx, &bookmark)
 		s.NoError(err2)
-		s.Greater((*duplicate.CreatedAt).Unix(), createdAt)
+		s.Greater(duplicate.CreatedAt.Unix(), createdAt)
+	})
+}
+
+func (s *StoreTestSuite) TestFetchCedarSystemIsBookmarkedLOADER() {
+	ctx := context.Background()
+
+	cedarSystemID := uuid.New().String()
+	s.NotEmpty(cedarSystemID)
+
+	otherID := uuid.New().String()
+	s.NotEmpty(otherID)
+
+	s.Run("saves and retrieves a bookmark", func() {
+		_, err := s.store.CreateCedarSystemBookmark(ctx, &models.CedarSystemBookmark{
+			CedarSystemID: cedarSystemID,
+		})
+		s.NoError(err)
+
+		m := map[string]any{
+			"cedar_system_id": cedarSystemID,
+			"eua_user_id":     appcontext.Principal(ctx).ID(),
+		}
+
+		b, err := json.Marshal(m)
+		s.NoError(err)
+
+		bookmarkMap, err := s.store.FetchCedarSystemIsBookmarkedLOADER(ctx, fmt.Sprintf("[%s]", string(b)))
+		s.NoError(err)
+		s.NotEmpty(bookmarkMap)
+
+		found := false
+		for k := range bookmarkMap {
+			if k == cedarSystemID {
+				found = true
+			}
+		}
+		s.True(found)
 	})
 }
