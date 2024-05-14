639cee08aa6fe388f870b7483a230c8713bfd008
samoddball <sam.lawson@oddball.io>
2024-05-14 15:42:30 -0700

    only get OPEN requests

--- pkg/sqlqueries/SQL/cedar_linked_requests/select.sql
+++ pkg/sqlqueries/SQL/cedar_linked_requests/select.sql
@@ -1,3 +1,33 @@
 SELECT
-	(SELECT SUM(CASE WHEN trs.system_id = $1 THEN 1 ELSE 0 END) FROM trb_request_systems trs) AS trb_count,
-	(SELECT SUM(CASE WHEN sis.system_id = $1 THEN 1 ELSE 0 END) FROM system_intake_systems sis) AS intake_count;
+	(
+		SELECT
+			COALESCE(
+				SUM(
+					CASE
+						WHEN trs.system_id = $1
+						AND tr.state = 'OPEN' THEN 1
+						ELSE 0
+					END
+				),
+				0
+			)
+		FROM
+			trb_request_systems trs
+			LEFT JOIN trb_request tr ON trs.system_id = tr.id :: TEXT
+	) AS trb_count,
+	(
+		SELECT
+			COALESCE(
+				SUM(
+					CASE
+						WHEN sis.system_id = $1
+						AND si.state = 'OPEN' THEN 1
+						ELSE 0
+					END
+				),
+				0
+			)
+		FROM
+			system_intake_systems sis
+			LEFT JOIN system_intakes si ON sis.system_id = si.id :: TEXT
+	) AS intake_count;
