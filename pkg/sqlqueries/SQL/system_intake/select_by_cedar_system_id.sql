275d2ecd0e3a4812745372b61b241400f0127d27
samoddball <sam.lawson@oddball.io>
2024-05-15 15:21:43 -0700

    add 'OPEN' to sql statements, write one of two tests

--- pkg/sqlqueries/SQL/system_intake/select_by_cedar_system_id.sql
+++ pkg/sqlqueries/SQL/system_intake/select_by_cedar_system_id.sql
@@ -4,4 +4,5 @@ FROM
     system_intakes si
     LEFT JOIN system_intake_systems sis ON sis.system_intake_id = si.id
 WHERE
-    sis.system_id = $1;
+    sis.system_id = $1
+    AND si.state = 'OPEN';
--- pkg/sqlqueries/SQL/trb_request/select_by_cedar_system_id.sql
+++ pkg/sqlqueries/SQL/trb_request/select_by_cedar_system_id.sql
@@ -4,4 +4,5 @@ FROM
     trb_request tr
     LEFT JOIN trb_request_systems trs ON trs.trb_request_id = tr.id
 WHERE
-    trs.system_id = $1;
+    trs.system_id = $1
+    AND tr.state = 'OPEN';
