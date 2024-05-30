3c31e1b82742c76bb0c58062e5f6381908198d4d
samoddball <sam.lawson@oddball.io>
2024-05-30 07:38:30 -0700

    working on middleware

---
+++ pkg/dataloaders2/middleware.go
@@ -0,0 +1,21 @@
+package dataloaders2
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func dataloaderMiddleware(loaders *DataLoaders, next http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx := CTXWithLoaders(r.Context(), loaders)
+		r = r.WithContext(ctx)
+		next.ServeHTTP(w, r)
+	}
+}
+
+func NewDataloaderMiddleware(loaders *DataLoaders) mux.MiddlewareFunc {
+	return func(next http.Handler) http.Handler {
+		return dataloaderMiddleware(loaders, next)
+	}
+}
