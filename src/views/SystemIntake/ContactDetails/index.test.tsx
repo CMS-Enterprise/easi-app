dbe6c61bcc3bcdb3b031a5d82f49c7e77b7cebb0
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2024-06-03 15:20:21 -0500

    Fix SystemIntake fragment returning empty object

--- src/views/SystemIntake/ContactDetails/index.test.tsx
+++ src/views/SystemIntake/ContactDetails/index.test.tsx
@@ -45,6 +45,7 @@ describe('System intake form - Contact Details', () => {
   it('renders fields for new request', async () => {
     render(
       <VerboseMockedProvider
+        addTypename
         mocks={[
           getSystemIntakeQuery(emptySystemIntake),
           getSystemIntakeContactsQuery
@@ -69,6 +70,7 @@ describe('System intake form - Contact Details', () => {
   it('renders error messages', async () => {
     render(
       <VerboseMockedProvider
+        addTypename
         mocks={[
           getSystemIntakeQuery(emptySystemIntake),
           getSystemIntakeContactsQuery
