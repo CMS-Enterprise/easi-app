233aee469b4387f2b1e9958f900f6d7944bea725
samoddball <sam.lawson@oddball.io>
2024-06-04 08:13:06 -0700

    (borked for now) branch hop

--- pkg/sqlqueries/SQL/cedar_bookmarked_systems/select_LOADER.sql
+++ pkg/sqlqueries/SQL/cedar_bookmarked_systems/select_LOADER.sql
@@ -1,3 +1,2 @@
-SELECT cedar_system_id
-FROM cedar_system_bookmarks
-WHERE (cedar_system_id, eua_user_id) = UNNEST(:bookmark_requests);
+SELECT cedar_system_id, eua_user_id, created_at
+FROM cedar_system_bookmarks WHERE ;
