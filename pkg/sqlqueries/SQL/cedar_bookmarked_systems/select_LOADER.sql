ce742a2ddbc903a73963a0ea8671b1758029e849
samoddball <sam.lawson@oddball.io>
2024-06-04 11:43:21 -0700

    spacing - still testing this statement

--- pkg/sqlqueries/SQL/cedar_bookmarked_systems/select_LOADER.sql
+++ pkg/sqlqueries/SQL/cedar_bookmarked_systems/select_LOADER.sql
@@ -1,3 +1,3 @@
 SELECT eua_user_id, cedar_system_id
 FROM cedar_system_bookmarks
-WHERE (eua_user_id, cedar_system_id) = ANY (SELECT UNNEST($1::TEXT[]), UNNEST($2::TEXT[]));
+WHERE (eua_user_id, cedar_system_id) = ANY(SELECT UNNEST($1::TEXT[]), UNNEST($2::TEXT[]));
