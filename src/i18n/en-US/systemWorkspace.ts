0524ebfbc0caa0a5cec4afd9a6d4293de9c37d12
adamodd <adam.suharja@oddball.io>
2024-06-03 20:56:41 -0700

    Leaving link modal

--- src/i18n/en-US/systemWorkspace.ts
+++ src/i18n/en-US/systemWorkspace.ts
@@ -75,7 +75,14 @@ const systemWorkspace = {
       noIsso: 'This system has no ISSO',
       contact: 'Contact ISSO',
       cfacts: 'Go to CFACTs',
-      learn: 'Learn more'
+      learn: 'Learn more',
+      modal: {
+        header: 'Are you sure you want to leave EASi?',
+        text:
+          'CFACTS requires additional job codes and/or permissions as well as connectivity via VPN in order to access content within the system.',
+        continue: 'Continue to CFACTS',
+        cancel: 'Go back to EASi'
+      }
     }
   }
 };
--- src/views/SystemWorkspace/components/AtoCard/index.tsx
+++ src/views/SystemWorkspace/components/AtoCard/index.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import {
+  Button,
+  ButtonGroup,
   Card,
   CardBody,
   CardFooter,
@@ -11,6 +13,7 @@ import {
 } from '@trussworks/react-uswds';
 import classnames from 'classnames';
 
+import Modal from 'components/Modal';
 import AtoStatusTag from 'components/shared/AtoStatusTag';
 import Divider from 'components/shared/Divider';
 import { ATO_LEARN_MORE, CFACTS } from 'constants/externalUrls';
@@ -36,71 +39,106 @@ function AtoCard({
 }: Props) {
   const { t } = useTranslation('systemWorkspace');
 
+  // Assigned url is also purposed for modal open vs false to close
+  const [leavingModalUrl, setLeavingModalUrl] = useState<string | false>(false);
+
   return (
-    <Grid
-      tablet={{ col: 12 }}
-      desktop={{ col: 6 }}
-      className="display-flex flex-align-stretch"
-    >
-      <Card
-        className={classnames(
-          className,
-          'radius-sm width-full workspaces__card'
-        )}
+    <>
+      <Grid
+        tablet={{ col: 12 }}
+        desktop={{ col: 6 }}
+        className="display-flex flex-align-stretch"
       >
-        <CardHeader className="text-bold padding-0 line-height-serif-2 margin-bottom-1 text-body-lg">
-          {t('spaces.ato.header')}
-        </CardHeader>
+        <Card
+          className={classnames(
+            className,
+            'radius-sm width-full workspaces__card'
+          )}
+        >
+          <CardHeader className="text-bold padding-0 line-height-serif-2 margin-bottom-1 text-body-lg">
+            {t('spaces.ato.header')}
+          </CardHeader>
 
-        <CardBody className="padding-0 flex-1 workspaces__fill-card-space">
-          <p className="text-base margin-o">{t('spaces.ato.description')}</p>
-          <div className="display-flex">
-            <AtoStatusTag
-              status={status}
-              className="display-flex flex-align-center margin-right-1"
-            />
-            {dateAuthorizationMemoExpires && (
-              <span className="display-flex flex-align-center text-base">
-                {status === 'Expired' ? 'Expired' : 'Expires'}{' '}
-                {formatDateUtc(dateAuthorizationMemoExpires, 'MM/dd/yyyy')}
-              </span>
-            )}
-          </div>
-          <p>
-            <strong>{t('spaces.ato.isso')}</strong>
-            <br />
-            {showVal(isso, {
-              format: v => getPersonFullName(v),
-              defaultVal: t('spaces.ato.noIsso')
-            })}
-          </p>
-        </CardBody>
+          <CardBody className="padding-0 flex-1 workspaces__fill-card-space">
+            <p className="text-base margin-o">{t('spaces.ato.description')}</p>
+            <div className="display-flex">
+              <AtoStatusTag
+                status={status}
+                className="display-flex flex-align-center margin-right-1"
+              />
+              {dateAuthorizationMemoExpires && (
+                <span className="display-flex flex-align-center text-base">
+                  {status === 'Expired' ? 'Expired' : 'Expires'}{' '}
+                  {formatDateUtc(dateAuthorizationMemoExpires, 'MM/dd/yyyy')}
+                </span>
+              )}
+            </div>
+            <p>
+              <strong>{t('spaces.ato.isso')}</strong>
+              <br />
+              {showVal(isso, {
+                format: v => getPersonFullName(v),
+                defaultVal: t('spaces.ato.noIsso')
+              })}
+            </p>
+          </CardBody>
 
-        <Divider className="margin-y-2" />
+          <Divider className="margin-y-2" />
 
-        <CardFooter className="padding-0 margin-bottom-05 display-flex">
-          <Link
-            className={classnames('usa-button', {
-              'usa-button--disabled': !isso?.assigneeEmail
-            })}
-            href={`mailto:${isso?.assigneeEmail}`}
+          <CardFooter className="padding-0 margin-bottom-05 display-flex">
+            <Link
+              className={classnames('usa-button', {
+                'usa-button--disabled': !isso?.assigneeEmail
+              })}
+              href={`mailto:${isso?.assigneeEmail}`}
+            >
+              {t('spaces.ato.contact')}
+            </Link>
+            <Link
+              className="usa-button usa-button--outline"
+              href={CFACTS}
+              onClick={e => {
+                e.preventDefault();
+                setLeavingModalUrl((e.target as HTMLAnchorElement).href);
+              }}
+            >
+              {t('spaces.ato.cfacts')}
+            </Link>
+            <Link
+              variant="unstyled"
+              href={ATO_LEARN_MORE}
+              className="margin-left-1 display-flex flex-align-center text-primary"
+            >
+              {t('spaces.ato.learn')}
+              <IconLaunch className="margin-left-1" />
+            </Link>
+          </CardFooter>
+        </Card>
+      </Grid>
+      {/* Leaving easi modal */}
+      <Modal
+        isOpen={!!leavingModalUrl}
+        closeModal={() => setLeavingModalUrl(false)}
+      >
+        <h2 className="usa-modal__heading margin-bottom-2">
+          {t('spaces.ato.modal.header')}
+        </h2>
+        <p className="margin-y-0">{t('spaces.ato.modal.text')}</p>
+        <ButtonGroup className="margin-top-3">
+          <Link className="usa-button" href={CFACTS}>
+            {t('spaces.ato.modal.continue')}
+          </Link>
+          <Button
+            type="button"
+            unstyled
+            className="margin-left-1"
+            onClick={() => setLeavingModalUrl(false)}
           >
-            {t('spaces.ato.contact')}
-          </Link>
-          <Link className="usa-button usa-button--outline" href={CFACTS}>
-            {t('spaces.ato.cfacts')}
-          </Link>
-          <Link
-            variant="unstyled"
-            href={ATO_LEARN_MORE}
-            className="margin-left-1 display-flex flex-align-center text-primary"
-          >
-            {t('spaces.ato.learn')}
-            <IconLaunch className="margin-left-1" />
-          </Link>
-        </CardFooter>
-      </Card>
-    </Grid>
+            {t('spaces.ato.modal.cancel')}
+          </Button>
+        </ButtonGroup>
+      </Modal>
+    </>
   );
 }
 
