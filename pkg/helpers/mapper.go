89a44ece31a548e9d2cc15af950e180f41414a04
ClayBenson94 <clay.benson@oddball.io>
2024-06-13 09:05:45 -0400

    Modify OneToMany / GetMappingID to use generics. Add some comments to OneToMany

--- pkg/helpers/mapper.go
+++ pkg/helpers/mapper.go
@@ -1,30 +1,34 @@
 package helpers
 
-import (
-	"github.com/google/uuid"
-)
-
 // GetMappingID can be implemented so that it can use OneToMany below to map a flat list of values
 // to a flat list of their relative keys
-type GetMappingID interface {
-	GetMappingID() uuid.UUID
+type GetMappingID[keyT comparable] interface {
+	GetMappingID() keyT
 }
 
 // OneToMany takes a list of keys and a list of values which map one-to-many (key-to-value)
 // ex: vals could be a list of contract numbers where more than one value has the same mapped ID
-func OneToMany[valT GetMappingID](keys []uuid.UUID, vals []valT) [][]valT {
-	store := map[uuid.UUID][]valT{}
+func OneToMany[valT GetMappingID[keyT], keyT comparable](keys []keyT, vals []valT) [][]valT {
+	// create a map to store values grouped by key (of type keyT)
+	// each key will map to a slice of values (of type valT)
+	store := map[keyT][]valT{}
 
-	// populate map
+	// populate the map with empty slices for each key
 	for _, key := range keys {
 		store[key] = []valT{}
 	}
 
+	// iterate over each value in the flat slice and append it to the correct key in the map,
+	// based on the value's GetMappingID() method
 	for _, val := range vals {
 		id := val.GetMappingID()
 		store[id] = append(store[id], val)
 	}
 
+	// now we have a map of keys to slices of values, but we want to convert that to
+	// a 2D slice of values, where each slice is a list of values that share the same key
+	//
+	// to do this, we iterate over the keys slice and append the corresponding value slice from the map
 	var out [][]valT
 	for _, key := range keys {
 		out = append(out, store[key])
