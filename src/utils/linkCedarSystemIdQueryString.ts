38e9f5d176d9eee43463135552ced60471b1745a
adamodd <adam.suharja@oddball.io>
2024-06-25 13:23:39 -0700

    `useLinkCedarSystemIdQueryParam()`

--- src/utils/linkCedarSystemIdQueryString.ts
+++ src/utils/linkCedarSystemIdQueryString.ts
@@ -1,6 +1,13 @@
+import { StringParam, useQueryParam } from 'use-query-params';
+
 export default function linkCedarSystemIdQueryString(
   cedarSystemId: string | null | undefined
 ): string | undefined {
   if (!cedarSystemId) return undefined;
   return `linkCedarSystemId=${encodeURIComponent(cedarSystemId)}`;
+}
+
+export function useLinkCedarSystemIdQueryParam() {
+  const [linkCedarSystemId] = useQueryParam('linkCedarSystemId', StringParam);
+  return linkCedarSystemId;
 }
