8e4df3d106296f569835d2335446274bcc3c470c
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2024-07-02 14:59:55 -0500

    useAuth hook to check job codes / roles

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,35 +1,123 @@
-import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useLazyQuery } from '@apollo/client';
+import { useFlags } from 'launchdarkly-react-client-sdk';
 
-const useAuth = (auth: any): [boolean, any, () => Promise<void>] => {
-  const [authenticated, setAuthenticated] = useState(false);
-  const [user, setUser] = useState(null);
+import {
+  BASIC_USER_PROD,
+  GOVTEAM_DEV,
+  GOVTEAM_PROD,
+  TRB_ADMIN_DEV,
+  TRB_ADMIN_PROD
+} from 'constants/jobCodes';
+import GetSystemIntakeGrbReviewersQuery from 'queries/GetSystemIntakeGrbReviewersQuery';
+import {
+  GetSystemIntakeGrbReviewers,
+  GetSystemIntakeGrbReviewersVariables
+} from 'queries/types/GetSystemIntakeGrbReviewers';
+import { AppState } from 'reducers/rootReducer';
 
-  const handleLogout = async () => {
-    const isAuthenticated = await auth.isAuthenticated();
-    if (isAuthenticated) {
-      auth.logout('/');
-    } else {
-      document.location.href = '/';
+type UseAuthReturn = {
+  /**
+   * Object with helper functions to check job codes / roles
+   *
+   * Returns undefined if user is not set
+   */
+  user:
+    | {
+        isTrbAdmin: () => boolean;
+        isGrtReviewer: () => boolean;
+        isBasicUser: () => boolean;
+        isGrbReviewer: (systemIntakeId: string) => boolean;
+      }
+    | undefined;
+  /** Returns true if function with query is called and loading */
+  loading: boolean;
+};
+
+/** Hook that returns functions to check for roles / job codes */
+const useAuth = (): UseAuthReturn => {
+  const auth = useSelector((state: AppState) => state.auth);
+  const { groups, euaId } = auth;
+
+  const flags = useFlags();
+
+  const [getGrbReviewers, { data, called, loading }] = useLazyQuery<
+    GetSystemIntakeGrbReviewers,
+    GetSystemIntakeGrbReviewersVariables
+  >(GetSystemIntakeGrbReviewersQuery);
+
+  const isUserSet = auth.isUserSet && !loading;
+
+  /** Check if user is TRB admin */
+  const isTrbAdmin = (): boolean => {
+    if (flags.downgradeTrbAdmin) {
+      return false;
     }
+
+    if (groups.includes(TRB_ADMIN_DEV) || groups.includes(TRB_ADMIN_PROD)) {
+      return true;
+    }
+
+    return false;
   };
 
-  useEffect(() => {
-    auth.isAuthenticated().then((isAuthenticated: any) => {
-      if (isAuthenticated !== authenticated) {
-        setAuthenticated(isAuthenticated);
-      }
-    });
-  });
-
-  useEffect(() => {
-    if (authenticated) {
-      auth.getUser().then(setUser);
-    } else {
-      setUser(null);
+  /** Check if user is GrtReviewer */
+  const isGrtReviewer = (): boolean => {
+    if (flags.downgradeGovTeam) {
+      return false;
     }
-  }, [auth, authenticated]);
 
-  return [authenticated, user, handleLogout];
+    if (groups.includes(GOVTEAM_DEV) || groups.includes(GOVTEAM_PROD)) {
+      return true;
+    }
+
+    return false;
+  };
+
+  /** Check if basic user */
+  const isBasicUser = (): boolean => {
+    if (groups.includes(BASIC_USER_PROD)) {
+      return true;
+    }
+    if (groups.length === 0) {
+      return true;
+    }
+    if (!isGrtReviewer() && !isTrbAdmin()) {
+      return true;
+    }
+    return false;
+  };
+
+  /** Check if user is set as system intake GRB reviewer */
+  const isGrbReviewer = (systemIntakeId: string) => {
+    if (!called) {
+      getGrbReviewers({
+        variables: { id: systemIntakeId }
+      });
+    }
+
+    const grbReviewers = data?.systemIntake?.grbReviewers;
+
+    if (!grbReviewers || grbReviewers.length === 0) {
+      return false;
+    }
+
+    return grbReviewers.some(
+      ({ userAccount }) => userAccount.username === euaId
+    );
+  };
+
+  const user: UseAuthReturn['user'] = {
+    isTrbAdmin,
+    isGrtReviewer,
+    isBasicUser,
+    isGrbReviewer
+  };
+
+  return {
+    user: isUserSet || !loading ? user : undefined,
+    loading
+  };
 };
 
 export default useAuth;
