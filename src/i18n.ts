d90f26d5ff4504d1df6dcd728fac11010ba8c9ac
Patrick Segura <patrick.segura@oddball.io>
2024-07-03 09:54:58 -0500

    Added escapeValue false for i18n

--- src/i18n.ts
+++ src/i18n.ts
@@ -11,7 +11,10 @@ const resources = {
 i18n.use(LanguageDetector).use(initReactI18next).init({
   lng: 'en-US',
   fallbackLng: 'en-US',
-  resources
+  resources,
+  interpolation: {
+    escapeValue: false // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
+  }
 });
 
 export default i18n;
