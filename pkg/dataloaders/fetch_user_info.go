603dd89cd8fe728ec05ba0afcdc98a0d271acc03
Lee Warrick <lee.warrick@oddball.io>
2024-07-12 17:30:00 -0400

    add dataloader to trb request form resolver

--- pkg/dataloaders/fetch_user_info.go
+++ pkg/dataloaders/fetch_user_info.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 
+	"github.com/cms-enterprise/easi-app/pkg/helpers"
 	"github.com/cms-enterprise/easi-app/pkg/models"
 )
 
@@ -12,19 +13,7 @@ func (d *dataReader) fetchUserInfosByEUAUserIDs(ctx context.Context, euaUserIDs 
 	if err != nil {
 		return nil, []error{err}
 	}
-
-	store := map[string]*models.UserInfo{}
-
-	for _, info := range data {
-		store[info.Username] = info
-	}
-
-	var out []*models.UserInfo
-	for _, id := range euaUserIDs {
-		out = append(out, store[id])
-	}
-
-	return out, nil
+	return helpers.OneToOne(euaUserIDs, data), nil
 }
 
 func FetchUserInfoByEUAUserID(ctx context.Context, euaUserID string) (*models.UserInfo, error) {
