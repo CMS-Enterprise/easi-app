877a6dd45e022a9d455b98b80974dc12955dd180
samoddball <156127704+samoddball@users.noreply.github.com>
2024-07-17 09:04:30 -0700

    [EASI-4486] system intake doc uploads (#2691)
    * (no verify) fresh branch with only system intake(no verify) fresh branch with only system intake(no verify) fresh branch with only system intake(no verify) fresh branch with only system intake(no verify) fresh branch with only system intake(no verify) fresh branch with only system intake(no verify) fresh branch with only system intake(no verify) fresh branch with only system intake(no verify) fresh branch with only system intake
    
    * (no-verify) update comment
    
    * finish up auth functions
    
    * update test
    
    * remove double intellisense
    
    * fix bug in canCreate
    
    * give test user a role
    
    * use const for role in test
    
    * check err no rows
    
    * naming
    
    * all three validators share similar input signature and same return signature

---
+++ pkg/sqlqueries/SQL/system_intake_document/delete.sql
@@ -0,0 +1,16 @@
+DELETE
+FROM system_intake_documents
+WHERE id = :id
+RETURNING
+  id,
+  system_intake_id,
+  file_name,
+  document_type,
+  other_type,
+  bucket,
+  s3_key,
+  created_by,
+  created_at,
+  modified_by,
+  modified_at,
+  uploader_role;
