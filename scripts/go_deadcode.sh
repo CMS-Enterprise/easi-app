59abc250faf24f8be20c3a15facd55bde92edaf3
ClayBenson94 <clay.benson@oddball.io>
2024-07-24 09:21:56 -0400

    Add script for go deadcode

---
+++ scripts/go_deadcode.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Capture all arguments passed to the script
+# This is mainly useful for passing extra flags to deadcode (-test being a common one)
+args=("$@")
+
+# Define the output from the command
+output=$(deadcode "${args[@]}" -generated ./cmd/... ./pkg/...)
+
+# Define an array of exclusions (RegEx is supported here!)
+exclusions=(
+	# Generated Code
+	"pkg/graph/generated/generated.go" # gqlgen files
+	"/gen/" # Anything in a /gen/ directory (cedar core and cedar intake, namely)
+
+	# Other Exclusions
+	"pkg/models/gql_scalars.go" # GQL Scalars are called by reflection, and don't get detected properly
+)
+
+# Function to build the grep pattern
+build_grep_pattern() {
+    local pattern=""
+    for exclusion in "${exclusions[@]}"; do
+        if [ -z "$pattern" ]; then
+            pattern="$exclusion"
+        else
+            pattern="$pattern|$exclusion"
+        fi
+    done
+    echo "$pattern"
+}
+
+# Build the grep pattern
+pattern=$(build_grep_pattern)
+
+# Exclude lines containing any of the exclusions
+if [ -n "$pattern" ]; then
+    echo "$output" | grep -Ev "$pattern"
+else
+    echo "$output"
+fi
