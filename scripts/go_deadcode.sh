a81044d3fd221937e3b080f360ffb0bab6fab4b6
Clay Benson <clay.benson@oddball.io>
2024-07-25 10:34:58 -0400

    Noref/fix deadcode (#2719)
    * Ignore things in ./cmd directory
    
    * Remove reference to easiencoding.DecodeBase64String
    
    * Remove package handlers' backfill-related stuff
    
    * Remove unused cedar_easi.go - this appeared to be some kind of mock client for the CEDAR Intake functionality, but wasnt used anywhere, and has also been replaced by just setting the CEDAR_INTAKE_ENABLED=false env var
    
    * Remove pkg/services/backfill.go - the handler was removed in cee4a78a69fb9934a37cee5e051e730372bff4a2, but this service function/file wasn't removed until this commit
    
    * Remove appcontext Logger function (since it was deprecated a long time ago in favor of ZLogger)
    
    * Clarify comment in go_deadcode.sh
    
    * Clean up unused methods in pkg validate
    
    * Indent script using tabs

--- scripts/go_deadcode.sh
+++ scripts/go_deadcode.sh
@@ -16,6 +16,11 @@ exclusions=(
 	"pkg/graph/generated/generated.go" # gqlgen files
 	"/gen/" # Anything in a /gen/ directory (cedar core and cedar intake, namely)
 
+	# Anything in the ./cmd/ directory generally shouldn't be considered dead code, it's usually
+	# just scripts or one-off commands
+	# NOTE: We need to keep the ./cmd/ directory in the deadcode check, as it's where the main entrypoint is for the rest of the code in ./pkg/
+	"^cmd/.*"
+
 	# Other Exclusions
 	"pkg/models/gql_scalars.go" # GQL Scalars are called by reflection, and don't get detected properly
 	"pkg/testhelpers/" # Anything in the testhelpers directory is likely to only be used by tests, and can be ignored
@@ -23,15 +28,15 @@ exclusions=(
 
 # Function to build the grep pattern
 build_grep_pattern() {
-    local pattern=""
-    for exclusion in "${exclusions[@]}"; do
-        if [ -z "$pattern" ]; then
-            pattern="$exclusion"
-        else
-            pattern="$pattern|$exclusion"
-        fi
-    done
-    echo "$pattern"
+	local pattern=""
+	for exclusion in "${exclusions[@]}"; do
+		if [ -z "$pattern" ]; then
+			pattern="$exclusion"
+		else
+			pattern="$pattern|$exclusion"
+		fi
+	done
+	echo "$pattern"
 }
 
 # Build the grep pattern
@@ -39,7 +44,7 @@ pattern=$(build_grep_pattern)
 
 # Exclude lines containing any of the exclusions
 if [ -n "$pattern" ]; then
-    echo "$output" | grep -Ev "$pattern"
+	echo "$output" | grep -Ev "$pattern"
 else
-    echo "$output"
+	echo "$output"
 fi
