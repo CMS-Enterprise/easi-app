5eb6b746a56ccaaf7b13dd7e8db4f531a73b54a2
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2024-08-05 15:07:13 -0500

    [EASI-4555] GRB participants table (#2737)
    * Add fields to query
    
    * Participants table
    
    * Add action buttons column to table
    
    * Unit tests
    
    * Unit test fix
    
    * Empty table state
    
    * edit/remove button functionality
    
    * Fix duplicate reviewer field validation bug
    
    Validation should only check for duplicate reviewers if form is adding new reviewer - not when editing existing reviewer.
    
    * Unit test fixes

--- src/views/GovernanceReviewTeam/GRBReview/index.tsx
+++ src/views/GovernanceReviewTeam/GRBReview/index.tsx
@@ -1,12 +1,14 @@
-import React, { useContext, useState } from 'react';
+import React, { useCallback, useContext, useMemo, useState } from 'react';
 import { Trans, useTranslation } from 'react-i18next';
 import { useHistory, useLocation, useParams } from 'react-router-dom';
+import { Column, useSortBy, useTable } from 'react-table';
 import { useMutation } from '@apollo/client';
 import {
   Button,
   ButtonGroup,
   ModalFooter,
-  ModalHeading
+  ModalHeading,
+  Table
 } from '@trussworks/react-uswds';
 
 import UswdsReactLink from 'components/LinkWrapper';
@@ -24,6 +26,11 @@ import {
 } from 'queries/types/DeleteSystemIntakeGRBReviewer';
 import { SystemIntakeGRBReviewer } from 'queries/types/SystemIntakeGRBReviewer';
 import { SystemIntakeState } from 'types/graphql-global-types';
+import {
+  currentTableSortDescription,
+  getColumnSortStatus,
+  getHeaderSortIcon
+} from 'utils/tableSort';
 
 import IsGrbViewContext from '../IsGrbViewContext';
 import { ReviewerKey } from '../subNavItems';
@@ -69,29 +76,105 @@ const GRBReview = ({ id, state, grbReviewers }: GRBReviewProps) => {
     ]
   });
 
-  const removeGRBReviewer = (reviewer: SystemIntakeGRBReviewer) => {
-    mutate({ variables: { input: { reviewerID: reviewer.id } } })
-      .then(() =>
-        showMessage(
-          <Trans
-            i18nKey="grbReview:removeSuccess"
-            values={{ commonName: reviewer.userAccount.commonName }}
-          >
-            success
-          </Trans>,
-          { type: 'success' }
+  const removeGRBReviewer = useCallback(
+    (reviewer: SystemIntakeGRBReviewer) => {
+      mutate({ variables: { input: { reviewerID: reviewer.id } } })
+        .then(() =>
+          showMessage(
+            <Trans
+              i18nKey="grbReview:removeSuccess"
+              values={{ commonName: reviewer.userAccount.commonName }}
+            >
+              success
+            </Trans>,
+            { type: 'success' }
+          )
         )
-      )
-      .catch(() => showMessage(t('removeError'), { type: 'error' }));
+        .catch(() => showMessage(t('removeError'), { type: 'error' }));
 
-    // Reset `reviewerToRemove` to close modal
-    setReviewerToRemove(null);
+      // Reset `reviewerToRemove` to close modal
+      setReviewerToRemove(null);
 
-    // If removing reviewer from form, go to GRB Review page
-    if (isForm) {
-      history.push(`/${reviewerType}/${id}/grb-review`);
-    }
-  };
+      // If removing reviewer from form, go to GRB Review page
+      if (isForm) {
+        history.push(`/${reviewerType}/${id}/grb-review`);
+      }
+    },
+    [history, isForm, id, mutate, reviewerType, showMessage, t]
+  );
+
+  const columns = useMemo<Column<SystemIntakeGRBReviewer>[]>(() => {
+    /** Column with action buttons to display for GRT admins */
+    const actionColumn: Column<SystemIntakeGRBReviewer> = {
+      Header: t<string>('participantsTable.actions'),
+      Cell: ({
+        row: { original: reviewer }
+      }: {
+        row: { original: SystemIntakeGRBReviewer };
+      }) => {
+        return (
+          <ButtonGroup data-testid="grbReviewerActions">
+            <Button
+              type="button"
+              onClick={() => history.push(`${pathname}/edit`, reviewer)}
+              unstyled
+            >
+              {t('Edit')}
+            </Button>
+            <Button
+              type="button"
+              onClick={() => setReviewerToRemove(reviewer)}
+              className="text-error"
+              unstyled
+            >
+              {t('Remove')}
+            </Button>
+          </ButtonGroup>
+        );
+      }
+    };
+
+    return [
+      {
+        Header: t<string>('participantsTable.name'),
+        id: 'commonName',
+        accessor: ({ userAccount }) => userAccount.commonName
+      },
+      {
+        Header: t<string>('participantsTable.votingRole'),
+        accessor: 'votingRole',
+        Cell: ({ value }) => t<string>(`votingRoles.${value}`)
+      },
+      {
+        Header: t<string>('participantsTable.grbRole'),
+        accessor: 'grbRole',
+        Cell: ({ value }) => t<string>(`reviewerRoles.${value}`)
+      },
+      // Only display action column if user is GRT admin
+      ...(isGrbView ? [] : [actionColumn])
+    ];
+  }, [t, isGrbView, setReviewerToRemove, history, pathname]);
+
+  const table = useTable(
+    {
+      columns,
+      data: grbReviewers,
+      autoResetSortBy: false,
+      autoResetPage: true,
+      initialState: {
+        sortBy: useMemo(() => [{ id: 'commonName', desc: false }], [])
+      }
+    },
+    useSortBy
+  );
+
+  const {
+    getTableBodyProps,
+    getTableProps,
+    headerGroups,
+    prepareRow,
+    rows
+  } = table;
 
   return (
     <>
@@ -133,11 +216,12 @@ const GRBReview = ({ id, state, grbReviewers }: GRBReviewProps) => {
           grbReviewers={grbReviewers}
         />
       ) : (
-        <>
+        <div className="padding-bottom-4">
           <PageHeading className="margin-y-0">{t('title')}</PageHeading>
           <p className="font-body-md line-height-body-4 text-light margin-top-05 margin-bottom-3">
             {t('description')}
           </p>
+
           {/* Feature in progress alert */}
           <Alert
             type="info"
@@ -199,6 +283,7 @@ const GRBReview = ({ id, state, grbReviewers }: GRBReviewProps) => {
                     : 'addGrbReviewer'
                 )}
               </Button>
+
               {state === SystemIntakeState.CLOSED && (
                 <p className="desktop:margin-y-0 desktop:margin-left-1">
                   <Trans
@@ -217,42 +302,76 @@ const GRBReview = ({ id, state, grbReviewers }: GRBReviewProps) => {
               )}
             </div>
           )}
-          {/**
-           * TODO EASI-4350: add participants table
-           *
-           * This list of reviewers is temporary for testing purposes
-           */}
-          <ul>
-            {grbReviewers.map(reviewer => {
-              return (
-                <li key={reviewer.id}>
-                  <span>
-                    {reviewer.userAccount.commonName} (
-                    {t(`votingRoles.${reviewer.votingRole}`)}) -{' '}
-                    {t(`reviewerRoles.${reviewer.grbRole}`)}
-                  </span>
-                  <ButtonGroup>
-                    <Button
-                      type="button"
-                      onClick={() => history.push(`${pathname}/edit`, reviewer)}
-                      unstyled
+
+          <div className="margin-top-3">
+            <Table bordered={false} fullWidth scrollable {...getTableProps()}>
+              <thead>
+                {headerGroups.map(headerGroup => (
+                  <tr {...headerGroup.getHeaderGroupProps()}>
+                    {headerGroup.headers.map((column, index) => (
+                      <th
+                        {...column.getHeaderProps(
+                          column.getSortByToggleProps()
+                        )}
+                        aria-sort={getColumnSortStatus(column)}
+                        scope="col"
+                        className="border-bottom-2px"
+                      >
+                        <Button
+                          type="button"
+                          unstyled
+                          className="width-full display-flex"
+                          {...column.getSortByToggleProps()}
+                        >
+                          <div className="flex-fill text-no-wrap">
+                            {column.render('Header')}
+                          </div>
+                          <div className="position-relative width-205 margin-left-05">
+                            {getHeaderSortIcon(column)}
+                          </div>
+                        </Button>
+                      </th>
+                    ))}
+                  </tr>
+                ))}
+              </thead>
+              <tbody {...getTableBodyProps()}>
+                {rows.map(row => {
+                  prepareRow(row);
+                  return (
+                    <tr
+                      {...row.getRowProps()}
+                      data-testid={`grbReviewer-${row.original.userAccount.username}`}
                     >
-                      {t('Edit')}
-                    </Button>
-                    <Button
-                      type="button"
-                      onClick={() => setReviewerToRemove(reviewer)}
-                      className="text-error"
-                      unstyled
-                    >
-                      {t('Remove')}
-                    </Button>
-                  </ButtonGroup>
-                </li>
-              );
-            })}
-          </ul>
-        </>
+                      {row.cells.map((cell, index) => {
+                        return (
+                          <td {...cell.getCellProps()}>
+                            {cell.render('Cell')}
+                          </td>
+                        );
+                      })}
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </Table>
+
+            {rows.length === 0 && (
+              <p className="text-italic margin-top-neg-1">
+                {t('participantsTable.noReviewers')}
+              </p>
+            )}
+          </div>
+
+          {rows.length > 0 && (
+            <p
+              className="usa-sr-only usa-table__announcement-region"
+              aria-live="polite"
+            >
+              {currentTableSortDescription(headerGroups[0])}
+            </p>
+          )}
+        </div>
       )}
     </>
   );
