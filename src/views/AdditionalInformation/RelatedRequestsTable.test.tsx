2e1269ff05cdb9e5c56b0630afdc55caaa71f02e
samoddball <sam.lawson@oddball.io>
2024-08-06 10:28:49 -0700

    tests run!

--- src/views/AdditionalInformation/RelatedRequestsTable.test.tsx
+++ src/views/AdditionalInformation/RelatedRequestsTable.test.tsx
@@ -3,29 +3,41 @@ import { MemoryRouter } from 'react-router-dom';
 import { MockedProvider } from '@apollo/client/testing';
 import { render, screen } from '@testing-library/react';
 
-import {
-  getSystemIntakeRelatedRequests,
-  systemIntake
-} from 'data/mock/systemIntake';
-import { MessageProvider } from 'hooks/useMessage';
-import VerboseMockedProvider from 'utils/testing/VerboseMockedProvider';
+import { systemIntake } from 'data/mock/systemIntake';
+import GetSystemIntakeRelatedRequestsQuery from 'queries/GetSystemIntakeRelatedRequestsQuery';
 
 import RelatedRequestsTable from './RelatedRequestsTable';
 
 describe('Related Requests table', () => {
   it('renders Related Requests table', async () => {
+    const mocks = [
+      {
+        request: {
+          query: GetSystemIntakeRelatedRequestsQuery,
+          variables: {
+            systemIntakeID: systemIntake.id
+          }
+        },
+        result: {
+          data: {
+            systemIntake: {
+              __typename: 'SystemIntake',
+              id: systemIntake.id,
+              relatedIntakes: [],
+              relatedTRBRequests: []
+            }
+          }
+        }
+      }
+    ];
     render(
       <MemoryRouter>
-        <MockedProvider mocks={[getSystemIntakeRelatedRequests]}>
-          <VerboseMockedProvider>
-            <MessageProvider>
-              <RelatedRequestsTable requestID={systemIntake.id} />
-            </MessageProvider>
-          </VerboseMockedProvider>
+        <MockedProvider mocks={mocks} addTypename={false}>
+          <RelatedRequestsTable requestID={systemIntake.id} />
         </MockedProvider>
       </MemoryRouter>
     );
 
-    // expect(await screen.findByRole('heading', { name: 'Related requests' }));
+    expect(await screen.findByRole('heading', { name: 'Related requests' }));
   });
 });
