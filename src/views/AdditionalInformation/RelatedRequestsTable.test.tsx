1961eb36a4069efcba149797d70f27cf5bee7052
samoddball <sam.lawson@oddball.io>
2024-08-08 15:08:10 -0700

    tests for trb table

--- src/views/AdditionalInformation/RelatedRequestsTable.test.tsx
+++ src/views/AdditionalInformation/RelatedRequestsTable.test.tsx
@@ -12,12 +12,14 @@ import {
 } from 'types/graphql-global-types';
 import easiMockStore from 'utils/testing/easiMockStore';
 
+import { trbRequest } from '../../data/mock/trbRequest';
 import { MessageProvider } from '../../hooks/useMessage';
+import GetTRBRequestRelatedRequestsQuery from '../../queries/GetTRBRequestRelatedRequestsQuery';
 
 import RelatedRequestsTable from './RelatedRequestsTable';
 
 describe('Related Requests table', () => {
-  it('renders empty Related Requests table', async () => {
+  it('renders empty related requests table for system intake', async () => {
     const mockStore = easiMockStore();
 
     const mocks = [
@@ -61,7 +63,51 @@ describe('Related Requests table', () => {
     );
   });
 
-  it('renders Related Requests table with data', async () => {
+  it('renders empty related requests table for trb request', async () => {
+    const mockStore = easiMockStore();
+
+    const mocks = [
+      {
+        request: {
+          query: GetTRBRequestRelatedRequestsQuery,
+          variables: {
+            trbRequestID: trbRequest.id
+          }
+        },
+        result: {
+          data: {
+            trbRequest: {
+              __typename: 'TRBRequest',
+              id: trbRequest.id,
+              relatedIntakes: [],
+              relatedTRBRequests: []
+            }
+          }
+        }
+      }
+    ];
+
+    render(
+      <MemoryRouter>
+        <MockedProvider mocks={mocks}>
+          <Provider store={mockStore}>
+            <MessageProvider>
+              <RelatedRequestsTable requestID={trbRequest.id} type="trb" />
+            </MessageProvider>
+          </Provider>
+        </MockedProvider>
+      </MemoryRouter>
+    );
+
+    expect(await screen.findByRole('heading', { name: 'Related requests' }));
+    expect(
+      await screen.findByText(
+        'There are no additional requests linked to this system'
+      )
+    );
+  });
+
+  it('renders system intake related requests table with data', async () => {
     const mockStore = easiMockStore();
 
     const mocks = [
@@ -123,7 +169,69 @@ describe('Related Requests table', () => {
     ).not.toBeInTheDocument();
   });
 
-  it('should provide clickable admin link if user is admin', async () => {
+  it('renders trb request related requests table with data', async () => {
+    const mockStore = easiMockStore();
+
+    const mocks = [
+      {
+        request: {
+          query: GetTRBRequestRelatedRequestsQuery,
+          variables: {
+            trbRequestID: trbRequest.id
+          }
+        },
+        result: {
+          data: {
+            trbRequest: {
+              __typename: 'TRBRequest',
+              id: trbRequest.id,
+              relatedIntakes: [
+                {
+                  id: '1',
+                  requestName: 'related intake 1',
+                  contractNumbers: ['1', '2'],
+                  decisionState: SystemIntakeDecisionState.NO_DECISION,
+                  submittedAt: new Date()
+                }
+              ],
+              relatedTRBRequests: [
+                {
+                  id: '2',
+                  name: 'related trb 1',
+                  contractNumbers: ['3', '4'],
+                  status: TRBRequestStatus.FOLLOW_UP_REQUESTED,
+                  createdAt: new Date()
+                }
+              ]
+            }
+          }
+        }
+      }
+    ];
+
+    render(
+      <MemoryRouter>
+        <MockedProvider mocks={mocks}>
+          <Provider store={mockStore}>
+            <MessageProvider>
+              <RelatedRequestsTable requestID={trbRequest.id} type="trb" />
+            </MessageProvider>
+          </Provider>
+        </MockedProvider>
+      </MemoryRouter>
+    );
+
+    expect(await screen.findByRole('heading', { name: 'Related requests' }));
+
+    // this should NOT be in rendered if there is incoming table data
+    expect(
+      screen.queryByText(
+        'There are no additional requests linked to this system'
+      )
+    ).not.toBeInTheDocument();
+  });
+
+  it('should provide clickable admin link if user is admin on system intake related requests table', async () => {
     // mock with both admin types
     const mockStore = easiMockStore({
       groups: [
@@ -187,7 +295,71 @@ describe('Related Requests table', () => {
     expect(await screen.findByRole('link', { name: 'related trb 1' }));
   });
 
-  it('should provide clickable TRB admin link if user is TRB admin', async () => {
+  it('should provide clickable admin link if user is admin on trb request related requests table', async () => {
+    // mock with both admin types
+    const mockStore = easiMockStore({
+      groups: [
+        'EASI_TRB_ADMIN_D',
+        'EASI_TRB_ADMIN_P',
+        'EASI_D_GOVTEAM',
+        'EASI_P_GOVTEAM'
+      ]
+    });
+
+    const mocks = [
+      {
+        request: {
+          query: GetTRBRequestRelatedRequestsQuery,
+          variables: {
+            trbRequestID: trbRequest.id
+          }
+        },
+        result: {
+          data: {
+            trbRequest: {
+              __typename: 'TRBRequest',
+              id: trbRequest.id,
+              relatedIntakes: [
+                {
+                  id: '1',
+                  requestName: 'related intake 1',
+                  contractNumbers: ['1', '2'],
+                  decisionState: SystemIntakeDecisionState.NO_DECISION,
+                  submittedAt: new Date()
+                }
+              ],
+              relatedTRBRequests: [
+                {
+                  id: '2',
+                  name: 'related trb 1',
+                  contractNumbers: ['3', '4'],
+                  status: TRBRequestStatus.FOLLOW_UP_REQUESTED,
+                  createdAt: new Date()
+                }
+              ]
+            }
+          }
+        }
+      }
+    ];
+
+    render(
+      <MemoryRouter>
+        <MockedProvider mocks={mocks}>
+          <Provider store={mockStore}>
+            <MessageProvider>
+              <RelatedRequestsTable requestID={trbRequest.id} type="trb" />
+            </MessageProvider>
+          </Provider>
+        </MockedProvider>
+      </MemoryRouter>
+    );
+
+    expect(await screen.findByRole('link', { name: 'related intake 1' }));
+    expect(await screen.findByRole('link', { name: 'related trb 1' }));
+  });
+
+  it('should provide clickable TRB admin link if user is TRB admin for system intake table', async () => {
     // mock with TRB admin only
     const mockStore = easiMockStore({
       groups: ['EASI_TRB_ADMIN_D', 'EASI_TRB_ADMIN_P']
@@ -250,7 +422,70 @@ describe('Related Requests table', () => {
     ).not.toBeInTheDocument();
   });
 
-  it('should provide clickable ITGov admin link if user is ITGov admin', async () => {
+  it('should provide clickable TRB admin link if user is TRB admin for trb request table', async () => {
+    // mock with TRB admin only
+    const mockStore = easiMockStore({
+      groups: ['EASI_TRB_ADMIN_D', 'EASI_TRB_ADMIN_P']
+    });
+
+    const mocks = [
+      {
+        request: {
+          query: GetTRBRequestRelatedRequestsQuery,
+          variables: {
+            trbRequestID: trbRequest.id
+          }
+        },
+        result: {
+          data: {
+            trbRequest: {
+              __typename: 'TRBRequest',
+              id: trbRequest.id,
+              relatedIntakes: [
+                {
+                  id: '1',
+                  requestName: 'related intake 1',
+                  contractNumbers: ['1', '2'],
+                  decisionState: SystemIntakeDecisionState.NO_DECISION,
+                  submittedAt: new Date()
+                }
+              ],
+              relatedTRBRequests: [
+                {
+                  id: '2',
+                  name: 'related trb 1',
+                  contractNumbers: ['3', '4'],
+                  status: TRBRequestStatus.FOLLOW_UP_REQUESTED,
+                  createdAt: new Date()
+                }
+              ]
+            }
+          }
+        }
+      }
+    ];
+
+    render(
+      <MemoryRouter>
+        <MockedProvider mocks={mocks}>
+          <Provider store={mockStore}>
+            <MessageProvider>
+              <RelatedRequestsTable requestID={trbRequest.id} type="trb" />
+            </MessageProvider>
+          </Provider>
+        </MockedProvider>
+      </MemoryRouter>
+    );
+
+    // expect only TRB to be clickable
+    expect(await screen.findByRole('link', { name: 'related trb 1' }));
+
+    expect(
+      screen.queryByRole('link', { name: 'related intake 1' })
+    ).not.toBeInTheDocument();
+  });
+
+  it('should provide clickable ITGov admin link if user is ITGov admin for system intake table', async () => {
     // mock with ITGov admin only
     const mockStore = easiMockStore({
       groups: ['EASI_D_GOVTEAM', 'EASI_P_GOVTEAM']
@@ -313,6 +548,69 @@ describe('Related Requests table', () => {
     ).not.toBeInTheDocument();
   });
 
+  it('should provide clickable ITGov admin link if user is ITGov admin trb request table', async () => {
+    // mock with ITGov admin only
+    const mockStore = easiMockStore({
+      groups: ['EASI_D_GOVTEAM', 'EASI_P_GOVTEAM']
+    });
+
+    const mocks = [
+      {
+        request: {
+          query: GetTRBRequestRelatedRequestsQuery,
+          variables: {
+            trbRequestID: trbRequest.id
+          }
+        },
+        result: {
+          data: {
+            trbRequest: {
+              __typename: 'TRBRequest',
+              id: trbRequest.id,
+              relatedIntakes: [
+                {
+                  id: '1',
+                  requestName: 'related intake 1',
+                  contractNumbers: ['1', '2'],
+                  decisionState: SystemIntakeDecisionState.NO_DECISION,
+                  submittedAt: new Date()
+                }
+              ],
+              relatedTRBRequests: [
+                {
+                  id: '2',
+                  name: 'related trb 1',
+                  contractNumbers: ['3', '4'],
+                  status: TRBRequestStatus.FOLLOW_UP_REQUESTED,
+                  createdAt: new Date()
+                }
+              ]
+            }
+          }
+        }
+      }
+    ];
+
+    render(
+      <MemoryRouter>
+        <MockedProvider mocks={mocks}>
+          <Provider store={mockStore}>
+            <MessageProvider>
+              <RelatedRequestsTable requestID={trbRequest.id} type="trb" />
+            </MessageProvider>
+          </Provider>
+        </MockedProvider>
+      </MemoryRouter>
+    );
+
+    // expect only ITGov to be clickable
+    expect(await screen.findByRole('link', { name: 'related intake 1' }));
+
+    expect(
+      screen.queryByRole('link', { name: 'related trb 1' })
+    ).not.toBeInTheDocument();
+  });
+
   it('should provide non-clickable text if user is not an admin', () => {
     const mockStore = easiMockStore({
       groups: ['EASI_P_USER']
@@ -361,6 +659,68 @@ describe('Related Requests table', () => {
           <Provider store={mockStore}>
             <MessageProvider>
               <RelatedRequestsTable requestID={systemIntake.id} type="itgov" />
+            </MessageProvider>
+          </Provider>
+        </MockedProvider>
+      </MemoryRouter>
+    );
+
+    expect(
+      screen.queryByRole('link', { name: 'related intake 1' })
+    ).not.toBeInTheDocument();
+    expect(
+      screen.queryByRole('link', { name: 'related trb 1' })
+    ).not.toBeInTheDocument();
+  });
+
+  it('should provide non-clickable text if user is not an admin', () => {
+    const mockStore = easiMockStore({
+      groups: ['EASI_P_USER']
+    });
+
+    const mocks = [
+      {
+        request: {
+          query: GetTRBRequestRelatedRequestsQuery,
+          variables: {
+            systemIntakeID: trbRequest.id
+          }
+        },
+        result: {
+          data: {
+            trbRequest: {
+              __typename: 'TRBRequest',
+              id: trbRequest.id,
+              relatedIntakes: [
+                {
+                  id: '1',
+                  requestName: 'related intake 1',
+                  contractNumbers: ['1', '2'],
+                  decisionState: SystemIntakeDecisionState.NO_DECISION,
+                  submittedAt: new Date()
+                }
+              ],
+              relatedTRBRequests: [
+                {
+                  id: '2',
+                  name: 'related trb 1',
+                  contractNumbers: ['3', '4'],
+                  status: TRBRequestStatus.FOLLOW_UP_REQUESTED,
+                  createdAt: new Date()
+                }
+              ]
+            }
+          }
+        }
+      }
+    ];
+
+    render(
+      <MemoryRouter>
+        <MockedProvider mocks={mocks}>
+          <Provider store={mockStore}>
+            <MessageProvider>
+              <RelatedRequestsTable requestID={trbRequest.id} type="trb" />
             </MessageProvider>
           </Provider>
         </MockedProvider>
