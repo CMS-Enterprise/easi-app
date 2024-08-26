250092f255ec488b36b7b9fddd8fc6c764d2b5ba
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2024-08-26 10:13:36 -0500

    Remove hidden nav links from array

--- src/views/GovernanceReviewTeam/subNavItems.ts
+++ src/views/GovernanceReviewTeam/subNavItems.ts
@@ -20,71 +20,75 @@ const subNavItems = (
   /** Whether current user is IT Gov admin */
   itGovAdmin: boolean,
   flags?: Flags
-): SubNavItems => [
-  {
-    route: `/it-governance/${systemId}/intake-request`,
-    text: 'general:intake',
-    aria: 'aria.openIntake'
-  },
-  {
-    route: `/it-governance/${systemId}/documents`,
-    text: 'intake:documents.supportingDocuments',
-    aria: 'aria.openDocuments'
-  },
-  {
-    route: `/it-governance/${systemId}/business-case`,
-    text: 'general:businessCase',
-    aria: 'aria.openBusiness',
-    groupEnd: flags && !flags.grbReviewTab
-  },
-  {
-    route: `/it-governance/${systemId}/grb-review`,
-    text: 'grbReview:title',
-    aria: 'grbReview:aria',
-    groupEnd: true,
-    // Only show GRB Review tab when flag is turned on
-    hidden: !flags?.grbReviewTab
-  },
-  {
-    route: `/it-governance/${systemId}/feedback`,
-    text: 'feedback.title',
-    aria: 'aria.openFeedback'
-  },
-  {
-    route: `/it-governance/${systemId}/decision`,
-    text: 'decision.title',
-    aria: 'aria.openDecision'
-  },
-  {
-    route: `/it-governance/${systemId}/lcid`,
-    text: 'lifecycleID.title',
-    aria: 'aria.openLcid',
-    groupEnd: true
-  },
-  {
-    route: `/it-governance/${systemId}/additional-information`,
-    text: 'additionalInformation.title',
-    aria: 'aria.openAdditionalInformation',
-    groupEnd: itGovAdmin
-  },
-  {
-    route: `/it-governance/${systemId}/actions`,
-    text: 'actions',
-    // GRT only link
-    hidden: !itGovAdmin
-  },
-  {
-    route: `/it-governance/${systemId}/notes`,
-    text: 'notes.heading',
-    // GRT only link
-    hidden: !itGovAdmin
-  },
-  {
-    route: `/it-governance/${systemId}/dates`,
-    text: 'dates.heading',
-    // GRT only link
-    hidden: !itGovAdmin
-  }
-];
+): SubNavItems => {
+  const links: SubNavItems = [
+    {
+      route: `/it-governance/${systemId}/intake-request`,
+      text: 'general:intake',
+      aria: 'aria.openIntake'
+    },
+    {
+      route: `/it-governance/${systemId}/documents`,
+      text: 'intake:documents.supportingDocuments',
+      aria: 'aria.openDocuments'
+    },
+    {
+      route: `/it-governance/${systemId}/business-case`,
+      text: 'general:businessCase',
+      aria: 'aria.openBusiness',
+      groupEnd: flags && !flags.grbReviewTab
+    },
+    {
+      route: `/it-governance/${systemId}/grb-review`,
+      text: 'grbReview:title',
+      aria: 'grbReview:aria',
+      groupEnd: true,
+      // Only show GRB Review tab when flag is turned on
+      hidden: !flags?.grbReviewTab
+    },
+    {
+      route: `/it-governance/${systemId}/feedback`,
+      text: 'feedback.title',
+      aria: 'aria.openFeedback'
+    },
+    {
+      route: `/it-governance/${systemId}/decision`,
+      text: 'decision.title',
+      aria: 'aria.openDecision'
+    },
+    {
+      route: `/it-governance/${systemId}/lcid`,
+      text: 'lifecycleID.title',
+      aria: 'aria.openLcid',
+      groupEnd: true
+    },
+    {
+      route: `/it-governance/${systemId}/additional-information`,
+      text: 'additionalInformation.title',
+      aria: 'aria.openAdditionalInformation',
+      groupEnd: itGovAdmin
+    },
+    {
+      route: `/it-governance/${systemId}/actions`,
+      text: 'actions',
+      // GRT only link
+      hidden: !itGovAdmin
+    },
+    {
+      route: `/it-governance/${systemId}/notes`,
+      text: 'notes.heading',
+      // GRT only link
+      hidden: !itGovAdmin
+    },
+    {
+      route: `/it-governance/${systemId}/dates`,
+      text: 'dates.heading',
+      // GRT only link
+      hidden: !itGovAdmin
+    }
+  ];
+
+  return links.filter(({ hidden }) => !hidden);
+};
 
 export default subNavItems;
