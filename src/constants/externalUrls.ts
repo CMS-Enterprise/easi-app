1582feb7199e948a5504376ff187f4755affca7c
Nick Downey <Nicholas.Downey@cms.hhs.gov>
2024-08-28 11:43:32 -0400

    Add AI mailbox to External URL constants

--- src/constants/externalUrls.ts
+++ src/constants/externalUrls.ts
@@ -2,6 +2,7 @@ export const SLACK_OIT_DEV_FEEDBACK =
   'https://cmsgov.slack.com/archives/C038D4UJ6KC';
 
 export const CMS_TRB_EMAIL = 'cms-trb@cms.hhs.gov';
+export const CMS_AI_EMAIL = 'AI@cms.hhs.gov';
 
 export const IT_GOV_EMAIL = 'IT_Governance@cms.hhs.gov';
 export const IT_INVESTMENT_EMAIL = 'IT_Investment@cms.hhs.gov';
