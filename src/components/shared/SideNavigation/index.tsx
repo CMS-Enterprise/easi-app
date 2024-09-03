84963e7938ede42a269162749cd55b6f221e8689
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2024-09-03 11:17:02 -0500

    SideNavigation component

---
+++ src/components/shared/SideNavigation/index.tsx
@@ -0,0 +1,108 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { useLocation } from 'react-router-dom';
+import { IconArrowBack } from '@trussworks/react-uswds';
+import classNames from 'classnames';
+
+import UswdsReactLink from 'components/LinkWrapper';
+
+import IconLink from '../IconLink';
+
+import './index.scss';
+
+export type SubNavLinkProps = {
+  route: string;
+  text: string;
+};
+
+export type NavLinkProps = SubNavLinkProps & {
+  children?: SubNavLinkProps[];
+  /** Designates end of navigation group with bottom border */
+  groupEnd?: boolean;
+};
+
+type SideNavigationProps = {
+  items: Array<NavLinkProps>;
+  /** Link with back arrow displayed at top of navigation */
+  returnLink?: {
+    to: string;
+    text: string;
+  };
+  className?: string;
+};
+
+/**
+ * Side navigation component
+ */
+const SideNavigation = ({
+  items,
+  returnLink,
+  className
+}: SideNavigationProps) => {
+  const { t } = useTranslation();
+  const { pathname, hash } = useLocation();
+
+  const pathnameWithHash = `${pathname}${hash}`;
+
+  return (
+    <nav className={className}>
+      <ul className="usa-sidenav border-bottom-0">
+        {
+          // Return arrow link at top of navigation list
+          returnLink && (
+            <li>
+              <IconLink
+                to={returnLink.to}
+                icon={<IconArrowBack />}
+                className="text-primary hover:text-primary-dark text-underline margin-bottom-4"
+              >
+                {t(returnLink.text)}
+              </IconLink>
+            </li>
+          )
+        }
+
+        {items.map(item => {
+          return (
+            <li
+              className={classNames('usa-sidenav__item border-top-0', {
+                'border-bottom-1px border-disabled-light': item?.groupEnd
+              })}
+            >
+              <UswdsReactLink
+                to={item.route}
+                className={classNames({
+                  'usa-current': item.route === pathname
+                })}
+              >
+                {t(item.text)}
+              </UswdsReactLink>
+
+              {
+                // Child navigation links
+                item?.children && (
+                  <ul className="usa-sidenav__sublist">
+                    {item.children.map(child => (
+                      <li className="usa-sidenav__item border-top-0">
+                        <UswdsReactLink
+                          to={child.route}
+                          className={classNames({
+                            'usa-current': pathnameWithHash === child.route
+                          })}
+                        >
+                          {t(child.text)}
+                        </UswdsReactLink>
+                      </li>
+                    ))}
+                  </ul>
+                )
+              }
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+export default SideNavigation;
