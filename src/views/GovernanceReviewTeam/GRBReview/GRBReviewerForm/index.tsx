a0307288353a092714772d39461d2f1099eea25e
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2024-09-12 12:01:14 -0500

    Restructure tabs

--- src/views/GovernanceReviewTeam/GRBReview/GRBReviewerForm/index.tsx
+++ src/views/GovernanceReviewTeam/GRBReview/GRBReviewerForm/index.tsx
@@ -26,6 +26,7 @@ import CedarContactSelect from 'components/CedarContactSelect';
 import { useEasiForm } from 'components/EasiForm';
 import Alert from 'components/shared/Alert';
 import CollapsableLink from 'components/shared/CollapsableLink';
+import Divider from 'components/shared/Divider';
 import FieldErrorMsg from 'components/shared/FieldErrorMsg';
 import HelpText from 'components/shared/HelpText';
 import IconLink from 'components/shared/IconLink';
@@ -169,7 +170,7 @@ const GRBReviewerForm = ({
         as={<Alert type="error" className="margin-top-2" />}
       />
 
-      <Grid className="padding-y-4">
+      <Grid className="padding-y-4 margin-bottom-205">
         <h1 className="margin-bottom-1">{t('form.title')}</h1>
         <p className="font-body-md line-height-body-4 text-light margin-top-05 margin-bottom-105 tablet:grid-col-8">
           {t('form.description')}
@@ -190,195 +191,192 @@ const GRBReviewerForm = ({
           {t('form.returnToRequest', { context: action })}
         </IconLink>
 
-        <Tabs
-          defaultActiveTab={t('form.addViaEUA')}
-          className="margin-bottom-8"
-        >
-          <TabPanel id="addReviewerFromEua" tabName={t('form.addViaEUA')}>
-            <Form onSubmit={submit} className="maxw-none tablet:grid-col-6">
-              <FormGroup>
-                <Label htmlFor="react-select-userAccount-input" required>
-                  {t('form.grbMemberName')}
-                </Label>
-                <HelpText id="userAccountHelpText" className="margin-top-05">
-                  {t('form.grbMemberNameHelpText')}
-                </HelpText>
-                <ErrorMessage
-                  errors={errors}
-                  name="userAccount"
-                  as={<FieldErrorMsg />}
-                />
-                <CedarContactSelect
-                  {...{ ...register('userAccount'), ref: null }}
-                  onChange={contact =>
-                    contact?.euaUserId &&
-                    setValue(
-                      'userAccount',
-                      {
-                        username: contact.euaUserId,
-                        commonName: contact.commonName,
-                        email: contact.email || ''
-                      },
-                      { shouldValidate: isSubmitted }
-                    )
-                  }
-                  value={{
-                    euaUserId: watch('userAccount.username'),
-                    commonName: watch('userAccount.commonName'),
-                    email: watch('userAccount.email')
-                  }}
-                  id="userAccount"
-                  ariaDescribedBy="userAccountHelpText"
-                  disabled={!!activeReviewer}
-                />
-              </FormGroup>
-
-              <FormGroup>
-                <Label htmlFor="votingRole" required>
-                  {t('form.votingRole')}
-                </Label>
-                <ErrorMessage
-                  errors={errors}
-                  name="votingRole"
-                  as={<FieldErrorMsg />}
-                />
-                <Dropdown
-                  {...register('votingRole')}
-                  ref={null}
-                  id="votingRole"
+        <Form onSubmit={submit} className="maxw-none">
+          <Tabs defaultActiveTab={t('form.addViaEUA')}>
+            <TabPanel id="addReviewerFromEua" tabName={t('form.addViaEUA')}>
+              <div className="tablet:grid-col-6 margin-bottom-4">
+                <FormGroup>
+                  <Label htmlFor="react-select-userAccount-input" required>
+                    {t('form.grbMemberName')}
+                  </Label>
+                  <HelpText id="userAccountHelpText" className="margin-top-05">
+                    {t('form.grbMemberNameHelpText')}
+                  </HelpText>
+                  <ErrorMessage
+                    errors={errors}
+                    name="userAccount"
+                    as={<FieldErrorMsg />}
+                  />
+                  <CedarContactSelect
+                    {...{ ...register('userAccount'), ref: null }}
+                    onChange={contact =>
+                      contact?.euaUserId &&
+                      setValue(
+                        'userAccount',
+                        {
+                          username: contact.euaUserId,
+                          commonName: contact.commonName,
+                          email: contact.email || ''
+                        },
+                        { shouldValidate: isSubmitted }
+                      )
+                    }
+                    value={{
+                      euaUserId: watch('userAccount.username'),
+                      commonName: watch('userAccount.commonName'),
+                      email: watch('userAccount.email')
+                    }}
+                    id="userAccount"
+                    ariaDescribedBy="userAccountHelpText"
+                    disabled={!!activeReviewer}
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Label htmlFor="votingRole" required>
+                    {t('form.votingRole')}
+                  </Label>
+                  <ErrorMessage
+                    errors={errors}
+                    name="votingRole"
+                    as={<FieldErrorMsg />}
+                  />
+                  <Dropdown
+                    {...register('votingRole')}
+                    ref={null}
+                    id="votingRole"
+                  >
+                    <option value="">{t('form:dropdownInitialSelect')}</option>
+                    {grbReviewerVotingRoles.map(key => (
+                      <option value={key} key={key}>
+                        {t(`votingRoles.${key}`)}
+                      </option>
+                    ))}
+                  </Dropdown>
+                </FormGroup>
+                <CollapsableLink
+                  id="votingRolesInfoList"
+                  label={t('form.votingRolesInfo.label')}
+                  className="margin-top-2"
                 >
-                  <option value="">{t('form:dropdownInitialSelect')}</option>
-                  {grbReviewerVotingRoles.map(key => (
-                    <option value={key} key={key}>
-                      {t(`votingRoles.${key}`)}
-                    </option>
-                  ))}
-                </Dropdown>
-              </FormGroup>
+                  <dl className="margin-y-neg-1 padding-left-2">
+                    {t<string[]>('form.votingRolesInfo.items', {
+                      returnObjects: true
+                    }).map(item => (
+                      <div key={item} className="display-list-item margin-y-1">
+                        <Trans
+                          defaults={item}
+                          components={{
+                            dt: <dt className="text-bold display-inline" />,
+                            dd: <dd className="margin-0 display-inline" />
+                          }}
+                        />
+                      </div>
+                    ))}
+                  </dl>
+                </CollapsableLink>
+                <FormGroup>
+                  <Label htmlFor="grbRole" required>
+                    {t('form.grbRole')}
+                  </Label>
+                  <HelpText id="grbRoleHelpText" className="margin-top-05">
+                    {t('form.grbRoleHelpText')}
+                  </HelpText>
+                  <ErrorMessage
+                    errors={errors}
+                    name="grbRole"
+                    as={<FieldErrorMsg />}
+                  />
+                  <Dropdown
+                    {...register('grbRole')}
+                    ref={null}
+                    id="grbRole"
+                    aria-describedby="grbRoleHelpText"
+                  >
+                    <option value="">{t('form:dropdownInitialSelect')}</option>
+                    {grbReviewerRoles.map(key => (
+                      <option value={key} key={key}>
+                        {t(`reviewerRoles.${key}`)}
+                      </option>
+                    ))}
+                  </Dropdown>
+                </FormGroup>
+                {activeReviewer && (
+                  <Button
+                    type="button"
+                    onClick={() => setReviewerToRemove(activeReviewer)}
+                    className="text-error margin-bottom-4"
+                    unstyled
+                  >
+                    {t('form.removeGrbReviewer')}
+                  </Button>
+                )}
+              </div>
+            </TabPanel>
+            <TabPanel
+              id="addReviewersFromRequest"
+              tabName={t('form.addFromRequest')}
+            >
+              <div className="tablet:grid-col-6">
+                <p className="line-height-body-5 margin-top-3">
+                  {t('form.addFromRequestDescription')}
+                </p>
 
-              <CollapsableLink
-                id="votingRolesInfoList"
-                label={t('form.votingRolesInfo.label')}
-                className="margin-top-2"
-              >
-                <dl className="margin-y-neg-1 padding-left-2">
-                  {t<string[]>('form.votingRolesInfo.items', {
-                    returnObjects: true
-                  }).map(item => (
-                    <div key={item} className="display-list-item margin-y-1">
-                      <Trans
-                        defaults={item}
-                        components={{
-                          dt: <dt className="text-bold display-inline" />,
-                          dd: <dd className="margin-0 display-inline" />
-                        }}
-                      />
-                    </div>
-                  ))}
-                </dl>
-              </CollapsableLink>
+                <FormGroup>
+                  <Label
+                    htmlFor="itGovernanceRequests"
+                    className="text-normal margin-bottom-05"
+                  >
+                    {t('form.itGovernanceRequests')}
+                  </Label>
+                  <HelpText id="itGovernanceRequestsHelpText">
+                    {t('form.itGovernanceRequestsHelpText')}
+                  </HelpText>
 
-              <FormGroup>
-                <Label htmlFor="grbRole" required>
-                  {t('form.grbRole')}
-                </Label>
-                <HelpText id="grbRoleHelpText" className="margin-top-05">
-                  {t('form.grbRoleHelpText')}
-                </HelpText>
-                <ErrorMessage
-                  errors={errors}
-                  name="grbRole"
-                  as={<FieldErrorMsg />}
-                />
-                <Dropdown
-                  {...register('grbRole')}
-                  ref={null}
-                  id="grbRole"
-                  aria-describedby="grbRoleHelpText"
-                >
-                  <option value="">{t('form:dropdownInitialSelect')}</option>
-                  {grbReviewerRoles.map(key => (
-                    <option value={key} key={key}>
-                      {t(`reviewerRoles.${key}`)}
-                    </option>
-                  ))}
-                </Dropdown>
-              </FormGroup>
+                  <ComboBox
+                    id="itGovernanceRequests"
+                    name="itGovernanceRequests"
+                    className="maxw-none"
+                    // TODO: onChange
+                    onChange={val => null}
+                    // TODO: Set requests as options
+                    options={[
+                      { value: 'One', label: 'One' },
+                      { value: 'Two', label: 'Two' },
+                      { value: 'Three', label: 'Three' }
+                    ]}
+                  />
+                </FormGroup>
 
-              {activeReviewer && (
-                <Button
-                  type="button"
-                  onClick={() => setReviewerToRemove(activeReviewer)}
-                  className="text-error margin-bottom-4"
-                  unstyled
-                >
-                  {t('form.removeGrbReviewer')}
-                </Button>
-              )}
-
-              {action === 'add' && (
-                <Alert type="info" slim className="margin-top-8">
-                  {t('form.infoAlert')}
+                <Alert type="info" className="margin-top-3" slim>
+                  {t('form.selectRequestAlert')}
                 </Alert>
-              )}
 
-              <Pager
-                next={{
-                  text: t('form.submit', { context: action }),
-                  disabled:
-                    !watch('userAccount') ||
-                    !watch('votingRole') ||
-                    !watch('grbRole')
-                }}
-                taskListUrl={grbReviewPath}
-                saveExitText={t('form.returnToRequest', { context: action })}
-                border={false}
-                className="margin-top-4"
-                submitDisabled
-              />
-            </Form>
-          </TabPanel>
-          <TabPanel
-            id="addReviewersFromRequest"
-            tabName={t('form.addFromRequest')}
-          >
-            <div className="tablet:grid-col-6">
-              <p className="line-height-body-5 margin-top-3">
-                {t('form.addFromRequestDescription')}
-              </p>
+                <Divider className="margin-top-6" />
+              </div>
+            </TabPanel>
+          </Tabs>
 
-              <FormGroup>
-                <Label
-                  htmlFor="itGovernanceRequests"
-                  className="text-normal margin-bottom-05"
-                >
-                  {t('form.itGovernanceRequests')}
-                </Label>
-                <HelpText id="itGovernanceRequestsHelpText">
-                  {t('form.itGovernanceRequestsHelpText')}
-                </HelpText>
+          {action === 'add' && (
+            <Alert type="info" slim className="margin-top-4 tablet:grid-col-6">
+              {t('form.infoAlert')}
+            </Alert>
+          )}
 
-                <ComboBox
-                  id="itGovernanceRequests"
-                  name="itGovernanceRequests"
-                  className="maxw-none"
-                  // TODO: onChange
-                  onChange={val => null}
-                  // TODO: Set requests as options
-                  options={[
-                    { value: 'One', label: 'One' },
-                    { value: 'Two', label: 'Two' },
-                    { value: 'Three', label: 'Three' }
-                  ]}
-                />
-              </FormGroup>
-
-              <Alert type="info" className="margin-top-3" slim>
-                {t('form.selectRequestAlert')}
-              </Alert>
-            </div>
-          </TabPanel>
-        </Tabs>
+          <Pager
+            next={{
+              text: t('form.submit', { context: action }),
+              disabled:
+                !watch('userAccount') ||
+                !watch('votingRole') ||
+                !watch('grbRole')
+            }}
+            taskListUrl={grbReviewPath}
+            saveExitText={t('form.returnToRequest', { context: action })}
+            border={false}
+            className="margin-top-4"
+            submitDisabled
+          />
+        </Form>
       </Grid>
     </>
   );
