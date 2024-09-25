b891a41667fd73d3ac38e06cca7ba8822451b67b
ClayBenson94 <clay.benson@oddball.io>
2024-09-25 16:53:27 -0400

    Upgrade sass and move it to devDependencies. Replace @cypress/webpack-preprocessor with cypress-vite

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,7 +11,7 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 const cypressOTP = require('cypress-otp');
-const wp = require('@cypress/webpack-preprocessor');
+const vitePreprocessor = require('cypress-vite');
 
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
@@ -36,7 +36,7 @@ module.exports = (on, config) => {
       }
     }
   };
-  on('file:preprocessor', wp(options));
+  on('file:preprocessor', vitePreprocessor(options));
 
   const newConfig = config;
   newConfig.env.oktaDomain = process.env.OKTA_DOMAIN;
