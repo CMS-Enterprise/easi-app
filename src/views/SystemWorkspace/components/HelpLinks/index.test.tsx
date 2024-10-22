44e258b6c05e448daaaec92cc20519ec9da3ff86
adamodd <adam.suharja@oddball.io>
2024-10-22 12:21:35 -0700

    Test broken client

--- src/views/SystemWorkspace/components/HelpLinks/index.test.tsx
+++ src/views/SystemWorkspace/components/HelpLinks/index.test.tsx
@@ -1,27 +1,23 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
-import i18next from 'i18next';
 
-import { HelpLinkType } from 'i18n/en-US/systemWorkspace';
-
+// import i18next from 'i18next';
+// import { HelpLinkType } from 'i18n/en-US/systemWorkspace';
 import HelpCardGroup from './SystemCardGroup';
 
 describe('Help Links component', () => {
-  const helpCards = i18next.t<HelpLinkType[]>(
-    'systemWorkspace:helpLinks.links',
-    {
-      returnObjects: true
-    }
-  );
+  // const helpCards = i18next.t<HelpLinkType[]>(
+  //   'systemWorkspace:helpLinks.links',
+  //   {
+  //     returnObjects: true
+  //   }
+  // );
 
   it('matches snapshot', async () => {
     const { asFragment } = render(
       <MemoryRouter>
-        <HelpCardGroup
-          cards={helpCards}
-          linkSearchQuery="linkCedarSystemId=1"
-        />
+        <HelpCardGroup cards={[]} linkSearchQuery="linkCedarSystemId=1" />
       </MemoryRouter>
     );
 
