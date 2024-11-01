56ad4b75b510c4d57580aa04a6f77603490f4535
samoddball <sam.lawson@oddball.io>
2024-11-01 10:39:35 -0700

    customize unmarshal functionality for the category enum type. add Note [gql enums]

--- pkg/models/trb_guidance_letter_recommendation.go
+++ pkg/models/trb_guidance_letter_recommendation.go
@@ -1,6 +1,9 @@
 package models
 
 import (
+	"fmt"
+	"io"
+	"strconv"
 	"time"
 
 	"github.com/google/uuid"
@@ -19,3 +22,65 @@ type TRBGuidanceLetterRecommendation struct {
 	DeletedAt        *time.Time                              `json:"deletedAt" db:"deleted_at"`
 	Category         TRBGuidanceLetterRecommendationCategory `json:"category" db:"category"`
 }
+
+type TRBGuidanceLetterRecommendationCategory string
+
+const (
+	TRBGuidanceLetterRecommendationCategoryRequirement    TRBGuidanceLetterRecommendationCategory = "REQUIREMENT"
+	TRBGuidanceLetterRecommendationCategoryRecommendation TRBGuidanceLetterRecommendationCategory = "RECOMMENDATION"
+	TRBGuidanceLetterRecommendationCategoryConsideration  TRBGuidanceLetterRecommendationCategory = "CONSIDERATION"
+	TRBGuidanceLetterRecommendationCategoryUncategorized  TRBGuidanceLetterRecommendationCategory = "UNCATEGORIZED"
+)
+
+var AllTRBGuidanceLetterRecommendationCategory = []TRBGuidanceLetterRecommendationCategory{
+	TRBGuidanceLetterRecommendationCategoryRequirement,
+	TRBGuidanceLetterRecommendationCategoryRecommendation,
+	TRBGuidanceLetterRecommendationCategoryConsideration,
+	TRBGuidanceLetterRecommendationCategoryUncategorized,
+}
+
+func (e TRBGuidanceLetterRecommendationCategory) IsValid() bool {
+	switch e {
+	case TRBGuidanceLetterRecommendationCategoryRequirement, TRBGuidanceLetterRecommendationCategoryRecommendation, TRBGuidanceLetterRecommendationCategoryConsideration, TRBGuidanceLetterRecommendationCategoryUncategorized:
+		return true
+	}
+	return false
+}
+
+func (e TRBGuidanceLetterRecommendationCategory) String() string {
+	return string(e)
+}
+
+// UnmarshalGQL is a custom implementation of what gqlgen would normally generate
+// see Note [gql enums]
+func (e *TRBGuidanceLetterRecommendationCategory) UnmarshalGQL(v interface{}) error {
+	switch t := v.(type) {
+	case TRBGuidanceLetterRecommendationCategory:
+		*e = t
+		if !e.IsValid() {
+			return fmt.Errorf("%s is not a valid TRBGuidanceLetterRecommendationCategory", t.String())
+		}
+
+		return nil
+
+	case string:
+		*e = TRBGuidanceLetterRecommendationCategory(t)
+		if !e.IsValid() {
+			return fmt.Errorf("%s is not a valid TRBGuidanceLetterRecommendationCategory", t)
+		}
+
+		return nil
+	}
+
+	return fmt.Errorf("could not parse %v as a TRBGuidanceLetterRecommendationCategory", v)
+}
+
+func (e TRBGuidanceLetterRecommendationCategory) MarshalGQL(w io.Writer) {
+	fmt.Fprint(w, strconv.Quote(e.String()))
+}
+
+// Note [gql enums]
+// For cases where the error "enums must be strings" is seen, this can be caused by sending an enum value
+// to an endpoint which takes in a @goModel(model: "map[string]interface{}")
+// GQL is unable to interpret the incoming value as a string, so we have to customize this behavior
+// possibly a bug in gqlgen library
