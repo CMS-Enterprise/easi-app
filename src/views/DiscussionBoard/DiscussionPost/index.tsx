5c211069d684d9e5b7b714475010e41ab177f9f8
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2024-11-18 15:52:16 -0600

    Fix line height within side panel

--- src/views/DiscussionBoard/DiscussionPost/index.tsx
+++ src/views/DiscussionBoard/DiscussionPost/index.tsx
@@ -61,7 +61,7 @@ const DiscussionPost = ({ replies, ...initialPost }: DiscussionPostProps) => {
   }, [replies, t]);
 
   return (
-    <div className="easi-discussion-post display-flex">
+    <div className="easi-discussion-post display-flex line-height-body-1">
       <div className="margin-right-105">
         <AvatarCircle user={userAccount.commonName} />
       </div>
