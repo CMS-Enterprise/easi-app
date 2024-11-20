db2017862261f98bb2d80f7a1a9c4cfe97c0b50d
adamodd <adam.suharja@oddball.io>
2024-11-20 11:07:55 -0800

    Text

--- src/components/MentionTextArea/index.tsx
+++ src/components/MentionTextArea/index.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 // import { useTranslation } from 'react-i18next';
 import Mention from '@tiptap/extension-mention';
 import {
@@ -79,7 +80,7 @@ const MentionTextArea = ({
   initialContent?: string;
   className?: string;
 }) => {
-  // const { t } = useTranslation('');
+  const { t } = useTranslation('discussions');
 
   const [tagAlert, setTagAlert] = useState<boolean>(false);
 
@@ -168,9 +169,7 @@ const MentionTextArea = ({
 
       {tagAlert && editable && (
         <Alert type="info" slim>
-          {/* t() */}
-          When you save your discussion, the selected team(s) and individual(s)
-          will be notified via email.
+          {t('general.alerts.saveDiscussion')}
         </Alert>
       )}
     </>
