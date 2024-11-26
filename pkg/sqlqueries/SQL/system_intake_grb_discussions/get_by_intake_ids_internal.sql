7c588680a22a7b149017038d93ce835467645b31
ClayBenson94 <clay.benson@oddball.io>
2024-11-26 12:13:22 -0500

    Fix GRB Discussions SQL files with new SQLFluff linter

--- pkg/sqlqueries/SQL/system_intake_grb_discussions/get_by_intake_ids_internal.sql
+++ pkg/sqlqueries/SQL/system_intake_grb_discussions/get_by_intake_ids_internal.sql
@@ -1,13 +1,13 @@
 SELECT
-  id,
-  content,
-  voting_role,
-  grb_role,
-  system_intake_id,
-  reply_to_id,
-  modified_at,
-  modified_by,
-  created_at,
-  created_by
+    id,
+    content,
+    voting_role,
+    grb_role,
+    system_intake_id,
+    reply_to_id,
+    modified_at,
+    modified_by,
+    created_at,
+    created_by
 FROM system_intake_internal_grb_review_discussion_posts
 WHERE system_intake_id = ANY(:system_intake_ids);
