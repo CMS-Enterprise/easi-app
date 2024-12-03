37fb0f310455e97073928118ff325e848fee3f80
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2024-12-03 14:16:18 -0600

    MentionTextArea unit tests

--- src/components/MentionTextArea/index.test.tsx
+++ src/components/MentionTextArea/index.test.tsx
@@ -8,6 +8,38 @@ const content =
   'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
 describe('MentionTextArea component', () => {
+  it('renders the component to view text', () => {
+    const { asFragment } = render(
+      <MentionTextArea
+        id="mentionTextArea"
+        initialContent={content}
+        editable={false}
+      />
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+
+    expect(screen.getByText(content)).toBeInTheDocument();
+
+    // Hide "Read more" button when `truncateText` is false
+    expect(screen.queryByRole('button', { name: 'Read more' })).toBeNull();
+  });
+
+  it('renders the editable text area component', () => {
+    const { asFragment } = render(
+      <MentionTextArea id="mentionTextArea" editable />
+    );
+
+    const editor = screen.getByRole('textbox');
+    const text = 'This is the text!';
+
+    userEvent.type(editor, text);
+
+    expect(editor).toHaveTextContent(text);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
   it('truncates text with read more/less button', async () => {
     render(
       <MentionTextArea
@@ -31,5 +63,19 @@ describe('MentionTextArea component', () => {
     expect(readLessButton).toBeInTheDocument();
 
     expect(screen.getByText(content)).toBeInTheDocument();
+  });
+
+  it('handles truncated text shorter than character limit', () => {
+    const shortenedString = content.slice(0, 100);
+
+    render(
+      <MentionTextArea
+        id="mentionTextArea"
+        initialContent={shortenedString}
+        truncateText
+      />
+    );
+
+    expect(screen.queryByRole('button', { name: 'Read more' })).toBeNull();
   });
 });
