af7c9b7596f9ef3805c354badc80246321c4775b
samoddball <sam.lawson@oddball.io>
2024-12-04 13:40:36 -0700

    fill in reply functionality, will see if there is a way to DRY this up

--- pkg/graph/resolvers/system_intake_grb_discussions.go
+++ pkg/graph/resolvers/system_intake_grb_discussions.go
@@ -88,7 +88,6 @@ func CreateSystemIntakeGRBDiscussionPost(
 		}
 
 		// check if the grb group is being emailed, in which case we should make sure we do not send any individual emails out
-		// unless they are admins
 		var grbGroupFound bool
 		uniqueTags := input.Content.UniqueTags()
 		for _, tag := range uniqueTags {
@@ -200,22 +199,28 @@ func CreateSystemIntakeGRBDiscussionReply(
 	input models.CreateSystemIntakeGRBDiscussionReplyInput,
 ) (*models.SystemIntakeGRBReviewDiscussionPost, error) {
 	return sqlutils.WithTransactionRet(ctx, store, func(tx *sqlx.Tx) (*models.SystemIntakeGRBReviewDiscussionPost, error) {
+		logger := appcontext.ZLogger(ctx)
+
 		initialPost, err := store.GetSystemIntakeGRBDiscussionPostByID(ctx, tx, input.InitialPostID)
 		if err != nil {
 			return nil, err
 		}
+
 		intakeID := initialPost.SystemIntakeID
 		if initialPost.ReplyToID != nil {
 			return nil, errors.New("only top level posts can be replied to")
 		}
+
 		principalGRBReviewer, err := GetPrincipalGRBReviewerBySystemIntakeID(ctx, intakeID)
 		if err != nil {
 			return nil, err
 		}
+
 		isAdmin := services.AuthorizeRequireGRTJobCode(ctx)
 		if principalGRBReviewer == nil && !isAdmin {
 			return nil, errors.New("user not authorized to create discussion post")
 		}
+
 		post := models.NewSystemIntakeGRBReviewDiscussionPost(appcontext.Principal(ctx).Account().ID)
 		post.Content = input.Content.RawContent
 		post.SystemIntakeID = intakeID
@@ -224,7 +229,174 @@ func CreateSystemIntakeGRBDiscussionReply(
 			post.VotingRole = &principalGRBReviewer.GRBVotingRole
 			post.GRBRole = &principalGRBReviewer.GRBReviewerRole
 		}
-		return store.CreateSystemIntakeGRBDiscussionPost(ctx, tx, post)
+
+		result, err := store.CreateSystemIntakeGRBDiscussionPost(ctx, tx, post)
+		if err != nil {
+			return nil, err
+		}
+
+		systemIntake, err := store.FetchSystemIntakeByIDNP(ctx, tx, intakeID)
+		if err != nil {
+			return nil, err
+		}
+
+		// the initial poster will receive a notification
+		// in the event the initial poster is also tagged in a reply, we do not send both emails
+		// we only send the "someone replied" email
+		initialPoster, err := store.UserAccountGetByID(ctx, tx, initialPost.CreatedBy)
+		if err != nil {
+			return nil, err
+		}
+
+		// get user who made this reply post
+		replyPoster, err := store.UserAccountGetByID(ctx, tx, post.ID)
+		if err != nil {
+			return nil, err
+		}
+
+		// so first, we can send the reply email
+		if err := emailClient.SystemIntake.SendGRBReviewDiscussionReplyEmail(ctx, email.SendGRBReviewDiscussionReplyEmailInput{
+			SystemIntakeID:           intakeID,
+			UserName:                 replyPoster.Username,
+			RequestName:              systemIntake.ProjectName.String,
+			Role:                     roleFromPoster(post),
+			DiscussionContent:        post.Content.ToTemplate(),
+			ITGovernanceInboxAddress: "IT_Governance@cms.hhs.gov",
+			Recipient:                models.EmailAddress(initialPoster.Email),
+		}); err != nil {
+			return nil, err
+		}
+
+		// if no tags, we can return here
+		if len(input.Content.Tags) < 1 {
+			return result, nil
+		}
+
+		// get grb reviewers
+		grbReviewers, err := store.SystemIntakeGRBReviewersBySystemIntakeIDsNP(ctx, tx, []uuid.UUID{intakeID})
+		if err != nil {
+			return nil, err
+		}
+
+		grbReviewerCache := map[uuid.UUID]*models.SystemIntakeGRBReviewer{}
+		// map for ease
+		for _, grbReviewer := range grbReviewers {
+			// not sure if possible, but just in case
+			if grbReviewer == nil {
+				continue
+			}
+
+			grbReviewerCache[grbReviewer.ID] = grbReviewer
+		}
+
+		// check if the grb group is being emailed, in which case we should make sure we do not send any individual emails out
+		var grbGroupFound bool
+		uniqueTags := input.Content.UniqueTags()
+		for _, tag := range uniqueTags {
+			if tag.TagType == models.TagTypeGroupGrbReviewers {
+				grbGroupFound = true
+				break
+			}
+		}
+
+		// send emails here
+		for _, tag := range uniqueTags {
+			// don't think this can happen, just for safety
+			if tag == nil {
+				continue
+			}
+
+			switch tag.TagType {
+			case models.TagTypeGroupItGov:
+				// this is a group tag, and we need to email ITGov box
+				if err := emailClient.SystemIntake.SendGRBReviewDiscussionGroupTaggedEmail(ctx, email.SendGRBReviewDiscussionGroupTaggedEmailInput{
+					SystemIntakeID:           systemIntake.ID,
+					UserName:                 replyPoster.Username,
+					GroupName:                "Governance Admin Team",
+					RequestName:              systemIntake.ProjectName.String,
+					Role:                     roleFromPoster(post),
+					DiscussionContent:        input.Content.ToTemplate(),
+					ITGovernanceInboxAddress: "IT_Governance@cms.hhs.gov",
+					Recipients:               []models.EmailAddress{"IT_Governance@cms.hhs.gov"},
+				}); err != nil {
+					return nil, err
+				}
+
+			case models.TagTypeGroupGrbReviewers:
+				// this is a group tag, and we need to gather everyone from that group
+				// make sure we don't send duplicates (i.e., if an individual user and the entire GRB team is tagged, only send one)
+
+				// gather grb member emails
+				var uuids []uuid.UUID
+				for id := range grbReviewerCache {
+
+					// do not send an additional email to the initial poster as they will have already received one
+					// letting them know that someone has replied
+					if id == initialPoster.ID {
+						continue
+					}
+
+					uuids = append(uuids, id)
+				}
+
+				users, err := store.UserAccountsByIDsNP(ctx, tx, uuids)
+				if err != nil {
+					return nil, err
+				}
+
+				var emails []models.EmailAddress
+				for _, user := range users {
+					emails = append(emails, models.EmailAddress(user.Email))
+				}
+
+				if err := emailClient.SystemIntake.SendGRBReviewDiscussionGroupTaggedEmail(ctx, email.SendGRBReviewDiscussionGroupTaggedEmailInput{
+					SystemIntakeID:           uuid.UUID{},
+					UserName:                 replyPoster.Username,
+					GroupName:                "GRB",
+					RequestName:              systemIntake.ProjectName.String,
+					Role:                     roleFromPoster(post),
+					DiscussionContent:        input.Content.ToTemplate(),
+					ITGovernanceInboxAddress: "IT_Governance@cms.hhs.gov",
+					Recipients:               emails,
+				}); err != nil {
+					return nil, err
+				}
+
+			case models.TagTypeUserAccount:
+				// if we are emailing the group, we do not need to send out individual emails
+				if grbGroupFound {
+					continue
+				}
+
+				// this is an individual tag
+				if _, ok := grbReviewerCache[tag.TaggedContentID]; !ok {
+					// this means someone was tagged who should not have been
+					logger.Warn("tagged user is not a grb reviewer for this intake", zap.String("systemIntakeID", intakeID.String()))
+					continue
+				}
+
+				// this is an individual tag, and we need to build an email based on the passed in UUID
+				recipient, err := store.UserAccountGetByID(ctx, tx, tag.TaggedContentID)
+				if err != nil {
+					logger.Error("problem getting recipient by id when sending out tag email notifications", zap.Error(err), zap.String("userID", tag.TaggedContentID.String()))
+					continue
+				}
+
+				if err := emailClient.SystemIntake.SendGRBReviewDiscussionIndividualTaggedEmail(ctx, email.SendGRBReviewDiscussionIndividualTaggedEmailInput{
+					SystemIntakeID:           intakeID,
+					UserName:                 replyPoster.Username,
+					RequestName:              systemIntake.ProjectName.String,
+					Role:                     roleFromPoster(post),
+					DiscussionContent:        input.Content.ToTemplate(),
+					ITGovernanceInboxAddress: "IT_Governance@cms.hhs.gov",
+					Recipient:                models.EmailAddress(recipient.Email),
+				}); err != nil {
+					return nil, err
+				}
+			}
+		}
+
+		return result, nil
 	})
 }
 
