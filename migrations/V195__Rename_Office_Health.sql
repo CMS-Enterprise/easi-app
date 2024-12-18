2ef2c815e0c266308307f06df0cac1d24103d6a3
samoddball <sam.lawson@oddball.io>
2024-12-18 10:45:03 -0700

    add `trb_request_forms` and `trb_request_attendees`

--- migrations/V195__Rename_Office_Health.sql
+++ migrations/V195__Rename_Office_Health.sql
@@ -5,3 +5,11 @@ WHERE component = 'Office of Burden Reduction and Health Informatics';
 UPDATE system_intake_contacts
 SET component = 'Office of Healthcare Experience and Interoperability'
 WHERE component = 'Office of Burden Reduction and Health Informatics';
+
+UPDATE trb_request_forms
+SET component = 'Office of Healthcare Experience and Interoperability'
+WHERE component = 'Office of Burden Reduction and Health Informatics';
+
+UPDATE trb_request_attendees
+SET component = 'Office of Healthcare Experience and Interoperability'
+WHERE component = 'Office of Burden Reduction and Health Informatics';
