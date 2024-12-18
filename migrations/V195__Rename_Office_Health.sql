aee38bce5c436b762b9929eb7cad9ef562d591dc
samoddball <sam.lawson@oddball.io>
2024-12-18 10:19:22 -0700

    add `system_intake_contacts`

--- migrations/V195__Rename_Office_Health.sql
+++ migrations/V195__Rename_Office_Health.sql
@@ -1,3 +1,7 @@
 UPDATE system_intakes
 SET component = 'Office of Healthcare Experience and Interoperability'
 WHERE component = 'Office of Burden Reduction and Health Informatics';
+
+UPDATE system_intake_contacts
+SET component = 'Office of Healthcare Experience and Interoperability'
+WHERE component = 'Office of Burden Reduction and Health Informatics';
