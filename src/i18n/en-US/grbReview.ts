33ca25eeaa07fe951e28cba7af348be2cda661ba
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2024-12-19 09:50:37 -0600

    Text update

--- src/i18n/en-US/grbReview.ts
+++ src/i18n/en-US/grbReview.ts
@@ -54,7 +54,7 @@ export default {
     'Many projects coming through the IT Governance process require review by the Governance Review Board (GRB). These reviews may happen at regularly scheduled GRB meetings or during an alternate asynchronous review process. GRB members will review project details, discuss any concerns, and vote on the merits of the project under consideration. ',
   featureInProgress: 'Feature in progress',
   featureInProgressText:
-    'The capabilities currently available allow Governance Admin Team members to invite GRB reviewers to access documentation related to requests. Future capabilities will include voting, discussions with project teams and GRB members, timelines, and more. Currently, however, all communication with the GRB and the project team must be completed via email or during meetings. If you have any questions, please complete the <a>feedback form</a> in the footer of this page.',
+    'The capabilities currently available allow Governance Admin Team members to invite GRB reviewers to access documentation related to requests. Future capabilities will include voting, discussions with project teams, timelines, and more. Currently, however, all communication with the project team must be completed via email or during meetings. If you have any questions, please complete the <a>feedback form</a> in the footer of this page.',
   startGrbReview: 'Start GRB review',
   reviewStartedOn: 'Review started on {{date}}',
   startReviewModal: {
