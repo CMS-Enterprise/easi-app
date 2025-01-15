ca9e21f909bfd02a4245b1a64785c9fe1bf3ad54
samoddball <sam.lawson@oddball.io>
2025-01-15 14:37:45 -0700

    err when row not found

--- pkg/storage/system_intake_grb_presentation_links.go
+++ pkg/storage/system_intake_grb_presentation_links.go
@@ -2,15 +2,32 @@ package storage
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/google/uuid"
+	"go.uber.org/zap"
 
+	"github.com/cms-enterprise/easi-app/pkg/appcontext"
 	"github.com/cms-enterprise/easi-app/pkg/sqlqueries"
 )
 
 func (s *Store) DeleteSystemIntakeGRBPresentationLinks(ctx context.Context, systemIntakeID uuid.UUID) error {
-	_, err := namedExec(ctx, s.db, sqlqueries.SystemIntakeGRBPresentationLinks.Delete, args{
+	res, err := namedExec(ctx, s.db, sqlqueries.SystemIntakeGRBPresentationLinks.Delete, args{
 		"system_intake_id": systemIntakeID,
 	})
-	return err
+	if err != nil {
+		appcontext.ZLogger(ctx).Error("failed to delete GRB presentation links", zap.Error(err))
+		return err
+	}
+
+	rowsAffected, err := res.RowsAffected()
+	if err != nil {
+		appcontext.ZLogger(ctx).Error("failed to get rows affected when deleting GRB presentation links", zap.Error(err))
+	}
+
+	if rowsAffected != 1 {
+		return fmt.Errorf("problem deleting GRB presentation links, expected 1 row deleted, got %d", rowsAffected)
+	}
+
+	return nil
 }
