0b9006fc87d0304e6f48f1e9f35a006859d8191f
samoddball <sam.lawson@oddball.io>
2025-01-23 10:45:57 -0700

    initial tests - will rework the entire testing file to be cohesive

--- pkg/storage/system_intake_grb_presentation_links_test.go
+++ pkg/storage/system_intake_grb_presentation_links_test.go
@@ -6,12 +6,55 @@ import (
 	"github.com/google/uuid"
 	"github.com/guregu/null"
 
+	"github.com/cms-enterprise/easi-app/pkg/appcontext"
+	"github.com/cms-enterprise/easi-app/pkg/helpers"
 	"github.com/cms-enterprise/easi-app/pkg/models"
 )
 
+func (s *StoreTestSuite) TestSetSystemIntakeGRBPresentationLinks() {
+	ctx := context.Background()
+	euaID := "ABCD"
+
+	intake, err := s.store.CreateSystemIntake(ctx, &models.SystemIntake{
+		RequestType: models.SystemIntakeRequestTypeNEW,
+		EUAUserID:   null.StringFrom(euaID),
+	})
+	s.NoError(err)
+	s.NotNil(intake)
+
+	// build links
+	links := models.NewSystemIntakeGRBPresentationLinks(appcontext.Principal(ctx).Account().ID)
+	links.SystemIntakeID = intake.ID
+	links.RecordingLink = helpers.PointerTo("test recording link")
+	links.RecordingPasscode = helpers.PointerTo("secret password")
+	links.PresentationDeckS3Key = helpers.PointerTo("prez deck s3 key")
+	links.PresentationDeckFileName = helpers.PointerTo("prez file name")
+
+	// set initial links
+	s.Run("set links on a system intake", func() {
+		out, err := s.store.SetSystemIntakeGRBPresentationLinks(ctx, links)
+		s.NoError(err)
+		s.NotNil(out)
+
+		// check that those links can be retrieved
+
+		data, err := s.store.SystemIntakeGRBPresentationLinksByIntakeIDs(ctx, []uuid.UUID{intake.ID})
+		s.NoError(err)
+		s.Len(data, 1)
+
+		s.NotNil(data[0].RecordingLink)
+		s.NotNil(data[0].RecordingPasscode)
+		s.NotNil(data[0].PresentationDeckS3Key)
+		s.NotNil(data[0].PresentationDeckFileName)
+
+		s.Nil(data[0].TranscriptLink)
+		s.Nil(data[0].TranscriptS3Key)
+		s.Nil(data[0].TranscriptFileName)
+	})
+}
+
 func (s *StoreTestSuite) TestSystemIntakeGRBPresentationLinksByIntakeIDs() {
 	ctx := context.Background()
-
 	euaID := "ABCD"
 
 	intake, err := s.store.CreateSystemIntake(ctx, &models.SystemIntake{
