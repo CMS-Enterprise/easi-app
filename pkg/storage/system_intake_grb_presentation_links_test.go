ea0a88aad9affd309868b75a05a1e4216c65418c
samoddball <sam.lawson@oddball.io>
2025-01-23 13:28:50 -0700

    combine tests and clean up

--- pkg/storage/system_intake_grb_presentation_links_test.go
+++ pkg/storage/system_intake_grb_presentation_links_test.go
@@ -65,41 +65,15 @@ func (s *StoreTestSuite) TestSystemIntakeGRBPresentationLinks() {
 		s.NotNil(data[0].RecordingLink)
 
 		s.Equal(*data[0].RecordingLink, newRecordingLink)
-	})
 
-}
+		// delete these links
+		err = s.store.DeleteSystemIntakeGRBPresentationLinks(ctx, intake.ID)
+		s.NoError(err)
 
-func (s *StoreTestSuite) TestSystemIntakeGRBPresentationLinksByIntakeIDs() {
-	ctx := context.Background()
-	euaID := "ABCD"
-
-	intake, err := s.store.CreateSystemIntake(ctx, &models.SystemIntake{
-		RequestType: models.SystemIntakeRequestTypeNEW,
-		EUAUserID:   null.StringFrom(euaID),
-	})
-	s.NoError(err)
-	s.NotNil(intake)
-
-	s.Run("query given intake id", func() {
-		data, err := s.store.SystemIntakeGRBPresentationLinksByIntakeIDs(ctx, []uuid.UUID{intake.ID})
+		// confirm deleted
+		data, err = s.store.SystemIntakeGRBPresentationLinksByIntakeIDs(ctx, []uuid.UUID{intake.ID})
 		s.NoError(err)
 		s.Len(data, 0)
 	})
-}
 
-func (s *StoreTestSuite) TestDeleteSystemIntakeGRBPresentationLinks() {
-	ctx := context.Background()
-	euaID := "ABCD"
-
-	intake, err := s.store.CreateSystemIntake(ctx, &models.SystemIntake{
-		RequestType: models.SystemIntakeRequestTypeNEW,
-		EUAUserID:   null.StringFrom(euaID),
-	})
-	s.NoError(err)
-	s.NotNil(intake)
-
-	s.Run("test error case (only case for now)", func() {
-		err := s.store.DeleteSystemIntakeGRBPresentationLinks(ctx, intake.ID)
-		s.Error(err)
-	})
 }
