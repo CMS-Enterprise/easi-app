53a7c85d05ac4af3ef7e0e5a32a8317a1b1ae9ca
Gary Zhao <gary.j.zhao@gmail.com>
2025-01-28 13:40:01 -0500

    Add description to translations

--- src/i18n/en-US/externalLinkModal.ts
+++ src/i18n/en-US/externalLinkModal.ts
@@ -1,6 +1,16 @@
 const externalLinkModal = {
   heading: 'You are leaving EASi.',
   genericHeading: 'Are you sure you want to leave EASi?',
+  description: {
+    cfacts:
+      'CFACTS requires connectivity via VPN and may require job codes and/or permissions in order to access specific content in the system.',
+    cloud:
+      'The Cloud homepage requires connectivity via VPN and may require job codes and/or permissions in order to access specific content on the site.',
+    generic:
+      'If you are accessing another CMS system or site, it could require connectivity via VPN and/or job codes and permissions to access content.',
+    contactInfo:
+      'Please contact the CMS IT Service Desk at 410-786-2580 or 800-562-1963 if you need assistance.'
+  },
   redirectingCopy: 'This link is redirecting you to:',
   notConfident:
     'If you are not confident in the safety of this site, you may return to EASi.',
