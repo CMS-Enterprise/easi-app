e5db87be4bcb80fce3b61e28059316176624c59f
Gary Zhao <gary.j.zhao@gmail.com>
2025-01-28 13:41:33 -0500

    Add more conditioanl renders for the modal

--- src/components/shared/ExternalLinkAndModal/index.tsx
+++ src/components/shared/ExternalLinkAndModal/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Button } from '@trussworks/react-uswds';
+import { Button, Icon } from '@trussworks/react-uswds';
 
 import Modal from 'components/Modal';
 import PageHeading from 'components/PageHeading';
@@ -9,52 +9,61 @@ import ExternalLink from '../ExternalLink';
 
 const ExternalLinkAndModal = ({
   children,
-  href
+  href,
+  type = 'GENERIC'
 }: {
   children: React.ReactNode;
   href: string;
+  type?: 'CFACTS' | 'CLOUD' | 'GENERIC';
 }) => {
   const { t: externalT } = useTranslation('externalLinkModal');
   const [isModalOpen, setIsModalOpen] = useState(false);
 
+  const renderDescription = () => {
+    const description = {
+      CFACTS: externalT('description.cfacts'),
+      CLOUD: externalT('description.cloud'),
+      GENERIC: externalT('description.generic')
+    };
+    return description[type];
+  };
+
   return (
     <>
-      {/* MODAL TO GO HERE */}
       <Modal
         isOpen={isModalOpen}
         shouldCloseOnOverlayClick
         closeModal={() => setIsModalOpen(false)}
         className="external-link-modal maxw-mobile-lg height-auto"
       >
-        <PageHeading
-          headingLevel="h3"
-          className="margin-top-neg-3 margin-bottom-2"
-        >
-          {externalT('heading')}
+        <PageHeading headingLevel="h3" className="margin-top-0 margin-bottom-2">
+          {externalT('genericHeading')}
         </PageHeading>
 
         <p
-          className="font-body-md line-height-sans-4 margin-top-0 margin-bottom-4"
+          className="font-body-md line-height-sans-4 margin-top-0 margin-bottom-2"
           style={{ whiteSpace: 'break-spaces' }}
         >
-          {toEchimp ? externalT('descriptionEchimp') : externalT('description')}
+          {renderDescription()}
         </p>
+        {!type && (
+          <p
+            className="font-body-md line-height-sans-4 margin-top-0 margin-bottom-3"
+            style={{ whiteSpace: 'break-spaces' }}
+          >
+            {externalT('description.contactInfo')}
+          </p>
+        )}
 
-        {/* <Link
+        <ExternalLink
           href={href}
-          aria-label={
-            toEchimp ? externalT('continueEchimp') : externalT('continueButton')
-          }
-          target="_blank"
-          rel="noopener noreferrer"
           className="usa-button text-white text-no-underline"
-          onClick={() => setIsModalOpen(false)}
-          variant={variant}
+          variant="unstyled"
         >
-          {toEchimp ? externalT('continueEchimp') : externalT('leave')}
-        </Link> */}
-
-        <ExternalLink href={href}>{children}</ExternalLink>
+          {type
+            ? externalT('continueButton', { value: type })
+            : externalT('leaveEasi')}
+        </ExternalLink>
 
         <Button
           type="button"
@@ -64,9 +73,24 @@ const ExternalLinkAndModal = ({
             setIsModalOpen(false);
           }}
         >
-          {externalT('return')}
+          {externalT('goBackToEasi')}
         </Button>
       </Modal>
+      <Button
+        type="button"
+        unstyled
+        onClick={() => {
+          setIsModalOpen(true);
+        }}
+      >
+        <span
+          className="display-flex flex-align-center"
+          style={{ columnGap: '0.25rem' }}
+        >
+          {children}
+          <Icon.Launch />
+        </span>
+      </Button>
     </>
   );
 };
