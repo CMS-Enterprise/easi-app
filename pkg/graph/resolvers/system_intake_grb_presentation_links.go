11d37d88f2850117890ab03e5ad40d92b54ac6d5
ClayBenson94 <clay.benson@oddball.io>
2025-02-17 14:44:19 -0500

    Update backend to nil out transcript file data when uploading a link

--- pkg/graph/resolvers/system_intake_grb_presentation_links.go
+++ pkg/graph/resolvers/system_intake_grb_presentation_links.go
@@ -45,6 +45,11 @@ func SetSystemIntakeGRBPresentationLinks(ctx context.Context, store *storage.Sto
 
 	if value, ok := input.TranscriptLink.ValueOK(); ok {
 		links.TranscriptLink = value
+
+		// setting a transcript link means we need to also un-set all the other transcript-related fields
+		// for the transcript_link_or_doc_null_check SQL check
+		links.TranscriptFileName = nil
+		links.TranscriptS3Key = nil
 	}
 
 	links.ModifiedBy = &userID
