fb6e158708fbc5b96bb5fc9fae02c84e323f0071
Patrick Segura <patrick.segura@oddball.io>
2025-02-18 15:45:18 -0600

    Fixed unit test

--- src/views/GovernanceTaskList/PresentationDeckUpload/index.test.tsx
+++ src/views/GovernanceTaskList/PresentationDeckUpload/index.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { MockedProvider } from '@apollo/client/testing';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import i18next from 'i18next';
 
 import { MessageProvider } from 'hooks/useMessage';
@@ -10,7 +10,7 @@ import PresentationDeckUpload from '.';
 
 describe('UploadForm page', () => {
   it('renders correct navigation text if admin', async () => {
-    const { findByText } = render(
+    render(
       <MemoryRouter>
         <MockedProvider>
           <MessageProvider>
@@ -21,16 +21,16 @@ describe('UploadForm page', () => {
     );
 
     expect(
-      findByText(
+      screen.getAllByText(
         i18next.t<string>(
-          'grbReview:presentationLinks.presentationDeckUpload:dontUploadAdmin'
-        )[0]
-      )
+          'grbReview:presentationLinks.presentationUpload:dontUploadAdmin'
+        )
+      )[0]
     );
   });
 
   it('renders correct navigation text if requester and takes a snapshot', async () => {
-    const { asFragment, findByText } = render(
+    const { asFragment } = render(
       <MemoryRouter>
         <MockedProvider>
           <MessageProvider>
@@ -41,11 +41,11 @@ describe('UploadForm page', () => {
     );
 
     expect(
-      findByText(
+      screen.getAllByText(
         i18next.t<string>(
-          'grbReview:presentationLinks.presentationDeckUpload:dontUploadRequester'
-        )[0]
-      )
+          'grbReview:presentationLinks.presentationUpload:dontUploadRequester'
+        )
+      )[0]
     );
     expect(asFragment()).toMatchSnapshot();
   });
