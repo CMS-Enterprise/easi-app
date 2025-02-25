d56b58cfa1b148a44ff52b3f7dac9442fdbd8a81
Gary Zhao <gary.j.zhao@gmail.com>
2025-02-25 14:38:57 -0500

    update conditnioalrenders

--- src/features/ITGovernance/Requester/TaskList/GovTaskGrbMeeting/index.tsx
+++ src/features/ITGovernance/Requester/TaskList/GovTaskGrbMeeting/index.tsx
@@ -45,51 +45,32 @@ const GovTaskGrbMeeting = ({
             </>
           )}
 
-        {/* Scheduled or attended meeting date info */}
-        {(grbMeetingStatus === ITGovGRBStatus.SCHEDULED ||
-          grbMeetingStatus === ITGovGRBStatus.COMPLETED) &&
-          grbDate && (
-            <Alert slim type="info">
-              {t(
-                `taskList.step.${stepKey}.${
-                  grbMeetingStatus === ITGovGRBStatus.SCHEDULED
-                    ? 'scheduledInfo'
-                    : 'attendedInfo'
-                }`,
-                { date: formatDateUtc(grbDate, 'MMMM d, yyyy') }
-              )}
-            </Alert>
-          )}
+        <div className="margin-top-2">
+          <UswdsReactLink
+            variant="unstyled"
+            className="usa-button"
+            to="/help/it-governance/prepare-for-grb"
+            target="_blank"
+          >
+            {t(`taskList.step.${stepKey}.presentationUploadButton`)}
+          </UswdsReactLink>
+        </div>
 
-        {/* Button to prepare for the grb meeting */}
-        {(grbMeetingStatus === ITGovGRBStatus.READY_TO_SCHEDULE ||
-          grbMeetingStatus === ITGovGRBStatus.SCHEDULED) && (
-          <div className="margin-top-2">
-            <UswdsReactLink
-              variant="unstyled"
-              className="usa-button"
-              to="/help/it-governance/prepare-for-grb"
-              target="_blank"
-            >
-              {t(`taskList.step.${stepKey}.button`)}
-            </UswdsReactLink>
-          </div>
-        )}
-
-        {/* Link to prepare for the grb meeting */}
-        {(grbMeetingStatus === ITGovGRBStatus.CANT_START ||
-          grbMeetingStatus === ITGovGRBStatus.NOT_NEEDED ||
-          grbMeetingStatus === ITGovGRBStatus.AWAITING_DECISION ||
-          grbMeetingStatus === ITGovGRBStatus.COMPLETED) && (
-          <div className="margin-top-2">
-            <UswdsReactLink
-              to="/help/it-governance/prepare-for-grb"
-              target="_blank"
-            >
-              {t(`taskList.step.${stepKey}.link`)}
-            </UswdsReactLink>
-          </div>
-        )}
+        <div className="margin-top-2 display-flex">
+          <UswdsReactLink
+            to="/help/it-governance/prepare-for-grb"
+            target="_blank"
+            className="margin-right-2 padding-right-2 border-right-1px border-base-lighter"
+          >
+            {t(`taskList.step.${stepKey}.button`)}
+          </UswdsReactLink>
+          <UswdsReactLink
+            to="/help/it-governance/prepare-for-grb"
+            target="_blank"
+          >
+            {t(`taskList.step.${stepKey}.learnMore`)}
+          </UswdsReactLink>
+        </div>
       </TaskListDescription>
     </TaskListItem>
   );
--- src/i18n/en-US/itGov.ts
+++ src/i18n/en-US/itGov.ts
@@ -83,6 +83,8 @@ export default {
           'The Governance Admin Team will select whether this review will be completed in a synchronous (meeting) format or an asynchronous review format and will direct you to complete any additional necessary steps. During the review, the Governance Review Board (GRB) will discuss and make a decision based on your Business Case and any recommendations from the Governance Review Team (GRT). You may be asked to answer questions about your project.',
         link: 'Prepare for the GRB review (opens in a new tab)',
         button: 'Prepare for the GRB review',
+        learnMore: 'Learn more about the GRB review types',
+        presentationUploadButton: 'Upload presentation deck',
         scheduledInfo: 'GRB review scheduled for {{date}}.',
         attendedInfo: 'You attended the GRB review on {{date}}.',
         reviewType: '<strong>Review type:</strong> {{type}}',
