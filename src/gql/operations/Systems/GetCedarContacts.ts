f7ad5a21b3d38bf3959b68a35fa6f9e73b70e923
Patrick Segura <patrick.segura@oddball.io>
2025-02-26 13:20:41 -0600

    Added another gql conversion

--- src/gql/operations/Systems/GetCedarContacts.ts
+++ src/gql/operations/Systems/GetCedarContacts.ts
@@ -1,6 +1,6 @@
 import { gql } from '@apollo/client';
 
-export default gql`
+export default gql(/* GraphQL */ `
   query GetCedarContacts($commonName: String!) {
     cedarPersonsByCommonName(commonName: $commonName) {
       commonName
@@ -8,4 +8,4 @@ export default gql`
       euaUserId
     }
   }
-`;
+`);
