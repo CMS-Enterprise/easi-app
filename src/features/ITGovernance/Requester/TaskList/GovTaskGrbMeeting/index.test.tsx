bd1563afd27482e5a3936ed4e0dc476057e1f5f7
Gary Zhao <gary.j.zhao@gmail.com>
2025-03-04 11:34:20 -0500

    Add tests to test uploading/interacting with presentation upload

--- src/features/ITGovernance/Requester/TaskList/GovTaskGrbMeeting/index.test.tsx
+++ src/features/ITGovernance/Requester/TaskList/GovTaskGrbMeeting/index.test.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render, screen } from '@testing-library/react';
-import { SystemIntakeGRBReviewType } from 'gql/generated/graphql';
+import { fireEvent, render, screen } from '@testing-library/react';
+import {
+  SystemIntakeDocumentStatus,
+  SystemIntakeGRBReviewType
+} from 'gql/generated/graphql';
 import i18next from 'i18next';
 import { taskListState } from 'tests/mock/govTaskList';
 
+import { MessageProvider } from 'hooks/useMessage';
 import { ITGovTaskSystemIntake } from 'types/itGov';
 import {
   expectTaskStatusTagToHaveTextKey,
   getByRoleWithNameTextKey,
   getExpectedAlertType
 } from 'utils/testing/helpers';
+import VerboseMockedProvider from 'utils/testing/VerboseMockedProvider';
 
 import GovTaskGrbMeeting from '.';
 
@@ -18,7 +23,11 @@ describe('Gov Task: Attend the GRB meeting statuses', () => {
   function renderGovTaskGrbMeeting(mockdata: ITGovTaskSystemIntake) {
     return render(
       <MemoryRouter>
-        <GovTaskGrbMeeting {...mockdata} />
+        <VerboseMockedProvider>
+          <MessageProvider>
+            <GovTaskGrbMeeting {...mockdata} />
+          </MessageProvider>
+        </VerboseMockedProvider>
       </MemoryRouter>
     );
   }
@@ -175,6 +184,128 @@ describe('Gov Task: Attend the GRB meeting statuses', () => {
       );
       // Link to prep grb meeting
       getByRoleWithNameTextKey('link', 'itGov:taskList.step.grbMeeting.button');
+    });
+
+    it('Opens and closes the remove presentation modal', () => {
+      const modifiedMock = {
+        ...taskListState.grbMeetingInProgressNotScheduled.systemIntake!,
+        grbReviewType: SystemIntakeGRBReviewType.ASYNC
+      };
+      renderGovTaskGrbMeeting(modifiedMock);
+
+      // Click the Learn more button
+      fireEvent.click(
+        screen.getByText(
+          i18next.t<string>('itGov:taskList.step.grbMeeting.learnMore')
+        )
+      );
+      // Expect Modal to pop up
+      expect(
+        screen.getByText(
+          i18next.t<string>(
+            'itGov:taskList.step.grbMeeting.reviewTypeModal.title'
+          )
+        )
+      ).toBeInTheDocument();
+
+      // Click the Go back button
+      fireEvent.click(
+        screen.getByText(
+          i18next.t<string>(
+            'itGov:taskList.step.grbMeeting.reviewTypeModal.goBack'
+          )
+        )
+      );
+      // Expect Modal to close
+      expect(
+        screen.queryByText(
+          i18next.t<string>(
+            'itGov:taskList.step.grbMeeting.reviewTypeModal.title'
+          )
+        )
+      ).not.toBeInTheDocument();
+    });
+
+    describe('Testing Presentation Deck', () => {
+      it('Display document scanning', () => {
+        const modifiedMock = {
+          ...taskListState.grbMeetingInProgressNotScheduled.systemIntake!,
+          grbReviewType: SystemIntakeGRBReviewType.ASYNC,
+          grbPresentationLinks: {
+            __typename: 'SystemIntakeGRBPresentationLinks' as const,
+            presentationDeckFileStatus: SystemIntakeDocumentStatus.PENDING
+          }
+        };
+        renderGovTaskGrbMeeting(modifiedMock);
+
+        expect(
+          screen.getByText(
+            i18next.t<string>('itGov:taskList.step.grbMeeting.scanning')
+          )
+        ).toBeInTheDocument();
+      });
+
+      it('displays the correct presentation file name and buttons work', () => {
+        const modifiedMock = {
+          ...taskListState.grbMeetingInProgressNotScheduled.systemIntake!,
+          grbReviewType: SystemIntakeGRBReviewType.ASYNC,
+          grbPresentationLinks: {
+            __typename: 'SystemIntakeGRBPresentationLinks' as const,
+            presentationDeckFileName: 'presentationDeckFileName.pdf',
+            presentationDeckFileStatus: SystemIntakeDocumentStatus.AVAILABLE,
+            presentationDeckFileURL:
+              'http://example.com/presentationDeckFileName.pdf'
+          }
+        };
+        renderGovTaskGrbMeeting(modifiedMock);
+
+        expect(
+          screen.queryByText(
+            i18next.t<string>('itGov:taskList.step.grbMeeting.scanning')
+          )
+        ).not.toBeInTheDocument();
+
+        expect(
+          screen.getByText(/Uploaded presentation deck:/i)
+        ).toBeInTheDocument();
+
+        // Ensure the filename appears
+        expect(
+          screen.getByText(
+            modifiedMock.grbPresentationLinks?.presentationDeckFileName
+          )
+        ).toBeInTheDocument();
+
+        // Check the "View" link is present with correct URL
+        const links = screen.getAllByRole('link', { name: /View/i });
+        const correctLink = links.find(
+          link =>
+            link.getAttribute('href') ===
+            'http://example.com/presentationDeckFileName.pdf'
+        );
+
+        expect(correctLink).toBeInTheDocument();
+
+        // Ensure the "Remove" button exists
+        expect(
+          screen.getByRole('button', { name: /Remove/i })
+        ).toBeInTheDocument();
+
+        fireEvent.click(screen.getByRole('button', { name: /Remove/i }));
+
+        // Expect Modal to pop up
+        expect(
+          screen.getByText(
+            i18next.t<string>(
+              'itGov:taskList.step.grbMeeting.removeModal.title'
+            )
+          )
+        ).toBeInTheDocument();
+
+        expect(
+          screen.getByRole('button', { name: /Remove presentation/i })
+        ).toBeInTheDocument();
+      });
     });
   });
 });
