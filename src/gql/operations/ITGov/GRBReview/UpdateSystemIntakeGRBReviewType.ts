50c1f4fe13f807ecf52e200d3142fccf14fcf30b
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2025-03-05 11:08:21 -0600

    Remove fragment imports in queries

--- src/gql/operations/ITGov/GRBReview/UpdateSystemIntakeGRBReviewType.ts
+++ src/gql/operations/ITGov/GRBReview/UpdateSystemIntakeGRBReviewType.ts
@@ -1,8 +1,6 @@
 import { gql } from '@apollo/client';
-import SystemIntakeGRBReview from 'gql/operations/Fragments/SystemIntakeGRBReview';
 
 export default gql(/* GraphQL */ `
-  ${SystemIntakeGRBReview}
   mutation UpdateSystemIntakeGRBReviewType(
     $input: updateSystemIntakeGRBReviewTypeInput!
   ) {
