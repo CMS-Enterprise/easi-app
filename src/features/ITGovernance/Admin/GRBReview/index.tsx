627b82f072f836ca5d9d94cdf7d8c82fe703769e
Gary Zhao <gary.j.zhao@gmail.com>
2025-03-06 14:01:49 -0500

    wip

--- src/features/ITGovernance/Admin/GRBReview/index.tsx
+++ src/features/ITGovernance/Admin/GRBReview/index.tsx
@@ -78,6 +78,7 @@ const GRBReview = ({
   });
 
   const history = useHistory();
+  console.log(history);
 
   const { action } = useParams<{
     action?: GRBReviewFormAction;
@@ -443,6 +444,7 @@ const GRBReview = ({
               className="margin-top-4 margin-bottom-6"
             />
 
+            <p>asdf</p>
             <ParticipantsTable
               id={id}
               state={state}
