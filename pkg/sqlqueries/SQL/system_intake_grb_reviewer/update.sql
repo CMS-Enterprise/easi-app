122a65b8e2e20477cd12bc565fcc5802b010a8f9
samoddball <sam.lawson@oddball.io>
2025-03-11 14:39:14 -0700

    postman, fix sql

--- pkg/sqlqueries/SQL/system_intake_grb_reviewer/update.sql
+++ pkg/sqlqueries/SQL/system_intake_grb_reviewer/update.sql
@@ -4,14 +4,18 @@ SET
     voting_role=:voting_role,
     grb_role=:grb_role,
     modified_at=CURRENT_TIMESTAMP,
-    modified_by=:modified_by
-WHERE id=:reviewer_id
+    modified_by=:modified_by,
+    vote=:vote,
+    vote_comment=:vote_comment
+WHERE id = :reviewer_id
 RETURNING
     id,
     system_intake_id,
     user_id,
     voting_role,
     grb_role,
+    vote,
+    vote_comment,
     modified_at,
     modified_by,
     created_at,
