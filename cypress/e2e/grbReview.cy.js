830782749267092cff0aa9088a39b0e19a8fd073
Gary Zhao <gary.j.zhao@gmail.com>
2025-03-25 15:50:44 -0400

    continuing with the standard type

--- cypress/e2e/grbReview.cy.js
+++ cypress/e2e/grbReview.cy.js
@@ -109,14 +109,62 @@ describe('GRB review', () => {
 
 
   it('completes required fields for Standard Review Type', () => {
-    // cy.visit('/');
+    cy.intercept('POST', '/api/graph/query', req => {
+      if (req.body.operationName === 'UpdateSystemIntakeGRBReviewType') {
+        req.alias = 'updateReviewType';
+      }
+      if (req.body.operationName === 'SendPresentationDeckReminder') {
+        req.alias = 'sendReminder';
+      }
+      if (req.body.operationName === 'UpdateSystemIntakeGRBReviewAsyncPresentation') {
+        req.alias = 'updatePresentation';
+      }
+      if (req.body.operationName === 'UpdateSystemIntakeGRBReviewFormInputTimeframeAsync') {
+        req.alias = 'updateAsyncTimeframe';
+      }
+    });
 
-    // cy.contains('button', 'Set up GRB review').click();
+    // cy.visit('/');
+    // Go to a different GRB Review than above
+    cy.visit('/it-governance/69357721-1e0c-4a37-a90f-64bb29814e7a/grb-review');
+
+    cy.contains('button', 'Set up GRB review').click();
 
     cy.url().should('include', '/review-type');
 
-    // Click Standard review
-    // cy.get('input#grbReviewTypeStandard').check({ force: true });
+    cy.get('input#grbReviewTypeStandard').check({ force: true });
+
+    cy.contains('button', 'Next').click();
+
+    cy.wait('@updateReviewType').its('response.statusCode').should('eq', 200);
+
+    cy.get('[data-testid="date-picker-external-input"]').type('01/01/2022');
+
+    cy.get('[data-testid="alert"]').should('be.visible').and('contain.text', "You've entered a date that is in the past. Please double-check to make sure this date is accurate.");
+
+    cy.get('[data-testid="date-picker-external-input"]').clear()
+    cy.get('[data-testid="date-picker-external-input"]').type('01/01/2226');
+
+    // Hit Send reminder button
+    cy.contains('button', 'Send reminder').click();
+    cy.wait('@sendReminder').its('response.statusCode').should('eq', 200);
+
+    cy.contains('button', 'Reminder sent').should('be.disabled');
+
+    cy.contains('button', 'Next').click();
+    cy.wait('@updatePresentation').its('response.statusCode').should('eq', 200);
+
+    // Additional documents page
+    cy.url().should('include', '/documents');
+    cy.contains('button.usa-button.margin-top-0', 'Next').click();
+
+    // Participants page
+    cy.url().should('include', '/participants');
+    cy.contains('button', 'Complete and begin review').click();
+    cy.wait('@updateAsyncTimeframe').its('response.statusCode').should('eq', 200);
+
+    // Returns to the GRB review page
+    cy.url().should('include', '/grb-review');
 
 
   });
