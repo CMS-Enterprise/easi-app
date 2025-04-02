d60f6a31c374322c0e141983967ba9a7c2a2302f
samoddball <sam.lawson@oddball.io>
2025-04-02 10:18:42 -0700

    whoops - remove old test

--- pkg/models/system_intake_grb_voting_information_test.go
+++ pkg/models/system_intake_grb_voting_information_test.go
@@ -156,126 +156,14 @@ func (s *ModelTestSuite) TestQuorumReached() {
 }
 
 func (s *ModelTestSuite) TestVotingStatus() {
-	info := &GRBVotingInformation{}
-
-	// confirm empty intake errors
-	votingStatus := info.VotingStatus()
-	s.Equal(votingStatus, GRBVSNotStarted)
-
-	info.SystemIntake = &SystemIntake{}
-
-	// confirm empty reviewers errors
-	votingStatus = info.VotingStatus()
-	s.Equal(votingStatus, GRBVSNotStarted)
-
-	// add one objection and one no-objection, then add one non-voting role vote to make sure we don't count it
-	info.GRBReviewers = []*SystemIntakeGRBReviewer{
-		{
-			GRBVotingRole: SystemIntakeGRBReviewerVotingRoleVoting,
-			Vote:          helpers.PointerTo(SystemIntakeAsyncGRBVotingOptionNoObjection),
-		},
-		{
-			GRBVotingRole: SystemIntakeGRBReviewerVotingRoleVoting,
-			Vote:          helpers.PointerTo(SystemIntakeAsyncGRBVotingOptionNoObjection),
-		},
-		{
-			GRBVotingRole: SystemIntakeGRBReviewerVotingRoleNonVoting,
-			Vote:          helpers.PointerTo(SystemIntakeAsyncGRBVotingOptionNoObjection),
-		},
+	type testCase struct {
+		name     string
+		expected GRBVotingInformationStatus
+		info     *GRBVotingInformation
 	}
 
-	// confirm missing startedAt date errors
-	votingStatus = info.VotingStatus()
-	s.Equal(votingStatus, GRBVSNotStarted)
-
-	// put in future for later test
-	info.SystemIntake.GRBReviewStartedAt = helpers.PointerTo(time.Now().AddDate(0, 0, 1))
-
-	// confirm missing end date errors
-	votingStatus = info.VotingStatus()
-	s.Equal(votingStatus, GRBVSNotStarted)
-
-	info.SystemIntake.GrbReviewAsyncEndDate = helpers.PointerTo(time.Now().AddDate(0, 0, 2))
-
-	// confirm `not started` when start date is in the future
-	votingStatus = info.VotingStatus()
-	s.Equal(votingStatus, GRBVSNotStarted)
-
-	// set startedAt date in the past to get `in progress`
-	info.SystemIntake.GRBReviewStartedAt = helpers.PointerTo(time.Now().AddDate(0, 0, -1))
-
-	votingStatus = info.VotingStatus()
-	s.Equal(votingStatus, GRBVSInProgress)
-
-	// set end date in the past, should get `in progress` because we don't have quorum at this point
-	// move start date back two days
-	info.SystemIntake.GRBReviewStartedAt = helpers.PointerTo(time.Now().AddDate(0, 0, -2))
-	// move end date back one day
-	info.SystemIntake.GrbReviewAsyncEndDate = helpers.PointerTo(time.Now().AddDate(0, 0, -1))
-
-	votingStatus = info.VotingStatus()
-	s.Equal(votingStatus, GRBVSInProgress)
-
-	// add more no-objection votes (zero objection votes at this point)
-	// should get `Approved`
-	info.GRBReviewers = append(info.GRBReviewers,
-		&SystemIntakeGRBReviewer{
-			GRBVotingRole: SystemIntakeGRBReviewerVotingRoleVoting,
-			Vote:          helpers.PointerTo(SystemIntakeAsyncGRBVotingOptionNoObjection),
-		},
-		&SystemIntakeGRBReviewer{
-			GRBVotingRole: SystemIntakeGRBReviewerVotingRoleVoting,
-			Vote:          helpers.PointerTo(SystemIntakeAsyncGRBVotingOptionNoObjection),
-		},
-		&SystemIntakeGRBReviewer{
-			GRBVotingRole: SystemIntakeGRBReviewerVotingRoleVoting,
-			Vote:          helpers.PointerTo(SystemIntakeAsyncGRBVotingOptionNoObjection),
-		},
-		&SystemIntakeGRBReviewer{
-			GRBVotingRole: SystemIntakeGRBReviewerVotingRoleVoting,
-			Vote:          helpers.PointerTo(SystemIntakeAsyncGRBVotingOptionNoObjection),
-		})
-
-	votingStatus = info.VotingStatus()
-	s.Equal(votingStatus, GRBVSApproved)
-
-	// add two objections, should get `Not Approved`
-	info.GRBReviewers = append(info.GRBReviewers,
-		&SystemIntakeGRBReviewer{
-			GRBVotingRole: SystemIntakeGRBReviewerVotingRoleVoting,
-			Vote:          helpers.PointerTo(SystemIntakeAsyncGRBVotingOptionObjection),
-		},
-		&SystemIntakeGRBReviewer{
-			GRBVotingRole: SystemIntakeGRBReviewerVotingRoleVoting,
-			Vote:          helpers.PointerTo(SystemIntakeAsyncGRBVotingOptionObjection),
-		},
-	)
-
-	votingStatus = info.VotingStatus()
-	s.Equal(votingStatus, GRBVSNotApproved)
-
-	// remove one of the objections, should get `Inconclusive`
-	info.GRBReviewers = info.GRBReviewers[:len(info.GRBReviewers)-1]
-
-	votingStatus = info.VotingStatus()
-	s.Equal(votingStatus, GRBVSInconclusive)
-
-	// add a manual end date of yesterday
-	info.SystemIntake.GrbReviewAsyncManualEndDate = helpers.PointerTo(time.Now().AddDate(0, 0, -1))
-
-	votingStatus = info.VotingStatus()
-	s.Equal(votingStatus, GRBVSInconclusive)
-}
-
-type votingStatusTestCase struct {
-	name     string
-	expected GRBVotingInformationStatus
-	info     *GRBVotingInformation
-}
-
-func (s *ModelTestSuite) TestVotingStatus2() {
 	now := time.Now()
-	testCases := []votingStatusTestCase{
+	testCases := []testCase{
 		{
 			name:     "Empty intake",
 			expected: GRBVSNotStarted,
@@ -482,10 +370,10 @@ func (s *ModelTestSuite) TestVotingStatus2() {
 		},
 	}
 
-	for _, testCase := range testCases {
-		s.Run(testCase.name, func() {
-			actual := testCase.info.VotingStatus()
-			s.Equal(testCase.expected, actual)
+	for _, tc := range testCases {
+		s.Run(tc.name, func() {
+			actual := tc.info.VotingStatus()
+			s.Equal(tc.expected, actual)
 		})
 	}
 }
