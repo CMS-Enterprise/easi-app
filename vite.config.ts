7dcd893f454210f4fcc5b35736e082dde3f57aa6
Gary Zhao <gary.j.zhao@gmail.com>
2025-04-09 14:48:51 -0400

    addressing ERR_REQUIRE_ESM error from failign test

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,12 @@
-/// <reference types="vitest" />
 import react from '@vitejs/plugin-react';
 import autoprefixer from 'autoprefixer';
 import { defineConfig } from 'vite';
 import svgrPlugin from 'vite-plugin-svgr';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   build: {
     outDir: 'build',
-    // Node modules that use `require` statements need to be transpiled to use `import`
-    // One of the problematic modules is (at time of writing) @okta/okta-signin-widget
     commonjsOptions: {
       transformMixedEsModules: true
     }
