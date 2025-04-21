8eb4bdbc8bb54b1ddbd5d7f82f2c0218a5ed5ff2
samoddball <sam.lawson@oddball.io>
2025-04-21 09:27:47 -0700

    update test, add todo

--- pkg/scheduler/grb_email_jobs_test.go
+++ pkg/scheduler/grb_email_jobs_test.go
@@ -14,11 +14,11 @@ func (suite *SchedulerTestSuite) TestSendAsyncVotingHalfwayThroughEmailJobFuncti
 
 	stubJob := suite.NewScheduledJobStub(testScheduler)
 
-	// create test intake that is more than halfway through (started 4 days ago, ends tomorrow)
+	// create test intake that is halfway through (started 3 days ago, ends in 4 days)
 	now := time.Now()
 	testIntake := &models.SystemIntake{
-		GRBReviewStartedAt:    helpers.PointerTo(now.AddDate(0, 0, -4)),
-		GrbReviewAsyncEndDate: helpers.PointerTo(now.AddDate(0, 0, 1)),
+		GRBReviewStartedAt:    helpers.PointerTo(now.AddDate(0, 0, -3)),
+		GrbReviewAsyncEndDate: helpers.PointerTo(now.AddDate(0, 0, 4)),
 		Step:                  models.SystemIntakeStepINITIALFORM,
 		RequestType:           models.SystemIntakeRequestTypeNEW,
 	}
@@ -26,6 +26,8 @@ func (suite *SchedulerTestSuite) TestSendAsyncVotingHalfwayThroughEmailJobFuncti
 	suite.NoError(err)
 	suite.NotNil(createdIntake)
 
+	// TODO: return successCount/failureCount from these functions and update `OneTimeJob` to take a more generic style `func (ctx) error` or something similar
+	// once that is done, we can test if this worked vs just not-errored
 	err = sendAsyncVotingHalfwayThroughEmailJobFunction(suite.testConfigs.Context, stubJob)
 	suite.NoError(err)
 }
