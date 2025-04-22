5071762e016f03166076429ee2a45c74ab8adb13
samoddball <sam.lawson@oddball.io>
2025-04-22 12:19:21 -0700

    ad initial test, fix sql

--- pkg/sqlqueries/SQL/system_intake/get_where_grb_review_past_due_no_quorum.sql
+++ pkg/sqlqueries/SQL/system_intake/get_where_grb_review_past_due_no_quorum.sql
@@ -1,18 +1,16 @@
 SELECT *
 FROM system_intakes si
-LEFT JOIN LATERAL (
+WHERE (
     SELECT COUNT(id)
     FROM system_intake_grb_reviewers sigr
     WHERE
         sigr.id = si.id
         AND sigr.voting_role = 'VOTING'
         AND sigr.vote IS NOT NULL
-) AS vote_count ON TRUE
-WHERE
-    vote_count < 5
-    AND si.grb_review_type = 'ASYNC'
-    AND si.grb_review_started_at IS NOT NULL
-    AND si.grb_review_async_end_date IS NOT NULL
-    AND si.archived_at IS NULL
-    AND si.grb_review_async_end_date BETWEEN NOW() AND (NOW() - INTERVAL '1 DAY')
+) > 5
+AND si.grb_review_type = 'ASYNC'
+AND si.grb_review_started_at IS NOT NULL
+AND si.grb_review_async_end_date IS NOT NULL
+AND si.archived_at IS NULL
+AND si.grb_review_async_end_date BETWEEN NOW() AND (NOW() - INTERVAL '1 DAY')
 ORDER BY si.created_at DESC;
