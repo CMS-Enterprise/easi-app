b93ce633ab5e1bbdea7c362952e88824e46cc334
samoddball <sam.lawson@oddball.io>
2025-05-28 11:34:53 -0700

    add retired and retiring soon

--- cmd/devdata/main.go
+++ cmd/devdata/main.go
@@ -964,7 +964,6 @@ func main() {
 			completeOtherSteps: true,
 		},
 	)
-	// Don't add new requests here as the Cypress tests are reliant on their intakes showing up on the first page of results
 
 	// expiring soon
 	intakeID = uuid.New()
@@ -1010,8 +1009,55 @@ func main() {
 		time.Now().AddDate(0, 0, -4),
 	)
 
-	//
-	//retireLCID()
+	// retiring soon
+	intakeID = uuid.New()
+	intake = makeSystemIntakeAndIssueLCID(
+		ctx,
+		"With LCID Retiring soon",
+		&intakeID,
+		mock.PrincipalUser,
+		store,
+		time.Now().AddDate(2, 0, 0),
+	)
+
+	retireLCID(ctx, store, intake, time.Now().AddDate(0, 0, 4))
+
+	intakeID = uuid.New()
+	intake = makeSystemIntakeAndIssueLCID(
+		ctx,
+		"With LCID Retiring soon 2",
+		&intakeID,
+		mock.AccessibilityUser,
+		store,
+		time.Now().AddDate(2, 0, 0),
+	)
+
+	retireLCID(ctx, store, intake, time.Now().AddDate(0, 0, 5))
+
+	// retired
+	intakeID = uuid.New()
+	intake = makeSystemIntakeAndIssueLCID(
+		ctx,
+		"With LCID already retired",
+		&intakeID,
+		mock.PrincipalUser,
+		store,
+		time.Now().AddDate(2, 0, 0),
+	)
+
+	retireLCID(ctx, store, intake, time.Now().AddDate(0, 0, -5))
+
+	intakeID = uuid.New()
+	intake = makeSystemIntakeAndIssueLCID(
+		ctx,
+		"With LCID already retired 2",
+		&intakeID,
+		mock.AccessibilityUser,
+		store,
+		time.Now().AddDate(2, 0, 0),
+	)
+
+	retireLCID(ctx, store, intake, time.Now().AddDate(0, 0, -4))
 }
 
 func must(_ interface{}, err error) {
