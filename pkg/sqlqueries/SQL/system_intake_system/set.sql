ae3eb5072e6d9d3af47053d9eb2a193329c0795e
Cooper Heinrichs <cooper.heinrichs@oddball.io>
2025-05-30 16:05:34 -0600

    update the set sql

--- pkg/sqlqueries/SQL/system_intake_system/set.sql
+++ pkg/sqlqueries/SQL/system_intake_system/set.sql
@@ -3,12 +3,16 @@ INSERT INTO system_intake_systems (
     system_intake_id,
     system_id,
     created_by,
-    modified_by
+    modified_by,
+    relationship_type,
+    other_system_relationship
 )
 VALUES (
     :id,
     :system_intake_id,
     :system_id,
     :created_by,
-    :modified_by
+    :modified_by,
+    relationship_type,
+    other_system_relationship
 ) ON CONFLICT DO NOTHING;
