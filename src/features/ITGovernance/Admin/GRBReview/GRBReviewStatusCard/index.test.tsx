5b29cd81e06f10672dd9995077a8d3f3119a537b
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2025-06-04 15:45:53 -0500

    Unit tests

--- src/features/ITGovernance/Admin/GRBReview/GRBReviewStatusCard/index.test.tsx
+++ src/features/ITGovernance/Admin/GRBReview/GRBReviewStatusCard/index.test.tsx
@@ -131,16 +131,19 @@ describe('GRBReviewStatusCard', () => {
     ).toBeInTheDocument();
   });
 
-  it('renders nothing if grbReviewStartedAt is not set', () => {
+  it('renders if review has not started', () => {
     const reviewWithoutStartDate = {
       ...mockStandardReview,
       grbReviewStartedAt: null
     };
 
-    const { container } = renderComponent(reviewWithoutStartDate);
+    renderComponent(reviewWithoutStartDate);
 
-    // Ensure the component renders nothing
-    expect(container.firstChild).toBeNull();
+    expect(screen.getByTestId('async-status')).toHaveTextContent('Not started');
+
+    expect(
+      screen.getByRole('link', { name: 'Set up GRB review' })
+    ).toBeInTheDocument();
   });
 
   it('renders GRB reviewer view - in progress', () => {
