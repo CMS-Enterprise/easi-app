979b181a833c637e174f7c8e2a12aa2594c0d711
samoddball <sam.lawson@oddball.io>
2025-06-10 10:05:55 -0700

    sqlfluff

--- pkg/sqlqueries/SQL/system_intake/get_by_grb_reviewer_id.sql
+++ pkg/sqlqueries/SQL/system_intake/get_by_grb_reviewer_id.sql
@@ -1,5 +1,7 @@
 SELECT si.*
-FROM system_intake_grb_reviewers  sigr
-JOIN system_intakes si  ON si.id = sigr.system_intake_id
-WHERE sigr.id = :grb_reviewer_id AND si.archived_at IS NULL
+FROM system_intake_grb_reviewers sigr
+JOIN system_intakes si ON si.id = sigr.system_intake_id
+WHERE
+    sigr.id = :grb_reviewer_id
+    AND si.archived_at IS NULL
 LIMIT 1;
