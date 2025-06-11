3da812604c76b8dfbd37c2bba3a2e440ce6c027b
Patrick Segura <patrick.segura@oddball.io>
2025-06-11 12:42:57 -0500

    updated commented out unit test

--- src/features/ITGovernance/Requester/BusinessCase/AlternativeSolution/AlternativeSolutionA.test.tsx
+++ src/features/ITGovernance/Requester/BusinessCase/AlternativeSolution/AlternativeSolutionA.test.tsx
@@ -4,17 +4,18 @@ import { MemoryRouter, Route } from 'react-router-dom';
 import {
   render,
   screen,
+  waitFor,
   waitForElementToBeRemoved
 } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
+import userEvent from '@testing-library/user-event';
 import BusinessCase from 'features/ITGovernance/Requester/BusinessCase';
 import { SystemIntakeStep } from 'gql/generated/graphql';
 import configureMockStore from 'redux-mock-store';
 import { getGovernanceTaskListQuery } from 'tests/mock/systemIntake';
 
 import {
-  businessCaseInitialData
-  // defaultProposedSolution
+  businessCaseInitialData,
+  defaultProposedSolution
 } from 'data/businessCase';
 import VerboseMockedProvider from 'utils/testing/VerboseMockedProvider';
 
@@ -23,12 +24,16 @@ const SYSTEM_INTAKE_ID = '943916ee-7a30-4213-990e-02c4fb97382a';
 const renderPage = async (
   store: any,
   systemIntakeId: string = SYSTEM_INTAKE_ID,
-  step: SystemIntakeStep = SystemIntakeStep.DRAFT_BUSINESS_CASE
+  step: SystemIntakeStep = SystemIntakeStep.DRAFT_BUSINESS_CASE,
+  alternative:
+    | 'alternative-solution-a'
+    | 'alternative-solution-b'
+    | 'alternative-analysis' = 'alternative-solution-a'
 ) => {
   render(
     <MemoryRouter
       initialEntries={[
-        '/business/75746af8-9a9b-4558-a375-cf9848eb2b0d/alternative-solution-a'
+        `/business/75746af8-9a9b-4558-a375-cf9848eb2b0d/${alternative}`
       ]}
     >
       <Provider store={store}>
@@ -82,115 +87,120 @@ describe('Business case alternative a solution', () => {
     expect(screen.getByTestId('alternative-solution-a')).toBeInTheDocument();
   });
 
-  // it('does not run validations', async () => {
-  //   await renderPage(defaultStore);
+  it('adds alternative a and navigates to it', async () => {
+    await renderPage(defaultStore);
 
-  //   await userEvent.click(screen.getByRole('button', { name: /back/i }));
+    userEvent.click(
+      screen.getByRole('button', { name: /Finish alternative A/i })
+    );
 
-  //   expect(screen.getByTestId('preferred-solution')).toBeInTheDocument();
-  // });
+    await waitFor(() => {
+      expect(screen.getByTestId('alternative-solution-a')).toBeInTheDocument();
+    });
+  });
 
-  // it('adds alternative b and navigates to it', async () => {
-  //   await renderPage(defaultStore);
+  describe('with alternative b', () => {
+    const withAlternativeBStore = mockStore({
+      auth: {
+        euaId: 'AAAA'
+      },
+      businessCase: {
+        form: {
+          ...businessCaseInitialData,
+          id: '75746af8-9a9b-4558-a375-cf9848eb2b0d',
+          systemIntakeId: SYSTEM_INTAKE_ID,
+          alternativeB: {
+            ...defaultProposedSolution,
+            title: 'Alt B'
+          }
+        },
+        isLoading: false,
+        isSaving: false,
+        error: null
+      },
+      action: {
+        isPosting: false,
+        error: null,
+        actions: []
+      }
+    });
 
-  //   await userEvent.click(
-  //     screen.getByRole('button', { name: /alternative b/i })
-  //   );
+    it('hides adding alternative b when it exists already', async () => {
+      await renderPage(
+        withAlternativeBStore,
+        undefined,
+        undefined,
+        'alternative-analysis'
+      );
 
-  //   expect(screen.getByTestId('alternative-solution-b')).toBeInTheDocument();
-  // });
+      expect(
+        screen.queryByRole('button', { name: /Add alternative B/i })
+      ).not.toBeInTheDocument();
+    });
 
-  // it('navigates back to alternative analysis', async () => {
-  //   await renderPage(defaultStore);
+    it('navigates forward to alternative b', async () => {
+      await renderPage(
+        withAlternativeBStore,
+        undefined,
+        undefined,
+        'alternative-solution-b'
+      );
 
-  //   await userEvent.click(screen.getByRole('button', { name: /next/i }));
+      userEvent.click(
+        screen.getByRole('button', { name: /Finish alternative B/i })
+      );
 
-  //   expect(screen.getByTestId('business-case-review')).toBeInTheDocument();
-  // });
+      await waitFor(() => {
+        expect(
+          screen.getByTestId('alternative-solution-b')
+        ).toBeInTheDocument();
+      });
+    });
+  });
 
-  // describe('with alternative b', () => {
-  //   const withAlternativeBStore = mockStore({
-  //     auth: {
-  //       euaId: 'AAAA'
-  //     },
-  //     businessCase: {
-  //       form: {
-  //         ...businessCaseInitialData,
-  //         id: '75746af8-9a9b-4558-a375-cf9848eb2b0d',
-  //         systemIntakeId: SYSTEM_INTAKE_ID,
-  //         alternativeB: {
-  //           ...defaultProposedSolution,
-  //           title: 'Alt B'
-  //         }
-  //       },
-  //       isLoading: false,
-  //       isSaving: false,
-  //       error: null
-  //     },
-  //     action: {
-  //       isPosting: false,
-  //       error: null,
-  //       actions: []
-  //     }
-  //   });
+  describe('BIZ_CASE_FINAL_NEEDED', () => {
+    const bizCaseFinalStore = mockStore({
+      auth: {
+        euaId: 'AAAA'
+      },
+      businessCase: {
+        form: {
+          ...businessCaseInitialData,
+          id: '75746af8-9a9b-4558-a375-cf9848eb2b0d',
+          systemIntakeId: 'a4158ad8-1236-4a55-9ad5-7e15a5d49de2'
+        },
+        isLoading: false,
+        isSaving: false,
+        error: null
+      },
+      action: {
+        isPosting: false,
+        error: null,
+        actions: []
+      }
+    });
 
-  //   it('hides adding alternative b when it exists already', async () => {
-  //     await renderPage(withAlternativeBStore);
+    it('renders validation errors', async () => {
+      await renderPage(
+        bizCaseFinalStore,
+        'a4158ad8-1236-4a55-9ad5-7e15a5d49de2',
+        SystemIntakeStep.FINAL_BUSINESS_CASE
+      );
 
-  //     expect(
-  //       screen.queryByTestId('draft-business-case-fields-alert')
-  //     ).not.toBeInTheDocument();
-  //   });
+      // Fill one field so we can trigger validation errors
+      const titleField = screen.getByRole('textbox', {
+        name: /title/i
+      });
+      userEvent.type(titleField, 'Alternative A solution title');
+      expect(titleField).toHaveValue('Alternative A solution title');
 
-  //   it('navigates forward to alternative b', async () => {
-  //     await renderPage(withAlternativeBStore);
+      userEvent.click(
+        screen.getByRole('button', { name: /Finish alternative A/i })
+      );
 
-  //     await userEvent.click(screen.getByRole('button', { name: /next/i }));
-
-  //     expect(screen.getByTestId('alternative-solution-b')).toBeInTheDocument();
-  //   });
-  // });
-
-  // describe('BIZ_CASE_FINAL_NEEDED', () => {
-  //   const bizCaseFinalStore = mockStore({
-  //     auth: {
-  //       euaId: 'AAAA'
-  //     },
-  //     businessCase: {
-  //       form: {
-  //         ...businessCaseInitialData,
-  //         id: '75746af8-9a9b-4558-a375-cf9848eb2b0d',
-  //         systemIntakeId: 'a4158ad8-1236-4a55-9ad5-7e15a5d49de2'
-  //       },
-  //       isLoading: false,
-  //       isSaving: false,
-  //       error: null
-  //     },
-  //     action: {
-  //       isPosting: false,
-  //       error: null,
-  //       actions: []
-  //     }
-  //   });
-
-  //   it('renders validation errors', async () => {
-  //     await renderPage(
-  //       bizCaseFinalStore,
-  //       'a4158ad8-1236-4a55-9ad5-7e15a5d49de2',
-  //       SystemIntakeStep.FINAL_BUSINESS_CASE
-  //     );
-
-  //     // Fill one field so we can trigger validation errors
-  //     const titleField = screen.getByRole('textbox', {
-  //       name: /title/i
-  //     });
-  //     userEvent.type(titleField, 'Alternative A solution title');
-  //     expect(titleField).toHaveValue('Alternative A solution title');
-
-  //     await userEvent.click(screen.getByRole('button', { name: /next/i }));
-  //     expect(
-  //       await screen.findByTestId('formik-validation-errors')
-  //     ).toBeInTheDocument();
-  //   });
-  // });
+      expect(
+        await screen.findByTestId('formik-validation-errors')
+      ).toBeInTheDocument();
+    });
+  });
 });
--- src/features/ITGovernance/Requester/BusinessCase/AlternativeSolution/AlternativeSolutionFields.test.tsx
+++ src/features/ITGovernance/Requester/BusinessCase/AlternativeSolution/AlternativeSolutionFields.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
+import { render, screen, within } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { Form, Formik, FormikProps } from 'formik';
 
 import { defaultProposedSolution } from 'data/businessCase';
@@ -39,147 +39,147 @@ describe('Alternative Solution Fields', () => {
     ).toBeInTheDocument();
   });
 
-  // it('fill deepest question branch', () => {
-  //   renderFields();
+  it('fill deepest question branch', () => {
+    renderFields();
 
-  //   const titleField = screen.getByRole('textbox', {
-  //     name: /title/i
-  //   });
-  //   userEvent.type(titleField, 'Alternative A solution title');
-  //   expect(titleField).toHaveValue('Alternative A solution title');
+    const titleField = screen.getByRole('textbox', {
+      name: /title/i
+    });
+    userEvent.type(titleField, 'Alternative A solution title');
+    expect(titleField).toHaveValue('Alternative A solution title');
 
-  //   const summaryField = screen.getByRole('textbox', {
-  //     name: /summary/i
-  //   });
-  //   userEvent.type(summaryField, 'Alternative A solution summary');
-  //   expect(summaryField).toHaveValue('Alternative A solution summary');
+    const summaryField = screen.getByRole('textbox', {
+      name: /summary/i
+    });
+    userEvent.type(summaryField, 'Alternative A solution summary');
+    expect(summaryField).toHaveValue('Alternative A solution summary');
 
-  //   const acquisitionApproachField = screen.getByRole('textbox', {
-  //     name: /acquisition approach/i
-  //   });
-  //   userEvent.type(
-  //     acquisitionApproachField,
-  //     'Alternative A solution acquisition approach'
-  //   );
-  //   expect(acquisitionApproachField).toHaveValue(
-  //     'Alternative A solution acquisition approach'
-  //   );
+    const acquisitionApproachField = screen.getByRole('textbox', {
+      name: /acquisition approach/i
+    });
+    userEvent.type(
+      acquisitionApproachField,
+      'Alternative A solution acquisition approach'
+    );
+    expect(acquisitionApproachField).toHaveValue(
+      'Alternative A solution acquisition approach'
+    );
 
-  //   const securityFieldGroup = screen.getByTestId('security-approval');
-  //   const securityNoRadio = within(securityFieldGroup).getByRole('radio', {
-  //     name: /no/i
-  //   });
-  //   securityNoRadio.click();
-  //   expect(securityNoRadio).toBeChecked();
+    const securityFieldGroup = screen.getByTestId('security-approval');
+    const securityNoRadio = within(securityFieldGroup).getByRole('radio', {
+      name: /no/i
+    });
+    securityNoRadio.click();
+    expect(securityNoRadio).toBeChecked();
 
-  //   const securityApprovalProgressGroup = screen.getByTestId(
-  //     'security-approval-in-progress'
-  //   );
-  //   const securityApprovalProgressYesRadio = within(
-  //     securityApprovalProgressGroup
-  //   ).getByRole('radio', {
-  //     name: /yes/i
-  //   });
-  //   securityApprovalProgressYesRadio.click();
-  //   expect(securityApprovalProgressYesRadio).toBeChecked();
+    const securityApprovalProgressGroup = screen.getByTestId(
+      'security-approval-in-progress'
+    );
+    const securityApprovalProgressYesRadio = within(
+      securityApprovalProgressGroup
+    ).getByRole('radio', {
+      name: /yes/i
+    });
+    securityApprovalProgressYesRadio.click();
+    expect(securityApprovalProgressYesRadio).toBeChecked();
 
-  //   const cloudHostingRadio = screen.getByRole('radio', { name: /cloud/i });
-  //   cloudHostingRadio.click();
-  //   expect(cloudHostingRadio).toBeChecked();
+    const cloudHostingRadio = screen.getByRole('radio', { name: /cloud/i });
+    cloudHostingRadio.click();
+    expect(cloudHostingRadio).toBeChecked();
 
-  //   const hostingLocationField = screen.getByRole('textbox', {
-  //     name: /where are you planning to host/i
-  //   });
-  //   userEvent.type(
-  //     hostingLocationField,
-  //     'Alternative A solution hosting location'
-  //   );
-  //   expect(hostingLocationField).toHaveValue(
-  //     'Alternative A solution hosting location'
-  //   );
+    const hostingLocationField = screen.getByRole('textbox', {
+      name: /where are you planning to host/i
+    });
+    userEvent.type(
+      hostingLocationField,
+      'Alternative A solution hosting location'
+    );
+    expect(hostingLocationField).toHaveValue(
+      'Alternative A solution hosting location'
+    );
 
-  //   const cloudServiceTypeField = screen.getByRole('textbox', {
-  //     name: /cloud service/i
-  //   });
-  //   userEvent.type(
-  //     cloudServiceTypeField,
-  //     'Alternative A solution hosting cloud service'
-  //   );
-  //   expect(cloudServiceTypeField).toHaveValue(
-  //     'Alternative A solution hosting cloud service'
-  //   );
+    const cloudServiceTypeField = screen.getByRole('textbox', {
+      name: /cloud service/i
+    });
+    userEvent.type(
+      cloudServiceTypeField,
+      'Alternative A solution hosting cloud service'
+    );
+    expect(cloudServiceTypeField).toHaveValue(
+      'Alternative A solution hosting cloud service'
+    );
 
-  //   const userInterfaceGroup = screen.getByTestId('user-interface-group');
-  //   const userInterfaceYesRadio = within(userInterfaceGroup).getByRole(
-  //     'radio',
-  //     {
-  //       name: /yes/i
-  //     }
-  //   );
-  //   userInterfaceYesRadio.click();
-  //   expect(userInterfaceYesRadio).toBeChecked();
+    const userInterfaceGroup = screen.getByTestId('user-interface-group');
+    const userInterfaceYesRadio = within(userInterfaceGroup).getByRole(
+      'radio',
+      {
+        name: /yes/i
+      }
+    );
+    userInterfaceYesRadio.click();
+    expect(userInterfaceYesRadio).toBeChecked();
 
-  //   const prosField = screen.getByRole('textbox', {
-  //     name: /pros/i
-  //   });
-  //   userEvent.type(prosField, 'Alternative A solution pros');
-  //   expect(prosField).toHaveValue('Alternative A solution pros');
+    const prosField = screen.getByRole('textbox', {
+      name: /pros/i
+    });
+    userEvent.type(prosField, 'Alternative A solution pros');
+    expect(prosField).toHaveValue('Alternative A solution pros');
 
-  //   const consField = screen.getByRole('textbox', {
-  //     name: /cons/i
-  //   });
-  //   userEvent.type(consField, 'Alternative A solution cons');
-  //   expect(consField).toHaveValue('Alternative A solution cons');
+    const consField = screen.getByRole('textbox', {
+      name: /cons/i
+    });
+    userEvent.type(consField, 'Alternative A solution cons');
+    expect(consField).toHaveValue('Alternative A solution cons');
 
-  //   const costSavingsField = screen.getByRole('textbox', {
-  //     name: /cost savings/i
-  //   });
-  //   userEvent.type(costSavingsField, 'Alternative A solution cost savings');
-  //   expect(costSavingsField).toHaveValue('Alternative A solution cost savings');
-  // }, 10000); // Increase timeout to 10000ms for this test
+    const costSavingsField = screen.getByRole('textbox', {
+      name: /cost savings/i
+    });
+    userEvent.type(costSavingsField, 'Alternative A solution cost savings');
+    expect(costSavingsField).toHaveValue('Alternative A solution cost savings');
+  }, 10000); // Increase timeout to 10000ms for this test
 
-  // it('is approved by cms security', () => {
-  //   renderFields();
+  it('is approved by cms security', () => {
+    renderFields();
 
-  //   const securityApprovalGroup = screen.getByTestId('security-approval');
-  //   const approvedRadio = within(securityApprovalGroup).getByRole('radio', {
-  //     name: /yes/i
-  //   });
-  //   approvedRadio.click();
-  //   expect(approvedRadio).toBeChecked();
-  //   expect(
-  //     screen.queryByTestId('security-approval-in-progress')
-  //   ).not.toBeInTheDocument();
-  // });
+    const securityApprovalGroup = screen.getByTestId('security-approval');
+    const approvedRadio = within(securityApprovalGroup).getByRole('radio', {
+      name: /yes/i
+    });
+    approvedRadio.click();
+    expect(approvedRadio).toBeChecked();
+    expect(
+      screen.queryByTestId('security-approval-in-progress')
+    ).not.toBeInTheDocument();
+  });
 
-  // it('fills out data center branch', () => {
-  //   renderFields();
+  it('fills out data center branch', () => {
+    renderFields();
 
-  //   const dataCenterHostingRadio = screen.getByRole('radio', {
-  //     name: /data center/i
-  //   });
-  //   dataCenterHostingRadio.click();
-  //   expect(dataCenterHostingRadio).toBeChecked();
+    const dataCenterHostingRadio = screen.getByRole('radio', {
+      name: /data center/i
+    });
+    dataCenterHostingRadio.click();
+    expect(dataCenterHostingRadio).toBeChecked();
 
-  //   const dataCenterLocationField = screen.getByRole('textbox', {
-  //     name: /which data center/i
-  //   });
-  //   userEvent.type(
-  //     dataCenterLocationField,
-  //     'Alternative A solution data center location'
-  //   );
-  //   expect(dataCenterLocationField).toHaveValue(
-  //     'Alternative A solution data center location'
-  //   );
-  // });
+    const dataCenterLocationField = screen.getByRole('textbox', {
+      name: /which data center/i
+    });
+    userEvent.type(
+      dataCenterLocationField,
+      'Alternative A solution data center location'
+    );
+    expect(dataCenterLocationField).toHaveValue(
+      'Alternative A solution data center location'
+    );
+  });
 
-  // it('fills out no hosting branch', () => {
-  //   renderFields();
+  it('fills out no hosting branch', () => {
+    renderFields();
 
-  //   const dataCenterHostingRadio = screen.getByRole('radio', {
-  //     name: /hosting is not needed/i
-  //   });
-  //   dataCenterHostingRadio.click();
-  //   expect(dataCenterHostingRadio).toBeChecked();
-  // });
+    const dataCenterHostingRadio = screen.getByRole('radio', {
+      name: /hosting is not needed/i
+    });
+    dataCenterHostingRadio.click();
+    expect(dataCenterHostingRadio).toBeChecked();
+  });
 });
