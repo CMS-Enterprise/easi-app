80390972347596b1b0cfdbdaaf448c3b3aebc897
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2025-07-01 11:51:07 -0500

    New fix for dynamically updating value

--- src/components/DatePickerFormatted/index.tsx
+++ src/components/DatePickerFormatted/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo } from 'react';
+import React, { useEffect, useState } from 'react';
 import { DatePicker } from '@trussworks/react-uswds';
 // eslint-disable-next-line import/no-unresolved
 import { DatePickerProps } from '@trussworks/react-uswds/lib/components/forms/DatePicker/DatePicker';
@@ -7,6 +7,11 @@ import { DateTime } from 'luxon';
 function defaultFormat(dt: DateTime): string | null {
   return dt.toUTC().toISO();
 }
+
+type DatePickerFormattedProps = Omit<DatePickerProps, 'value'> & {
+  format?: (dt: DateTime) => string | null;
+  value?: string;
+};
 
 /**
  * A `DatePicker` wrapper with date formatting. Defaults to the utc iso format.
@@ -17,32 +22,26 @@ const DatePickerFormatted = ({
   onChange,
   format,
   ...props
-}: DatePickerProps & { format?: (dt: DateTime) => string | null }) => {
+}: DatePickerFormattedProps) => {
+  /**
+   * Store value in state to use as key for <DatePicker>.
+   * Fixes bug where <DatePicker> does not rerender to show updated value when set dynamically.
+   * https://github.com/trussworks/react-uswds/issues/3000#issuecomment-2884731383
+   */
+  const [value, setValue] = useState(props.value || props.defaultValue || '');
+
   const dtFormat = format || defaultFormat;
 
-  /** Memoized current field value */
-  const value = useMemo(() => {
-    if (typeof props.value === 'string' && props.value.length > 0) {
-      return props.value;
+  useEffect(() => {
+    // Check if props.value contains a valid string before updating state
+    // This ensures that the key prop is only updated when the value changes
+    if (props.value) {
+      setValue(props.value);
     }
-
-    return props.defaultValue || '';
-  }, [props.defaultValue, props.value]);
-
-  /**
-   * Fix for bug where <DatePicker> does not rerender to show updated value when set dynamically
-   *
-   * Forces re-render of component when props.value or props.defaultValue is updated
-   */
-  const FieldCallback = useCallback(
-    (fieldProps: DatePickerProps) => {
-      return <DatePicker {...fieldProps} defaultValue={value} />;
-    },
-    [value]
-  );
+  }, [props.value]);
 
   return (
-    <FieldCallback
+    <DatePicker
       {...props}
       onChange={val => {
         if (typeof onChange === 'function') {
@@ -55,6 +54,9 @@ const DatePickerFormatted = ({
           }
         }
       }}
+      // Set `defaultValue` and `key` props to the value in state
+      defaultValue={value}
+      key={value}
     />
   );
 };
