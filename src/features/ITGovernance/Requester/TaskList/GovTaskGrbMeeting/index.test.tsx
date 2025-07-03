ee2ca50a0bf0507590b7773775daa4b3aa5e138a
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2025-07-03 14:04:10 -0500

    Remove duplicate unit tests

--- src/features/ITGovernance/Requester/TaskList/GovTaskGrbMeeting/index.test.tsx
+++ src/features/ITGovernance/Requester/TaskList/GovTaskGrbMeeting/index.test.tsx
@@ -2,10 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { MemoryRouter } from 'react-router-dom';
 import { fireEvent, render, screen } from '@testing-library/react';
-import {
-  SystemIntakeDocumentStatus,
-  SystemIntakeGRBReviewType
-} from 'gql/generated/graphql';
+import { SystemIntakeGRBReviewType } from 'gql/generated/graphql';
 import i18next from 'i18next';
 import { getSystemIntakeGRBDiscussionsQuery } from 'tests/mock/discussions';
 import { taskListState } from 'tests/mock/govTaskList';
@@ -281,78 +278,6 @@ describe('Gov Task: Attend the GRB meeting statuses', () => {
       expect(
         screen.getByRole('link', { name: 'Prepare for the GRB review' })
       ).not.toHaveClass('usa-button');
-    });
-  });
-
-  describe('Presentation deck', () => {
-    it('displays document scanning', () => {
-      const modifiedMock = {
-        ...taskListState.grbMeetingInProgressNotScheduled.systemIntake!,
-        grbReviewType: SystemIntakeGRBReviewType.ASYNC,
-        grbPresentationLinks: {
-          __typename: 'SystemIntakeGRBPresentationLinks' as const,
-          presentationDeckFileStatus: SystemIntakeDocumentStatus.PENDING
-        }
-      };
-      renderGovTaskGrbMeeting(modifiedMock);
-
-      expect(
-        screen.getByText(
-          i18next.t<string>('itGov:taskList.step.grbMeeting.scanning')
-        )
-      ).toBeInTheDocument();
-    });
-
-    it('displays the correct presentation file name and buttons work', () => {
-      const modifiedMock = {
-        ...taskListState.grbMeetingInProgressNotScheduled.systemIntake!,
-        grbReviewType: SystemIntakeGRBReviewType.ASYNC,
-        grbPresentationLinks
-      };
-      renderGovTaskGrbMeeting(modifiedMock);
-
-      expect(
-        screen.queryByText(
-          i18next.t<string>('itGov:taskList.step.grbMeeting.scanning')
-        )
-      ).not.toBeInTheDocument();
-
-      expect(
-        screen.getByText(/Uploaded presentation deck:/i)
-      ).toBeInTheDocument();
-
-      // Ensure the filename appears
-      expect(
-        screen.getByText(grbPresentationLinks.presentationDeckFileName!)
-      ).toBeInTheDocument();
-
-      // Check the "View" link is present with correct URL
-      const links = screen.getAllByRole('link', { name: /View/i });
-      const correctLink = links.find(
-        link =>
-          link.getAttribute('href') ===
-          grbPresentationLinks.presentationDeckFileURL
-      );
-
-      expect(correctLink).toBeInTheDocument();
-
-      // Ensure the "Remove" button exists
-      expect(
-        screen.getByRole('button', { name: /Remove/i })
-      ).toBeInTheDocument();
-
-      fireEvent.click(screen.getByRole('button', { name: /Remove/i }));
-
-      // Expect Modal to pop up
-      expect(
-        screen.getByText(
-          i18next.t<string>('itGov:taskList.step.grbMeeting.removeModal.title')
-        )
-      ).toBeInTheDocument();
-
-      expect(
-        screen.getByRole('button', { name: /Remove presentation/i })
-      ).toBeInTheDocument();
     });
   });
 
