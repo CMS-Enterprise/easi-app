c634a3fc5b23c789d21b6624b0ce5133f6580ef3
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2025-07-09 10:39:50 -0500

    View discussion board button styling

--- src/features/ITGovernance/Requester/TaskList/GovTaskGrbMeeting/_components/RequesterDiscussionsCard/index.test.tsx
+++ src/features/ITGovernance/Requester/TaskList/GovTaskGrbMeeting/_components/RequesterDiscussionsCard/index.test.tsx
@@ -54,6 +54,11 @@ describe('Requester discussions card', () => {
       await screen.findByText('There were no discussions during this review.')
     ).toBeInTheDocument();
 
+    // View button is unstyled
+    expect(
+      screen.getByRole('button', { name: 'View discussion board' })
+    ).toHaveClass('usa-button--unstyled');
+
     // Start a discussion button should be hidden when awaiting decision
     expect(
       screen.queryByRole('button', { name: 'Start a discussion' })
@@ -75,6 +80,11 @@ describe('Requester discussions card', () => {
         </VerboseMockedProvider>
       </MemoryRouter>
     );
+
+    // View button is not unstyled
+    expect(
+      screen.getByRole('button', { name: 'View discussion board' })
+    ).not.toHaveClass('usa-button--unstyled');
 
     expect(
       await screen.findByText(
