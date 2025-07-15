c3b4b822e22838bed39771220f2420958ef0c265
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2025-07-15 11:27:14 -0500

    unit tests

--- src/features/ITGovernance/Admin/GRBReview/GRBReviewAdminTask/index.test.tsx
+++ src/features/ITGovernance/Admin/GRBReview/GRBReviewAdminTask/index.test.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render, screen } from '@testing-library/react';
+import { render, screen, within } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { SystemIntakeGRBReviewerVotingRole } from 'gql/generated/graphql';
+import {
+  SendSystemIntakeGRBReviewerReminderDocument,
+  SendSystemIntakeGRBReviewerReminderMutation,
+  SystemIntakeGRBReviewerVotingRole
+} from 'gql/generated/graphql';
 import i18next from 'i18next';
 import { grbReviewers } from 'tests/mock/grbReview';
 import { systemIntake } from 'tests/mock/systemIntake';
 
 import { MessageProvider } from 'hooks/useMessage';
+import { MockedQuery } from 'types/util';
 import { formatDateLocal, formatTimeLocal } from 'utils/date';
 import VerboseMockedProvider from 'utils/testing/VerboseMockedProvider';
 
@@ -117,6 +122,75 @@ describe('GRBReviewAdminTask', () => {
       );
 
       expect(modalText).toBeInTheDocument();
+    });
+
+    it('clears error message on modal close', async () => {
+      const sendReminderMutation: MockedQuery<SendSystemIntakeGRBReviewerReminderMutation> =
+        {
+          request: {
+            query: SendSystemIntakeGRBReviewerReminderDocument,
+            variables: {
+              systemIntakeID: systemIntake.id
+            }
+          },
+          error: new Error()
+        };
+
+      render(
+        <MemoryRouter>
+          <VerboseMockedProvider mocks={[sendReminderMutation]}>
+            <MessageProvider>
+              <GRBReviewAdminTask
+                systemIntakeId={systemIntake.id}
+                grbReviewStartedAt="2025-05-08T14:00:39.089005Z"
+                grbReviewReminderLastSent={null}
+                grbReviewers={[]}
+                isITGovAdmin
+              />
+            </MessageProvider>
+          </VerboseMockedProvider>
+        </MemoryRouter>
+      );
+
+      const adminAction = screen.getByTestId('grb-review-admin-task');
+
+      const openModalButton = within(adminAction).getByRole('button', {
+        name: 'Send reminder'
+      });
+
+      // Open send remindermodal
+
+      userEvent.click(openModalButton);
+
+      const modal = await screen.findByRole('dialog');
+
+      const sendReminderButton = within(modal).getByRole('button', {
+        name: 'Send reminder'
+      });
+
+      // Check that clicking the send reminder button shows an error message
+
+      userEvent.click(sendReminderButton);
+
+      expect(await screen.findByTestId('alert')).toHaveTextContent(
+        'There was an issue sending your reminder.'
+      );
+
+      // Close modal
+
+      userEvent.click(
+        screen.getByRole('button', { name: 'Go back without sending' })
+      );
+
+      expect(modal).not.toBeInTheDocument();
+
+      // Open modal and check that error message has been cleared
+
+      userEvent.click(openModalButton);
+
+      expect(await screen.findByRole('dialog')).toBeInTheDocument();
+
+      expect(screen.queryByTestId('alert')).not.toBeInTheDocument();
     });
   });
 });
