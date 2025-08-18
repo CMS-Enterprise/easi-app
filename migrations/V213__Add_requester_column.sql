aa41b17e17deaf2f30541dffb3c7bba3e0235a2f
samoddball <sam.lawson@oddball.io>
2025-08-18 15:50:20 -0700

    use user id, not eua. add unique constraint

--- migrations/V213__Add_requester_column.sql
+++ migrations/V213__Add_requester_column.sql
@@ -7,26 +7,30 @@ ADD COLUMN roles TEXT[] NOT NULL DEFAULT '{}';
 -- create copy of table for safety and testing purposes
 CREATE TABLE IF NOT EXISTS copy_system_intake_contacts AS TABLE system_intake_contacts;
 
-
 -- operate only on the copy table
 UPDATE copy_system_intake_contacts c
 SET roles = sub.roles
 FROM (
-    SELECT eua_user_id, system_intake_id, ARRAY_AGG(DISTINCT role) AS roles
+    SELECT user_id, system_intake_id, ARRAY_AGG(DISTINCT role) AS roles
     FROM copy_system_intake_contacts
-    GROUP BY eua_user_id, system_intake_id
+    GROUP BY user_id, system_intake_id
 ) sub
 WHERE
-    c.eua_user_id = sub.eua_user_id
+    c.user_id = sub.user_id
     AND c.system_intake_id = sub.system_intake_id;
 
+-- remove duplicate rows
 DELETE
 FROM copy_system_intake_contacts c
 USING copy_system_intake_contacts dup
 WHERE
-    c.eua_user_id = dup.eua_user_id
+    c.user_id = dup.user_id
     AND c.system_intake_id = dup.system_intake_id
     AND c.ctid > dup.ctid;
 
 ALTER TABLE copy_system_intake_contacts
 DROP COLUMN role;
+
+-- unique constraint between user_id and system_intake_id
+ALTER TABLE copy_system_intake_contacts
+ADD CONSTRAINT user_intake_unique UNIQUE (user_id, system_intake_id);
