cf1a10f4ef754c466035a78e5f541085a18f0262
samoddball <sam.lawson@oddball.io>
2025-08-18 15:48:30 -0700

    nearly done with migration

--- migrations/V213__Add_requester_column.sql
+++ migrations/V213__Add_requester_column.sql
@@ -9,3 +9,24 @@ CREATE TABLE IF NOT EXISTS copy_system_intake_contacts AS TABLE system_intake_co
 
 
 -- operate only on the copy table
+UPDATE copy_system_intake_contacts c
+SET roles = sub.roles
+FROM (
+    SELECT eua_user_id, system_intake_id, ARRAY_AGG(DISTINCT role) AS roles
+    FROM copy_system_intake_contacts
+    GROUP BY eua_user_id, system_intake_id
+) sub
+WHERE
+    c.eua_user_id = sub.eua_user_id
+    AND c.system_intake_id = sub.system_intake_id;
+
+DELETE
+FROM copy_system_intake_contacts c
+USING copy_system_intake_contacts dup
+WHERE
+    c.eua_user_id = dup.eua_user_id
+    AND c.system_intake_id = dup.system_intake_id
+    AND c.ctid > dup.ctid;
+
+ALTER TABLE copy_system_intake_contacts
+DROP COLUMN role;
