76f0074c8318894c4d58579624b11d5d856b2997
samoddball <sam.lawson@oddball.io>
2025-08-18 15:04:22 -0700

    still researching

--- migrations/V211__Add_requester_column.sql
+++ migrations/V211__Add_requester_column.sql
@@ -1,12 +1,11 @@
 ALTER TABLE IF EXISTS system_intake_contacts
 ADD COLUMN is_requester BOOLEAN NOT NULL DEFAULT FALSE;
 
+ALTER TABLE IF EXISTS system_intake_contacts
+ADD COLUMN roles TEXT[] NOT NULL DEFAULT '{}';
+
 -- create copy of table for safety and testing purposes
 CREATE TABLE IF NOT EXISTS copy_system_intake_contacts AS TABLE system_intake_contacts;
 
--- insert everything from original table into copy
-INSERT INTO copy_system_intake_contacts
-SELECT *
-FROM system_intake_contacts;
 
 -- operate only on the copy table
