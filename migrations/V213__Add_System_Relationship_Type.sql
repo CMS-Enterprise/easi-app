27a52bad85d3c0e507cd21e364d572d131b0e09a
Steven Wade <steven.wade@oddball.io>
2025-08-19 09:25:03 -0700

    feat: update does not support systems to false for intakes that currently have links

--- migrations/V213__Add_System_Relationship_Type.sql
+++ migrations/V213__Add_System_Relationship_Type.sql
@@ -21,3 +21,10 @@ COMMENT ON CONSTRAINT system_intake_systems_check_other_system_relationship_desc
 ALTER TABLE system_intakes ADD COLUMN does_not_support_systems BOOLEAN;
 
 COMMENT ON COLUMN system_intakes.does_not_support_systems IS 'Indicates that the system does not support systems. This is used to indicate that the system does not have any relationships with other systems. When this is set to true, we dont expect any systems to be linked in the system_intake_systems table.';
+
+-- Set does_not_support_systems to false for all system_intakes with a related system in system_intake_systems
+UPDATE system_intakes
+SET does_not_support_systems = FALSE
+WHERE id IN (
+    SELECT DISTINCT system_intake_id FROM system_intake_systems
+);
