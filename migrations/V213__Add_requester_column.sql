83b8394a3f24d8e43e27842aa256862ddd9568d4
samoddball <sam.lawson@oddball.io>
2025-08-19 16:46:44 -0700

    little more tweaking

--- migrations/V213__Add_requester_column.sql
+++ migrations/V213__Add_requester_column.sql
@@ -1,17 +1,13 @@
-ALTER TABLE IF EXISTS system_intake_contacts
-ADD COLUMN is_requester BOOLEAN NOT NULL DEFAULT FALSE;
-
-ALTER TABLE IF EXISTS system_intake_contacts
-ADD COLUMN roles TEXT[] NOT NULL DEFAULT '{}';
-
 -- create copy of table for safety and testing purposes
 CREATE TABLE IF NOT EXISTS copy_system_intake_contacts AS TABLE system_intake_contacts;
 
-UPDATE copy_system_intake_contacts
-SET is_requester = TRUE
-WHERE role = 'Requester';
-
 -- operate only on the copy table
+ALTER TABLE IF EXISTS copy_system_intake_contacts
+ADD COLUMN is_requester BOOLEAN NOT NULL DEFAULT FALSE;
+
+ALTER TABLE IF EXISTS copy_system_intake_contacts
+ADD COLUMN roles TEXT[] NOT NULL DEFAULT '{}';
+
 UPDATE copy_system_intake_contacts c
 SET roles = sub.roles
 FROM (
@@ -23,6 +19,12 @@ WHERE
     c.user_id = sub.user_id
     AND c.system_intake_id = sub.system_intake_id;
 
+-- set requester which is a special case (also remove from `roles` array)
+UPDATE copy_system_intake_contacts
+SET
+    roles        = ARRAY_REMOVE(roles, 'Requester'),
+    is_requester = roles @> ARRAY ['Requester'];
+
 -- remove duplicate rows
 DELETE
 FROM copy_system_intake_contacts c
@@ -30,8 +32,25 @@ USING copy_system_intake_contacts c2
 WHERE
     c.user_id = c2.user_id
     AND c.system_intake_id = c2.system_intake_id
-    AND c.ctid > c2.ctid
-    AND c.is_requester IS NOT TRUE;
+    AND c.ctid > c2.ctid;
+
+UPDATE copy_system_intake_contacts c
+SET
+    roles        = sub.roles,
+    is_requester = sub.is_requester
+FROM (SELECT
+    user_id,
+    system_intake_id,
+    ARRAY_AGG(DISTINCT role) FILTER (WHERE role <> 'Requester') AS roles,
+    BOOL_OR(role = 'Requester')                                 AS is_requester
+FROM (
+    SELECT user_id, system_intake_id, role
+    FROM copy_system_intake_contacts
+) t
+GROUP BY user_id, system_intake_id) sub
+WHERE
+    c.user_id = sub.user_id
+    AND c.system_intake_id = sub.system_intake_id;
 
 ALTER TABLE copy_system_intake_contacts
 DROP COLUMN role;
