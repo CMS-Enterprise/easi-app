029853ea989c1229c7999bc44f134d9b288a0db1
samoddball <sam.lawson@oddball.io>
2025-08-27 08:12:46 -0700

    fix path

--- pkg/cedar/core/client.go
+++ pkg/cedar/core/client.go
@@ -83,7 +83,7 @@ func NewClient(ctx context.Context, cedarHost string, cedarAPIKey string, cedarA
 		},
 	}
 
-	basePath := "/gateway/CEDARIntake/" + cedarAPIVersion
+	basePath := "/gateway/CEDAR Core API/" + cedarAPIVersion
 	cedarPath = "http://" + cedarHost + basePath
 	client = &Client{
 		mockEnabled: mockEnabled,
