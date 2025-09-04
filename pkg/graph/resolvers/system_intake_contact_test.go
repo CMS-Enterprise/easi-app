1c6775ed68e3cb9a29818fbad03f14e2963cea27
Steven Wade <steven.wade@oddball.io>
2025-09-04 09:37:01 -0700

    feat: unit tests for updating the contact

--- pkg/graph/resolvers/system_intake_contact_test.go
+++ pkg/graph/resolvers/system_intake_contact_test.go
@@ -57,4 +57,35 @@ func (s *ResolverSuite) TestSystemIntakeContactDelete() {
 	s.NoError(err)
 }
 
-func (s *ResolverSuite) TestSystemIntakeContactUpdate() {}
+func (s *ResolverSuite) TestSystemIntakeContactUpdate() {
+	intake := s.createNewIntakeWithResolver()
+	createInput := models.CreateSystemIntakeContactInput{
+		SystemIntakeID: intake.ID,
+		EuaUserID:      "USR1",
+		Component:      models.SystemIntakeContactComponentCenterForClinicalStandardsAndQualityCcsq,
+		Roles:          []models.SystemIntakeContactRole{models.SystemIntakeContactRoleProductOwner, models.SystemIntakeContactRoleCloudNavigator},
+		IsRequester:    false,
+	}
+
+	createdContact, err := CreateSystemIntakeContact(s.ctxWithNewDataloaders(), s.testConfigs.Logger, s.testConfigs.Principal, s.testConfigs.Store, createInput, userhelpers.GetUserInfoAccountInfoWrapperFunc(s.fetchUserInfoStub))
+	s.NoError(err)
+	s.NotNil(createdContact.SystemIntakeContact)
+
+	updateStruct := models.UpdateSystemIntakeContactInput{
+		ID:          createdContact.SystemIntakeContact.ID,
+		Component:   models.SystemIntakeContactComponentCmsWide,
+		Roles:       []models.SystemIntakeContactRole{models.SystemIntakeContactRoleBusinessOwner},
+		IsRequester: false,
+	}
+	updatedContact, err := UpdateSystemIntakeContact(s.ctxWithNewDataloaders(), s.testConfigs.Logger, s.testConfigs.Principal, s.testConfigs.Store, updateStruct, userhelpers.GetUserInfoAccountInfoWrapperFunc(s.fetchUserInfoStub))
+
+	s.NoError(err)
+	s.NotNil(updatedContact.SystemIntakeContact)
+	s.EqualValues(updateStruct.Component, updatedContact.SystemIntakeContact.Component)
+	s.EqualValues(updateStruct.Roles, updatedContact.SystemIntakeContact.Roles)
+	s.EqualValues(updateStruct.IsRequester, updatedContact.SystemIntakeContact.IsRequester)
+}
+
+func (s *ResolverSuite) TestSystemIntakeContactRequesterContstraints() {
+	// TODO implement this
+}
