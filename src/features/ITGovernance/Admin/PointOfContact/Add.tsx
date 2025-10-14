dc9cab5aa20f14926d36dd5d3399827d8261b77a
Gary Zhao <gary.j.zhao@gmail.com>
2025-10-14 14:59:36 -0400

    workign on the add point of contact page

--- src/features/ITGovernance/Admin/PointOfContact/Add.tsx
+++ src/features/ITGovernance/Admin/PointOfContact/Add.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { SystemIntakeFragmentFragment } from 'gql/generated/graphql';
 
+import MainContent from 'components/MainContent';
 import PageHeading from 'components/PageHeading';
+import RequiredFieldsText from 'components/RequiredFieldsText';
 
 const AddPointOfContact = ({
   systemIntake
@@ -11,14 +13,18 @@ const AddPointOfContact = ({
 }) => {
   const { t } = useTranslation('requestHome');
   return (
-    <div id="add-poc">
-      <div>
+    <MainContent data-testid="add-poc" className="margin-top-6">
+      <div className="grid-col-8">
         <PageHeading className="margin-top-0 margin-bottom-1">
           {t('addPOC.title')}
         </PageHeading>
         <p className="easi-body-medium margin-y-0">{t('addPOC.description')}</p>
+        <RequiredFieldsText />
       </div>
-    </div>
+      <div className="grid-col-6">
+        <RequiredFieldsText />
+      </div>
+    </MainContent>
   );
 };
 
