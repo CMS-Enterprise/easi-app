725b66453f849091171af2776fc2b0760fc9f59f
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2025-10-19 15:54:36 -0500

    pubsub event

---
+++ pkg/models/pubsubevents/event_types.go
@@ -0,0 +1,8 @@
+package pubsubevents
+
+import "github.com/cms-enterprise/easi-app/pkg/pubsub"
+
+const (
+	// SystemProfileSectionLocksChanged is an event sent to subscribers indicating a change that has occurred
+	SystemProfileSectionLocksChanged pubsub.EventType = "system_profile_section.changed"
+)
