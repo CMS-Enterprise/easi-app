dea81f0123571d65ae0b1d7e7730e80da9c748ef
Ashley Terstriep <60187543+aterstriep@users.noreply.github.com>
2025-12-03 10:04:03 -0600

    Remove unused context and provider

--- src/app/Routes/index.tsx
+++ src/app/Routes/index.tsx
@@ -45,7 +45,6 @@ import SystemWorkspaceRequests from 'features/Systems/SystemWorkspace/SystemWork
 import TechnicalAssistance from 'features/TechnicalAssistance/Routes';
 import { useFlags } from 'launchdarkly-react-client-sdk';
 import AuthenticationWrapper from 'wrappers/AuthenticationWrapper';
-import ErrorMessageProvider from 'wrappers/ErrorContext';
 import FlagsWrapper from 'wrappers/FlagsWrapper';
 import TableStateWrapper from 'wrappers/TableStateWrapper';
 import TimeOutWrapper from 'wrappers/TimeOutWrapper';
@@ -246,27 +245,25 @@ const App = () => {
       </button>
       <BrowserRouter>
         <AuthenticationWrapper>
-          <ErrorMessageProvider>
-            <MessageProvider>
-              <FlagsWrapper>
-                <UserInfoWrapper>
-                  <TimeOutWrapper>
-                    <TableStateWrapper>
-                      <PageWrapper>
-                        <GovBanner />
-                        <Header />
-                        <Navigation>
-                          <CedarAlertBanner />
-                          <AppRoutes />
-                        </Navigation>
-                        <Footer />
-                      </PageWrapper>
-                    </TableStateWrapper>
-                  </TimeOutWrapper>
-                </UserInfoWrapper>
-              </FlagsWrapper>
-            </MessageProvider>
-          </ErrorMessageProvider>
+          <MessageProvider>
+            <FlagsWrapper>
+              <UserInfoWrapper>
+                <TimeOutWrapper>
+                  <TableStateWrapper>
+                    <PageWrapper>
+                      <GovBanner />
+                      <Header />
+                      <Navigation>
+                        <CedarAlertBanner />
+                        <AppRoutes />
+                      </Navigation>
+                      <Footer />
+                    </PageWrapper>
+                  </TableStateWrapper>
+                </TimeOutWrapper>
+              </UserInfoWrapper>
+            </FlagsWrapper>
+          </MessageProvider>
         </AuthenticationWrapper>
       </BrowserRouter>
       <ToastContainer
