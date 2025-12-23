5a06e5f8209fd36fdc0ab9ec0460f4280134557f
samoddball <sam.lawson@oddball.io>
2025-12-23 16:03:27 -0700

    add doc to UI

--- src/features/Systems/Wrapper/SystemIDWrapper.tsx
+++ src/features/Systems/Wrapper/SystemIDWrapper.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Redirect, useLocation, useParams } from 'react-router-dom';
 
+// SystemIDWrapper is used to wrap any routes that have to do with CEDAR systems (i.e., any routes that have `/systems` prefix)
+// and redirects any old style URLs (where the system id is wrapped in curly braces) to the new format (no curly braces)
 export default function SystemIDWrapper({ children }: React.PropsWithChildren) {
   const { pathname, search, hash } = useLocation();
   const { systemId } = useParams<{ systemId?: string }>();
