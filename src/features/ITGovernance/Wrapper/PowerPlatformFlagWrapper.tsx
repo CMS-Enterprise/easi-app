a0ebda9a49cc60bc949cab3ce4e6d7f19b62c02c
samoddball <sam.lawson@oddball.io>
2026-03-06 16:27:51 -0700

    return page loading instead of blank page

--- src/features/ITGovernance/Wrapper/PowerPlatformFlagWrapper.tsx
+++ src/features/ITGovernance/Wrapper/PowerPlatformFlagWrapper.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useFlags } from 'launchdarkly-react-client-sdk';
 
+import PageLoading from 'components/PageLoading';
+
 export default function PowerPlatformFlagWrapper({
   children
 }: React.PropsWithChildren) {
@@ -10,7 +12,7 @@ export default function PowerPlatformFlagWrapper({
 
   if (flags.enablePowerPlatform) {
     window.location.href = `https://icpg-dev.crm9.dynamics.com/main.aspx?appid=bc878d88-0468-f011-bec2-001dd8062d4a&pagetype=entityrecord&etn=new_systemintake&id=${systemIntakeID}`;
-    return null;
+    return <PageLoading />;
   }
 
   return <>{children}</>;
