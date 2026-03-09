21cf5ba0cdddee5874e5162cd92d73557e44dc1a
samoddball <sam.lawson@oddball.io>
2026-03-09 15:58:32 -0700

    env based link return, untested

--- src/features/ITGovernance/Wrapper/PowerPlatformFlagWrapper.tsx
+++ src/features/ITGovernance/Wrapper/PowerPlatformFlagWrapper.tsx
@@ -4,13 +4,29 @@ import { useFlags } from 'launchdarkly-react-client-sdk';
 
 import PageLoading from 'components/PageLoading';
 
+function powerPlatformLink(id: string): string {
+  switch (import.meta.env.NODE_ENV) {
+    case 'production':
+      return `https://itgovernance.crm9.dynamics.com/main.aspx?appid=941458af-d8eb-f011-8544-001dd80f20e8&id=${id}`;
+    case 'development':
+      return `https://itgovernancedev.crm9.dynamics.com/main.aspx?appid=110e68fa-41bf-4a23-a577-e58b353d60c7&id=${id}`;
+    default:
+      return '';
+  }
+}
+
 export default function PowerPlatformFlagWrapper({
   children
 }: React.PropsWithChildren) {
-  const { id } = useParams<{ id?: string }>();
+  const { id } = useParams<{ id: string }>();
   const flags = useFlags();
 
   if (flags.enablePowerPlatform) {
+    const link = powerPlatformLink(id);
+    if (link.length < 1) {
+      return null;
+    }
+
     window.location.href = `https://icpg-dev.crm9.dynamics.com/main.aspx?appid=bc878d88-0468-f011-bec2-001dd8062d4a&pagetype=entityrecord&etn=new_systemintake&id=${id}`;
     return <PageLoading />;
   }
