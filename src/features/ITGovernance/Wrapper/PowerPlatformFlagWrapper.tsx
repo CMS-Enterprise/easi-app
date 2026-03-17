fda233dad6cc06c44c26b011838c04ffb6c1b94c
samoddball <sam.lawson@oddball.io>
2026-03-17 15:29:27 -0700

    account for no intake id if coming from `request-type` page

--- src/features/ITGovernance/Wrapper/PowerPlatformFlagWrapper.tsx
+++ src/features/ITGovernance/Wrapper/PowerPlatformFlagWrapper.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
-import { RouteComponentProps, withRouter } from 'react-router-dom';
+import { RouteComponentProps, useLocation, withRouter } from 'react-router-dom';
 import { useFlags } from 'launchdarkly-react-client-sdk';
 
 import PageLoading from 'components/PageLoading';
 
-function powerPlatformLink(id: string): string {
+function powerPlatformLink(id?: string): string {
+  let idSuffix = '';
+  if (id) {
+    idSuffix = `&id=${id}`;
+  }
   // return `https://itgovernancedev.crm9.dynamics.com/main.aspx?appid=110e68fa-41bf-4a23-a577-e58b353d60c7&id=${id}`;
-  switch (import.meta.env.NODE_ENV) {
+  switch (process.env.NODE_ENV) {
     case 'production':
-      return `https://itgovernance.crm9.dynamics.com/main.aspx?appid=941458af-d8eb-f011-8544-001dd80f20e8&id=${id}`;
+      return `https://itgovernance.crm9.dynamics.com/main.aspx?appid=941458af-d8eb-f011-8544-001dd80f20e8${idSuffix}`;
 
     // send local and dev to same spot
     case 'development':
-    case 'local':
-      return `https://itgovernancedev.crm9.dynamics.com/main.aspx?appid=110e68fa-41bf-4a23-a577-e58b353d60c7&id=${id}`;
+      return `https://itgovernancedev.crm9.dynamics.com/main.aspx?appid=110e68fa-41bf-4a23-a577-e58b353d60c7${idSuffix}`;
     default:
       return '';
   }
@@ -24,13 +27,15 @@ interface params {
 }
 
 function PowerPlatformFlagWrapper({ match }: RouteComponentProps<params>) {
-  console.info('in power platform wrapper');
   const { intakeId } = match.params;
   const flags = useFlags();
+  const location = useLocation();
 
-  if (flags.enablePowerPlatform && intakeId.length > 0) {
-    const link = powerPlatformLink(intakeId);
-    console.info(link);
+  const shouldRedirect =
+    intakeId.length > 0 || location.pathname === '/system/request-type';
+
+  if (flags.enablePowerPlatform && shouldRedirect) {
+    const link = powerPlatformLink(intakeId.length > 0 ? intakeId : undefined);
     if (link.length > 0) {
       window.location.href = link;
       return <PageLoading />;
