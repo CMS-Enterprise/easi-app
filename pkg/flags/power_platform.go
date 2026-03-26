ddc23808a1002ab158d8202cd1976920749ad0f7
samoddball <sam.lawson@oddball.io>
2026-03-26 15:20:28 -0700

    testing something

--- pkg/flags/power_platform.go
+++ pkg/flags/power_platform.go
@@ -9,6 +9,6 @@ import (
 const key = "enablePowerPlatform"
 
 func PowerPlatformEnabled(ctx context.Context, client *ld.LDClient) (bool, error) {
-	return false, nil
-	//return client.BoolVariation(key, Principal(ctx), false)
+	//return false, nil
+	return client.BoolVariation(key, Principal(ctx), false)
 }
