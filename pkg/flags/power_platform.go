7564421896341ab2418308c5b95043e3f2f6894b
samoddball <sam.lawson@oddball.io>
2026-03-26 15:34:13 -0700

    remove test code

--- pkg/flags/power_platform.go
+++ pkg/flags/power_platform.go
@@ -9,6 +9,5 @@ import (
 const key = "enablePowerPlatform"
 
 func PowerPlatformEnabled(ctx context.Context, client *ld.LDClient) (bool, error) {
-	//return false, nil
 	return client.BoolVariation(key, Principal(ctx), false)
 }
