03b838fd61c51b5b7669dcef30d5e717c6abe2a4
samoddball <sam.lawson@oddball.io>
2026-04-06 08:26:19 -0700

    fix case check

--- src/utils/powerPlatformLink.ts
+++ src/utils/powerPlatformLink.ts
@@ -15,7 +15,8 @@ export default function powerPlatformLink(id?: string): string {
       return `https://itgovernance.crm9.dynamics.com/main.aspx?appid=941458af-d8eb-f011-8544-001dd80f20e8${idSuffix}`;
 
     // send local and dev to same spot
-    case 'local' || 'development':
+    case 'local':
+    case 'development':
       return `https://itgovernancedev.crm9.dynamics.com/main.aspx?appid=110e68fa-41bf-4a23-a577-e58b353d60c7${idSuffix}`;
     default:
       return '';
