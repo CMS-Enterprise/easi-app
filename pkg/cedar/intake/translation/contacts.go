94e6787dfbed85be797d8fb30631dc99af4eca77
samoddball <sam.lawson@oddball.io>
2026-04-21 10:21:40 -0700

    some more ignored errors

--- pkg/cedar/intake/translation/contacts.go
+++ pkg/cedar/intake/translation/contacts.go
@@ -8,6 +8,7 @@ import (
 	"github.com/guregu/null"
 
 	"github.com/cms-enterprise/easi-app/pkg/graph/resolvers"
+	"github.com/cms-enterprise/easi-app/pkg/models"
 )
 
 // CoreTranslatableContacts holds the core contact information needed for translating a system intake contact for system intake
@@ -22,37 +23,64 @@ type CoreTranslatableContacts struct {
 }
 
 // GetCoreTranslatableContactInfo returns the core contact information needed for translating a system intake contact for system intake
-func GetCoreTranslatableContactInfo(ctx context.Context, systemIntakeID uuid.UUID) (retContacts *CoreTranslatableContacts, err error) {
+func GetCoreTranslatableContactInfo(ctx context.Context, systemIntakeID uuid.UUID) (*CoreTranslatableContacts, error) {
 	contacts, err := resolvers.SystemIntakeContactsGetBySystemIntakeID(ctx, systemIntakeID)
 	if err != nil {
 		return nil, fmt.Errorf("error getting contact information for system intake %s: %w", systemIntakeID.String(), err)
 	}
-	retContacts = &CoreTranslatableContacts{}
+	retContacts := &CoreTranslatableContacts{}
 
 	requester := contacts.Requester()
 	if requester != nil {
-		reqAccount, _ := requester.UserAccount(ctx)
-		if reqAccount != nil {
-			retContacts.Requester = reqAccount.Username
+		requesterUsername, err := systemIntakeContactUsername(ctx, systemIntakeID, "requester", requester)
+		if err != nil {
+			return nil, err
+		}
+		if requesterUsername != nil {
+			retContacts.Requester = *requesterUsername
 		}
 		retContacts.Component = null.StringFrom(string(requester.Component))
 	}
 	businessOwners := contacts.BusinessOwners()
 	if len(businessOwners) > 0 {
-		boAccount, _ := businessOwners[0].UserAccount(ctx)
-		if boAccount != nil {
-			retContacts.BusinessOwner = null.StringFrom(boAccount.Username)
+		businessOwner := businessOwners[0]
+		businessOwnerUsername, err := systemIntakeContactUsername(ctx, systemIntakeID, "business owner", businessOwner)
+		if err != nil {
+			return nil, err
 		}
-		retContacts.BusinessOwnerComponent = null.StringFrom(string(businessOwners[0].Component))
+		if businessOwnerUsername != nil {
+			retContacts.BusinessOwner = null.StringFrom(*businessOwnerUsername)
+		}
+		retContacts.BusinessOwnerComponent = null.StringFrom(string(businessOwner.Component))
 	}
 
 	productManagers := contacts.ProductManagers()
 	if len(productManagers) > 0 {
-		pmAccount, _ := productManagers[0].UserAccount(ctx)
-		if pmAccount != nil {
-			retContacts.ProductManager = null.StringFrom(pmAccount.Username)
+		productManager := productManagers[0]
+		productManagerUsername, err := systemIntakeContactUsername(ctx, systemIntakeID, "product manager", productManager)
+		if err != nil {
+			return nil, err
 		}
-		retContacts.ProductManagerComponent = null.StringFrom(string(productManagers[0].Component))
+		if productManagerUsername != nil {
+			retContacts.ProductManager = null.StringFrom(*productManagerUsername)
+		}
+		retContacts.ProductManagerComponent = null.StringFrom(string(productManager.Component))
 	}
 	return retContacts, nil
+}
+
+func systemIntakeContactUsername(ctx context.Context, systemIntakeID uuid.UUID, label string, contact *models.SystemIntakeContact) (*string, error) {
+	if contact == nil {
+		return nil, nil
+	}
+
+	account, err := contact.UserAccount(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("error getting %[1]s user account for system intake %[2]s: %[3]w", label, systemIntakeID.String(), err)
+	}
+	if account == nil {
+		return nil, nil
+	}
+
+	return &account.Username, nil
 }
