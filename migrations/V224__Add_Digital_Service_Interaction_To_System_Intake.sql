dffbd13faabc633eaf1b1133cb3f65cf4f7de561
Nick Downey <Nicholas.Downey@cms.hhs.gov>
2026-06-02 15:05:41 -0400

    Consolidate migrations into one file

--- migrations/V224__Add_Digital_Service_Interaction_To_System_Intake.sql
+++
@@ -1,6 +0,0 @@
--- Add digital_service_interaction and digital_service_interaction_description columns to system_intakes table
--- These fields capture whether a project enables digital service interaction
-
-ALTER TABLE system_intakes
-ADD COLUMN digital_service_interaction TEXT,
-ADD COLUMN digital_service_interaction_description TEXT;
--- migrations/V224__Add_Protected_CMS_Data_and_Digital_Service_To_System_Intake.sql
+++ migrations/V224__Add_Protected_CMS_Data_and_Digital_Service_To_System_Intake.sql
@@ -1,6 +1,11 @@
+-- Add digital_service_interaction and digital_service_interaction_description columns to system_intakes table
+-- These fields capture whether a project enables digital service interaction
+
 -- Add protected_cms_data_accessed_outside and protected_cms_data_accessed_outside_description columns to system_intakes table
 -- These fields capture whether a project accesses protected CMS data outside CMS-controlled information systems or environments
 
 ALTER TABLE system_intakes
+ADD COLUMN digital_service_interaction TEXT,
+ADD COLUMN digital_service_interaction_description TEXT;
 ADD COLUMN protected_cms_data_accessed_outside TEXT,
 ADD COLUMN protected_cms_data_accessed_outside_description TEXT;
