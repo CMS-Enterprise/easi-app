c4f6896333fe89d910268d5c6fe46d88d2f1614c
samoddball <sam.lawson@oddball.io>
2026-09-10 10:12:01 -0700

    bump version

--- pkg/cedar/intake/translation/constants.go
+++ pkg/cedar/intake/translation/constants.go
@@ -42,7 +42,7 @@ const (
 	IntakeInputSchemaEASIGrtFeedbackVersion SchemaVersion = "EASIGrtFeedbackV02"
 
 	// IntakeInputSchemaEASIIntakeVersion captures the current schema version for System Intakes
-	IntakeInputSchemaEASIIntakeVersion SchemaVersion = "EASIIntakeV08"
+	IntakeInputSchemaEASIIntakeVersion SchemaVersion = "EASIIntakeV09"
 
 	// IntakeInputSchemaEASINoteVersion captures the current schema version for Notes
 	IntakeInputSchemaEASINoteVersion SchemaVersion = "EASINoteV01"
